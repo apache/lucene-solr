dabd5e449f673f502a8705370162c5aa6bdf778d
Otis Gospodnetic <otis@apache.org>
2002-02-21 21:41:53 +0000

    - Made the code in the javadoc readable. - Removed unnecessary import statement. - Re-indented the whole class.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@149694 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/analysis/PorterStemFilter.java
+++ src/java/org/apache/lucene/analysis/PorterStemFilter.java
@@ -55,44 +55,44 @@ package org.apache.lucene.analysis;
  */
 
 import java.io.IOException;
-import java.util.Hashtable;
 
-/** Transforms the token stream as per the Porter stemming algorithm. 
-    Note: the input to the stemming filter must already be in lower case, 
+/** Transforms the token stream as per the Porter stemming algorithm.
+    Note: the input to the stemming filter must already be in lower case,
     so you will need to use LowerCaseFilter or LowerCaseTokenizer farther
-    down the Tokenizer chain in order for this to work properly!  
-
-    To use this filter with other analyzers, you'll want to write an 
-    Analyzer class that sets up the TokenStream chain as you want it.  
+    down the Tokenizer chain in order for this to work properly!
+    <P>
+    To use this filter with other analyzers, you'll want to write an
+    Analyzer class that sets up the TokenStream chain as you want it.
     To use this with LowerCaseTokenizer, for example, you'd write an
     analyzer like this:
-
+    <P>
+    <PRE>
     class MyAnalyzer extends Analyzer {
       public final TokenStream tokenStream(String fieldName, Reader reader) {
         return new PorterStemFilter(new LowerCaseTokenizer(reader));
       }
-    } 
-
+    }
+    </PRE>
 */
 
 public final class PorterStemFilter extends TokenFilter {
-  private PorterStemmer stemmer;
+    private PorterStemmer stemmer;
 
-  public PorterStemFilter(TokenStream in) {
-    stemmer = new PorterStemmer();
-    input = in;
-  }
-
-  /** Returns the next input Token, after being stemmed */
-  public final Token next() throws IOException {
-    Token token = input.next();
-    if (token == null)
-      return null;
-    else {
-      String s = stemmer.stem(token.termText);
-      if (s != token.termText) // Yes, I mean object reference comparison here
-        token.termText = s;
-      return token;
+    public PorterStemFilter(TokenStream in) {
+	stemmer = new PorterStemmer();
+	input = in;
     }
-  }
+
+    /** Returns the next input Token, after being stemmed */
+    public final Token next() throws IOException {
+	Token token = input.next();
+	if (token == null)
+	    return null;
+	else {
+	    String s = stemmer.stem(token.termText);
+	    if (s != token.termText) // Yes, I mean object reference comparison here
+		token.termText = s;
+	    return token;
+	}
+    }
 }
