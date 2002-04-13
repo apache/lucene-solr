2122be3e73d996710c153ac0e162d1d43dec19c3
Otis Gospodnetic <otis@apache.org>
2002-04-13 20:04:28 +0000

    - Fixed a spelling error and re-formatted the Javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@149722 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/analysis/LowerCaseTokenizer.java
+++ src/java/org/apache/lucene/analysis/LowerCaseTokenizer.java
@@ -56,15 +56,16 @@ package org.apache.lucene.analysis;
 
 import java.io.Reader;
 
-/** LowerCaseTokenizer performs the function of LetterTokenizer
-  and LowerCaseFilter together.  It divides text at non-letters and converts
-  them to lower case.  While it is functionally equivalent to the combination
-  of LetterTokenizer and LowerCaseFilter, there is a performance advantage
-  to doing the two tasks at once, hence this (redundent) implementation.
-
-  Note: this does a decent job for most European languages, but does a terrible
-  job for some Asian languages, where words are not separated by spaces. */
-
+/**
+ * LowerCaseTokenizer performs the function of LetterTokenizer
+ * and LowerCaseFilter together.  It divides text at non-letters and converts
+ * them to lower case.  While it is functionally equivalent to the combination
+ * of LetterTokenizer and LowerCaseFilter, there is a performance advantage
+ * to doing the two tasks at once, hence this (redundant) implementation.
+ * <P>
+ * Note: this does a decent job for most European languages, but does a terrible
+ * job for some Asian languages, where words are not separated by spaces.
+ */
 public final class LowerCaseTokenizer extends LetterTokenizer {
   /** Construct a new LowerCaseTokenizer. */
   public LowerCaseTokenizer(Reader in) {
