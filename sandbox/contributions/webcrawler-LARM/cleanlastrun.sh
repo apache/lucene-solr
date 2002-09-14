bc40fa1994cc7ff0e82d75068528bc271413f1a3
Otis Gospodnetic <otis@apache.org>
2002-09-14 19:22:45 +0000

    - Modified to clean dirs/files created by run.sh.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150817 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/contributions/webcrawler-LARM/cleanlastrun.sh
+++ sandbox/contributions/webcrawler-LARM/cleanlastrun.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-rm -r logs
-rm -r cachingqueue
 
+BASE_DIR=./runtime
+LOG_DIR=$BASE_DIR/logs
+CACHE_DIR=$BASE_DIR/cachingqueue
+SLEEP_TIME=2
+
+echo Removing $LOG_DIR...
+sleep $SLEEP_TIME
+rm -r $LOG_DIR
+echo Removing $CACHE_DIR...
+sleep $SLEEP_TIME
+rm -r $CACHE_DIR
