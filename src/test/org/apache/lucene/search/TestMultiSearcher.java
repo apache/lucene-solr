66185189988b35052f8ba5834bc45334d101a5a9
Otis Gospodnetic <otis@apache.org>
2002-09-19 12:35:43 +0000

    - Fixed last night's mistakes.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@149856 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/lucene/search/TestMultiSearcher.java
+++ src/test/org/apache/lucene/search/TestMultiSearcher.java
@@ -179,7 +179,7 @@ public class TestMultiSearcher extends TestCase
         // performing the same search
         Hits hits2 = mSearcher2.search(query);
 
-        assertEquals(4, hits.length());
+        assertEquals(4, hits2.length());
 
         try {
             // iterating over the hit documents
@@ -221,7 +221,7 @@ public class TestMultiSearcher extends TestCase
         // performing the same search
         Hits hits3 = mSearcher3.search(query);
 
-        assertEquals(3, hits.length());
+        assertEquals(3, hits3.length());
 
         try {
             // iterating over the hit documents
