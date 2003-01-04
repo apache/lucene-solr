ca1240bec645842d79b2ef8de1eb680a4ab047d3
Otis Gospodnetic <otis@apache.org>
2003-01-04 18:06:19 +0000

    - Fix for bug http://nagoya.apache.org/bugzilla/show_bug.cgi?id=12723 PR: 12723 Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@149915 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/document/Field.java
+++ src/java/org/apache/lucene/document/Field.java
@@ -132,10 +132,10 @@ public final class Field implements java.io.Serializable {
     return new Field(name, value, true, true, true);
   }
 
-  /** Constructs a Date-valued Field that is tokenized and indexed,
-      and is stored in the index, for return with hits. */
+  /** Constructs a Date-valued Field that is not tokenized and is indexed,
+      and stored in the index, for return with hits. */
   public static final Field Keyword(String name, Date value) {
-    return new Field(name, DateField.dateToString(value), true, true, true);
+    return new Field(name, DateField.dateToString(value), true, true, false);
   }
 
   /** Constructs a String-valued Field that is tokenized and indexed,
