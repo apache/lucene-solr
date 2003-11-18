1381c246056cbc611990cccd97baee5123167848
Otis Gospodnetic <otis@apache.org>
2003-11-18 11:35:57 +0000

    - Removed tabs.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150129 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/index/SegmentInfos.java
+++ src/java/org/apache/lucene/index/SegmentInfos.java
@@ -62,7 +62,7 @@ import org.apache.lucene.store.OutputStream;
 
 final class SegmentInfos extends Vector {
   public int counter = 0;			  // used to name new segments
-  
+
   public final SegmentInfo info(int i) {
     return (SegmentInfo)elementAt(i);
   }
@@ -70,11 +70,11 @@ final class SegmentInfos extends Vector {
   public final void read(Directory directory) throws IOException {
     InputStream input = directory.openFile("segments");
     try {
-      counter = input.readInt();		  // read counter
+      counter = input.readInt();		          // read counter
       for (int i = input.readInt(); i > 0; i--) { // read segmentInfos
-	SegmentInfo si = new SegmentInfo(input.readString(), input.readInt(),
-					 directory);
-	addElement(si);
+        SegmentInfo si = new SegmentInfo(input.readString(), input.readInt(),
+          directory);
+        addElement(si);
       }
     } finally {
       input.close();
@@ -87,9 +87,9 @@ final class SegmentInfos extends Vector {
       output.writeInt(counter);			  // write counter
       output.writeInt(size());			  // write infos
       for (int i = 0; i < size(); i++) {
-	SegmentInfo si = info(i);
-	output.writeString(si.name);
-	output.writeInt(si.docCount);
+        SegmentInfo si = info(i);
+        output.writeString(si.name);
+        output.writeInt(si.docCount);
       }
     } finally {
       output.close();
