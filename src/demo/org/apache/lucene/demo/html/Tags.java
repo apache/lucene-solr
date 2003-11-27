40dd950e3fcde989422cd29a2d067d51d6536b70
Christoph Goller <goller@apache.org>
2003-11-27 12:50:21 +0000

    closing tags added
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150147 13f79535-47bb-0310-9956-ffa450edef68

--- src/demo/org/apache/lucene/demo/html/Tags.java
+++ src/demo/org/apache/lucene/demo/html/Tags.java
@@ -66,24 +66,36 @@ public final class Tags {
   public final static Set WS_ELEMS = Collections.synchronizedSet(new HashSet());
   
   static{
-    WS_ELEMS.add("<p");
-    WS_ELEMS.add("<div");
     WS_ELEMS.add("<hr");
     WS_ELEMS.add("<hr/");  // note that "<hr />" does not need to be listed explicitly
     WS_ELEMS.add("<br");
     WS_ELEMS.add("<br/");
-    WS_ELEMS.add("<td");
-    WS_ELEMS.add("<li");
     WS_ELEMS.add("<p");
+    WS_ELEMS.add("</p");
+    WS_ELEMS.add("<div");
+    WS_ELEMS.add("</div");
+    WS_ELEMS.add("<td");
+    WS_ELEMS.add("</td");
+    WS_ELEMS.add("<li");
+    WS_ELEMS.add("</li");
     WS_ELEMS.add("<q");
+    WS_ELEMS.add("</q");
     WS_ELEMS.add("<blockquote");
+    WS_ELEMS.add("</blockquote");
     WS_ELEMS.add("<dt");
+    WS_ELEMS.add("</dt");
     WS_ELEMS.add("<h1");
+    WS_ELEMS.add("</h1");
     WS_ELEMS.add("<h2");
+    WS_ELEMS.add("</h2");
     WS_ELEMS.add("<h3");
+    WS_ELEMS.add("</h3");
     WS_ELEMS.add("<h4");
+    WS_ELEMS.add("</h4");
     WS_ELEMS.add("<h5");
+    WS_ELEMS.add("</h5");
     WS_ELEMS.add("<h6");
+    WS_ELEMS.add("</h6");
   }
 
 }
