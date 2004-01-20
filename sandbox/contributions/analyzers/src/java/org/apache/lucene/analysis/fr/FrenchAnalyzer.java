595559dedbb3ed3be0db09f9a23d1a4eac96875f
Erik Hatcher <ehatcher@apache.org>
2004-01-20 10:07:01 +0000

    #26268 - Add FrenchAnalyzer
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150932 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/contributions/analyzers/src/java/org/apache/lucene/analysis/fr/FrenchAnalyzer.java
@@ -0,0 +1,126 @@
+package org.apache.lucene.analysis.fr;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import java.io.File;
+import java.io.Reader;
+import java.util.Hashtable;
+import org.apache.lucene.analysis.de.WordlistLoader;
+
+/**
+ * Analyzer for french language. Supports an external list of stopwords (words that
+ * will not be indexed at all) and an external list of exclusions (word that will
+ * not be stemmed, but indexed).
+ * A default set of stopwords is used unless an other list is specified, the
+ * exclusionlist is empty by default.
+ *
+ * @author    Patrick Talbot (based on Gerhard Schwarz work for German)
+ * @version   $Id$
+ */
+public final class FrenchAnalyzer extends Analyzer {
+
+	/**
+	 * Extended list of typical french stopwords.
+	 */
+	private String[] FRENCH_STOP_WORDS = {
+		"a", "afin", "ai", "ainsi", "après", "attendu", "au", "aujourd", "auquel", "aussi",
+		"autre", "autres", "aux", "auxquelles", "auxquels", "avait", "avant", "avec", "avoir",
+		"c", "car", "ce", "ceci", "cela", "celle", "celles", "celui", "cependant", "certain",
+		"certaine", "certaines", "certains", "ces", "cet", "cette", "ceux", "chez", "ci",
+		"combien", "comme", "comment", "concernant", "contre", "d", "dans", "de", "debout",
+		"dedans", "dehors", "delà", "depuis", "derrière", "des", "désormais", "desquelles",
+		"desquels", "dessous", "dessus", "devant", "devers", "devra", "divers", "diverse",
+		"diverses", "doit", "donc", "dont", "du", "duquel", "durant", "dès", "elle", "elles",
+		"en", "entre", "environ", "est", "et", "etc", "etre", "eu", "eux", "excepté", "hormis",
+		"hors", "hélas", "hui", "il", "ils", "j", "je", "jusqu", "jusque", "l", "la", "laquelle",
+		"le", "lequel", "les", "lesquelles", "lesquels", "leur", "leurs", "lorsque", "lui", "là",
+		"ma", "mais", "malgré", "me", "merci", "mes", "mien", "mienne", "miennes", "miens", "moi",
+		"moins", "mon", "moyennant", "même", "mêmes", "n", "ne", "ni", "non", "nos", "notre",
+		"nous", "néanmoins", "nôtre", "nôtres", "on", "ont", "ou", "outre", "où", "par", "parmi",
+		"partant", "pas", "passé", "pendant", "plein", "plus", "plusieurs", "pour", "pourquoi",
+		"proche", "près", "puisque", "qu", "quand", "que", "quel", "quelle", "quelles", "quels",
+		"qui", "quoi", "quoique", "revoici", "revoilà", "s", "sa", "sans", "sauf", "se", "selon",
+		"seront", "ses", "si", "sien", "sienne", "siennes", "siens", "sinon", "soi", "soit",
+		"son", "sont", "sous", "suivant", "sur", "ta", "te", "tes", "tien", "tienne", "tiennes",
+		"tiens", "toi", "ton", "tous", "tout", "toute", "toutes", "tu", "un", "une", "va", "vers",
+		"voici", "voilà", "vos", "votre", "vous", "vu", "vôtre", "vôtres", "y", "à", "ça", "ès",
+		"été", "être", "ô"
+	};
+
+	/**
+	 * Contains the stopwords used with the StopFilter.
+	 */
+	private Hashtable stoptable = new Hashtable();
+	/**
+	 * Contains words that should be indexed but not stemmed.
+	 */
+	private Hashtable excltable = new Hashtable();
+
+	/**
+	 * Builds an analyzer.
+	 */
+	public FrenchAnalyzer() {
+		stoptable = StopFilter.makeStopTable( FRENCH_STOP_WORDS );
+	}
+
+	/**
+	 * Builds an analyzer with the given stop words.
+	 */
+	public FrenchAnalyzer( String[] stopwords ) {
+		stoptable = StopFilter.makeStopTable( stopwords );
+	}
+
+	/**
+	 * Builds an analyzer with the given stop words.
+	 */
+	public FrenchAnalyzer( Hashtable stopwords ) {
+		stoptable = stopwords;
+	}
+
+	/**
+	 * Builds an analyzer with the given stop words.
+	 */
+	public FrenchAnalyzer( File stopwords ) {
+		stoptable = WordlistLoader.getWordtable( stopwords );
+	}
+
+	/**
+	 * Builds an exclusionlist from an array of Strings.
+	 */
+	public void setStemExclusionTable( String[] exclusionlist ) {
+		excltable = StopFilter.makeStopTable( exclusionlist );
+	}
+	/**
+	 * Builds an exclusionlist from a Hashtable.
+	 */
+	public void setStemExclusionTable( Hashtable exclusionlist ) {
+		excltable = exclusionlist;
+	}
+	/**
+	 * Builds an exclusionlist from the words contained in the given file.
+	 */
+	public void setStemExclusionTable( File exclusionlist ) {
+		excltable = WordlistLoader.getWordtable( exclusionlist );
+	}
+
+	/**
+	 * Creates a TokenStream which tokenizes all the text in the provided Reader.
+	 *
+	 * @return  A TokenStream build from a StandardTokenizer filtered with
+	 * 			StandardFilter, StopFilter, FrenchStemFilter and LowerCaseFilter
+	 */
+	public final TokenStream tokenStream( String fieldName, Reader reader ) {
+		TokenStream result = new StandardTokenizer( reader );
+		result = new StandardFilter( result );
+		result = new StopFilter( result, stoptable );
+		result = new FrenchStemFilter( result, excltable );
+		// Convert to lowercase after stemming!
+		result = new LowerCaseFilter( result );
+		return result;
+	}
+}
+
---
+++ sandbox/contributions/analyzers/src/java/org/apache/lucene/analysis/fr/FrenchStemFilter.java
@@ -0,0 +1,76 @@
+package org.apache.lucene.analysis.fr;
+
+import org.apache.lucene.analysis.Token;
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import java.io.IOException;
+import java.util.Hashtable;
+
+/**
+ * A filter that stemms french words. It supports a table of words that should
+ * not be stemmed at all. The used stemmer can be changed at runtime after the
+ * filter object is created (as long as it is a FrenchStemmer).
+ *
+ * @author    Patrick Talbot (based on Gerhard Schwarz work for German)
+ * @version   $Id$
+ */
+public final class FrenchStemFilter extends TokenFilter {
+
+	/**
+	 * The actual token in the input stream.
+	 */
+	private Token token = null;
+	private FrenchStemmer stemmer = null;
+	private Hashtable exclusions = null;
+
+	public FrenchStemFilter( TokenStream in ) {
+    super(in);
+		stemmer = new FrenchStemmer();
+	}
+
+	/**
+	 * Builds a FrenchStemFilter that uses an exclusiontable.
+	 */
+	public FrenchStemFilter( TokenStream in, Hashtable exclusiontable ) {
+		this( in );
+		exclusions = exclusiontable;
+	}
+
+	/**
+	 * @return  Returns the next token in the stream, or null at EOS
+	 */
+	public final Token next()
+		throws IOException {
+		if ( ( token = input.next() ) == null ) {
+			return null;
+		}
+		// Check the exclusiontable
+		else if ( exclusions != null && exclusions.contains( token.termText() ) ) {
+			return token;
+		}
+		else {
+			String s = stemmer.stem( token.termText() );
+			// If not stemmed, dont waste the time creating a new token
+			if ( !s.equals( token.termText() ) ) {
+				return new Token( s, 0, s.length(), token.type() );
+			}
+			return token;
+		}
+	}
+	/**
+	 * Set a alternative/custom FrenchStemmer for this filter.
+	 */
+	public void setStemmer( FrenchStemmer stemmer ) {
+		if ( stemmer != null ) {
+			this.stemmer = stemmer;
+		}
+	}
+	/**
+	 * Set an alternative exclusion list for this filter.
+	 */
+	public void setExclusionTable( Hashtable exclusiontable ) {
+		exclusions = exclusiontable;
+	}
+}
+
+
