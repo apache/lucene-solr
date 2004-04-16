34b8f647fee17745c684d26497651a0debb4aac6
Christoph Goller <goller@apache.org>
2004-04-16 09:49:06 +0000

    added some javadoc (patch from Daniel)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150287 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/WildcardQuery.java
+++ src/java/org/apache/lucene/search/WildcardQuery.java
@@ -20,7 +20,15 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import java.io.IOException;
 
-/** Implements the wildcard search query */
+/** Implements the wildcard search query. Supported wildcards are <code>*</code>, which
+ * matches any character sequence (including the empty one), and <code>?</code>,
+ * which matches any single character. Note this query can be slow, as it
+ * needs to iterate over all terms. In order to prevent extremely slow WildcardQueries,
+ * a Wildcard term must not start with one of the wildcards <code>*</code> or
+ * <code>?</code>.
+ * 
+ * @see WildcardTermEnum
+ */
 public class WildcardQuery extends MultiTermQuery {
   public WildcardQuery(Term term) {
     super(term);
