1646bda9ef3ee82cfef7604ef0d2637e7cc41f52
Tim Jones <tjones@apache.org>
2004-04-21 20:15:55 +0000

    fix the toString(s) - thanks, Erik!
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150309 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/FilteredQuery.java
+++ src/java/org/apache/lucene/search/FilteredQuery.java
@@ -102,7 +102,7 @@ extends Query {
 
   /** Prints a user-readable version of this query. */
   public String toString (String s) {
-    return "filtered("+query.toString()+")";
+    return "filtered("+query.toString(s)+")";
   }
 
   /** Returns true iff <code>o</code> is equal to this. */
