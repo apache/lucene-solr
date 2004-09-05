c7fbb991c3e55fb6c3c3f3d2f0757176a7eccb06
Daniel Naber <dnaber@apache.org>
2004-09-05 21:27:29 +0000

    Add a new class that supports converting dates to readable strings, with limited resolution. Other public methods let people limit the resolution of their dates. DateField is not deprecated because it still has the advantage of slightly shorter fields in some cases.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150483 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/document/DateField.java
+++ src/java/org/apache/lucene/document/DateField.java
@@ -18,26 +18,27 @@ package org.apache.lucene.document;
 
 import java.util.Date;
 
+import org.apache.lucene.search.PrefixQuery;  // for javadoc
+import org.apache.lucene.search.RangeQuery;   // for javadoc
+
 /**
  * Provides support for converting dates to strings and vice-versa.
  * The strings are structured so that lexicographic sorting orders by date,
  * which makes them suitable for use as field values and search terms.
  * 
- * <P>
- * Note that you do not have to use this class, you can just save your
- * dates as strings if lexicographic sorting orders them by date. This is
- * the case for example for dates like <code>yyyy-mm-dd hh:mm:ss</code>
- * (of course you can leave out the delimiter characters to save some space).
- * The advantage with using such a format is that you can easily save dates
- * with the required granularity, e.g. leaving out seconds. This saves memory
- * when searching with a RangeQuery or PrefixQuery, as Lucene
- * expands these queries to a BooleanQuery with potentially very many terms. 
+ * <P>Note that this class saves dates with millisecond granularity,
+ * which is bad for {@link RangeQuery} and {@link PrefixQuery}, as those
+ * queries are expanded to a BooleanQuery with a potentially large number 
+ * of terms when searching. Thus you might want to use
+ * {@link DateTools} instead.
  * 
  * <P>
  * Note: dates before 1970 cannot be used, and therefore cannot be
- * indexed when using this class.
+ * indexed when using this class. See {@link DateTools} for an
+ * alternative without such a limitation.
  */
 public class DateField {
+  
   private DateField() {}
 
   // make date strings long enough to last a millenium
