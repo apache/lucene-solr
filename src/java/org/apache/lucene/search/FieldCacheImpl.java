b6d184b28feb1d732750fb0ff3890b7b6aa41ea6
Doug Cutting <cutting@apache.org>
2004-10-11 17:07:39 +0000

    Restore an import required by GCJ's compiler.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150593 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/FieldCacheImpl.java
+++ src/java/org/apache/lucene/search/FieldCacheImpl.java
@@ -20,6 +20,7 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.TermDocs;
 import org.apache.lucene.index.TermEnum;
+import org.apache.lucene.search.FieldCache.StringIndex; // required by GCJ
 
 import java.io.IOException;
 import java.util.Map;
