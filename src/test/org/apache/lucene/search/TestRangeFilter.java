c35d91635506ca4c553e2d078452071383fa6384
Daniel Naber <dnaber@apache.org>
2004-11-23 19:56:40 +0000

    adding license; typo fix
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150663 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/lucene/search/TestRangeFilter.java
+++ src/test/org/apache/lucene/search/TestRangeFilter.java
@@ -1,5 +1,20 @@
 package org.apache.lucene.search;
 
+/**
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.io.IOException;
 
@@ -11,7 +26,7 @@ import org.apache.lucene.index.Term;
  *
  * <p>
  * NOTE: at the moment, this class only tests for 'positive' results,
- * it does not verify the results to ensure their are no 'false positives',
+ * it does not verify the results to ensure there are no 'false positives',
  * nor does it adequately test 'negative' results.  It also does not test
  * that garbage in results in an Exception.
  */
