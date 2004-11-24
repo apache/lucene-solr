74f6ac46159d60c63719c2a98745c565318565b5
Christoph Goller <goller@apache.org>
2004-11-24 17:40:48 +0000

    Old class name replaced with new class name in Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150670 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/document/NumberTools.java
+++ src/java/org/apache/lucene/document/NumberTools.java
@@ -23,8 +23,8 @@ package org.apache.lucene.document;
  * 
  * <p>
  * That is, if l1 is less than l2 for any two longs l1 and l2, then
- * LongField.longToString(l1) is lexicographically less than
- * LongField.longToString(l2). (Similarly for "greater than" and "equals".)
+ * NumberTools.longToString(l1) is lexicographically less than
+ * NumberTools.longToString(l2). (Similarly for "greater than" and "equals".)
  * 
  * <p>
  * This class handles <b>all</b> long values (unlike
