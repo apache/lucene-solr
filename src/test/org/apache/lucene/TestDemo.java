c62435297bc2f5d6db052ce998c45fa0fc436666
Daniel Naber <dnaber@apache.org>
2004-12-11 11:15:51 +0000

    adding a simple indexing and searching example to the API documentation
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150702 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/lucene/TestDemo.java
@@ -0,0 +1,77 @@
+package org.apache.lucene;
+
+/**
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.Hits;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+import junit.framework.TestCase;
+
+/**
+ * A very simple demo used in the API documentation (src/java/overview.html).
+ * 
+ * @author Daniel Naber
+ */
+public class TestDemo extends TestCase {
+
+  public void testDemo() throws IOException, ParseException {
+
+    Analyzer analyzer = new StandardAnalyzer();
+
+    // Store the index in memory:
+    Directory directory = new RAMDirectory();
+    // To store an index on disk, use this instead (note that the 
+    // parameter true will overwrite the index in that directory
+    // if one exists):
+    //Directory directory = FSDirectory.getDirectory("/tmp/testindex", true);
+    IndexWriter iwriter = new IndexWriter(directory, analyzer, true);
+    Document doc = new Document();
+    String text = "This is the text to be indexed.";
+    doc.add(new Field("fieldname", text, Field.Store.YES,
+        Field.Index.TOKENIZED));
+    iwriter.addDocument(doc);
+    iwriter.close();
+    
+    // Now search the index:
+    IndexSearcher isearcher = new IndexSearcher(directory);
+    // Parse a simple query that searches for "text":
+    Query query = QueryParser.parse("text", "fieldname", analyzer);
+    Hits hits = isearcher.search(query);
+    assertEquals(1, hits.length());
+    // Iterate through the results:
+    for (int i = 0; i < hits.length(); i++) {
+      Document hitDoc = hits.doc(i);
+      assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
+    }
+    isearcher.close();
+    directory.close();
+    
+  }
+
+}
