4f40db9c8695a5400d1b8ef74e5edee3a8fa1c29
Daniel Naber <dnaber@apache.org>
2005-01-24 18:49:02 +0000

    make it work for me
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@151038 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/contributions/lucli/run.sh
+++ sandbox/contributions/lucli/run.sh
@@ -1,10 +1,11 @@
 LUCLI=.
 LUCLI_MEMORY=128M
 #JAVA_HOME=/home/dror/j2sdk1.4.1_03/
-CLASSPATH=${CLASSPATH}:$LUCLI/lib/libreadline-java.jar:$LUCLI/lib/lucene-1.3-rc3-dev.jar:$LUCLI/classes/lucli.jar
+CLASSPATH=${CLASSPATH}:$LUCLI/lib/libreadline-java.jar:$LUCLI/lib/lucene.jar:$LUCLI/dist/lucli-dev.jar
 PATH=${PATH}:$JAVA_HOME/bin
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$LUCLI
 export LD_LIBRARY_PATH
+export CLASSPATH
 $JAVA_HOME/bin/java -Xmx${LUCLI_MEMORY} lucli.Lucli
 #Use this line to enable tab completion. Depends on the Readline shares library
 #$JAVA_HOME/bin/java lucli.Lucli -r
