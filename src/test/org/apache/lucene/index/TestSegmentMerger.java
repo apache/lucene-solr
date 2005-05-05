0209ce959b947248afb1686284deaf4bf55d87ca
Daniel Naber <dnaber@apache.org>
2005-05-05 15:22:03 +0000

    don't print to stdout in test cases
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@168338 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/lucene/index/TestSegmentMerger.java
+++ src/test/org/apache/lucene/index/TestSegmentMerger.java
@@ -97,7 +97,7 @@ public class TestSegmentMerger extends TestCase {
       
       Collection stored = mergedReader.getFieldNames(IndexReader.FieldOption.INDEXED_WITH_TERMVECTOR);
       assertTrue(stored != null);
-      System.out.println("stored size: " + stored.size());
+      //System.out.println("stored size: " + stored.size());
       assertTrue(stored.size() == 2);
       
       TermFreqVector vector = mergedReader.getTermFreqVector(0, DocHelper.TEXT_FIELD_2_KEY);
