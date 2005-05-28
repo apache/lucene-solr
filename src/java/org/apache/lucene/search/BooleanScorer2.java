3dc686157110e6dacbfb0576ef2ccddad2c3ccc1
Daniel Naber <dnaber@apache.org>
2005-05-28 15:57:54 +0000

    not public anymore + javadoc fixes (by Paul Elschot)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@178872 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/BooleanScorer2.java
+++ src/java/org/apache/lucene/search/BooleanScorer2.java
@@ -25,7 +25,7 @@ import java.util.Iterator;
  * <br>Uses ConjunctionScorer, DisjunctionScorer, ReqOptScorer and ReqExclScorer.
  * <br>Implements skipTo(), and has no limitations on the numbers of added scorers.
  */
-public class BooleanScorer2 extends Scorer {
+class BooleanScorer2 extends Scorer {
   private ArrayList requiredScorers = new ArrayList();
   private ArrayList optionalScorers = new ArrayList();
   private ArrayList prohibitedScorers = new ArrayList();
@@ -292,6 +292,14 @@ public class BooleanScorer2 extends Scorer {
     return sum * coordinator.coordFactor();
   }
 
+  /** Skips to the first match beyond the current whose document number is
+   * greater than or equal to a given target.
+   * 
+   * <p>When this method is used the {@link #explain(int)} method should not be used.
+   * 
+   * @param target The target document number.
+   * @return true iff there is such a match.
+   */
   public boolean skipTo(int target) throws IOException {
     if (countingSumScorer == null) {
       initCountingSumScorer();
@@ -299,7 +307,12 @@ public class BooleanScorer2 extends Scorer {
     return countingSumScorer.skipTo(target);
   }
 
-  public Explanation explain(int doc) throws IOException {
+  /** Throws an UnsupportedOperationException.
+   * TODO: Implement an explanation of the coordination factor.
+   * @param doc The document number for the explanation.
+   * @throws UnsupportedOperationException
+   */
+  public Explanation explain(int doc) {
     throw new UnsupportedOperationException();
  /* How to explain the coordination factor?
     initCountingSumScorer();
