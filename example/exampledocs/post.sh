1aec5a3da74073c31370c8c434d963efc269422f
Yonik Seeley <yonik@apache.org>
2006-02-23 19:18:22 +0000

    changes in the schema, and some sample docs
    git-svn-id: https://svn.apache.org/repos/asf/incubator/solr/trunk@380201 13f79535-47bb-0310-9956-ffa450edef68

---
+++ example/exampledocs/post.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+FILES=$*
+URL=http://localhost:8983/solr/update
+
+for f in $FILES; do
+  echo Posting file $f to $URL
+  curl $URL --data-binary @$f
+done
+
+#send the commit command to make sure all the changes are flushed and visible
+curl $URL --data-binary '<commit/>'
+
