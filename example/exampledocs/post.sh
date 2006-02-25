865611e7494caca8d464a1cb09a702e054f3ade6
Chris M. Hostetter <hossman@apache.org>
2006-02-25 02:53:14 +0000

    newlines are good
    git-svn-id: https://svn.apache.org/repos/asf/incubator/solr/trunk@380886 13f79535-47bb-0310-9956-ffa450edef68

--- example/exampledocs/post.sh
+++ example/exampledocs/post.sh
@@ -5,8 +5,9 @@ URL=http://localhost:8983/solr/update
 for f in $FILES; do
   echo Posting file $f to $URL
   curl $URL --data-binary @$f
+  echo
 done
 
 #send the commit command to make sure all the changes are flushed and visible
 curl $URL --data-binary '<commit/>'
-
+echo
