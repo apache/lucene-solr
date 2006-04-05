022a7db1a4639825962ca18a57747d3f09ad05e9
Yonik Seeley <yonik@apache.org>
2006-04-05 15:44:14 +0000

    error if path is missing and default is not supplied
    git-svn-id: https://svn.apache.org/repos/asf/incubator/solr/trunk@391644 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/core/Config.java
+++ src/java/org/apache/solr/core/Config.java
@@ -144,7 +144,7 @@ public class Config {
   }
 
   public int getInt(String path) {
-    return Integer.parseInt(getVal(path, false));
+    return Integer.parseInt(getVal(path, true));
   }
 
   public int getInt(String path, int def) {
@@ -153,7 +153,7 @@ public class Config {
   }
 
   public boolean getBool(String path) {
-    return Boolean.parseBoolean(getVal(path, false));
+    return Boolean.parseBoolean(getVal(path, true));
   }
 
   public boolean getBool(String path, boolean def) {
@@ -162,7 +162,7 @@ public class Config {
   }
 
   public float getFloat(String path) {
-    return Float.parseFloat(getVal(path, false));
+    return Float.parseFloat(getVal(path, true));
   }
 
   public float getFloat(String path, float def) {
