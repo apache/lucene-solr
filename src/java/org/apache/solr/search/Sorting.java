9909374e0447834d98b8aa7be23d3221582f4e0f
Yonik Seeley <yonik@apache.org>
2006-07-15 19:29:43 +0000

    fix reverse sorting when sortMissingFirst=true
    git-svn-id: https://svn.apache.org/repos/asf/incubator/solr/trunk@422280 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/search/Sorting.java
+++ src/java/org/apache/solr/search/Sorting.java
@@ -44,7 +44,7 @@ public class Sorting {
       if (!reverse) return new SortField(fieldName, nullStringLastComparatorSource);
       else return new SortField(fieldName, SortField.STRING, true);
     } else if (nullFirst) {
-      if (reverse) return new SortField(fieldName, nullStringLastComparatorSource);
+      if (reverse) return new SortField(fieldName, nullStringLastComparatorSource, true);
       else return new SortField(fieldName, SortField.STRING, false);
     } else {
       return new SortField(fieldName, SortField.STRING, reverse);
