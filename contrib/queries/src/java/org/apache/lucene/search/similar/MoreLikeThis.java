dab153b256b58ec2f4e05b1b2ca34bb86966b54d
Daniel Naber <dnaber@apache.org>
2006-11-22 22:41:33 +0000

    LUCENE-722: DEFAULT spelled DEFALT in MoreLikeThis.java
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@478354 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/queries/src/java/org/apache/lucene/search/similar/MoreLikeThis.java
+++ contrib/queries/src/java/org/apache/lucene/search/similar/MoreLikeThis.java
@@ -174,7 +174,7 @@ public final class MoreLikeThis {
 	 * @see #getMinDocFreq
 	 * @see #setMinDocFreq	 
      */
-    public static final int DEFALT_MIN_DOC_FREQ = 5;
+    public static final int DEFAULT_MIN_DOC_FREQ = 5;
 
     /**
      * Boost terms in query based on score.
@@ -239,7 +239,7 @@ public final class MoreLikeThis {
     /**
      * Ignore words which do not occur in at least this many docs.
      */
-    private int minDocFreq = DEFALT_MIN_DOC_FREQ;
+    private int minDocFreq = DEFAULT_MIN_DOC_FREQ;
 
     /**
      * Should we apply a boost to the Query based on the scores?
@@ -332,7 +332,7 @@ public final class MoreLikeThis {
 
     /**
      * Returns the frequency at which words will be ignored which do not occur in at least this
-     * many docs. The default frequency is {@link #DEFALT_MIN_DOC_FREQ}.
+     * many docs. The default frequency is {@link #DEFAULT_MIN_DOC_FREQ}.
      *
      * @return the frequency at which words will be ignored which do not occur in at least this
      * many docs.
