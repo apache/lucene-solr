8e82ba6386d164a7e4e59da0993c2cd110827eb3
Erik Hatcher <ehatcher@apache.org>
2007-01-30 06:07:43 +0000

    Define Solr::Request first thing, allowing future simplification of the request class definitions
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@501319 13f79535-47bb-0310-9956-ffa450edef68

--- client/ruby/solrb/lib/solr/request.rb
+++ client/ruby/solrb/lib/solr/request.rb
@@ -10,6 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+module Solr; module Request; end; end
 require 'solr/request/add_document'
 require 'solr/request/base'
 require 'solr/request/commit'
