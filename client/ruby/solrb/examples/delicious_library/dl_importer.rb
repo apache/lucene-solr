ff7c75418bc2816a864f8757f360737be351d1b7
Erik Hatcher <ehatcher@apache.org>
2007-02-03 07:36:04 +0000

    Add Delicious Library <http://www.delicious-monster.com/> importer and small sample set.  This is way more fun than processing MARC records!
    To launch Solr properly for this dataset, cd solr: java -Dsolr.solr.home=../examples/delicious_library/ -jar start.jar - this will keep this index separate
    from any others you may be experimenting with.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@503178 13f79535-47bb-0310-9956-ffa450edef68

---
+++ client/ruby/solrb/examples/delicious_library/dl_importer.rb
@@ -0,0 +1,68 @@
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TODO: distill common shell script needs into common file for parsing parameters for Solr URL, input filename, -debug, etc
+#       script/runner or script/console-like, from Rails.  A data mapper would be a great generalizable piece.
+
+require 'solr'
+
+solr_url = ENV["SOLR_URL"] || "http://localhost:8983/solr"
+dl_filename = ARGV[0]
+debug = ARGV[1] == "-debug"
+
+solr = Solr::Connection.new(solr_url)
+
+lines = IO.readlines(dl_filename)
+headers = lines[0].split("\t")
+puts headers.join(','),"-----" if debug
+
+# Exported column names
+# medium,associatedURL,boxHeightInInches,boxLengthInInches,boxWeightInPounds,boxWidthInInches,
+# scannednumber,upc,asin,country,title,fullTitle,series,numberInSeries,edition,aspect,mediacount,
+# genre,price,currentValue,language,netrating,description,owner,publisher,published,rare,purchaseDate,rating,
+# used,signed,hasExperienced,notes,location,paid,condition,notowned,author,illustrator,pages
+mapping = {
+  :id => :upc,
+  :medium_facet => :medium,
+  :country_facet => :country,
+  :signed_facet => :signed,
+  :rating_facet => :netrating,
+  :language_facet => :language,
+  :genre_facet => Proc.new {|data| data.genre.split('/').map {|s| s.strip}},
+  :title_text => :title,
+  :notes_text => :notes,
+  :publisher_text => :publisher,
+  :description_text => :description
+}
+
+lines[1..-1].each do |line|
+  data = headers.zip(line.split("\t"))
+  def data.method_missing(key)
+    self.assoc(key.to_s)[1]
+  end
+
+  doc = {}
+  mapping.each do |solr_name, data_column_or_proc|
+    if data_column_or_proc.is_a? Proc
+      value = data_column_or_proc.call(data)
+    else
+      value = data.send(data_column_or_proc)
+    end  
+    doc[solr_name] = value if value
+  end
+  
+  puts doc.inspect if debug
+  solr.add doc unless debug
+
+end
+
+solr.commit unless debug
