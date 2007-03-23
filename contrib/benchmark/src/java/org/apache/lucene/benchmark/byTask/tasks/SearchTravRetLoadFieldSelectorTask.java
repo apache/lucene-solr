cfff81d1dcc467eda4b3c2cdda7d9fa08a7f7f7d
Grant Ingersoll <gsingers@apache.org>
2007-03-23 12:26:08 +0000

    LUCENE-837: fix copyright
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@521698 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/benchmark/src/java/org/apache/lucene/benchmark/byTask/tasks/SearchTravRetLoadFieldSelectorTask.java
+++ contrib/benchmark/src/java/org/apache/lucene/benchmark/byTask/tasks/SearchTravRetLoadFieldSelectorTask.java
@@ -1,12 +1,20 @@
 package org.apache.lucene.benchmark.byTask.tasks;
 /**
- * Created by IntelliJ IDEA.
- * User: Grant Ingersoll
- * Date: Mar 22, 2007
- * Time: 10:04:49 PM
- * $Id:$
- * Copyright 2007.  Center For Natural Language Processing
+ * Copyright 2005 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 
 import org.apache.lucene.benchmark.byTask.PerfRunData;
 import org.apache.lucene.document.FieldSelector;
