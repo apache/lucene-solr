dcf9e3ea669ed272a9739af367aa5c48890f18f9
Erik Hatcher <ehatcher@apache.org>
2007-04-03 01:01:25 +0000

    Dynamically load all tests for suite.rb (contribution from Josh Goebel)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@524986 13f79535-47bb-0310-9956-ffa450edef68

--- client/ruby/solr-ruby/test/unit/suite.rb
+++ client/ruby/solr-ruby/test/unit/suite.rb
@@ -10,16 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO dynamically require all *_test files
-
-require 'add_document_test'
-require 'commit_test'
-require 'connection_test'
-require 'delete_test'
-require 'document_test'
-require 'field_test'
-require 'ping_test'
-require 'request_test'
-require 'response_test'
-require 'standard_request_test'
-require 'standard_response_test'
+# dynamically require all tests files
+Dir.glob("*_test.rb").each do | file |
+ require file
+end
