2137fae1161debbf34b68551839a163716b3b76c
Otis Gospodnetic <otis@apache.org>
2007-04-09 17:25:33 +0000

    - Removed a @todo and replaced it with javadoc about RemoteCachingWrapperFilter that was added in LUCENE-853
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@526846 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/search/CachingWrapperFilter.java
+++ src/java/org/apache/lucene/search/CachingWrapperFilter.java
@@ -33,8 +33,8 @@ public class CachingWrapperFilter extends Filter {
   protected Filter filter;
 
   /**
-   * @todo What about serialization in RemoteSearchable?  Caching won't work.
-   *       Should transient be removed?
+   * A transient Filter cache.  To cache Filters even when using {@link RemoteSearchable} use
+   * {@link RemoteCachingWrapperFilter} instead.
    */
   protected transient Map cache;
 
