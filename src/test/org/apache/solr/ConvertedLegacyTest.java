fa6d8ba5dc4ae481a6068db3f46c4ca6c8d66337
Ryan McKinley <ryan@apache.org>
2007-04-29 06:05:25 +0000

    changing test to expect error code 400 rather then 1 for unknown sort field
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@533466 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/solr/ConvertedLegacyTest.java
+++ src/test/org/apache/solr/ConvertedLegacyTest.java
@@ -787,7 +787,7 @@ public class ConvertedLegacyTest extends AbstractSolrTestCase {
         req( "id_i:1000; nullfirst" ), 400 );
 
     assertQEx( "unknown sort field",
-        req( "id_i:1000; abcde12345 asc" ), 1 ); 
+        req( "id_i:1000; abcde12345 asc" ), 400 ); 
 
     assertQEx( "unknown sort order",
         req( "id_i:1000; nullfirst aaa" ), 400 ); 
