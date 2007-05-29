d45042208b89d55ce150b4ce7e98557aaa531a25
Yonik Seeley <yonik@apache.org>
2007-05-29 21:11:35 +0000

    add ASF license
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@542653 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/search/QueryUtils.java
+++ src/java/org/apache/solr/search/QueryUtils.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.solr.search;
 
 import org.apache.lucene.search.Query;
@@ -66,37 +83,6 @@ public class QueryUtils {
       }
       return newBq;
     }
-
-
-    /*** TODO: use after next lucene update
-    List <BooleanClause> clauses = (List <BooleanClause>)bq.clauses();
-    // A single filtered out stopword currently causes a BooleanQuery with
-    // zero clauses.
-    if (clauses.size()==0) return q;
-
-    for (BooleanClause clause: clauses) {
-      if (!clause.isProhibited()) return q;
-    }
-
-    if (clauses.size()==1) {
-      // if only one clause, dispense with the wrapping BooleanQuery
-      Query negClause = clauses.get(0).getQuery();
-      // we shouldn't need to worry about adjusting the boosts since the negative
-      // clause would have never been selected in a positive query, and hence the
-      // boost is meaningless.
-      return negClause;
-    } else {
-      BooleanQuery newBq = new BooleanQuery(bq.isCoordDisabled());
-      newBq.setBoost(bq.getBoost());
-      // ignore minNrShouldMatch... it doesn't make sense for a negative query
-
-      // the inverse of -a -b is a b
-      for (BooleanClause clause: clauses) {
-        newBq.add(clause.getQuery(), BooleanClause.Occur.SHOULD);
-      }
-      return newBq;
-    }
-    ***/
   }
 
   /** Makes negative queries suitable for querying by
