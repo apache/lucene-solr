8f8b006c1c55cf01d37ff4d257c5953f402cc305
Ryan McKinley <ryan@apache.org>
2007-06-14 17:07:06 +0000

    SOLR-20: Adding a document list class in 'common'
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@547314 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/solr/common/SolrDocumentList.java
@@ -0,0 +1,60 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.solr.common;
+
+import java.util.ArrayList;
+
+
+/**
+ * Represent a list of SolrDocuments returned from a search.  This includes
+ * position and offset information.
+ * 
+ * @author ryan
+ * @version $Id$
+ * @since solr 1.3
+ */
+public class SolrDocumentList extends ArrayList<SolrDocument>
+{ 
+  private int numFound = 0;
+  private int start = 0;
+  private Float maxScore = null;
+  
+  public Float getMaxScore() {
+    return maxScore;
+  }
+  
+  public void setMaxScore(Float maxScore) {
+    this.maxScore = maxScore;
+  }
+  
+  public int getNumFound() {
+    return numFound;
+  }
+  
+  public void setNumFound(int numFound) {
+    this.numFound = numFound;
+  }
+  
+  public int getStart() {
+    return start;
+  }
+  
+  public void setStart(int start) {
+    this.start = start;
+  }
+}
