7c660c8526a457e95641130bb8b55c48db13b4af
Yonik Seeley <yonik@apache.org>
2008-01-08 15:50:45 +0000

    don't default an encapsulator for split CSV fields
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@610032 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/handler/CSVRequestHandler.java
+++ src/java/org/apache/solr/handler/CSVRequestHandler.java
@@ -322,9 +322,11 @@ abstract class CSVLoader {
         String sepStr = params.getFieldParam(fname,SEPARATOR);
         char fsep = sepStr==null || sepStr.length()==0 ? ',' : sepStr.charAt(0);
         String encStr = params.getFieldParam(fname,ENCAPSULATOR);
-        char fenc = encStr==null || encStr.length()==0 ? '\'' : encStr.charAt(0);
+        char fenc = encStr==null || encStr.length()==0 ? (char)-2 : encStr.charAt(0);
+        String escStr = params.getFieldParam(fname,ESCAPE);
+        char fesc = escStr==null || encStr.length()==0 ? CSVStrategy.ESCAPE_DISABLED : escStr.charAt(0);
 
-        CSVStrategy fstrat = new CSVStrategy(fsep,fenc,CSVStrategy.COMMENTS_DISABLED);
+        CSVStrategy fstrat = new CSVStrategy(fsep,fenc,CSVStrategy.COMMENTS_DISABLED,fesc, false, false, false, false);
         adders[i] = new CSVLoader.FieldSplitter(fstrat, adders[i]);
       }
     }
