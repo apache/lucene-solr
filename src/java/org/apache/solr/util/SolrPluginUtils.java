fb295f381c8d918584f6206028d310052ee97e9d
Yonik Seeley <yonik@apache.org>
2008-03-04 23:35:43 +0000

    StringBuffer -> StringBuilder
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@633706 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/util/SolrPluginUtils.java
+++ src/java/org/apache/solr/util/SolrPluginUtils.java
@@ -659,7 +659,7 @@ public class SolrPluginUtils {
    * @see QueryParser#escape
    */
   public static CharSequence partialEscape(CharSequence s) {
-    StringBuffer sb = new StringBuffer();
+    StringBuilder sb = new StringBuilder();
     for (int i = 0; i < s.length(); i++) {
       char c = s.charAt(i);
       if (c == '\\' || c == '!' || c == '(' || c == ')' ||
