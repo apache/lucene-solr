86571cc5b9faa9d036048fa7dd25526f701487ad
Michael McCandless <mikemccand@apache.org>
2008-03-28 19:24:06 +0000

    LUCENE-1251: be sure to close the descriptor even if super.close() hits an exception, in FSIndexOutput.close
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/branches/lucene_2_3@642341 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/store/FSDirectory.java
+++ src/java/org/apache/lucene/store/FSDirectory.java
@@ -601,9 +601,21 @@ public class FSDirectory extends Directory {
     public void close() throws IOException {
       // only close the file if it has not been closed yet
       if (isOpen) {
-        super.close();
-        file.close();
-        isOpen = false;
+        boolean success = false;
+        try {
+          super.close();
+          success = true;
+        } finally {
+          isOpen = false;
+          if (!success) {
+            try {
+              file.close();
+            } catch (Throwable t) {
+              // Suppress so we don't mask original exception
+            }
+          } else
+            file.close();
+        }
       }
     }
   
