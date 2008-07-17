14d8ee73894ed99cc7da18234c614c328e6842b6
Yonik Seeley <yonik@apache.org>
2008-07-17 13:03:08 +0000

    javadoc fix, update to new localparams syntax
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@677573 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/search/NestedQParserPlugin.java
+++ src/java/org/apache/solr/search/NestedQParserPlugin.java
@@ -30,9 +30,9 @@ import org.apache.solr.search.function.ValueSource;
  * Create a nested query, with the ability of that query to redefine it's type via
  * local parameters.  This is useful in specifying defaults in configuration and
  * letting clients indirectly reference them.
- * <br>Example: <code>&lt;!query defType=func v=$q1&gt;</code>
+ * <br>Example: <code>{!query defType=func v=$q1}</code>
  * <br> if the q1 parameter is <code>price</code> then the query would be a function query on the price field.
- * <br> if the q1 parameter is <code>&lt;!lucene&gt;inStock:true</code> then a term query is
+ * <br> if the q1 parameter is <code>{!lucene}inStock:true</code> then a term query is
  *     created from the lucene syntax string that matches documents with inStock=true.
  */
 public class NestedQParserPlugin extends QParserPlugin {
