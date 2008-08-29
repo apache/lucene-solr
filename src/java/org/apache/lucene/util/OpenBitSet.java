0b9512aa23aab5d1fb2792f83263b2bb83676dbc
Yonik Seeley <yonik@apache.org>
2008-08-29 15:33:44 +0000

    fix OpenBitSet.hashCode rotate
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@690302 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/util/OpenBitSet.java
+++ src/java/org/apache/lucene/util/OpenBitSet.java
@@ -763,7 +763,7 @@ public class OpenBitSet extends DocIdSet implements Cloneable, Serializable {
       long h = 0x98761234;  // something non-zero for length==0
       for (int i = bits.length; --i>=0;) {
       h ^= bits[i];
-      h = (h << 1) | (h >>> 31); // rotate left
+      h = (h << 1) | (h >>> 63); // rotate left
     }
     return (int)((h>>32) ^ h);  // fold leftmost bits into right
   }
