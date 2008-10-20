da78e20bd06c3cda875cac30cf1885be8611f7a5
Michael McCandless <mikemccand@apache.org>
2008-10-20 09:12:46 +0000

    LUCENE-1416: don't fail contrib/ant's unit test when file path contains non-URL safe characters like space
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@706160 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/ant/src/test/org/apache/lucene/ant/DocumentTestCase.java
+++ contrib/ant/src/test/org/apache/lucene/ant/DocumentTestCase.java
@@ -19,6 +19,7 @@ package org.apache.lucene.ant;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
 
 import junit.framework.TestCase;
 
@@ -31,8 +32,8 @@ public abstract class DocumentTestCase extends TestCase
     protected File getFile(String filename) throws IOException {
         String fullname =
                      this.getClass().getResource(filename).getFile();
-                     
-        File file = new File(fullname);
+        
+        File file = new File(URLDecoder.decode(fullname, "UTF-8"));
         
         return file;
     }
