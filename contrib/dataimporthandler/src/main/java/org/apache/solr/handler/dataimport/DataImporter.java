dec0603c36b6d88262c700ac6511fd576da884f6
Shalin Shekhar Mangar <shalin@apache.org>
2008-10-28 09:05:14 +0000

    Use tryLock instead of isLocked
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@708487 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/DataImporter.java
+++ contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/DataImporter.java
@@ -385,7 +385,7 @@ public class DataImporter {
   }
 
   void runCmd(RequestParams reqParams, SolrWriter sw, Map<String, String> variables) {
-    if (importLock.isLocked())
+    if (importLock.tryLock())
       return;
     importLock.lock();
     try {
