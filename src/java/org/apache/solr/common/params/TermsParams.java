497a61a2fbf3f76fe9751977f1becde9d03ce0b4
Ryan McKinley <ryan@apache.org>
2008-11-29 15:37:18 +0000

    make TermsParams an interface rather then a class (like all the other params classes)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@721684 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/common/params/TermsParams.java
+++ src/java/org/apache/solr/common/params/TermsParams.java
@@ -1,4 +1,3 @@
-package org.apache.solr.common.params;
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,12 +15,14 @@ package org.apache.solr.common.params;
  * limitations under the License.
  */
 
+package org.apache.solr.common.params;
+
 
 /**
  *
  *
  **/
-public class TermsParams {
+public interface TermsParams {
   /**
    * The component name.  Set to true to turn on the TermsComponent
    */
