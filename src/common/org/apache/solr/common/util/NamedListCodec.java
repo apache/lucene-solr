0f95d25fcd2993fe8b712a4d280b474a50d6530c
Shalin Shekhar Mangar <shalin@apache.org>
2008-12-23 06:51:37 +0000

    SOLR-885 follow up: Overriding constructors for backward compatibility
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@728887 13f79535-47bb-0310-9956-ffa450edef68

--- src/common/org/apache/solr/common/util/NamedListCodec.java
+++ src/common/org/apache/solr/common/util/NamedListCodec.java
@@ -21,4 +21,11 @@ package org.apache.solr.common.util;
  */
 @Deprecated
 public class NamedListCodec extends JavaBinCodec {
+  public NamedListCodec() {
+    super();
+  }
+
+  public NamedListCodec(ObjectResolver resolver) {
+    super(resolver);
+  }
 }
