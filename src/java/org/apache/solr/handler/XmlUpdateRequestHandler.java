d9e76a7cca7074ef21af9870ec01669f95be5ece
Ryan McKinley <ryan@apache.org>
2008-12-30 23:11:22 +0000

    add deprecation links
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@730269 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/handler/XmlUpdateRequestHandler.java
+++ src/java/org/apache/solr/handler/XmlUpdateRequestHandler.java
@@ -56,8 +56,20 @@ public class XmlUpdateRequestHandler extends ContentStreamHandlerBase {
 
   public static final String OVERWRITE = "overwrite";
   public static final String COMMIT_WITHIN = "commitWithin";
-  public static final String OVERWRITE_COMMITTED = "overwriteCommitted"; // @Deprecated
-  public static final String OVERWRITE_PENDING = "overwritePending";  // @Deprecated
+  
+  /**
+   * @deprecated use {@link #OVERWRITE}
+   */
+  public static final String OVERWRITE_COMMITTED = "overwriteCommitted";
+  
+  /**
+   * @deprecated use {@link #OVERWRITE}
+   */
+  public static final String OVERWRITE_PENDING = "overwritePending";
+
+  /**
+   * @deprecated use {@link #OVERWRITE}
+   */
   public static final String ALLOW_DUPS = "allowDups";
 
   XMLInputFactory inputFactory;
