cc963a1aea13b82dae96b6b5950da4e4f052b12b
Yonik Seeley <yonik@apache.org>
2009-03-20 22:03:12 +0000

    SOLR-1072: fix FileUtilsTest for windows
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@756783 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/solr/common/util/FileUtilsTest.java
+++ src/test/org/apache/solr/common/util/FileUtilsTest.java
@@ -25,8 +25,9 @@ import junit.framework.TestCase;
 public class FileUtilsTest extends TestCase {  
 
   public void testResolve() throws IOException {
+    String cwd = new File(".").getAbsolutePath();
     assertEquals(new File("conf/data"), FileUtils.resolvePath(new File("conf"), "data"));
-    assertEquals(new File("/conf/data"), FileUtils.resolvePath(new File("/conf"), "data"));
-    assertEquals(new File("/data"), FileUtils.resolvePath(new File("conf"), "/data"));
+    assertEquals(new File(cwd+"/conf/data"), FileUtils.resolvePath(new File(cwd+"/conf"), "data"));
+    assertEquals(new File(cwd+"/data"), FileUtils.resolvePath(new File("conf"), cwd+"/data"));
   }
 }
