be0a47b7e34e27d9035110606d67af956dbfd5cc
Michael McCandless <mikemccand@apache.org>
2009-05-14 10:50:52 +0000

    LUCENE-1629: move CHANGES entry to contrib; add TestArabicAnalyzer
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@774727 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/analyzers/src/test/org/apache/lucene/analysis/ar/TestArabicAnalyzer.java
@@ -0,0 +1,36 @@
+package org.apache.lucene.analysis.ar;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import junit.framework.TestCase;
+
+/**
+ * Test the Arabic Analyzer
+ *
+ */
+public class TestArabicAnalyzer extends TestCase {
+  
+  /** This test fails with NPE when the 
+   * stopwords file is missing in classpath */
+  public void testResourcesAvailable() {
+    new ArabicAnalyzer();
+  }
+  
+  /* TODO: more tests */
+
+}
