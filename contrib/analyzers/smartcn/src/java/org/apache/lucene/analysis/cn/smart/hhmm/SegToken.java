ab0de2448a1ff461c6cd1b42f0a871adf3c37d71
Michael McCandless <mikemccand@apache.org>
2009-08-07 16:48:09 +0000

    fix smartcn to be JDK 1.4 only
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@802085 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/analyzers/smartcn/src/java/org/apache/lucene/analysis/cn/smart/hhmm/SegToken.java
+++ contrib/analyzers/smartcn/src/java/org/apache/lucene/analysis/cn/smart/hhmm/SegToken.java
@@ -91,7 +91,9 @@ public class SegToken {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + Arrays.hashCode(charArray);
+    for(int i=0;i<charArray.length;i++) {
+      result = prime * result + charArray[i];
+    }
     result = prime * result + endOffset;
     result = prime * result + index;
     result = prime * result + startOffset;
--- contrib/analyzers/smartcn/src/java/org/apache/lucene/analysis/cn/smart/hhmm/SegTokenPair.java
+++ contrib/analyzers/smartcn/src/java/org/apache/lucene/analysis/cn/smart/hhmm/SegTokenPair.java
@@ -51,7 +51,9 @@ class SegTokenPair {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + Arrays.hashCode(charArray);
+    for(int i=0;i<charArray.length;i++) {
+      result = prime * result + charArray[i];
+    }
     result = prime * result + from;
     result = prime * result + to;
     long temp;
