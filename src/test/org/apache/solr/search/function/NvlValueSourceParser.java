6a1e2c9b1498a3180b988dcf04801d518a73d285
Shalin Shekhar Mangar <shalin@apache.org>
2009-08-30 06:27:37 +0000

    Added license headers and removed unused imports
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@809250 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/solr/search/function/NvlValueSourceParser.java
+++ src/test/org/apache/solr/search/function/NvlValueSourceParser.java
@@ -1,12 +1,26 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.solr.search.function;
 
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.search.FunctionQParser;
 import org.apache.solr.search.ValueSourceParser;
-import org.apache.solr.search.function.DocValues;
-import org.apache.solr.search.function.SimpleFloatFunction;
-import org.apache.solr.search.function.ValueSource;
 
 /**
  * A sample ValueSourceParser for testing. Approximates the oracle NVL function,
