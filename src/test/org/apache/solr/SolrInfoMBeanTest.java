4f492a5ee1f69dd0581cae9df2293664152f1143
Shalin Shekhar Mangar <shalin@apache.org>
2009-08-30 19:08:49 +0000

    Added license headers and removed unused imports
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@809403 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/solr/SolrInfoMBeanTest.java
+++ src/test/org/apache/solr/SolrInfoMBeanTest.java
@@ -1,16 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.solr;
 
-import java.io.File;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-
-import javax.management.Query;
-
-import org.apache.lucene.search.function.FieldCacheSource;
-import org.apache.solr.common.util.NamedList;
+import junit.framework.TestCase;
 import org.apache.solr.core.SolrInfoMBean;
 import org.apache.solr.handler.StandardRequestHandler;
 import org.apache.solr.handler.admin.LukeRequestHandler;
@@ -18,10 +24,12 @@ import org.apache.solr.handler.component.SearchComponent;
 import org.apache.solr.handler.component.SearchHandler;
 import org.apache.solr.highlight.DefaultSolrHighlighter;
 import org.apache.solr.search.LRUCache;
-import org.apache.solr.search.QueryUtils;
-import org.apache.solr.update.UpdateHandler;
 
-import junit.framework.TestCase;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * A simple test used to increase code coverage for some standard things...
--- src/test/org/apache/solr/highlight/HighlighterConfigTest.java
+++ src/test/org/apache/solr/highlight/HighlighterConfigTest.java
@@ -1,18 +1,25 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.solr.highlight;
 
-import java.io.IOException;
-import java.util.HashMap;
-
-import org.apache.lucene.search.Query;
-import org.apache.solr.common.util.NamedList;
-import org.apache.solr.common.util.SimpleOrderedMap;
-import org.apache.solr.core.Config;
-import org.apache.solr.core.SolrCore;
-import org.apache.solr.request.SolrQueryRequest;
-import org.apache.solr.search.DocList;
 import org.apache.solr.util.AbstractSolrTestCase;
 import org.apache.solr.util.TestHarness;
 
+import java.util.HashMap;
 
 public class HighlighterConfigTest extends AbstractSolrTestCase {
 	  @Override public String getSchemaFile() { return "schema.xml"; }
