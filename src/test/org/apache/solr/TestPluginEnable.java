325f20fc5054b6bb66ba08e011e861f7644e2724
Noble Paul <noble@apache.org>
2009-08-31 09:24:49 +0000

    autoprops setup
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@809514 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/solr/TestPluginEnable.java
+++ src/test/org/apache/solr/TestPluginEnable.java
@@ -5,29 +5,28 @@ import org.apache.solr.util.AbstractSolrTestCase;
 
 /**
  * <p> Test disabling components</p>
- * @version $Id:$
+ *
+ * @version $Id$
  * @since solr 1.4
  */
 public class TestPluginEnable extends AbstractSolrTestCase {
 
 
   public void testSimple() throws SolrServerException {
-    assertNull( h.getCore().getRequestHandler("disabled"));
-    assertNotNull( h.getCore().getRequestHandler("enabled"));
+    assertNull(h.getCore().getRequestHandler("disabled"));
+    assertNotNull(h.getCore().getRequestHandler("enabled"));
 
   }
 
 
+  @Override
+  public String getSchemaFile() {
+    return "schema-replication1.xml";
+  }
 
-
-    @Override
-    public String getSchemaFile() {
-      return "schema-replication1.xml";
-    }
-
-    @Override
-    public String getSolrConfigFile() {
-      return "solrconfig-enableplugin.xml";
-    }
+  @Override
+  public String getSolrConfigFile() {
+    return "solrconfig-enableplugin.xml";
+  }
 
 }
