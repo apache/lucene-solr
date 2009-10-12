2e4b1427fa583365d416def51806980fd768a6e3
Ryan McKinley <ryan@apache.org>
2009-10-12 16:01:51 +0000

    SOLR-1497 - remove solrjs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@824396 13f79535-47bb-0310-9956-ffa450edef68

--- client/javascript/example/reuters/testdata/download-dataset.sh
+++
@@ -1,4 +0,0 @@
-wget http://kdd.ics.uci.edu/databases/reuters21578/reuters21578.tar.gz
-tar xvf reuters21578.tar.gz
-wget http://kdd.ics.uci.edu/databases/reuters21578/reuters21578.tar.gz
-tar xvf reuters21578.tar.gz
--- client/javascript/lib/jsdoc/jsdoc-toolkit/app/plugins/symbolLink.js
+++
@@ -1,9 +0,0 @@
-JSDOC.PluginManager.registerPlugin(
-	"JSDOC.symbolLink",
-	{
-		onSymbolLink: function(link) {
-			// modify link.linkPath
-			// or link.linkText here
-		}
-	}
-);
