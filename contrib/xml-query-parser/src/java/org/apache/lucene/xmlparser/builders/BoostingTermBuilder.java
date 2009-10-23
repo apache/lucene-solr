1b59256ab8a98ae6a026744d7050d4a1bd4a1296
Uwe Schindler <uschindler@apache.org>
2009-10-23 12:21:43 +0000

    LUCENE-1973: Remove deprecated BoostingTermQuery
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@829020 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/xml-query-parser/src/java/org/apache/lucene/xmlparser/builders/BoostingTermBuilder.java
+++ contrib/xml-query-parser/src/java/org/apache/lucene/xmlparser/builders/BoostingTermBuilder.java
@@ -3,7 +3,8 @@ package org.apache.lucene.xmlparser.builders;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.spans.SpanQuery;
 import org.apache.lucene.search.spans.SpanTermQuery;
-import org.apache.lucene.search.payloads.BoostingTermQuery;
+import org.apache.lucene.search.payloads.PayloadTermQuery;
+import org.apache.lucene.search.payloads.AveragePayloadFunction;
 import org.apache.lucene.xmlparser.DOMUtils;
 import org.apache.lucene.xmlparser.ParserException;
 import org.w3c.dom.Element;
@@ -35,7 +36,7 @@ public class BoostingTermBuilder extends SpanBuilderBase
 	{
  		String fieldName=DOMUtils.getAttributeWithInheritanceOrFail(e,"fieldName");
  		String value=DOMUtils.getNonBlankTextOrFail(e);
-  		BoostingTermQuery btq = new BoostingTermQuery(new Term(fieldName,value));
+  		PayloadTermQuery btq = new PayloadTermQuery(new Term(fieldName,value), new AveragePayloadFunction());
 
   		btq.setBoost(DOMUtils.getAttribute(e,"boost",1.0f));
 		return btq;
