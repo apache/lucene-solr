6a06b0f0dddefb7d545fc47fb809931a29dae67e
Uwe Schindler <uschindler@apache.org>
2009-10-23 12:44:44 +0000

    We can only remove this in 3.x at earliest (depending on our policy change).
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@829033 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/util/Parameter.java
+++ src/java/org/apache/lucene/util/Parameter.java
@@ -25,7 +25,7 @@ import java.util.Map;
 
 /**
  * A serializable Enum class.
- * @deprecated Use Java 5 enum
+ * @deprecated Use Java 5 enum, will be removed in a later Lucene 3.x release.
  */
 @SuppressWarnings("serial")
 public abstract class Parameter implements Serializable
