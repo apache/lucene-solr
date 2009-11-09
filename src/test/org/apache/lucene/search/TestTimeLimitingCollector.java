9db9f77743a15fadeacd6dd133420e8131aceb37
Michael McCandless <mikemccand@apache.org>
2009-11-09 10:40:46 +0000

    fix bug that causes false positive failure in the test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@834011 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/lucene/search/TestTimeLimitingCollector.java
+++ src/test/org/apache/lucene/search/TestTimeLimitingCollector.java
@@ -84,7 +84,8 @@ public class TestTimeLimitingCollector extends LuceneTestCase {
     searcher = new IndexSearcher(directory, true);
 
     String qtxt = "one";
-    for (int i = 0; i < docText.length; i++) {
+    // start from 1, so that the 0th doc never matches
+    for (int i = 1; i < docText.length; i++) {
       qtxt += ' ' + docText[i]; // large query so that search will be longer
     }
     QueryParser queryParser = new QueryParser(Version.LUCENE_CURRENT, FIELD_NAME, new WhitespaceAnalyzer());
