6331869d2ff0df7813a4d9f50e8b920bed5ff9c6
Noble Paul <noble@apache.org>
2010-01-05 14:03:36 +0000

    removed unused imports
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@896042 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/solr/core/PluginInfo.java
+++ src/java/org/apache/solr/core/PluginInfo.java
@@ -21,8 +21,6 @@ import org.apache.solr.common.util.DOMUtil;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
 import java.util.*;
 import static java.util.Collections.unmodifiableList;
 import static java.util.Collections.unmodifiableMap;
