11f6b9185ef99fc835d3176b1260314357380454
Uwe Schindler <uschindler@apache.org>
2010-02-12 11:20:35 +0000

    LUCENE-2260: Fix AttributeSource to not hold a strong reference to the Attribute/AttributeImpl classes which prevents unloading of custom attributes loaded by other classloaders. The same fix applies to VirtualMethod (3.1 only).
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@909360 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/util/VirtualMethod.java
+++ src/java/org/apache/lucene/util/VirtualMethod.java
@@ -20,7 +20,7 @@ package org.apache.lucene.util;
 import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.IdentityHashMap;
+import java.util.WeakHashMap;
 import java.util.Set;
 
 /**
@@ -64,8 +64,8 @@ public final class VirtualMethod<C> {
   private final Class<C> baseClass;
   private final String method;
   private final Class<?>[] parameters;
-  private final IdentityHashMap<Class<? extends C>, Integer> cache =
-    new IdentityHashMap<Class<? extends C>, Integer>();
+  private final WeakHashMap<Class<? extends C>, Integer> cache =
+    new WeakHashMap<Class<? extends C>, Integer>();
 
   /**
    * Creates a new instance for the given {@code baseClass} and method declaration.
