84de57f4a5d4d6af2dbc9d23227bea08a732a750
Mark Robert Miller <markrmiller@apache.org>
2010-03-14 20:36:46 +0000

    move branch
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@922951 13f79535-47bb-0310-9956-ffa450edef68

--- solr/client/ruby/flare/vendor/plugins/engines/lib/engines/rails_extensions.rb
+++
@@ -1,6 +0,0 @@
-# This module contains all the extensions that the engines plugin makes to
-# Rails core. Explanations of how each extension works are included as RDoc
-# documentation for each of the modules listed.
-module Engines::RailsExtensions
-  # let's not rely *entirely* on Rails' magic modules. Not just yet.
-end
--- solr/example/multicore/README.txt
+++
@@ -1,3 +0,0 @@
-This is an alternative setup structure to support multiple cores.
-
-For general examples on standard solr configuration, see the "solr" directory.
