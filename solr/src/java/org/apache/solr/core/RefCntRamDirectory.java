aa9b85c26058ac544028daf40c7dceab9d3cb730
Mark Robert Miller <markrmiller@apache.org>
2010-03-20 15:15:34 +0000

    tweaks
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/branches/newtrunk@925626 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/java/org/apache/solr/core/RefCntRamDirectory.java
+++ solr/src/java/org/apache/solr/core/RefCntRamDirectory.java
@@ -7,37 +7,35 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
 public class RefCntRamDirectory extends RAMDirectory {
-  
+
   private final AtomicInteger refCount = new AtomicInteger();
-  
+
   public RefCntRamDirectory() {
     super();
-    incRef();
+    refCount.set(1);
   }
 
   public RefCntRamDirectory(Directory dir) throws IOException {
     this();
     Directory.copy(dir, this, false);
   }
-  
+
   public void incRef() {
     ensureOpen();
     refCount.incrementAndGet();
   }
-  
+
   public void decRef() {
     ensureOpen();
     if (refCount.getAndDecrement() == 1) {
       super.close();
     }
   }
-  
+
   public final synchronized void close() {
-    if (isOpen) {
-      decRef();
-    }
+    decRef();
   }
-  
+
   public boolean isOpen() {
     return isOpen;
   }
