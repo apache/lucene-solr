97dfb8c53c82aa3cd51848b6a1562299337fe698
Grant Ingersoll <gsingers@apache.org>
2010-03-29 18:56:33 +0000

    LUCENE-2148: fix the Solr test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@928867 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/schema/PolyFieldTest.java
+++ solr/src/test/org/apache/solr/schema/PolyFieldTest.java
@@ -194,7 +194,7 @@ public class PolyFieldTest extends SolrTestCaseJ4 {
       }
     }
     assertU(commit());
-    CartesianPolyFilterBuilder cpfb = new CartesianPolyFilterBuilder("");
+    CartesianPolyFilterBuilder cpfb = new CartesianPolyFilterBuilder("", 4, 15);
     //Get the box based on this point and our distance
     final Shape shape = cpfb.getBoxShape(45, -80, 10);//There's a bit of a bug in here that requires a small tier filter here.
     final List<Double> boxIds = shape.getArea();
