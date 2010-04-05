1b3d0eb274cad3713077de9fd1656a192a8ef0ba
Michael McCandless <mikemccand@apache.org>
2010-04-05 19:31:43 +0000

    LUCENE-2365: IndexWriter.newestSegment returns null if there are no segments
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@930932 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/src/java/org/apache/lucene/index/IndexWriter.java
@@ -4587,7 +4587,7 @@ public class IndexWriter implements Closeable {
 
   // utility routines for tests
   SegmentInfo newestSegment() {
-    return segmentInfos.info(segmentInfos.size()-1);
+    return segmentInfos.size() > 0 ? segmentInfos.info(segmentInfos.size()-1) : null;
   }
 
   public synchronized String segString() {
