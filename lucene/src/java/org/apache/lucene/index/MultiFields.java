c8d0754cc0a9d9af72b0c7a12569c3a0c585b980
Michael McCandless <mikemccand@apache.org>
2010-04-09 20:06:51 +0000

    fix 1.5 specific javadoc warning
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@932569 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/MultiFields.java
+++ lucene/src/java/org/apache/lucene/index/MultiFields.java
@@ -32,7 +32,7 @@ import org.apache.lucene.util.MultiBits;
  * Exposes flex API, merged from flex API of sub-segments.
  * This is useful when you're interacting with an {@link
  * IndexReader} implementation that consists of sequential
- * sub-readers (eg {@link DirectoryReader} or {@link
+ * sub-readers (eg DirectoryReader or {@link
  * MultiReader}).
  *
  * <p><b>NOTE</b>: for multi readers, you'll get better
