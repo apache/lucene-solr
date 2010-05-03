59a62bcb64af514a67f835c3ffd89367c50e0b45
Chris M. Hostetter <hossman@apache.org>
2010-05-03 18:38:18 +0000

    spelling mistakes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@940563 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/common/org/apache/solr/common/params/FacetParams.java
+++ solr/src/common/org/apache/solr/common/params/FacetParams.java
@@ -204,12 +204,12 @@ public interface FacetParams {
    * <li>lower = all gap based ranges include their lower bound</li>
    * <li>upper = all gap based ranges include their upper bound</li>
    * <li>edge = the first and last gap ranges include their edge bounds (ie: lower 
-   *     for the first one, upper for the last one) even if the corrisponding 
+   *     for the first one, upper for the last one) even if the corresponding 
    *     upper/lower option is not specified
    * </li>
    * <li>outer = the FacetDateOther.BEFORE and FacetDateOther.AFTER ranges 
    *     should be inclusive of their bounds, even if the first or last ranges 
-   *     already include thouse boundaries.
+   *     already include those boundaries.
    * </li>
    * <li>all = shorthand for lower, upper, edge, and outer</li>
    * </ul>
