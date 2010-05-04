87b2943215067e004b723f29e5a9f8fd06a84999
Michael McCandless <mikemccand@apache.org>
2010-05-04 23:43:56 +0000

    LUCENE-2387: merge to 3x
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@941106 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/analysis/Tokenizer.java
+++ lucene/src/java/org/apache/lucene/analysis/Tokenizer.java
@@ -67,7 +67,12 @@ public abstract class Tokenizer extends TokenStream {
   /** By default, closes the input Reader. */
   @Override
   public void close() throws IOException {
-    input.close();
+    if (input != null) {
+      input.close();
+      // LUCENE-2387: don't hold onto Reader after close, so
+      // GC can reclaim
+      input = null;
+    }
   }
   
   /** Return the corrected offset. If {@link #input} is a {@link CharStream} subclass
