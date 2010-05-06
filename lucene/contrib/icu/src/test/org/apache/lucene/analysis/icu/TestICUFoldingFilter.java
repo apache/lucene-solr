534809c2eae7addcd93ede794e1e42d72a87e105
Robert Muir <rmuir@apache.org>
2010-05-06 12:27:18 +0000

    LUCENE-1343: backport
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@941694 13f79535-47bb-0310-9956-ffa450edef68

---
+++ lucene/contrib/icu/src/test/org/apache/lucene/analysis/icu/TestICUFoldingFilter.java
@@ -0,0 +1,72 @@
+package org.apache.lucene.analysis.icu;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.BaseTokenStreamTestCase;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.WhitespaceTokenizer;
+
+/**
+ * Tests ICUFoldingFilter
+ */
+public class TestICUFoldingFilter extends BaseTokenStreamTestCase {
+  public void testDefaults() throws IOException {
+    Analyzer a = new Analyzer() {
+      @Override
+      public TokenStream tokenStream(String fieldName, Reader reader) {
+        return new ICUFoldingFilter(
+            new WhitespaceTokenizer(TEST_VERSION_CURRENT, reader));
+      }
+    };
+
+    // case folding
+    assertAnalyzesTo(a, "This is a test", new String[] { "this", "is", "a", "test" });
+
+    // case folding
+    assertAnalyzesTo(a, "Ruß", new String[] { "russ" });
+    
+    // case folding with accent removal
+    assertAnalyzesTo(a, "ΜΆΪΟΣ", new String[] { "μαιοσ" });
+    assertAnalyzesTo(a, "Μάϊος", new String[] { "μαιοσ" });
+
+    // supplementary case folding
+    assertAnalyzesTo(a, "𐐖", new String[] { "𐐾" });
+    
+    // normalization
+    assertAnalyzesTo(a, "ﴳﴺﰧ", new String[] { "طمطمطم" });
+
+    // removal of default ignorables
+    assertAnalyzesTo(a, "क्‍ष", new String[] { "कष" });
+    
+    // removal of latin accents (composed)
+    assertAnalyzesTo(a, "résumé", new String[] { "resume" });
+    
+    // removal of latin accents (decomposed)
+    assertAnalyzesTo(a, "re\u0301sume\u0301", new String[] { "resume" });
+    
+    // fold native digits
+    assertAnalyzesTo(a, "৭০৬", new String[] { "706" });
+    
+    // ascii-folding-filter type stuff
+    assertAnalyzesTo(a, "đis is cræzy", new String[] { "dis", "is", "craezy" });
+  }
+}
