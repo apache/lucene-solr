023746d375c90424bc96f38fca6efb198be23c75
Yonik Seeley <yonik@apache.org>
2010-05-16 23:48:50 +0000

    SOLR comments: add node about passing deleted docs to termsEnum
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@944940 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/java/org/apache/solr/request/SimpleFacets.java
+++ solr/src/java/org/apache/solr/request/SimpleFacets.java
@@ -572,6 +572,7 @@ public class SimpleFacets {
 
             // TODO: specialize when base docset is a bitset or hash set (skipDocs)?  or does it matter for this?
             // TODO: do this per-segment for better efficiency (MultiDocsEnum just uses base class impl)
+            // TODO: would passing deleted docs lead to better efficiency over checking the fastForRandomSet?
             docsEnum = termsEnum.docs(null, docsEnum);
 
             // this should be the same bulk result object if sharing of the docsEnum succeeded
