18d21f9f116096fb3f809f3b1e7fbb234bb33dd1
Uwe Schindler <uschindler@apache.org>
2010-06-24 10:24:15 +0000

    LUCENE-2142: merge 3.x test only, as trunk uses different StringIndex variant
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@957492 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/search/TestSort.java
+++ lucene/src/test/org/apache/lucene/search/TestSort.java
@@ -1039,4 +1039,23 @@ public class TestSort extends LuceneTestCase implements Serializable {
     dir.close();
   }
 
+  public void testLUCENE2142() throws IOException {
+    RAMDirectory indexStore = new RAMDirectory ();
+    IndexWriter writer = new IndexWriter(indexStore, new IndexWriterConfig(
+        TEST_VERSION_CURRENT, new MockAnalyzer()));
+    for (int i=0; i<5; i++) {
+        Document doc = new Document();
+        doc.add (new Field ("string", "a"+i, Field.Store.NO, Field.Index.NOT_ANALYZED));
+        doc.add (new Field ("string", "b"+i, Field.Store.NO, Field.Index.NOT_ANALYZED));
+        writer.addDocument (doc);
+    }
+    writer.optimize(); // enforce one segment to have a higher unique term count in all cases
+    writer.close();
+    sort.setSort(
+        new SortField("string", SortField.STRING),
+        SortField.FIELD_DOC );
+    // this should not throw AIOOBE or RuntimeEx
+    new IndexSearcher (indexStore, true).search(new MatchAllDocsQuery(), null, 500, sort);
+  }
+
 }
