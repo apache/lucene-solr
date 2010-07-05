c86c44481fa4c1bda92286f82d567e60240433f6
Koji Sekiguchi <koji@apache.org>
2010-07-05 14:47:09 +0000

    LUCENE-2464: FastVectorHighlighter: add SingleFragListBuilder
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@960611 13f79535-47bb-0310-9956-ffa450edef68

---
+++ lucene/contrib/highlighter/src/java/org/apache/lucene/search/vectorhighlight/SingleFragListBuilder.java
@@ -0,0 +1,58 @@
+package org.apache.lucene.search.vectorhighlight;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.lucene.search.vectorhighlight.FieldFragList.WeightedFragInfo;
+import org.apache.lucene.search.vectorhighlight.FieldPhraseList.WeightedPhraseInfo;
+
+/**
+ * An implementation class of {@link FragListBuilder} that generates one {@link WeightedFragInfo} object.
+ * Typical use case of this class is that you can get an entire field contents
+ * by using both of this class and {@link SimpleFragmentsBuilder}.<br/>
+ * <pre>
+ * FastVectorHighlighter h = new FastVectorHighlighter( true, true,
+ *   new SingleFragListBuilder(), new SimpleFragmentsBuilder() );
+ * </pre>
+ */
+public class SingleFragListBuilder implements FragListBuilder {
+
+  public FieldFragList createFieldFragList(FieldPhraseList fieldPhraseList,
+      int fragCharSize) {
+
+    FieldFragList ffl = new FieldFragList( fragCharSize );
+
+    List<WeightedPhraseInfo> wpil = new ArrayList<WeightedPhraseInfo>();
+    Iterator<WeightedPhraseInfo> ite = fieldPhraseList.phraseList.iterator();
+    WeightedPhraseInfo phraseInfo = null;
+    while( true ){
+      if( !ite.hasNext() ) break;
+      phraseInfo = ite.next();
+      if( phraseInfo == null ) break;
+
+      wpil.add( phraseInfo );
+    }
+    if( wpil.size() > 0 )
+      ffl.add( 0, Integer.MAX_VALUE, wpil );
+    return ffl;
+  }
+
+}
