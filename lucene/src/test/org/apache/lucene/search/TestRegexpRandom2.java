ce249eefc50b7d8627c0b085d3cbeedc74b3a35f
Robert Muir <rmuir@apache.org>
2010-07-15 16:23:26 +0000

    add TODO about not testing the empty string in this test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@964479 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/search/TestRegexpRandom2.java
+++ lucene/src/test/org/apache/lucene/search/TestRegexpRandom2.java
@@ -55,6 +55,9 @@ public class TestRegexpRandom2 extends LuceneTestCase {
     super.setUp();
     random = newRandom();
     dir = new MockRAMDirectory();
+    // TODO: fix mocktokenizer to not extend chartokenizer, so you can have an 'empty' keyword.
+    // currently, this means 'empty tokens' arent created/tested in the enumeration:
+    // <mikemccand> it's like having a big hairy scary monster in the basement but being upset that it doesn't have fangs
     RandomIndexWriter writer = new RandomIndexWriter(random, dir, new IndexWriterConfig(TEST_VERSION_CURRENT,
                                                                                         new MockAnalyzer(MockTokenizer.KEYWORD, false)));
     
