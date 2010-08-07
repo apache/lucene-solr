7b3d0d6b30a6ee82faa14da4e466f72b8515c52a
Koji Sekiguchi <koji@apache.org>
2010-08-07 11:51:17 +0000

    SOLR-2030: forgot to update test for FVH
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@983216 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/highlight/FastVectorHighlighterTest.java
+++ solr/src/test/org/apache/solr/highlight/FastVectorHighlighterTest.java
@@ -17,15 +17,15 @@
 
 package org.apache.solr.highlight;
 
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
 import java.util.HashMap;
 
 import org.apache.solr.SolrTestCaseJ4;
-import org.apache.solr.util.AbstractSolrTestCase;
 import org.apache.solr.util.TestHarness;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class FastVectorHighlighterTest extends SolrTestCaseJ4 {
 
@@ -49,11 +49,11 @@ public class FastVectorHighlighterTest extends SolrTestCaseJ4 {
     // Make sure we loaded the one fragmentsBuilder
     SolrFragmentsBuilder solrFbNull = highlighter.fragmentsBuilders.get( null );
     SolrFragmentsBuilder solrFbEmpty = highlighter.fragmentsBuilders.get( "" );
-    SolrFragmentsBuilder solrFbColored = highlighter.fragmentsBuilders.get( "colored" );
+    SolrFragmentsBuilder solrFbSimple = highlighter.fragmentsBuilders.get( "simple" );
     SolrFragmentsBuilder solrFbSO = highlighter.fragmentsBuilders.get( "scoreOrder" );
     assertSame( solrFbNull, solrFbEmpty );
     assertTrue( solrFbNull instanceof ScoreOrderFragmentsBuilder );
-    assertTrue( solrFbColored instanceof MultiColoredScoreOrderFragmentsBuilder );
+    assertTrue( solrFbSimple instanceof SimpleFragmentsBuilder );
     assertTrue( solrFbSO instanceof ScoreOrderFragmentsBuilder );
   }
 
