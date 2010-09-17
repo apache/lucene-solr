34cc899c7d86168356dce1975ab3eeb2daa972f0
Michael McCandless <mikemccand@apache.org>
2010-09-17 10:30:03 +0000

    fix incorrect ramUSED message to IW infoStream
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@998055 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/DocumentsWriter.java
+++ lucene/src/java/org/apache/lucene/index/DocumentsWriter.java
@@ -636,6 +636,8 @@ final class DocumentsWriter {
       Collection<DocConsumerPerThread> threads = new HashSet<DocConsumerPerThread>();
       for(int i=0;i<threadStates.length;i++)
         threads.add(threadStates[i].consumer);
+
+      final long startNumBytesUsed = numBytesUsed;
       consumer.flush(threads, flushState);
 
       if (infoStream != null) {
@@ -643,10 +645,10 @@ final class DocumentsWriter {
             flushState.numDocs, directory, false, -1, flushState.segmentName,
             false, hasProx(), flushState.codec);
         final long newSegmentSize = si.sizeInBytes();
-        String message = "  ramUsed=" + nf.format(numBytesUsed/1024./1024.) + " MB" +
+        String message = "  ramUsed=" + nf.format(startNumBytesUsed/1024./1024.) + " MB" +
           " newFlushedSize=" + newSegmentSize +
           " docs/MB=" + nf.format(numDocsInRAM/(newSegmentSize/1024./1024.)) +
-          " new/old=" + nf.format(100.0*newSegmentSize/numBytesUsed) + "%";
+          " new/old=" + nf.format(100.0*newSegmentSize/startNumBytesUsed) + "%";
         message(message);
       }
 
