d204e7331cbf3f1f5e11e08ef6a5954b1121006a
Robert Muir <rmuir@apache.org>
2010-09-21 05:04:19 +0000

    LUCENE-2650: Added extra safety to MMapIndexInput clones
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@999223 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/store/MMapDirectory.java
+++ lucene/src/java/org/apache/lucene/store/MMapDirectory.java
@@ -259,6 +259,8 @@ public class MMapDirectory extends FSDirectory {
 
     @Override
     public Object clone() {
+      if (buffer == null)
+        throw new AlreadyClosedException("MMapIndexInput already closed");
       MMapIndexInput clone = (MMapIndexInput)super.clone();
       clone.isClone = true;
       clone.buffer = buffer.duplicate();
@@ -267,9 +269,9 @@ public class MMapDirectory extends FSDirectory {
 
     @Override
     public void close() throws IOException {
-      if (isClone || buffer == null) return;
       // unmap the buffer (if enabled) and at least unset it for GC
       try {
+        if (isClone || buffer == null) return;
         cleanMapping(buffer);
       } finally {
         buffer = null;
@@ -382,6 +384,8 @@ public class MMapDirectory extends FSDirectory {
   
     @Override
     public Object clone() {
+      if (buffers == null)
+        throw new AlreadyClosedException("MultiMMapIndexInput already closed");
       MultiMMapIndexInput clone = (MultiMMapIndexInput)super.clone();
       clone.isClone = true;
       clone.buffers = new ByteBuffer[buffers.length];
@@ -403,8 +407,8 @@ public class MMapDirectory extends FSDirectory {
   
     @Override
     public void close() throws IOException {
-      if (isClone || buffers == null) return;
       try {
+        if (isClone || buffers == null) return;
         for (int bufNr = 0; bufNr < buffers.length; bufNr++) {
           // unmap the buffer (if enabled) and at least unset it for GC
           try {
