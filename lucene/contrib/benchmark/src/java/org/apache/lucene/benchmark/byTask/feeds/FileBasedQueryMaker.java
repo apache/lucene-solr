e735c07411059eb48922a5012ebbce7a6b8471b9
Shai Erera <shaie@apache.org>
2010-09-22 15:05:22 +0000

    FileBasedQueryMaker (benchmark) would add a null Query to the list, if the query failed to parse
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1000022 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/contrib/benchmark/src/java/org/apache/lucene/benchmark/byTask/feeds/FileBasedQueryMaker.java
+++ lucene/contrib/benchmark/src/java/org/apache/lucene/benchmark/byTask/feeds/FileBasedQueryMaker.java
@@ -73,18 +73,14 @@ public class FileBasedQueryMaker extends AbstractQueryMaker implements QueryMake
           BufferedReader buffered = new BufferedReader(reader);
           String line = null;
           int lineNum = 0;
-          while ((line = buffered.readLine()) != null)
-          {
+          while ((line = buffered.readLine()) != null) {
             line = line.trim();
-            if (!line.equals("") && !line.startsWith("#"))
-            {
-              Query query = null;
+            if (line.length() != 0 && !line.startsWith("#")) {
               try {
-                query = qp.parse(line);
+                qq.add(qp.parse(line));
               } catch (ParseException e) {
                 System.err.println("Exception: " + e.getMessage() + " occurred while parsing line: " + lineNum + " Text: " + line);
               }
-              qq.add(query);
             }
             lineNum++;
           }
