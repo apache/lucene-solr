def6a179d7d6f02bad37b63a4344f50defb83139
Koji Sekiguchi <koji@apache.org>
2010-10-10 11:19:05 +0000

    SOLR-2148: Highlighter doesn't support q.alt
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1006273 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/java/org/apache/solr/search/DisMaxQParser.java
+++ solr/src/java/org/apache/solr/search/DisMaxQParser.java
@@ -224,7 +224,7 @@ public class DisMaxQParser extends QParser {
 
   @Override
   public Query getHighlightQuery() throws ParseException {
-    return parsedUserQuery;
+    return parsedUserQuery == null ? altUserQuery : parsedUserQuery;
   }
 
   public void addDebugInfo(NamedList<Object> debugInfo) {
