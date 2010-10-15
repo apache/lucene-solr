7cbcdb554f0889f4250496f34d535343d5fd003a
Robert Muir <rmuir@apache.org>
2010-10-15 14:35:30 +0000

    SOLR-2163: SolrRequestParserTest.testStreamURL test failure
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1022948 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/servlet/SolrRequestParserTest.java
+++ solr/src/test/org/apache/solr/servlet/SolrRequestParserTest.java
@@ -39,6 +39,7 @@ import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.ContentStream;
 import org.apache.solr.core.SolrCore;
 import org.junit.AfterClass;
+import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -119,8 +120,8 @@ public class SolrRequestParserTest extends SolrTestCaseJ4 {
       txt = IOUtils.toString( connection.getInputStream());
     }
     catch( Exception ex ) {
-      // TODO - should it fail/skip?
-      fail( "this test only works if you have a network connection." );
+      System.out.println( "this test only works if you have a network connection." );
+      Assume.assumeNoException(ex);
       return;
     }
 
