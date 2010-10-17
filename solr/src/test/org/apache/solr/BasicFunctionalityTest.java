87489d797c6d7ba27946657a06218e813a76d9ab
Yonik Seeley <yonik@apache.org>
2010-10-17 17:58:36 +0000

    tests: fix resource leak
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1023536 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/BasicFunctionalityTest.java
+++ solr/src/test/org/apache/solr/BasicFunctionalityTest.java
@@ -303,11 +303,13 @@ public class BasicFunctionalityTest extends SolrTestCaseJ4 {
     rsp.add("\"quoted\"", "\"value\"");
 
     StringWriter writer = new StringWriter(32000);
-    XMLWriter.writeResponse(writer,req("foo"),rsp);
+    SolrQueryRequest req = req("foo");
+    XMLWriter.writeResponse(writer,req,rsp);
 
     DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
     builder.parse(new ByteArrayInputStream
                   (writer.toString().getBytes("UTF-8")));
+    req.close();
   }
 
   @Test
@@ -327,6 +329,7 @@ public class BasicFunctionalityTest extends SolrTestCaseJ4 {
     assertEquals(2, arrayParams.length);
     assertEquals("array", arrayParams[0]);
     assertEquals("value", arrayParams[1]);
+    req.close();
   }
 
   @Test
@@ -442,7 +445,7 @@ public class BasicFunctionalityTest extends SolrTestCaseJ4 {
     assertEquals("SSS", p.get("ss"));
     assertEquals("XXX", p.get("xx"));
 
-    
+    req.close();
   }
 
   @Test
