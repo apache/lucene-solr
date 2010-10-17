3190a861b877125209bc31c4e39e8a2a6b7841ac
Robert Muir <rmuir@apache.org>
2010-10-17 19:17:56 +0000

    close the searcher in this test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1023562 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/util/SolrPluginUtilsTest.java
+++ solr/src/test/org/apache/solr/util/SolrPluginUtilsTest.java
@@ -82,7 +82,7 @@ public class SolrPluginUtilsTest extends SolrTestCaseJ4 {
     for (SolrDocument document : list) {
       assertNotNull(document.get("val_t"));
     }
-
+    srchr.close();
   }
 
   @Test
