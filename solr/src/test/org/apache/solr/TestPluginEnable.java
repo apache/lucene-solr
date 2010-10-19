ed308e6630a2b21c855cf8d7f7fca988f643478e
Yonik Seeley <yonik@apache.org>
2010-10-19 15:59:41 +0000

    tests: convert to junit4
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1024300 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/TestPluginEnable.java
+++ solr/src/test/org/apache/solr/TestPluginEnable.java
@@ -1,32 +1,24 @@
 package org.apache.solr;
 
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.util.AbstractSolrTestCase;
-
+import org.junit.BeforeClass;
+import org.junit.Test;
 /**
  * <p> Test disabling components</p>
  *
  * @version $Id$
  * @since solr 1.4
  */
-public class TestPluginEnable extends AbstractSolrTestCase {
-
-
+public class TestPluginEnable extends SolrTestCaseJ4 {
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    initCore("solrconfig-enableplugin.xml", "schema-replication1.xml");
+  }
+  
+  @Test
   public void testSimple() throws SolrServerException {
     assertNull(h.getCore().getRequestHandler("disabled"));
     assertNotNull(h.getCore().getRequestHandler("enabled"));
 
   }
-
-
-  @Override
-  public String getSchemaFile() {
-    return "schema-replication1.xml";
-  }
-
-  @Override
-  public String getSolrConfigFile() {
-    return "solrconfig-enableplugin.xml";
-  }
-
 }
