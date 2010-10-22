ceb8db5bb5aa50985ac56b6b23f42611e18d868c
Michael McCandless <mikemccand@apache.org>
2010-10-22 17:57:38 +0000

    LUCENE-2618: revert
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1026431 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/src/java/org/apache/lucene/index/IndexWriter.java
@@ -3662,7 +3662,7 @@ public class IndexWriter implements Closeable {
           // This merge (and, generally, any change to the
           // segments) may now enable new merges, so we call
           // merge policy & update pending merges.
-          if (success && !merge.isAborted() && (merge.optimize || (!closed && !closing))) {
+          if (success && !merge.isAborted() && !closed && !closing) {
             updatePendingMerges(merge.maxNumSegmentsOptimize, merge.optimize);
           }
         }
