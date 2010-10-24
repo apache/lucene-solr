3fc24d327e0a9ecea190f984ce1b8ed63e7009c5
Mark Robert Miller <markrmiller@apache.org>
2010-10-24 13:39:30 +0000

    fix previous - added false to wrong SolrException (had no affect)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1026799 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/java/org/apache/solr/core/SolrCore.java
+++ solr/src/java/org/apache/solr/core/SolrCore.java
@@ -419,7 +419,7 @@ public final class SolrCore implements SolrInfoMBean {
     try {
         clazz = getResourceLoader().findClass(className);
         if (cast != null && !cast.isAssignableFrom(clazz)) {
-          throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,"Error Instantiating "+msg+", "+className+ " is not a " +cast.getName(), false);
+          throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,"Error Instantiating "+msg+", "+className+ " is not a " +cast.getName());
         }
       //most of the classes do not have constructors which takes SolrCore argument. It is recommended to obtain SolrCore by implementing SolrCoreAware.
       // So invariably always it will cause a  NoSuchMethodException. So iterate though the list of available constructors
@@ -434,7 +434,7 @@ public final class SolrCore implements SolrInfoMBean {
     } catch (SolrException e) {
       throw e;
     } catch (Exception e) {
-      throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,"Error Instantiating "+msg+", "+className+ " failed to instantiate " +cast.getName(), e);
+      throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,"Error Instantiating "+msg+", "+className+ " failed to instantiate " +cast.getName(), e, false);
     }
   }
 
