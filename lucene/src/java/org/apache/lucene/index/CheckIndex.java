f8f7cb4ec52d1128cf5290bd65ad23559178b09e
Michael McCandless <mikemccand@apache.org>
2010-11-05 18:17:06 +0000

    LUCENE-2744: only print number of fields w/ norms on 'test: norms....' CheckIndex output
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1031689 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/CheckIndex.java
+++ lucene/src/java/org/apache/lucene/index/CheckIndex.java
@@ -552,8 +552,8 @@ public class CheckIndex {
       for (final String fieldName : fieldNames) {
         if (reader.hasNorms(fieldName)) {
           reader.norms(fieldName, b, 0);
+          ++status.totFields;
         }
-        ++status.totFields;
       }
 
       msg("OK [" + status.totFields + " fields]");
