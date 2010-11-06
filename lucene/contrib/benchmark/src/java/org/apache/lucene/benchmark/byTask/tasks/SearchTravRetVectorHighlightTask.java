acacf16ee27e8effc43c0222e08c6b535fbb212c
Simon Willnauer <simonw@apache.org>
2010-11-06 15:53:02 +0000

    removed super.setParam call since superclass does not understand the param syntax for SearchTravRetVectorHighlightTask
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1032100 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/contrib/benchmark/src/java/org/apache/lucene/benchmark/byTask/tasks/SearchTravRetVectorHighlightTask.java
+++ lucene/contrib/benchmark/src/java/org/apache/lucene/benchmark/byTask/tasks/SearchTravRetVectorHighlightTask.java
@@ -120,8 +120,9 @@ public class SearchTravRetVectorHighlightTask extends SearchTravTask {
 
   @Override
   public void setParams(String params) {
-    super.setParams(params);
-    String [] splits = params.split(",");
+    // can't call super because super doesn't understand our
+    // params syntax
+    final String [] splits = params.split(",");
     for (int i = 0; i < splits.length; i++) {
       if (splits[i].startsWith("size[") == true){
         traversalSize = (int)Float.parseFloat(splits[i].substring("size[".length(),splits[i].length() - 1));
