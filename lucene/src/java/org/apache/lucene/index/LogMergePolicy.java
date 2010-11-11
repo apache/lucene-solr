19976c302295e6bc7b9fdd3ab39743da4b2162b7
Michael McCandless <mikemccand@apache.org>
2010-11-11 18:00:54 +0000

    LUCENE-2118: fix intermittent failure in TestIndexWriterMergePolicy.testMaxBufferedDocsChange
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1034012 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/LogMergePolicy.java
+++ lucene/src/java/org/apache/lucene/index/LogMergePolicy.java
@@ -471,7 +471,7 @@ public abstract class LogMergePolicy extends MergePolicy {
       // Now search backwards for the rightmost segment that
       // falls into this level:
       float levelBottom;
-      if (maxLevel < levelFloor)
+      if (maxLevel <= levelFloor)
         // All remaining segments fall into the min level
         levelBottom = -1.0F;
       else {
