59e30fb7e83e87bd35d65cd31127f132a4189368
Robert Muir <rmuir@apache.org>
2010-11-15 18:58:26 +0000

    LUCENE-2760: Optimize SpanFirstQuery,SpanPositionRangeQuery
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1035411 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/search/spans/SpanFirstQuery.java
+++ lucene/src/java/org/apache/lucene/search/spans/SpanFirstQuery.java
@@ -37,8 +37,14 @@ public class SpanFirstQuery extends SpanPositionRangeQuery {
   }
 
   @Override
-  protected boolean acceptPosition(Spans spans) throws IOException {
-    return spans.end() <= end;
+  protected AcceptStatus acceptPosition(Spans spans) throws IOException {
+    assert spans.start() != spans.end();
+    if (spans.start() >= end)
+      return AcceptStatus.NO_AND_ADVANCE;
+    else if (spans.end() <= end)
+      return AcceptStatus.YES;
+    else
+      return AcceptStatus.NO;
   }
 
 
