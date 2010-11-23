d936615b0afc5f6c1a9ac2a8d60f97a08bc117d9
Simon Willnauer <simonw@apache.org>
2010-11-23 22:45:54 +0000

    added missing ASL headers
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/docvalues@1038368 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/values/Ints.java
+++ lucene/src/java/org/apache/lucene/index/values/Ints.java
@@ -1,24 +1,41 @@
 package org.apache.lucene.index.values;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.io.IOException;
 
 import org.apache.lucene.index.values.PackedIntsImpl.IntsReader;
 import org.apache.lucene.index.values.PackedIntsImpl.IntsWriter;
 import org.apache.lucene.store.Directory;
+
 //TODO - add bulk copy where possible
 public class Ints {
 
   private Ints() {
   }
-  
 
   public static Writer getWriter(Directory dir, String id, boolean useFixedArray)
       throws IOException {
-     //TODO - implement fixed?!
+    // TODO - implement fixed?!
     return new IntsWriter(dir, id);
   }
 
-  public static DocValues getValues(Directory dir, String id, boolean useFixedArray) throws IOException {
+  public static DocValues getValues(Directory dir, String id,
+      boolean useFixedArray) throws IOException {
     return new IntsReader(dir, id);
   }
 }
