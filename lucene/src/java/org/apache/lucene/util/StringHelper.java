5b672ef3972aad02346c3b3ce83d3ddc71822006
Simon Willnauer <simonw@apache.org>
2010-12-08 22:28:30 +0000

    LUCENE-2643: Removed unused StringHelper#stringDifference
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1043738 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/util/StringHelper.java
+++ lucene/src/java/org/apache/lucene/util/StringHelper.java
@@ -51,30 +51,7 @@ public abstract class StringHelper {
         return i;
     return len;
   }
-
-  /**
-   * Compares two strings, character by character, and returns the
-   * first position where the two strings differ from one another.
-   *
-   * @param s1 The first string to compare
-   * @param s2 The second string to compare
-   * @return The first position where the two strings differ.
-   * 
-   * @deprecated This method cannot handle supplementary characters.
-   */
-  @Deprecated
-  public static final int stringDifference(String s1, String s2) {
-    int len1 = s1.length();
-    int len2 = s2.length();
-    int len = len1 < len2 ? len1 : len2;
-    for (int i = 0; i < len; i++) {
-      if (s1.charAt(i) != s2.charAt(i)) {
-	      return i;
-      }
-    }
-    return len;
-  }
-
+  
   private StringHelper() {
   }
 }
