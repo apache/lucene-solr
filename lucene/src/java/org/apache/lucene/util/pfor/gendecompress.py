39732d273c67dcc9fa1975d855edb4b4ddf09a0b
Michael McCandless <mikemccand@apache.org>
2010-12-15 18:59:02 +0000

    move all gen'd FOR code into single class as static methods
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/bulkpostings@1049667 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/util/pfor/gendecompress.py
+++ lucene/src/java/org/apache/lucene/util/pfor/gendecompress.py
@@ -47,9 +47,42 @@ def bitsExpr(i, numFrameBits):
     bitsInInt += " & mask"
   return bitsInInt
 
+decodeAnyMethod = '''  static void decodeAnyFrame(
+        final IntBuffer compressedBuffer, int inputSize, int numFrameBits,
+        int[] output) {
 
-def genDecompressClass(numFrameBits):
-  className = "For" + str(numFrameBits) + "Decompress"
+    assert numFrameBits > 0 : numFrameBits;
+    assert numFrameBits <= 31 : numFrameBits;
+    final int mask = (int) ((1L<<numFrameBits) - 1);
+    int intValue1 = compressedBuffer.get();
+    output[outputOffset] = intValue1 & mask;
+    if (--inputSize == 0) return;
+    int bitPos = numFrameBits;
+
+    do {
+      while (bitPos <= (32 - numFrameBits)) {
+        // No mask needed when bitPos == (32 - numFrameBits), but prefer to avoid testing for this:
+        output[++outputOffset] = (intValue1 >>> bitPos) & mask;
+        if (--inputSize == 0) return;
+        bitPos += numFrameBits;
+      }
+      
+      int intValue2 = compressedBuffer.get();
+      output[++outputOffset] = ( (bitPos == 32)
+                                  ? intValue2
+                                  : ((intValue1 >>> bitPos) | (intValue2 << (32 - bitPos)))
+                               ) & mask;
+        
+      if (--inputSize == 0) return;
+      
+      intValue1 = intValue2;
+      bitPos += numFrameBits - 32;
+    } while (true);
+  }
+'''
+
+def genDecompress():
+  className = "ForDecompress"
   fileName = className + ".java"
   imports = "import java.nio.IntBuffer;\n"
   f = open(fileName, 'w')
@@ -71,51 +104,57 @@ def genDecompressClass(numFrameBits):
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */""")
-    w("\n/* This program is generated, do not modify. See gendecompress.py */\n\n")
-    w("import java.nio.IntBuffer;\n")
-    w("class " + className + " extends ForDecompress {\n")
-    w("  static final int numFrameBits = " + str(numFrameBits) + ";\n")
-    w("  static final int mask = (int) ((1L<<numFrameBits) - 1);\n")
-    w("\n")
-    w("""  static void decompressFrame(FrameOfRef frameOfRef) {
-    int[] output = frameOfRef.unCompressedData;
-    IntBuffer compressedBuffer = frameOfRef.compressedBuffer;
-    int outputOffset = frameOfRef.offset;
-    //int inputSize = frameOfRef.unComprSize;\n""")
-    if USE_SCRATCH:
-      w('    final int[] inputInts = frameOfRef.scratch;\n')
-    #w("    while (inputSize >= 32) {\n")
-    w('    for(int step=0;step<4;step++) {\n')
-    if USE_SCRATCH:
-      w('      compressedBuffer.get(inputInts, 0, %d);\n' % numFrameBits)
-    else:
-      for i in range(numFrameBits): # declare int vars and init from buffer
-        w("      int intValue" + str(i) + " = compressedBuffer.get();\n")
+ */
+ """)
 
-    for i in range(32): # set output from int vars
-      w("      output[" + str(i) + " + outputOffset] = " + bitsExpr(i, numFrameBits) + ";\n")
-    w("""      // inputSize -= 32;
-      outputOffset += 32;
-    }
-    
-    //if (inputSize > 0) {
-    //  decodeAnyFrame(compressedBuffer, bufIndex, inputSize, numFrameBits, output, outputOffset);
-    //}
-  }
-}
-""")
-  finally: f.close()
-  
-  
+    w("/* This code is generated, do not modify. See gendecompress.py */\n\n")
 
-def genDecompressClasses():
-  numFrameBits = 1
-  while numFrameBits <= 31: # 32 special case, not generated.
-    genDecompressClass(numFrameBits)
-    numFrameBits += 1
+    w("import java.nio.IntBuffer;\n\n")
 
+    w("final class ForDecompress {\n")
 
+    w('\n  // nocommit: assess perf of this to see if specializing is really needed\n')
+
+    w('  /*\n')
+    w(decodeAnyMethod)
+    w('  */\n\n')
+
+    # We don't need the 32 bit case -- unused since java ints are
+    # signed and we only use the unsigned range (31 bits):
+    for numFrameBits in xrange(1, 32):
+
+      w('\n  // NOTE: hardwired to blockSize == 128\n')
+      if USE_SCRATCH:
+        w('  public static void decode%d(final IntBuffer compressedBuffer, final int[] output, final int[] scratch) {\n' % numFrameBits)
+      else:
+        w('  public static void decode%d(final IntBuffer compressedBuffer, final int[] output) {\n' % numFrameBits)
+
+      w('    final int numFrameBits = %d;\n' % numFrameBits)
+      w('    final int mask = (int) ((1L<<numFrameBits) - 1);\n')
+      w('    int outputOffset = 0;\n')
+      
+      w('    for(int step=0;step<4;step++) {\n')
+
+      if USE_SCRATCH:
+        w('      compressedBuffer.get(scratch, 0, %d);\n' % numFrameBits)
+      else:
+        for i in range(numFrameBits): # declare int vars and init from buffer
+          w("      int intValue" + str(i) + " = compressedBuffer.get();\n")
+
+      for i in range(32): # set output from int vars
+        w("      output[" + str(i) + " + outputOffset] = " + bitsExpr(i, numFrameBits) + ";\n")
+      w('      outputOffset += 32;\n')
+      w('    }\n')
+      w('  }\n')
+    w('}\n')
+      
+  finally:
+    f.close()
+
+def genSwitch():
+  for numFrameBits in xrange(1, 32):
+    print '      case %d: ForDecompress.decode%d(compressedBuffer, unCompressedData); break;' % (numFrameBits, numFrameBits)
 
 if __name__ == "__main__":
-  genDecompressClasses()
+  genDecompress()
+  #genSwitch()
