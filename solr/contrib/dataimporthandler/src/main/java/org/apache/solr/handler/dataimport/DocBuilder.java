db175cce712580d44194ba2e97ad3ca8b51af9f7
Koji Sekiguchi <koji@apache.org>
2011-01-10 14:51:13 +0000

    SOLR-2310: getTimeElapsedSince() returns incorrect hour value when the elapse is over 60 hours
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1057221 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/DocBuilder.java
+++ solr/contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/DocBuilder.java
@@ -949,7 +949,7 @@ public class DocBuilder {
 
   static String getTimeElapsedSince(long l) {
     l = System.currentTimeMillis() - l;
-    return (l / (60000 * 60)) % 60 + ":" + (l / 60000) % 60 + ":" + (l / 1000)
+    return (l / (60000 * 60)) + ":" + (l / 60000) % 60 + ":" + (l / 1000)
             % 60 + "." + l % 1000;
   }
 
