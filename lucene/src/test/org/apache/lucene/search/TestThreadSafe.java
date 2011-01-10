1df9242981e959fe7a07281b1a7e8304ba666b2d
Michael McCandless <mikemccand@apache.org>
2011-01-10 20:08:17 +0000

    try (again) to stop false failures in this test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1057340 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/search/TestThreadSafe.java
+++ lucene/src/test/org/apache/lucene/search/TestThreadSafe.java
@@ -142,7 +142,7 @@ public class TestThreadSafe extends LuceneTestCase {
     buildDir(dir1, 15, 5, 2000);
 
     // do many small tests so the thread locals go away inbetween
-    int num = 100 * RANDOM_MULTIPLIER;
+    int num = 10 * RANDOM_MULTIPLIER;
     for (int i = 0; i < num; i++) {
       ir1 = IndexReader.open(dir1, false);
       doTest(10,10);
