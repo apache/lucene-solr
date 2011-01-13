3478bb9358622f52dd9f9f4447c3d474e3a31496
Koji Sekiguchi <koji@apache.org>
2011-01-13 02:39:35 +0000

    SOLR-2282: use solr home properly in the test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1058393 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/BaseDistributedSearchTestCase.java
+++ solr/src/test/org/apache/solr/BaseDistributedSearchTestCase.java
@@ -124,12 +124,21 @@ public abstract class BaseDistributedSearchTestCase extends SolrTestCaseJ4 {
     return randVals;
   }
 
+  /**
+   * Subclasses can override this to change a test's solr home
+   * (default is in test-files)
+   */
+  public String getSolrHome() {
+    return SolrTestCaseJ4.TEST_HOME;
+  }
+  
   @Override
   public void setUp() throws Exception {
     SolrTestCaseJ4.resetExceptionIgnores();  // ignore anything with ignore_exception in it
     super.setUp();
     System.setProperty("solr.test.sys.prop1", "propone");
     System.setProperty("solr.test.sys.prop2", "proptwo");
+    System.setProperty("solr.solr.home", getSolrHome());
     testDir = new File(TEMP_DIR,
             getClass().getName() + "-" + System.currentTimeMillis());
     testDir.mkdirs();
