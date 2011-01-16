15a72e7698194082c42cc42937b086ec9b1071e7
Shai Erera <shaie@apache.org>
2011-01-16 14:17:36 +0000

    OneComparatorFieldValueHitQueue redundantly checks array size - it is alredy guaranteed to be of the right one by FVHQ.create()
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1059569 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/search/FieldValueHitQueue.java
+++ lucene/src/java/org/apache/lucene/search/FieldValueHitQueue.java
@@ -57,9 +57,6 @@ public abstract class FieldValueHitQueue extends PriorityQueue<FieldValueHitQueu
     public OneComparatorFieldValueHitQueue(SortField[] fields, int size)
         throws IOException {
       super(fields);
-      if (fields.length == 0) {
-        throw new IllegalArgumentException("Sort must contain at least one field");
-      }
 
       SortField field = fields[0];
       setComparator(0,field.getComparator(size, 0));
