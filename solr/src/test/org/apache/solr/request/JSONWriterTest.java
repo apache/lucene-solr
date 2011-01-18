8492092537a9e63499bd8f1377310893377e5345
Chris M. Hostetter <hossman@apache.org>
2011-01-18 21:44:33 +0000

    SOLR-2307: merge trunk r1060585
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1060589 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/request/JSONWriterTest.java
+++ solr/src/test/org/apache/solr/request/JSONWriterTest.java
@@ -62,24 +62,9 @@ public class JSONWriterTest extends SolrTestCaseJ4 {
     buf = new StringWriter();
     w.write(buf, req, rsp);
     assertEquals(buf.toString(), "{\"data1\":\"NaN\",\"data2\":\"-Infinity\",\"data3\":\"Infinity\"}");
-
+    req.close();
   }
 
-  @Test
-  public void testPHPS() throws IOException {
-    SolrQueryRequest req = req("dummy");
-    SolrQueryResponse rsp = new SolrQueryResponse();
-    QueryResponseWriter w = new PHPSerializedResponseWriter();
-
-    StringWriter buf = new StringWriter();
-    rsp.add("data1", "hello");
-    rsp.add("data2", 42);
-    rsp.add("data3", true);
-    w.write(buf, req, rsp);
-    assertEquals(buf.toString(), "a:3:{s:5:\"data1\";s:5:\"hello\";s:5:\"data2\";i:42;s:5:\"data3\";b:1;}");
-  }
-
-  @Test
   public void testJSON() throws IOException {
     SolrQueryRequest req = req("wt","json","json.nl","arrarr");
     SolrQueryResponse rsp = new SolrQueryResponse();
@@ -93,7 +78,7 @@ public class JSONWriterTest extends SolrTestCaseJ4 {
 
     w.write(buf, req, rsp);
     assertEquals(buf.toString(), "{\"nl\":[[\"data1\",\"hello\"],[null,42]]}");
-
+    req.close();
   }
   
 }
