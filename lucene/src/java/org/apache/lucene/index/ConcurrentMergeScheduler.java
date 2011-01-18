99dcb1280756ca9a44f43c92093ad4a47a562b21
Michael McCandless <mikemccand@apache.org>
2011-01-18 16:16:55 +0000

    LUCENE-2755: fix accidental commit that caused CMS to deadlock
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1060444 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/ConcurrentMergeScheduler.java
+++ lucene/src/java/org/apache/lucene/index/ConcurrentMergeScheduler.java
@@ -283,7 +283,9 @@ public class ConcurrentMergeScheduler extends MergeScheduler {
   protected synchronized int mergeThreadCount() {
     int count = 0;
     for (MergeThread mt : mergeThreads) {
-      if (mt.isAlive()) count++;
+      if (mt.isAlive() && mt.getCurrentMerge() != null) {
+        count++;
+      }
     }
     return count;
   }
