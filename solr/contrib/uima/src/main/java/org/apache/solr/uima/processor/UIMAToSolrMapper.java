3497ced2f8eed7f4a889f5c26bbc446bcaba540f
Robert Muir <rmuir@apache.org>
2011-01-24 02:21:44 +0000

    SOLR-2129: Provide a Solr module for dynamic metadata extraction/indexing with Apache UIMA
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1062606 13f79535-47bb-0310-9956-ffa450edef68

---
+++ solr/contrib/uima/src/main/java/org/apache/solr/uima/processor/UIMAToSolrMapper.java
@@ -0,0 +1,83 @@
+package org.apache.solr.uima.processor;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Map;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.Type;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Map UIMA types and features over fields of a Solr document
+ * 
+ * @version $Id$
+ */
+public class UIMAToSolrMapper {
+
+  private final Logger log = LoggerFactory.getLogger(UIMAToSolrMapper.class);
+
+  private SolrInputDocument document;
+
+  private JCas cas;
+
+  public UIMAToSolrMapper(SolrInputDocument document, JCas cas) {
+    this.document = document;
+    this.cas = cas;
+  }
+
+  /**
+   * map features of a certain UIMA type to corresponding Solr fields based on the mapping
+   * 
+   * @param typeName
+   *          name of UIMA type to map
+   * @param featureFieldsmapping
+   */
+  public void map(String typeName, Map<String, String> featureFieldsmapping) {
+    try {
+      FeatureStructure fsMock = (FeatureStructure) Class.forName(typeName).getConstructor(
+              JCas.class).newInstance(cas);
+      Type type = fsMock.getType();
+      for (FSIterator<FeatureStructure> iterator = cas.getFSIndexRepository().getAllIndexedFS(type); iterator
+              .hasNext();) {
+        FeatureStructure fs = iterator.next();
+        for (String featureName : featureFieldsmapping.keySet()) {
+          String fieldName = featureFieldsmapping.get(featureName);
+          log.info(new StringBuffer("mapping ").append(typeName).append("@").append(featureName)
+                  .append(" to ").append(fieldName).toString());
+          String featureValue = null;
+          if (fs instanceof Annotation && "coveredText".equals(featureName)) {
+            featureValue = ((Annotation) fs).getCoveredText();
+          } else {
+            featureValue = fs.getFeatureValueAsString(type.getFeatureByBaseName(featureName));
+          }
+          log.info(new StringBuffer("writing ").append(featureValue).append(" in ").append(
+                  fieldName).toString());
+          document.addField(fieldName, featureValue, 1.0f);
+        }
+      }
+    } catch (Exception e) {
+      log.error(e.getLocalizedMessage());
+    }
+  }
+}
---
+++ solr/contrib/uima/src/main/java/org/apache/solr/uima/processor/ae/AEProviderFactory.java
@@ -0,0 +1,53 @@
+package org.apache.solr.uima.processor.ae;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Singleton factory class responsible of {@link AEProvider}s' creation
+ * 
+ * @version $Id$
+ */
+public class AEProviderFactory {
+
+  private static AEProviderFactory instance;
+
+  private Map<String, AEProvider> providerCache = new HashMap<String, AEProvider>();
+
+  private AEProviderFactory() {
+    // Singleton
+  }
+
+  public static AEProviderFactory getInstance() {
+    if (instance == null) {
+      instance = new AEProviderFactory();
+    }
+    return instance;
+  }
+
+  public synchronized AEProvider getAEProvider(String core, String aePath,
+          Map<String, String> runtimeParameters) {
+    String key = new StringBuilder(core).append(aePath).toString();
+    if (providerCache.get(key) == null) {
+      providerCache.put(key, new OverridingParamsAEProvider(aePath, runtimeParameters));
+    }
+    return providerCache.get(key);
+  }
+}
