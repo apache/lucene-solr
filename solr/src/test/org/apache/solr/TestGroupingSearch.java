30ed34d4fe4e9f8f2711eecd6789891beec19f52
Yonik Seeley <yonik@apache.org>
2011-02-03 16:07:26 +0000

    tests: fix doclist offset in grouping model
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1066847 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/TestGroupingSearch.java
+++ solr/src/test/org/apache/solr/TestGroupingSearch.java
@@ -526,8 +526,7 @@ public class TestGroupingSearch extends SolrTestCaseJ4 {
       Map<String,Object> resultSet = new LinkedHashMap<String,Object>();
       group.put("doclist", resultSet);
       resultSet.put("numFound", grp.docs.size());
-      resultSet.put("start", start);
-
+      resultSet.put("start", group_offset);
       List docs = new ArrayList();
       resultSet.put("docs", docs);
       for (int j=group_offset; j<grp.docs.size(); j++) {
