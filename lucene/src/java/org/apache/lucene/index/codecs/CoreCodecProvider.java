866f01e2adfb05eac6b4056795d74973689725b5
Michael McCandless <mikemccand@apache.org>
2011-02-11 18:08:08 +0000

    LUCENE-2915: make CoreCodecProvider public
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1069909 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/codecs/CoreCodecProvider.java
+++ lucene/src/java/org/apache/lucene/index/codecs/CoreCodecProvider.java
@@ -39,7 +39,7 @@ import org.apache.lucene.index.codecs.standard.StandardCodec;
  * </pre>
  */
 
-class CoreCodecProvider extends CodecProvider {
+public class CoreCodecProvider extends CodecProvider {
   CoreCodecProvider() {
     register(new StandardCodec());
     register(new PreFlexCodec());
