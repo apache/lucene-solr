45d7d312fd7df9913f619ff3b4bdddf385032d6c
Chris M. Hostetter <hossman@apache.org>
2011-02-18 00:17:28 +0000

    SOLR-1240: merge 1071842 from trunk, deprecating date faceting methods now that we have generalized range faceting
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1071843 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/java/org/apache/solr/request/SimpleFacets.java
+++ solr/src/java/org/apache/solr/request/SimpleFacets.java
@@ -579,8 +579,9 @@ public class SimpleFacets {
    * SolrParams
    *
    * @see FacetParams#FACET_DATE
+   * @deprecated Use getFacetRangeCounts which is more generalized
    */
-
+  @Deprecated
   public NamedList getFacetDateCounts()
     throws IOException, ParseException {
 
@@ -602,6 +603,10 @@ public class SimpleFacets {
     return resOuter;
   }
 
+  /**
+   * @deprecated Use getFacetRangeCounts which is more generalized
+   */
+  @Deprecated
   public void getFacetDateCounts(String dateFacet, NamedList resOuter)
       throws IOException, ParseException {
 
@@ -959,6 +964,10 @@ public class SimpleFacets {
     return searcher.numDocs(rangeQ ,base);
   }
 
+  /**
+   * @deprecated Use rangeCount(SchemaField,String,String,boolean,boolean) which is more generalized
+   */
+  @Deprecated
   protected int rangeCount(SchemaField sf, Date low, Date high,
                            boolean iLow, boolean iHigh) throws IOException {
     Query rangeQ = ((DateField)(sf.getType())).getRangeQuery(null, sf,low,high,iLow,iHigh);
