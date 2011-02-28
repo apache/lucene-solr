e60eb27cf9ab940f24f9f12aaf08f431d567d442
Uwe Schindler <uschindler@apache.org>
2011-02-28 11:04:19 +0000

    SOLR-2388: Fix possible NPE, fix indent
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1075289 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/common/org/apache/solr/common/util/XMLErrorLogger.java
+++ solr/src/common/org/apache/solr/common/util/XMLErrorLogger.java
@@ -28,45 +28,50 @@ import javax.xml.stream.Location;
 import javax.xml.stream.XMLReporter;
 
 public final class XMLErrorLogger implements ErrorHandler,ErrorListener,XMLReporter {
-  
+
   private final Logger log;
 
   public XMLErrorLogger(Logger log) {
     this.log = log;
   }
-  
+
   // ErrorHandler
-  
-	public void warning(SAXParseException e) {
-		log.warn("XML parse warning in \""+e.getSystemId()+"\", line "+e.getLineNumber()+", column "+e.getColumnNumber()+": "+e.getMessage());
-	}
 
-	public void error(SAXParseException e) throws SAXException {
-		throw e;
-	}
-
-	public void fatalError(SAXParseException e) throws SAXException {
-		throw e;
-	}
-  
-  // ErrorListener
-  
-	public void warning(TransformerException e) {
-		log.warn(e.getMessageAndLocation());
-	}
-
-	public void error(TransformerException e) throws TransformerException {
-		throw e;
-	}
-
-	public void fatalError(TransformerException e) throws TransformerException {
-		throw e;
-	}
-  
-  // XMLReporter
-  
-  public void report(String message, String errorType, Object relatedInformation, Location loc) {
-		log.warn("XML parse warning in \""+loc.getSystemId()+"\", line "+loc.getLineNumber()+", column "+loc.getColumnNumber()+": "+message);
+  public void warning(SAXParseException e) {
+    log.warn("XML parse warning in \""+e.getSystemId()+"\", line "+e.getLineNumber()+", column "+e.getColumnNumber()+": "+e.getMessage());
   }
-  
+
+  public void error(SAXParseException e) throws SAXException {
+    throw e;
+  }
+
+  public void fatalError(SAXParseException e) throws SAXException {
+    throw e;
+  }
+
+  // ErrorListener
+
+  public void warning(TransformerException e) {
+    log.warn(e.getMessageAndLocation());
+  }
+
+  public void error(TransformerException e) throws TransformerException {
+    throw e;
+  }
+
+  public void fatalError(TransformerException e) throws TransformerException {
+    throw e;
+  }
+
+  // XMLReporter
+
+  public void report(String message, String errorType, Object relatedInformation, Location loc) {
+    final StringBuilder sb = new StringBuilder("XML parser reported ").append(errorType);
+    if (loc !=  null) {
+      sb.append(" in \"").append(loc.getSystemId()).append("\", line ")
+        .append(loc.getLineNumber()).append(", column ").append(loc.getColumnNumber());
+    }
+    log.warn(sb.append(": ").append(message).toString());
+  }
+
 }
