969067c0e2dcb7aced7ee3142a364f65b93e4090
Robert Muir <rmuir@apache.org>
2011-03-11 20:12:04 +0000

    try to avoid jre bug in test, take 2
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1080748 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/cloud/ZkSolrClientTest.java
+++ solr/src/test/org/apache/solr/cloud/ZkSolrClientTest.java
@@ -28,6 +28,7 @@ import org.apache.solr.util.AbstractSolrTestCase;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.junit.Ignore;
 
 public class ZkSolrClientTest extends AbstractSolrTestCase {
   private static final boolean DEBUG = false;
@@ -159,6 +160,7 @@ public class ZkSolrClientTest extends AbstractSolrTestCase {
     }
   }
 
+  @Ignore("test case crashes modern jres")
   public void testWatchChildren() throws Exception {
     String zkDir = dataDir.getAbsolutePath() + File.separator
         + "zookeeper/server1/data";
