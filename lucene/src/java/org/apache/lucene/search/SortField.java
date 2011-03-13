ef2e92aff4b41ff0acf37abbd07ee6c074a783d6
Robert Muir <rmuir@apache.org>
2011-03-13 02:46:01 +0000

    revert hack
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/docvalues@1081034 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/search/SortField.java
+++ lucene/src/java/org/apache/lucene/search/SortField.java
@@ -101,11 +101,7 @@ public class SortField {
   private int type;  // defaults to determining type dynamically
   private Locale locale;    // defaults to "natural order" (no Locale)
   boolean reverse = false;  // defaults to natural order
-  
-  // This is not generified because of a javac bug in early JDK 1.5 (e.g. used by FreeBSD).
-  // With <?> as type param, getComparator() fails to compile because of cast problems.
-  @SuppressWarnings("unchecked") private CachedArrayCreator creator;
-  
+  private CachedArrayCreator<?> creator;
   public Object missingValue = null; // used for 'sortMissingFirst/Last'
 
   // Used for CUSTOM sort
