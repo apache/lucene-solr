3dcbc2cc1677a1602d2b78f3be30627f6324b09a
Michael McCandless <mikemccand@apache.org>
2011-03-19 10:34:37 +0000

    fix tests to use newDir
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1083153 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/index/TestIndexWriter.java
+++ lucene/src/test/org/apache/lucene/index/TestIndexWriter.java
@@ -688,7 +688,7 @@ public class TestIndexWriter extends LuceneTestCase {
     // Make sure it's OK to change RAM buffer size and
     // maxBufferedDocs in a write session
     public void testChangingRAMBuffer() throws IOException {
-      RAMDirectory dir = new RAMDirectory();      
+      Directory dir = newDirectory();      
       IndexWriter writer  = new IndexWriter(dir, newIndexWriterConfig(TEST_VERSION_CURRENT, new MockAnalyzer()));
       writer.getConfig().setMaxBufferedDocs(10);
       writer.getConfig().setRAMBufferSizeMB(IndexWriterConfig.DISABLE_AUTO_FLUSH);
@@ -742,7 +742,7 @@ public class TestIndexWriter extends LuceneTestCase {
     }
 
     public void testChangingRAMBuffer2() throws IOException {
-      RAMDirectory dir = new RAMDirectory();      
+      Directory dir = newDirectory();      
       IndexWriter writer  = new IndexWriter(dir, newIndexWriterConfig(TEST_VERSION_CURRENT, new MockAnalyzer()));
       writer.getConfig().setMaxBufferedDocs(10);
       writer.getConfig().setMaxBufferedDeleteTerms(10);
