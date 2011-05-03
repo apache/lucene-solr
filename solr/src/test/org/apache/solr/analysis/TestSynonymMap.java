2a6ac118f98a3d493d8616079c0911499afa99ca
Uwe Schindler <uschindler@apache.org>
2011-05-03 07:07:05 +0000

    Java 5 does not support @Override on interfaces
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1098943 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/test/org/apache/solr/analysis/TestSynonymMap.java
+++ solr/src/test/org/apache/solr/analysis/TestSynonymMap.java
@@ -267,7 +267,6 @@ public class TestSynonymMap extends LuceneTestCase {
     SynonymFilterFactory ff = new SynonymFilterFactory();
     ff.init(args);
     ff.inform( new ResourceLoader() {
-      @Override
       public List<String> getLines(String resource) throws IOException {
         if( !"something.txt".equals(resource) ) {
           throw new RuntimeException( "should not get a differnt resource" );
@@ -277,12 +276,10 @@ public class TestSynonymMap extends LuceneTestCase {
         return rules;
       }
 
-      @Override
       public Object newInstance(String cname, String... subpackages) {
         throw new RuntimeException("stub");
       }
 
-      @Override
       public InputStream openResource(String resource) throws IOException {
         throw new RuntimeException("stub");
       }
