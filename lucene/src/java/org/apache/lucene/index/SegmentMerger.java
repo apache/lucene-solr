3be9e4b90a2f71ee1dae5009fd584c1cfe565f3e
Robert Muir <rmuir@apache.org>
2011-05-23 16:36:22 +0000

    LUCENE-3117: move norms version constant out of SegmentMerger
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1126573 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/SegmentMerger.java
+++ lucene/src/java/org/apache/lucene/index/SegmentMerger.java
@@ -46,10 +46,6 @@ import org.apache.lucene.util.ReaderUtil;
  * @see #add
  */
 final class SegmentMerger {
-
-  /** norms header placeholder */
-  static final byte[] NORMS_HEADER = new byte[]{'N','R','M',-1};
-
   private Directory directory;
   private String segment;
   private int termIndexInterval = IndexWriterConfig.DEFAULT_TERM_INDEX_INTERVAL;
@@ -578,7 +574,7 @@ final class SegmentMerger {
         if (fi.isIndexed && !fi.omitNorms) {
           if (output == null) {
             output = directory.createOutput(IndexFileNames.segmentFileName(segment, "", IndexFileNames.NORMS_EXTENSION));
-            output.writeBytes(NORMS_HEADER,NORMS_HEADER.length);
+            output.writeBytes(SegmentNorms.NORMS_HEADER, SegmentNorms.NORMS_HEADER.length);
           }
           for (IndexReader reader : readers) {
             final int maxDoc = reader.maxDoc();
