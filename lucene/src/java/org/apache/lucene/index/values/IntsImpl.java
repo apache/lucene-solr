e8f9e80b743e31f3c69d643fb24e1455f5f79ff5
Michael McCandless <mikemccand@apache.org>
2011-06-05 14:34:12 +0000

    fix ob1 that caused us to use 64 bits not 1 bit, per value
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/docvalues@1132421 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/values/IntsImpl.java
+++ lucene/src/java/org/apache/lucene/index/values/IntsImpl.java
@@ -111,7 +111,7 @@ class IntsImpl {
           minValue = maxValue = 0;
         }
         // if we exceed the range of positive longs we must switch to fixed ints
-        if ((maxValue - minValue) < (((long)1) << 63) && (maxValue - minValue) > 0) {
+        if ((maxValue - minValue) < (((long)1) << 63) && (maxValue - minValue) >= 0) {
           writePackedInts(docCount);
         } else {
           writeFixedInts(docCount);
