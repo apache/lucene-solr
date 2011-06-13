b6a0959af698935f68d35a9dca0a3253742feec5
Shai Erera <shaie@apache.org>
2011-06-13 18:07:27 +0000

    LUCENE-3193: add TwoPhaseCommit interface
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1135204 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/src/java/org/apache/lucene/index/IndexWriter.java
@@ -55,6 +55,7 @@ import org.apache.lucene.util.Constants;
 import org.apache.lucene.util.StringHelper;
 import org.apache.lucene.util.ThreadInterruptedException;
 import org.apache.lucene.util.MapBackedSet;
+import org.apache.lucene.util.TwoPhaseCommit;
 
 /**
   An <code>IndexWriter</code> creates and maintains an index.
@@ -190,7 +191,7 @@ import org.apache.lucene.util.MapBackedSet;
  * referenced by the "front" of the index). For this, IndexFileDeleter
  * keeps track of the last non commit checkpoint.
  */
-public class IndexWriter implements Closeable {
+public class IndexWriter implements Closeable, TwoPhaseCommit {
   /**
    * Name of the write lock in the index.
    */
