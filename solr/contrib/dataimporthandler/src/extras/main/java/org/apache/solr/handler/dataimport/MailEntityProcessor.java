7e9aeac79b5a8e2c8851c77d36c61a2ea42c9c73
Noble Paul <noble@apache.org>
2011-06-14 11:39:50 +0000

    SOLR-2206 MailEntityProcessor has mispelled words
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1135495 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/dataimporthandler/src/extras/main/java/org/apache/solr/handler/dataimport/MailEntityProcessor.java
+++ solr/contrib/dataimporthandler/src/extras/main/java/org/apache/solr/handler/dataimport/MailEntityProcessor.java
@@ -90,7 +90,9 @@ public class MailEntityProcessor extends EntityProcessorBase {
     fetchSize = getIntFromContext("fetchSize", 32 * 1024);
     cTimeout = getIntFromContext("connectTimeout", 30 * 1000);
     rTimeout = getIntFromContext("readTimeout", 60 * 1000);
-    processAttachment = getBoolFromContext("processAttachement", true);
+    processAttachment = getBoolFromContext(
+              getStringFromContext("processAttachment",null) == null ? "processAttachement":"processAttachment"
+            , true);
 
     logConfig();
   }
