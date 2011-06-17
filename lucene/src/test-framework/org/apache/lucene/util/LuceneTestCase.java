427d5a8af9b5c10afdfc389c3d83019ffa61e130
Michael McCandless <mikemccand@apache.org>
2011-06-17 22:46:20 +0000

    LUCENE-3210: let app control how aggressive TMP is in targetting segments with deletions; default it to more aggressive than today
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1137063 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test-framework/org/apache/lucene/util/LuceneTestCase.java
+++ lucene/src/test-framework/org/apache/lucene/util/LuceneTestCase.java
@@ -816,6 +816,7 @@ public abstract class LuceneTestCase extends Assert {
     tmp.setSegmentsPerTier(_TestUtil.nextInt(r, 2, 20));
     tmp.setUseCompoundFile(r.nextBoolean());
     tmp.setNoCFSRatio(0.1 + r.nextDouble()*0.8);
+    tmp.setReclaimDeletesWeight(r.nextDouble()*4);
     return tmp;
   }
 
