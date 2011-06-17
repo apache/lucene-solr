a684a076c72907775a715e9224e9b7d5ad98b60b
Simon Willnauer <simonw@apache.org>
2011-06-17 14:15:42 +0000

    removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1136887 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/SegmentReader.java
+++ lucene/src/java/org/apache/lucene/index/SegmentReader.java
@@ -30,7 +30,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.FieldSelector;
 import org.apache.lucene.index.codecs.PerDocValues;
-import org.apache.lucene.index.values.IndexDocValues;
 import org.apache.lucene.store.BufferedIndexInput;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.IndexInput;
