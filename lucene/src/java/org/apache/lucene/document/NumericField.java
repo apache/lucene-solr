feb428d5fb4d8f3168496a4859419b265f578a23
Christopher John Male <chrism@apache.org>
2011-06-22 03:39:26 +0000

    LUCENE-3219: Fixed forgotten javadoc warning
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1138294 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/document/NumericField.java
+++ lucene/src/java/org/apache/lucene/document/NumericField.java
@@ -69,7 +69,7 @@ import org.apache.lucene.search.FieldCache; // javadocs
  * <code>NumericField</code>, use {@link NumericRangeQuery} or {@link
  * NumericRangeFilter}.  To sort according to a
  * <code>NumericField</code>, use the normal numeric sort types, eg
- * {@link SortField#INT}. <code>NumericField</code> values
+ * {@link SortField.Type#INT}. <code>NumericField</code> values
  * can also be loaded directly from {@link FieldCache}.</p>
  *
  * <p>By default, a <code>NumericField</code>'s value is not stored but
