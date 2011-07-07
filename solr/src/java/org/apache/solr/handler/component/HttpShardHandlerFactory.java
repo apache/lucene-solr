a66a97c5dbcacdef6df8ea44ceed85076bd5b16d
Noble Paul <noble@apache.org>
2011-07-07 06:38:01 +0000

    missing license headers
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1143672 13f79535-47bb-0310-9956-ffa450edef68

--- solr/src/java/org/apache/solr/handler/component/HttpShardHandlerFactory.java
+++ solr/src/java/org/apache/solr/handler/component/HttpShardHandlerFactory.java
@@ -1,4 +1,20 @@
 package org.apache.solr.handler.component;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
@@ -18,11 +34,7 @@ import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-/**
- * @author noble.paul@teamaol.com (noblep01)
- *         Date: 6/21/11
- *         Time: 12:14 PM
- */
+
 public class HttpShardHandlerFactory extends ShardHandlerFactory implements PluginInfoInitialized{
   protected static Logger log = LoggerFactory.getLogger(HttpShardHandlerFactory.class);
 
