df1c3a41af377a5a10d26565930c70ba7da79472
Robert Muir <rmuir@apache.org>
2011-07-18 13:48:47 +0000

    reformat code to match trunk
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1147878 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/contrib/queries/src/java/org/apache/lucene/search/similar/MoreLikeThisQuery.java
+++ lucene/contrib/queries/src/java/org/apache/lucene/search/similar/MoreLikeThisQuery.java
@@ -32,135 +32,123 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.similar.MoreLikeThis;
 
 /**
- * A simple wrapper for MoreLikeThis for use in scenarios where a Query object is required eg
- * in custom QueryParser extensions. At query.rewrite() time the reader is used to construct the
- * actual MoreLikeThis object and obtain the real Query object.
+ * A simple wrapper for MoreLikeThis for use in scenarios where a Query object
+ * is required eg in custom QueryParser extensions. At query.rewrite() time the
+ * reader is used to construct the actual MoreLikeThis object and obtain the
+ * real Query object.
  */
-public class MoreLikeThisQuery extends Query
-{
-
+public class MoreLikeThisQuery extends Query {
+  
+  private String likeText;
+  private String[] moreLikeFields;
+  private Analyzer analyzer;
+  float percentTermsToMatch = 0.3f;
+  int minTermFrequency = 1;
+  int maxQueryTerms = 5;
+  Set<?> stopWords = null;
+  int minDocFreq = -1;
+  
+  /**
+   * @param moreLikeFields
+   */
+  public MoreLikeThisQuery(String likeText, String[] moreLikeFields,
+      Analyzer analyzer) {
+    this.likeText = likeText;
+    this.moreLikeFields = moreLikeFields;
+    this.analyzer = analyzer;
+  }
+  
+  @Override
+  public Query rewrite(IndexReader reader) throws IOException {
+    MoreLikeThis mlt = new MoreLikeThis(reader);
     
-    private String likeText;
-    private String[] moreLikeFields;
-    private Analyzer analyzer;
-    float percentTermsToMatch=0.3f;
-    int minTermFrequency=1;
-    int maxQueryTerms=5;
-    Set<?> stopWords=null;
-	int minDocFreq=-1;
-    
-    
-    /**
-     * @param moreLikeFields
-     */
-    public MoreLikeThisQuery(String likeText, String[] moreLikeFields, Analyzer analyzer)
-    {
-        this.likeText=likeText;
-        this.moreLikeFields=moreLikeFields;
-        this.analyzer=analyzer;
+    mlt.setFieldNames(moreLikeFields);
+    mlt.setAnalyzer(analyzer);
+    mlt.setMinTermFreq(minTermFrequency);
+    if (minDocFreq >= 0) {
+      mlt.setMinDocFreq(minDocFreq);
     }
-    
-    @Override
-    public Query rewrite(IndexReader reader) throws IOException
-    {
-        MoreLikeThis mlt=new MoreLikeThis(reader);
-        
-        mlt.setFieldNames(moreLikeFields);
-        mlt.setAnalyzer(analyzer);
-        mlt.setMinTermFreq(minTermFrequency);
-        if(minDocFreq>=0)
-        {
-        	mlt.setMinDocFreq(minDocFreq);
-        }        
-        mlt.setMaxQueryTerms(maxQueryTerms);
-        mlt.setStopWords(stopWords);
-        BooleanQuery bq= (BooleanQuery) mlt.like(new ByteArrayInputStream(likeText.getBytes()));        
-        BooleanClause[] clauses = bq.getClauses();
-        //make at least half the terms match
-        bq.setMinimumNumberShouldMatch((int)(clauses.length*percentTermsToMatch));
-        return bq;
-    }
-    /* (non-Javadoc)
-     * @see org.apache.lucene.search.Query#toString(java.lang.String)
-     */
-    @Override
-    public String toString(String field)
-    {       
-        return "like:"+likeText;
-    }
-
-	public float getPercentTermsToMatch() {
-		return percentTermsToMatch;
-	}
-	public void setPercentTermsToMatch(float percentTermsToMatch) {
-		this.percentTermsToMatch = percentTermsToMatch;
-	}
-
-	public Analyzer getAnalyzer()
-	{
-		return analyzer;
-	}
-
-	public void setAnalyzer(Analyzer analyzer)
-	{
-		this.analyzer = analyzer;
-	}
-
-	public String getLikeText()
-	{
-		return likeText;
-	}
-
-	public void setLikeText(String likeText)
-	{
-		this.likeText = likeText;
-	}
-
-	public int getMaxQueryTerms()
-	{
-		return maxQueryTerms;
-	}
-
-	public void setMaxQueryTerms(int maxQueryTerms)
-	{
-		this.maxQueryTerms = maxQueryTerms;
-	}
-
-	public int getMinTermFrequency()
-	{
-		return minTermFrequency;
-	}
-
-	public void setMinTermFrequency(int minTermFrequency)
-	{
-		this.minTermFrequency = minTermFrequency;
-	}
-
-	public String[] getMoreLikeFields()
-	{
-		return moreLikeFields;
-	}
-
-	public void setMoreLikeFields(String[] moreLikeFields)
-	{
-		this.moreLikeFields = moreLikeFields;
-	}
-    public Set<?> getStopWords()
-    {
-        return stopWords;
-    }
-    public void setStopWords(Set<?> stopWords)
-    {
-        this.stopWords = stopWords;
-    }
-
-	public int getMinDocFreq()
-	{
-		return minDocFreq;
-	}
-
-	public void setMinDocFreq(int minDocFreq)
-	{
-		this.minDocFreq = minDocFreq;
-	}
+    mlt.setMaxQueryTerms(maxQueryTerms);
+    mlt.setStopWords(stopWords);
+    BooleanQuery bq = (BooleanQuery) mlt.like(new ByteArrayInputStream(likeText
+        .getBytes()));
+    BooleanClause[] clauses = bq.getClauses();
+    // make at least half the terms match
+    bq.setMinimumNumberShouldMatch((int) (clauses.length * percentTermsToMatch));
+    return bq;
+  }
+  
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.apache.lucene.search.Query#toString(java.lang.String)
+   */
+  @Override
+  public String toString(String field) {
+    return "like:" + likeText;
+  }
+  
+  public float getPercentTermsToMatch() {
+    return percentTermsToMatch;
+  }
+  
+  public void setPercentTermsToMatch(float percentTermsToMatch) {
+    this.percentTermsToMatch = percentTermsToMatch;
+  }
+  
+  public Analyzer getAnalyzer() {
+    return analyzer;
+  }
+  
+  public void setAnalyzer(Analyzer analyzer) {
+    this.analyzer = analyzer;
+  }
+  
+  public String getLikeText() {
+    return likeText;
+  }
+  
+  public void setLikeText(String likeText) {
+    this.likeText = likeText;
+  }
+  
+  public int getMaxQueryTerms() {
+    return maxQueryTerms;
+  }
+  
+  public void setMaxQueryTerms(int maxQueryTerms) {
+    this.maxQueryTerms = maxQueryTerms;
+  }
+  
+  public int getMinTermFrequency() {
+    return minTermFrequency;
+  }
+  
+  public void setMinTermFrequency(int minTermFrequency) {
+    this.minTermFrequency = minTermFrequency;
+  }
+  
+  public String[] getMoreLikeFields() {
+    return moreLikeFields;
+  }
+  
+  public void setMoreLikeFields(String[] moreLikeFields) {
+    this.moreLikeFields = moreLikeFields;
+  }
+  
+  public Set<?> getStopWords() {
+    return stopWords;
+  }
+  
+  public void setStopWords(Set<?> stopWords) {
+    this.stopWords = stopWords;
+  }
+  
+  public int getMinDocFreq() {
+    return minDocFreq;
+  }
+  
+  public void setMinDocFreq(int minDocFreq) {
+    this.minDocFreq = minDocFreq;
+  }
 }
