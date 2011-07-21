cf1120f7449056171ef34888e7160721ae846302
Robert Muir <rmuir@apache.org>
2011-07-21 19:08:28 +0000

    use IWC's codecprovider consistently for all operations
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1149319 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/java/org/apache/lucene/index/IndexUpgrader.java
+++ lucene/src/java/org/apache/lucene/index/IndexUpgrader.java
@@ -114,12 +114,12 @@ public final class IndexUpgrader {
   }
   
   public void upgrade() throws IOException {
-    if (!IndexReader.indexExists(dir)) {
+    if (!IndexReader.indexExists(dir, iwc.getCodecProvider())) {
       throw new IndexNotFoundException(dir.toString());
     }
   
     if (!deletePriorCommits) {
-      final Collection<IndexCommit> commits = IndexReader.listCommits(dir);
+      final Collection<IndexCommit> commits = DirectoryReader.listCommits(dir, iwc.getCodecProvider());
       if (commits.size() > 1) {
         throw new IllegalArgumentException("This tool was invoked to not delete prior commit points, but the following commits were found: " + commits);
       }
