c21707d20aeabdeaaf8fb97003b21cea8410672d
Shalin Shekhar Mangar <shalin@apache.org>
2011-07-21 11:02:46 +0000

    SOLR-2655 -- DIH multi threaded mode does not resolve attributes correctly
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1149112 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/dataimporthandler/src/java/org/apache/solr/handler/dataimport/EvaluatorBag.java
+++ solr/contrib/dataimporthandler/src/java/org/apache/solr/handler/dataimport/EvaluatorBag.java
@@ -218,7 +218,6 @@ public class EvaluatorBag {
         Evaluator evaluator = evaluators.get(fname);
         if (evaluator == null)
           return null;
-        VariableResolverImpl vri = VariableResolverImpl.CURRENT_VARIABLE_RESOLVER.get();
         return evaluator.evaluate(m.group(2), Context.CURRENT_CONTEXT.get());
       }
 
