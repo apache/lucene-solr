0dc4c9582f764e1610e3ecc10f2886ab560e4c01
Michael McCandless <mikemccand@apache.org>
2011-07-27 10:45:17 +0000

    fix false test failure
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1151422 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/index/TestIndexWriterDelete.java
+++ lucene/src/test/org/apache/lucene/index/TestIndexWriterDelete.java
@@ -1045,7 +1045,7 @@ public class TestIndexWriterDelete extends LuceneTestCase {
                                     setRAMBufferSizeMB(0.5).setMaxBufferedDocs(-1).setMergePolicy(NoMergePolicy.NO_COMPOUND_FILES).setReaderPooling(false)) {
         @Override
         public void doAfterFlush() {
-          assertTrue("only " + docsInSegment.get() + " in segment", closing.get() || docsInSegment.get() >= 10);
+          assertTrue("only " + docsInSegment.get() + " in segment", closing.get() || docsInSegment.get() >= 7);
           docsInSegment.set(0);
           sawAfterFlush.set(true);
         }
