ed828e46ffc88eb70939bc6dc9b57c3251b32687
Michael McCandless <mikemccand@apache.org>
2011-08-01 13:58:19 +0000

    fix silly minor bug in _TestUtil.randomFixedLengthUnicodeString
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1152778 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test-framework/org/apache/lucene/util/_TestUtil.java
+++ lucene/src/test-framework/org/apache/lucene/util/_TestUtil.java
@@ -220,7 +220,7 @@ public class _TestUtil {
       } else if (t <= 1) {
         chars[i++] = (char) random.nextInt(0x80);
       } else if (2 == t) {
-        chars[i++] = (char) nextInt(random, 0x80, 0x800);
+        chars[i++] = (char) nextInt(random, 0x80, 0x7ff);
       } else if (3 == t) {
         chars[i++] = (char) nextInt(random, 0x800, 0xd7ff);
       } else if (4 == t) {
