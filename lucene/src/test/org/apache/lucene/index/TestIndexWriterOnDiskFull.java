e1e8bc134a330e0565862854d5641ce80111bcc2
Simon Willnauer <simonw@apache.org>
2011-08-08 13:15:39 +0000

    removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1154953 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/index/TestIndexWriterOnDiskFull.java
+++ lucene/src/test/org/apache/lucene/index/TestIndexWriterOnDiskFull.java
@@ -28,7 +28,6 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.IOContext;
 import org.apache.lucene.store.MockDirectoryWrapper;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.LuceneTestCase;
