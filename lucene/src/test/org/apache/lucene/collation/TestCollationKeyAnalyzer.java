9b0db47074128e28e7a4a25a189e767fdd33f6e7
Robert Muir <rmuir@apache.org>
2011-08-17 16:55:56 +0000

    LUCENE-3378: move collationtestbase to tests-framework
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1158828 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/collation/TestCollationKeyAnalyzer.java
+++ lucene/src/test/org/apache/lucene/collation/TestCollationKeyAnalyzer.java
@@ -19,6 +19,7 @@ package org.apache.lucene.collation;
 
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CollationTestBase;
 
 import java.text.Collator;
 import java.util.Locale;
