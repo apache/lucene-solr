fbd84ca66ded921af46ffdb579e59e7462fd366b
Robert Muir <rmuir@apache.org>
2011-08-26 16:29:48 +0000

    fix one more use of wrong random
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1162158 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/index/TestIndexWriterExceptions.java
+++ lucene/src/test/org/apache/lucene/index/TestIndexWriterExceptions.java
@@ -111,7 +111,7 @@ public class TestIndexWriterExceptions extends LuceneTestCase {
 
       doc.add(newField(r, "content7", "aaa bbb ccc ddd", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
 
-      final Field idField = newField("id", "", Field.Store.YES, Field.Index.NOT_ANALYZED);
+      final Field idField = newField(r, "id", "", Field.Store.YES, Field.Index.NOT_ANALYZED);
       doc.add(idField);
 
       final long stopTime = System.currentTimeMillis() + 500;
