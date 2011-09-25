fdde857f243c5486b8bf1103d25b510d7db31827
Christopher John Male <chrism@apache.org>
2011-09-25 05:17:21 +0000

    SOLR-2769: Fixed bad javadoc
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1175299 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/analysis/HunspellStemFilterFactory.java
+++ solr/core/src/java/org/apache/solr/analysis/HunspellStemFilterFactory.java
@@ -35,7 +35,7 @@ import org.apache.solr.util.plugin.ResourceLoaderAware;
  *    dictionary=&quot;en_GB.dic,my_custom.dic&quot;
  *    affix=&quot;en_GB.aff&quot;/&gt;</pre>
  * Dictionaries for many languages are available through the OpenOffice project
- * @see http://wiki.services.openoffice.org/wiki/Dictionaries
+ * See http://wiki.services.openoffice.org/wiki/Dictionaries
  */
 public class HunspellStemFilterFactory extends BaseTokenFilterFactory implements ResourceLoaderAware {
   
