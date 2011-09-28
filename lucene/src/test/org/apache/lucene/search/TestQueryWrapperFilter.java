2edf662daaeb10eba4ea8533771f228b889e2ec3
Robert Muir <rmuir@apache.org>
2011-09-28 20:42:34 +0000

    backport additional test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1177055 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/search/TestQueryWrapperFilter.java
+++ lucene/src/test/org/apache/lucene/search/TestQueryWrapperFilter.java
@@ -28,6 +28,7 @@ import org.apache.lucene.index.RandomIndexWriter;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.English;
 import org.apache.lucene.util.LuceneTestCase;
 
 public class TestQueryWrapperFilter extends LuceneTestCase {
@@ -157,5 +158,31 @@ public class TestQueryWrapperFilter extends LuceneTestCase {
     }
     r.close();
     d.close();
+  }
+  
+  public void testThousandDocuments() throws Exception {
+    Directory dir = newDirectory();
+    RandomIndexWriter writer = new RandomIndexWriter(random, dir);
+    for (int i = 0; i < 1000; i++) {
+      Document doc = new Document();
+      doc.add(newField("field", English.intToEnglish(i), Field.Index.NOT_ANALYZED));
+      writer.addDocument(doc);
+    }
+    
+    IndexReader reader = writer.getReader();
+    writer.close();
+    
+    IndexSearcher searcher = newSearcher(reader);
+    
+    for (int i = 0; i < 1000; i++) {
+      TermQuery termQuery = new TermQuery(new Term("field", English.intToEnglish(i)));
+      QueryWrapperFilter qwf = new QueryWrapperFilter(termQuery);
+      TopDocs td = searcher.search(new MatchAllDocsQuery(), qwf, 10);
+      assertEquals(1, td.totalHits);
+    }
+    
+    searcher.close();
+    reader.close();
+    dir.close();
   }
 }
