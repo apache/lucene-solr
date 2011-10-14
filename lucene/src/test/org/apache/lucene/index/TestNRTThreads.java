609a807f1efaf9b86a54969581607b9ecee7514b
Michael McCandless <mikemccand@apache.org>
2011-10-14 12:38:58 +0000

    don't use mem intensive codecs for this test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1183333 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test/org/apache/lucene/index/TestNRTThreads.java
+++ lucene/src/test/org/apache/lucene/index/TestNRTThreads.java
@@ -22,11 +22,13 @@ import java.util.concurrent.ExecutorService;
 
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.MockDirectoryWrapper;
+import org.apache.lucene.util.LuceneTestCase.UseNoMemoryExpensiveCodec;
 
 // TODO
 //   - mix in optimize, addIndexes
 //   - randomoly mix in non-congruent docs
 
+@UseNoMemoryExpensiveCodec
 public class TestNRTThreads extends ThreadedIndexingAndSearchingTestCase {
   
   @Override
