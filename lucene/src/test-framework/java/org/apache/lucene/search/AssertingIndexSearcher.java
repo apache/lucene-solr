c70de144266999a8937cd974d298ff5d0ac3fb15
Robert Muir <rmuir@apache.org>
2011-11-17 14:53:40 +0000

    LUCENE-3580: add check to AssertingIndexSearcher that scorers obey DISI contract before next/advance
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1203209 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/test-framework/java/org/apache/lucene/search/AssertingIndexSearcher.java
+++ lucene/src/test-framework/java/org/apache/lucene/search/AssertingIndexSearcher.java
@@ -66,7 +66,18 @@ public class AssertingIndexSearcher extends IndexSearcher {
 
     @Override
     public Scorer scorer(IndexReader reader, boolean scoreDocsInOrder, boolean topScorer) throws IOException {
-      return w.scorer(reader, scoreDocsInOrder, topScorer);
+      Scorer scorer = w.scorer(reader, scoreDocsInOrder, topScorer);
+      if (scorer != null) {
+        // check that scorer obeys disi contract for docID() before next()/advance
+        try {
+          int docid = scorer.docID();
+          assert docid == -1 || docid == DocIdSetIterator.NO_MORE_DOCS;
+        } catch (UnsupportedOperationException ignored) {
+          // from a top-level BS1
+          assert topScorer;
+        }
+      }
+      return scorer;
     }
 
     @Override
