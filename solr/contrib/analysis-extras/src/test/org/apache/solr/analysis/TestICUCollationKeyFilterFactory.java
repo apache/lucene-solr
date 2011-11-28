4dd6b5fbedfcd6420550674f93c31a1339b33576
Robert Muir <rmuir@apache.org>
2011-11-28 11:03:24 +0000

    SOLR-2919: Localized rangequery support for icu/jdk collation filters, parametric tailoring for icu collators
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1207070 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/analysis-extras/src/test/org/apache/solr/analysis/TestICUCollationKeyFilterFactory.java
+++ solr/contrib/analysis-extras/src/test/org/apache/solr/analysis/TestICUCollationKeyFilterFactory.java
@@ -98,6 +98,133 @@ public class TestICUCollationKeyFilterFactory extends BaseTokenTestCase {
         new KeywordTokenizer(new StringReader(lowerCase)));
     assertCollatesToSame(tsUpper, tsLower);
   }
+  
+  /*
+   * Setting alternate=shifted to shift whitespace, punctuation and symbols
+   * to quaternary level 
+   */
+  public void testIgnorePunctuation() throws IOException {
+    String withPunctuation = "foo-bar";
+    String withoutPunctuation = "foo bar";
+    ICUCollationKeyFilterFactory factory = new ICUCollationKeyFilterFactory();
+    Map<String,String> args = new HashMap<String,String>();
+    args.put("locale", "en");
+    args.put("strength", "primary");
+    args.put("alternate", "shifted");
+    factory.init(args);
+    factory.inform(new StringMockSolrResourceLoader(""));
+    TokenStream tsPunctuation = factory.create(
+        new KeywordTokenizer(new StringReader(withPunctuation)));
+    TokenStream tsWithoutPunctuation = factory.create(
+        new KeywordTokenizer(new StringReader(withoutPunctuation)));
+    assertCollatesToSame(tsPunctuation, tsWithoutPunctuation);
+  }
+  
+  /*
+   * Setting alternate=shifted and variableTop to shift whitespace, but not 
+   * punctuation or symbols, to quaternary level 
+   */
+  public void testIgnoreWhitespace() throws IOException {
+    String withSpace = "foo bar";
+    String withoutSpace = "foobar";
+    String withPunctuation = "foo-bar";
+    ICUCollationKeyFilterFactory factory = new ICUCollationKeyFilterFactory();
+    Map<String,String> args = new HashMap<String,String>();
+    args.put("locale", "en");
+    args.put("strength", "primary");
+    args.put("alternate", "shifted");
+    args.put("variableTop", " ");
+    factory.init(args);
+    factory.inform(new StringMockSolrResourceLoader(""));
+    TokenStream tsWithSpace = factory.create(
+        new KeywordTokenizer(new StringReader(withSpace)));
+    TokenStream tsWithoutSpace = factory.create(
+        new KeywordTokenizer(new StringReader(withoutSpace)));
+    assertCollatesToSame(tsWithSpace, tsWithoutSpace);
+    // now assert that punctuation still matters: foo-bar < foo bar
+    tsWithSpace = factory.create(
+        new KeywordTokenizer(new StringReader(withSpace)));
+    TokenStream tsWithPunctuation = factory.create(
+        new KeywordTokenizer(new StringReader(withPunctuation)));
+    assertCollation(tsWithPunctuation, tsWithSpace, -1);
+  }
+  
+  /*
+   * Setting numeric to encode digits with numeric value, so that
+   * foobar-9 sorts before foobar-10
+   */
+  public void testNumerics() throws IOException {
+    String nine = "foobar-9";
+    String ten = "foobar-10";
+    ICUCollationKeyFilterFactory factory = new ICUCollationKeyFilterFactory();
+    Map<String,String> args = new HashMap<String,String>();
+    args.put("locale", "en");
+    args.put("numeric", "true");
+    factory.init(args);
+    factory.inform(new StringMockSolrResourceLoader(""));
+    TokenStream tsNine = factory.create(
+        new KeywordTokenizer(new StringReader(nine)));
+    TokenStream tsTen = factory.create(
+        new KeywordTokenizer(new StringReader(ten)));
+    assertCollation(tsNine, tsTen, -1);
+  }
+  
+  /*
+   * Setting caseLevel=true to create an additional case level between
+   * secondary and tertiary
+   */
+  public void testIgnoreAccentsButNotCase() throws IOException {
+    String withAccents = "résumé";
+    String withoutAccents = "resume";
+    String withAccentsUpperCase = "Résumé";
+    String withoutAccentsUpperCase = "Resume";
+    ICUCollationKeyFilterFactory factory = new ICUCollationKeyFilterFactory();
+    Map<String,String> args = new HashMap<String,String>();
+    args.put("locale", "en");
+    args.put("strength", "primary");
+    args.put("caseLevel", "true");
+    factory.init(args);
+    factory.inform(new StringMockSolrResourceLoader(""));
+    TokenStream tsWithAccents = factory.create(
+        new KeywordTokenizer(new StringReader(withAccents)));
+    TokenStream tsWithoutAccents = factory.create(
+        new KeywordTokenizer(new StringReader(withoutAccents)));
+    assertCollatesToSame(tsWithAccents, tsWithoutAccents);
+    
+    TokenStream tsWithAccentsUpperCase = factory.create(
+        new KeywordTokenizer(new StringReader(withAccentsUpperCase)));
+    TokenStream tsWithoutAccentsUpperCase = factory.create(
+        new KeywordTokenizer(new StringReader(withoutAccentsUpperCase)));
+    assertCollatesToSame(tsWithAccentsUpperCase, tsWithoutAccentsUpperCase);
+    
+    // now assert that case still matters: resume < Resume
+    TokenStream tsLower = factory.create(
+        new KeywordTokenizer(new StringReader(withoutAccents)));
+    TokenStream tsUpper = factory.create(
+        new KeywordTokenizer(new StringReader(withoutAccentsUpperCase)));
+    assertCollation(tsLower, tsUpper, -1);
+  }
+  
+  /*
+   * Setting caseFirst=upper to cause uppercase strings to sort
+   * before lowercase ones.
+   */
+  public void testUpperCaseFirst() throws IOException {
+    String lower = "resume";
+    String upper = "Resume";
+    ICUCollationKeyFilterFactory factory = new ICUCollationKeyFilterFactory();
+    Map<String,String> args = new HashMap<String,String>();
+    args.put("locale", "en");
+    args.put("strength", "tertiary");
+    args.put("caseFirst", "upper");
+    factory.init(args);
+    factory.inform(new StringMockSolrResourceLoader(""));
+    TokenStream tsLower = factory.create(
+        new KeywordTokenizer(new StringReader(lower)));
+    TokenStream tsUpper = factory.create(
+        new KeywordTokenizer(new StringReader(upper)));
+    assertCollation(tsUpper, tsLower, -1);
+  }
 
   /*
    * For german, you might want oe to sort and match with o umlaut.
@@ -156,15 +283,18 @@ public class TestICUCollationKeyFilterFactory extends BaseTokenTestCase {
     }
   }
   
-  private void assertCollatesToSame(TokenStream stream1, TokenStream stream2)
-      throws IOException {
+  private void assertCollatesToSame(TokenStream stream1, TokenStream stream2) throws IOException {
+    assertCollation(stream1, stream2, 0);
+  }
+  
+  private void assertCollation(TokenStream stream1, TokenStream stream2, int comparison) throws IOException {
     CharTermAttribute term1 = stream1
         .addAttribute(CharTermAttribute.class);
     CharTermAttribute term2 = stream2
         .addAttribute(CharTermAttribute.class);
     assertTrue(stream1.incrementToken());
     assertTrue(stream2.incrementToken());
-    assertEquals(term1.toString(), term2.toString());
+    assertEquals(Integer.signum(comparison), Integer.signum(term1.toString().compareTo(term2.toString())));
     assertFalse(stream1.incrementToken());
     assertFalse(stream2.incrementToken());
   }
