aedd1f9dc583866ce949abe7d484d9d025db38d5
Yonik Seeley <yonik@apache.org>
2011-11-30 15:52:01 +0000

    log read entry on class-cast exception
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/solrcloud@1208474 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/FSUpdateLog.java
+++ solr/core/src/java/org/apache/solr/update/FSUpdateLog.java
@@ -717,13 +717,13 @@ public class FSUpdateLog extends UpdateLog {
                 throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,  "Unknown Operation! " + oper);
             }
           } catch (IOException ex) {
-
+            log.warn("IOException reading log", ex);
+            // could be caused by an incomplete flush if recovering from log
           } catch (ClassCastException cl) {
-            log.warn("Corrupt log", cl);
+            log.warn("Unexpectged log entry or corrupt log.  Entry=" + o);
             // would be caused by a corrupt transaction log
           } catch (Exception ex) {
             log.warn("Exception replaying log", ex);
-
             // something wrong with the request?
           }
         }
