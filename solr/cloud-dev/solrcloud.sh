b15a04d01e5449e9e23ad6c75c61b900211e41c4
Mark Robert Miller <markrmiller@apache.org>
2011-12-08 04:31:46 +0000

    change to a 6 node cluster, 3 slices
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/solrcloud@1211754 13f79535-47bb-0310-9956-ffa450edef68

--- solr/cloud-dev/solrcloud.sh
+++ solr/cloud-dev/solrcloud.sh
@@ -6,11 +6,13 @@ rm -r -f example2
 rm -r -f example3
 rm -r -f example4
 rm -r -f example5
+rm -r -f example6
 
 cp -r example example2
 cp -r example example3
-#cp -r example example4
-#cp -r example example5
+cp -r example example4
+cp -r example example5
+cp -r example example6
 
 rm -r -f dist
 rm -r -f build
@@ -29,8 +31,11 @@ java -Djetty.port=7574 -DzkHost=localhost:9983 -jar start.jar &
 cd ../example3
 java -Djetty.port=7575 -DzkHost=localhost:9983 -jar start.jar &
 
-#cd ../example4
-#java -Djetty.port=7576 -DzkHost=localhost:9983 -jar start.jar &
+cd ../example4
+java -Djetty.port=7576 -DzkHost=localhost:9983 -jar start.jar &
 
-#cd ../example5
-#java -Djetty.port=7577 -DzkHost=localhost:9983 -jar start.jar &
+cd ../example5
+java -Djetty.port=7577 -DzkHost=localhost:9983 -jar start.jar &
+
+cd ../example6
+java -Djetty.port=7578 -DzkHost=localhost:9983 -jar start.jar &
