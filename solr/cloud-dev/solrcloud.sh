e2a22ce31d2c6999f3e5e4aa52c065e3d9e90f3e
Mark Robert Miller <markrmiller@apache.org>
2011-12-08 00:45:10 +0000

    put this script in somewhat working state
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/solrcloud@1211724 13f79535-47bb-0310-9956-ffa450edef68

--- solr/cloud-dev/solrcloud.sh
+++ solr/cloud-dev/solrcloud.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
 cd ..
+
+rm -r -f example2
+rm -r -f example3
+rm -r -f example4
+rm -r -f example5
+
 cp -r example example2
 cp -r example example3
-cp -r example example4
-cp -r example example5
+#cp -r example example4
+#cp -r example example5
+
+rm -r -f dist
+rm -r -f build
+rm -r -f example/solr/zoo_data
 
 ant example dist
 
-java -classpath lib/*:dist/* org.apache.solr.cloud.ZkController 127.0.0.1:9983 127.0.0.1 8983 solr ../example/solr/conf conf1 example/solr/zoo_data
+java -classpath lib/*:dist/*:build/lucene-libs/* org.apache.solr.cloud.ZkController 127.0.0.1:9983 127.0.0.1 8983 solr example/solr/conf conf1 example/solr/zoo_data
 
 cd example
 java -DzkRun -jar start.jar &
@@ -19,8 +29,8 @@ java -Djetty.port=7574 -DzkHost=localhost:9983 -jar start.jar &
 cd ../example3
 java -Djetty.port=7575 -DzkHost=localhost:9983 -jar start.jar &
 
-cd ../example4
-java -Djetty.port=7576 -DzkHost=localhost:9983 -jar start.jar &
+#cd ../example4
+#java -Djetty.port=7576 -DzkHost=localhost:9983 -jar start.jar &
 
-cd ../example5
-java -Djetty.port=7577 -DzkHost=localhost:9983 -jar start.jar &
+#cd ../example5
+#java -Djetty.port=7577 -DzkHost=localhost:9983 -jar start.jar &
