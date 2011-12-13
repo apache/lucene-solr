bd58a270bc96c5103ab26654efe4e27b0d5b2c9f
Mark Robert Miller <markrmiller@apache.org>
2011-12-13 19:01:45 +0000

    startup with 2 shards
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/solrcloud@1213866 13f79535-47bb-0310-9956-ffa450edef68

--- solr/cloud-dev/solrcloud.sh
+++ solr/cloud-dev/solrcloud.sh
@@ -8,35 +8,35 @@ rm -r -f example4
 rm -r -f example5
 rm -r -f example6
 
-cp -r example example2
-cp -r example example3
-cp -r example example4
-cp -r example example5
-cp -r example example6
+cp -r -f example example2
+cp -r -f example example3
+cp -r -f example example4
+cp -r -f example example5
+cp -r -f example example6
 
 rm -r -f dist
 rm -r -f build
 rm -r -f example/solr/zoo_data
-rm -r -f example/example.log
+rm -f example/example.log
 
 ant example dist
 
 java -classpath lib/*:dist/*:build/lucene-libs/* org.apache.solr.cloud.ZkController 127.0.0.1:9983 example/solr 8983 example/solr/conf conf1
 
 cd example
-java -DzkRun -DSTOP.PORT=7983 -DSTOP.KEY=key -jar start.jar 1>example.log 2>&1 &
+java -DzkRun -DnumShards=2 -DSTOP.PORT=7983 -DSTOP.KEY=key -jar start.jar 1>example.log 2>&1 &
 
 cd ../example2
-java -Djetty.port=7574 -DzkHost=localhost:9983 -DSTOP.PORT=6574 -DSTOP.KEY=key -jar start.jar 1>example2.log 2>&1 &
+java -Djetty.port=7574 -DzkHost=localhost:9983 -DnumShards=2 -DSTOP.PORT=6574 -DSTOP.KEY=key -jar start.jar 1>example2.log 2>&1 &
 
 cd ../example3
-java -Djetty.port=7575 -DzkHost=localhost:9983 -DSTOP.PORT=6575 -DSTOP.KEY=key -jar start.jar 1>example3.log 2>&1 &
+java -Djetty.port=7575 -DzkHost=localhost:9983 -DnumShards=2 -DSTOP.PORT=6575 -DSTOP.KEY=key -jar start.jar 1>example3.log 2>&1 &
 
 cd ../example4
-java -Djetty.port=7576 -DzkHost=localhost:9983 -DSTOP.PORT=6576 -DSTOP.KEY=key -jar start.jar 1>example4.log 2>&1 &
+java -Djetty.port=7576 -DzkHost=localhost:9983 -DnumShards=2 -DSTOP.PORT=6576 -DSTOP.KEY=key -jar start.jar 1>example4.log 2>&1 &
 
 cd ../example5
-java -Djetty.port=7577 -DzkHost=localhost:9983 -DSTOP.PORT=6577 -DSTOP.KEY=key -jar start.jar 1>example5.log 2>&1 &
+java -Djetty.port=7577 -DzkHost=localhost:9983 -DnumShards=2 -DSTOP.PORT=6577 -DSTOP.KEY=key -jar start.jar 1>example5.log 2>&1 &
 
 cd ../example6
-java -Djetty.port=7578 -DzkHost=localhost:9983 -DSTOP.PORT=6578 -DSTOP.KEY=key -jar start.jar 1>example6.log 2>&1 &
+java -Djetty.port=7578 -DzkHost=localhost:9983 -DnumShards=2 -DSTOP.PORT=6578 -DSTOP.KEY=key -jar start.jar 1>example6.log 2>&1 &
