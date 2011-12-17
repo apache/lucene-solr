aac854168c224f084ef4812ead6488c084030f5b
Erick Erickson <erick@apache.org>
2011-12-17 14:54:22 +0000

    Fixes for SOLR-2971, valType now optional for ExternalFileFields and, if present, types float, pfloat, tfloat all accepable.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1215504 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/schema/BadIndexSchemaTest.java
+++ solr/core/src/test/org/apache/solr/schema/BadIndexSchemaTest.java
@@ -26,6 +26,7 @@ import org.apache.solr.common.SolrException;
 import org.apache.solr.common.SolrException.ErrorCode;
 import org.apache.solr.core.SolrConfig;
 
+import org.apache.solr.search.SolrIndexSearcher;
 import org.junit.Test;
 
 /**
@@ -39,8 +40,10 @@ public class BadIndexSchemaTest extends SolrTestCaseJ4 {
     try {
       initCore( "solrconfig.xml", schema );
     } catch (SolrException e) {
-      // short circut out if we found what we expected
+      // short circuit out if we found what we expected
       if (-1 != e.getMessage().indexOf(errString)) return;
+      // Test the cause too in case the expected error is wrapped
+      if (-1 != e.getCause().getMessage().indexOf(errString)) return;
 
       // otherwise, rethrow it, possibly completley unrelated
       throw new SolrException
@@ -63,45 +66,74 @@ public class BadIndexSchemaTest extends SolrTestCaseJ4 {
   private Throwable findErrorWithSubstring( List<Throwable> err, String v )
   {
     for( Throwable t : err ) {
-      if( t.getMessage().indexOf( v ) > 0 ) {
+      if( t.getMessage().indexOf( v ) > -1 ) {
         return t;
       }
     }
     return null;
   }
-  
+
+  @Test
   public void testSevereErrors() throws Exception {
     final String bad_type = "StrField (bad_type)";
     try {
       initCore( "solrconfig.xml", "bad-schema.xml" );
-      
+
       ignoreException("_twice");
       ignoreException("ftAgain");
       ignoreException("fAgain");
       ignoreException(Pattern.quote(bad_type));
-      
+
       for( Throwable t : SolrConfig.severeErrors ) {
         log.info( "got ex:"+t.getMessage() );
       }
-    
+
       assertEquals( 4, SolrConfig.severeErrors.size() );
 
       List<Throwable> err = new LinkedList<Throwable>();
       err.addAll( SolrConfig.severeErrors );
-      
+
       Throwable t = findErrorWithSubstring( err, "*_twice" );
       assertNotNull( t );
       err.remove( t );
-      
+
       t = findErrorWithSubstring( err, "ftAgain" );
       assertNotNull( t );
       err.remove( t );
-      
+
       t = findErrorWithSubstring( err, "fAgain" );
       assertNotNull( t );
       err.remove( t );
-      
+
       t = findErrorWithSubstring( err, bad_type );
+      assertNotNull( t );
+      err.remove( t );
+
+      // make sure that's all of them
+      assertTrue( err.isEmpty() );
+    } finally {
+      SolrConfig.severeErrors.clear();
+      deleteCore();
+    }
+  }
+
+  @Test
+  public void testBadExternalFileField() throws Exception {
+    try {
+      initCore( "solrconfig.xml", "bad-schema-external-filefield.xml" );
+
+      ignoreException("Only float and pfloat");
+
+      for( Throwable t : SolrConfig.severeErrors ) {
+        log.info( "got ex:"+t.getMessage() );
+      }
+
+      assertEquals( 1, SolrConfig.severeErrors.size() );
+
+      List<Throwable> err = new LinkedList<Throwable>();
+      err.addAll( SolrConfig.severeErrors );
+
+      Throwable t = findErrorWithSubstring( err, "Only float and pfloat" );
       assertNotNull( t );
       err.remove( t );
 
@@ -109,6 +141,7 @@ public class BadIndexSchemaTest extends SolrTestCaseJ4 {
       assertTrue( err.isEmpty() );
     } finally {
       SolrConfig.severeErrors.clear();
+      deleteCore();
     }
   }
 }
