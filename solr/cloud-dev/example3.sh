aefa7eb78955b0fa843bb60767017f90b5cc2b50
Mark Robert Miller <markrmiller@apache.org>
2011-12-22 21:22:21 +0000

    add example2
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/solrcloud@1222460 13f79535-47bb-0310-9956-ffa450edef68

--- solr/cloud-dev/example3.sh
+++ solr/cloud-dev/example3.sh
@@ -4,7 +4,7 @@ cd ..
 
 rm -r -f example2
 rm -r -f example3
-rm -r -f example3
+rm -r -f example4
 
 rm -r -f dist
 rm -r -f build
@@ -19,16 +19,14 @@ cp -r -f example example4
 
 
 cd example
-java -DzkRun -DnumShards=2 -DSTOP.PORT=7983 -DSTOP.KEY=key -Dbootstrap_confdir=solr/conf -DzkHost=localhost:9983,localhost:8574,localhost:8575 -jar start.jar 1>example.log 2>&1 &
-
-sleep 10
+java -DzkRun -DnumShards=2 -DSTOP.PORT=7983 -DSTOP.KEY=key -Dbootstrap_confdir=solr/conf -DzkHost=localhost:9983,localhost:10574,localhost:10575 -jar start.jar 1>example.log 2>&1 &
 
 cd ../example2
-java -Djetty.port=7574 -DzkRun -DzkHost=localhost:9983,localhost:8574,localhost:8575 -DnumShards=2 -DSTOP.PORT=6574 -DSTOP.KEY=key -jar start.jar 1>example2.log 2>&1 &
+java -Djetty.port=9574 -DzkRun -DzkHost=localhost:9983,localhost:10574,localhost:10575 -DnumShards=2 -DSTOP.PORT=6574 -DSTOP.KEY=key -jar start.jar 1>example2.log 2>&1 &
 
 cd ../example3
-java -Djetty.port=7575 -DzkRun -DzkHost=localhost:9983,localhost:8574,localhost:8575 -DnumShards=2 -DSTOP.PORT=6575 -DSTOP.KEY=key -jar start.jar 1>example3.log 2>&1 &
+java -Djetty.port=9575 -DzkRun -DzkHost=localhost:9983,localhost:10574,localhost:10575 -DnumShards=2 -DSTOP.PORT=6575 -DSTOP.KEY=key -jar start.jar 1>example3.log 2>&1 &
 
 cd ../example4
-java -Djetty.port=7576 -DzkHost=localhost:9983,localhost:8574,localhost:8575 -DnumShards=2 -DSTOP.PORT=6576 -DSTOP.KEY=key -jar start.jar 1>example4.log 2>&1 &
+java -Djetty.port=9576 -DzkHost=localhost:9983,localhost:10574,localhost:10575 -DnumShards=2 -DSTOP.PORT=6576 -DSTOP.KEY=key -jar start.jar 1>example4.log 2>&1 &
 
