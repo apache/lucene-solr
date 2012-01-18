7bd595635175f933c53d2e661b06687699322b21
Jan Høydahl <janhoy@apache.org>
2012-01-18 14:56:36 +0000

    SOLR-2970: CSV ResponseWriter returns fields defined as stored=false in schema
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1232919 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/response/CSVResponseWriter.java
+++ solr/core/src/java/org/apache/solr/response/CSVResponseWriter.java
@@ -241,6 +241,7 @@ class CSVWriter extends TextResponseWriter {
     Collection<String> fields = returnFields;
 
     Object responseObj = rsp.getValues().get("response");
+    boolean returnOnlyStored = false;
     if (needListOfFields) {
       if (responseObj instanceof SolrDocumentList) {
         // get the list of fields from the SolrDocumentList
@@ -257,6 +258,7 @@ class CSVWriter extends TextResponseWriter {
       } else {
         fields.remove("score");
       }
+      returnOnlyStored = true;
     }
 
     CSVSharedBufPrinter csvPrinterMV = new CSVSharedBufPrinter(mvWriter, mvStrategy);
@@ -274,9 +276,9 @@ class CSVWriter extends TextResponseWriter {
         FieldType ft = new StrField();
         sf = new SchemaField(field, ft);
       }
-
-      // if we got the list of fields from the index, only list stored fields
-      if (returnFields==null && sf != null && !sf.stored()) {
+      
+      // Return only stored fields, unless an explicit field list is specified
+      if (returnOnlyStored && sf != null && !sf.stored()) {
         continue;
       }
 
