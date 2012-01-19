675b0066de1f45dfc3e08583d48cd61205d2c074
Mark Robert Miller <markrmiller@apache.org>
2012-01-19 21:23:16 +0000

    turn on for debug
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/solrcloud@1233576 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/ChaosMonkey.java
+++ solr/core/src/test/org/apache/solr/cloud/ChaosMonkey.java
@@ -287,12 +287,12 @@ public class ChaosMonkey {
       }
     }
     
-    //System.out.println("num active:" + numActive + " for " + slice);
-    
     if (numActive < 2) {
       // we cannot kill anyone
       return null;
     }
+    
+    System.out.println("num active:" + numActive + " for " + slice);
     
     int chance = random.nextInt(10);
     JettySolrRunner jetty;
