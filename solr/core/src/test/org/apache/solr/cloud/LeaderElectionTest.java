228a32406230339a2675b1e19e7d501af85d163c
Mark Robert Miller <markrmiller@apache.org>
2012-01-26 15:45:49 +0000

    raise timeout and try to un-ignore this test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1236250 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/LeaderElectionTest.java
+++ solr/core/src/test/org/apache/solr/cloud/LeaderElectionTest.java
@@ -32,15 +32,12 @@ import org.apache.solr.common.cloud.SolrZkClient;
 import org.apache.solr.common.cloud.ZkCoreNodeProps;
 import org.apache.solr.common.cloud.ZkNodeProps;
 import org.apache.solr.common.cloud.ZkStateReader;
-import org.apache.solr.core.SolrConfig;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.KeeperException.NoNodeException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore
 public class LeaderElectionTest extends SolrTestCaseJ4 {
   
   static final int TIMEOUT = 30000;
@@ -156,7 +153,7 @@ public class LeaderElectionTest extends SolrTestCaseJ4 {
   
   private String getLeaderUrl(final String collection, final String slice)
       throws KeeperException, InterruptedException {
-    int iterCount = 30;
+    int iterCount = 60;
     while (iterCount-- > 0)
       try {
         byte[] data = zkClient.getData(
@@ -166,7 +163,7 @@ public class LeaderElectionTest extends SolrTestCaseJ4 {
             ZkNodeProps.load(data));
         return leaderProps.getCoreUrl();
       } catch (NoNodeException e) {
-        Thread.sleep(100);
+        Thread.sleep(500);
       }
     throw new RuntimeException("Could not get leader props");
   }
