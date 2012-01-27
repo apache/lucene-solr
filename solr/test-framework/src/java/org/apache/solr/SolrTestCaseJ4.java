b141bbb52ebadc4aafacedbd89677a97af9d4099
Yonik Seeley <yonik@apache.org>
2012-01-27 18:59:50 +0000

    tests:remove tracing exception
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1236825 13f79535-47bb-0310-9956-ffa450edef68

--- solr/test-framework/src/java/org/apache/solr/SolrTestCaseJ4.java
+++ solr/test-framework/src/java/org/apache/solr/SolrTestCaseJ4.java
@@ -115,11 +115,6 @@ public abstract class SolrTestCaseJ4 extends LuceneTestCase {
     if (numOpens != 0 || numCloses != 0) {
       // NOTE: some other tests don't use this base class and hence won't reset the counts.
       log.warn("startTrackingSearchers: numOpens="+numOpens+" numCloses="+numCloses);
-      try {
-        throw new RuntimeException();
-      } catch (Exception e) {
-        log.error("",e);
-      }
       numOpens = numCloses = 0;
     }
   }
