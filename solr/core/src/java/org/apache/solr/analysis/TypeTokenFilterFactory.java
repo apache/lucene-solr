0e73d06216de933d4c107523c178792c63b361a2
Koji Sekiguchi <koji@apache.org>
2012-02-05 02:38:18 +0000

    SOLR-3054: add a period at the end of the first sentence in javadoc in order to avoid displaying the sample definition of schema.xml in o.a.s.analysis package table, and add indent to the sample for readability
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1240655 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/analysis/TypeTokenFilterFactory.java
+++ solr/core/src/java/org/apache/solr/analysis/TypeTokenFilterFactory.java
@@ -30,14 +30,14 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * Factory class for {@link TypeTokenFilter}
+ * Factory class for {@link TypeTokenFilter}.
  * <pre class="prettyprint" >
  * &lt;fieldType name="chars" class="solr.TextField" positionIncrementGap="100"&gt;
- * &lt;analyzer&gt;
- * &lt;tokenizer class="solr.StandardTokenizerFactory"/&gt;
- * &lt;filter class="solr.TypeTokenFilterFactory" types="stoptypes.txt" enablePositionIncrements="true"
- * useWhiteList="false"/&gt;
- * &lt;/analyzer&gt;
+ *   &lt;analyzer&gt;
+ *     &lt;tokenizer class="solr.StandardTokenizerFactory"/&gt;
+ *     &lt;filter class="solr.TypeTokenFilterFactory" types="stoptypes.txt"
+ *                   enablePositionIncrements="true" useWhiteList="false"/&gt;
+ *   &lt;/analyzer&gt;
  * &lt;/fieldType&gt;</pre>
  */
 public class TypeTokenFilterFactory extends BaseTokenFilterFactory implements ResourceLoaderAware {
