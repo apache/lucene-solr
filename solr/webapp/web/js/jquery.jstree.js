783404c19b222f1797d850719842299e6817ccfb
Erick Erickson <erick@apache.org>
2012-02-15 21:27:50 +0000

    Took out license information when the file was already licensed.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1244740 13f79535-47bb-0310-9956-ffa450edef68

--- solr/webapp/web/js/jquery.jstree.js
+++ solr/webapp/web/js/jquery.jstree.js
@@ -11,22 +11,6 @@
  * $Date: 2010-07-01 10:51:11 +0300 (четв, 01 юли 2010) $
  * $Revision: 191 $
  */
-/*
- Licensed to the Apache Software Foundation (ASF) under one or more
- contributor license agreements.  See the NOTICE file distributed with
- this work for additional information regarding copyright ownership.
- The ASF licenses this file to You under the Apache License, Version 2.0
- (the "License"); you may not use this file except in compliance with
- the License.  You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
 
 /*jslint browser: true, onevar: true, undef: true, bitwise: true, strict: true */
 /*global window : false, clearInterval: false, clearTimeout: false, document: false, setInterval: false, setTimeout: false, jQuery: false, navigator: false, XSLTProcessor: false, DOMParser: false, XMLSerializer: false*/
