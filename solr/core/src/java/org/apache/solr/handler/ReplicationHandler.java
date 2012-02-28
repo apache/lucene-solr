d54df6ff53bbb277cdd50a095a1f3e19f5fe7bc6
James Dyer <jdyer@apache.org>
2012-02-28 16:25:41 +0000

    SOLR-3168 / SOLR-3033:  "numberToKeep" backups doesn't work with "backupAfter" & doesn't ever keep more than 1.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1294718 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/handler/ReplicationHandler.java
+++ solr/core/src/java/org/apache/solr/handler/ReplicationHandler.java
@@ -19,6 +19,7 @@ package org.apache.solr.handler;
 import org.apache.lucene.index.IndexCommit;
 import org.apache.lucene.index.IndexDeletionPolicy;
 import org.apache.lucene.index.IndexReader;
+import org.apache.solr.common.SolrException.ErrorCode;
 import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.params.SolrParams;
@@ -85,6 +86,8 @@ public class ReplicationHandler extends RequestHandlerBase implements SolrCoreAw
   private boolean replicateOnCommit = false;
 
   private boolean replicateOnStart = false;
+  
+  private int numberBackupsToKeep = 0; //zero: do not delete old backups
 
   private int numTimesReplicated = 0;
 
@@ -278,18 +281,31 @@ public class ReplicationHandler extends RequestHandlerBase implements SolrCoreAw
     return snapPullLock.isLocked();
   }
 
-  private void doSnapShoot(SolrParams params, SolrQueryResponse rsp, SolrQueryRequest req) {
+  private void doSnapShoot(SolrParams params, SolrQueryResponse rsp,
+      SolrQueryRequest req) {
     try {
-      int numberToKeep = params.getInt(NUMBER_BACKUPS_TO_KEEP, Integer.MAX_VALUE);
+      int numberToKeep = params.getInt(NUMBER_BACKUPS_TO_KEEP_REQUEST_PARAM, 0);
+      if (numberToKeep > 0 && numberBackupsToKeep > 0) {
+        throw new SolrException(ErrorCode.BAD_REQUEST, "Cannot use "
+            + NUMBER_BACKUPS_TO_KEEP_REQUEST_PARAM + " if "
+            + NUMBER_BACKUPS_TO_KEEP_INIT_PARAM
+            + " was specified in the configuration.");
+      }
+      numberToKeep = Math.max(numberToKeep, numberBackupsToKeep);
+      if (numberToKeep < 1) {
+        numberToKeep = Integer.MAX_VALUE;
+      }
+      
       IndexDeletionPolicyWrapper delPolicy = core.getDeletionPolicy();
       IndexCommit indexCommit = delPolicy.getLatestCommit();
       
-      if(indexCommit == null) {
+      if (indexCommit == null) {
         indexCommit = req.getSearcher().getReader().getIndexCommit();
       }
       
       // small race here before the commit point is saved
-      new SnapShooter(core, params.get("location")).createSnapAsync(indexCommit, numberToKeep, this);
+      new SnapShooter(core, params.get("location")).createSnapAsync(
+          indexCommit, numberToKeep, this);
       
     } catch (Exception e) {
       LOG.warn("Exception during creating a snapshot", e);
@@ -778,6 +794,12 @@ public class ReplicationHandler extends RequestHandlerBase implements SolrCoreAw
     this.core = core;
     registerFileStreamResponseWriter();
     registerCloseHook();
+    Object nbtk = initArgs.get(NUMBER_BACKUPS_TO_KEEP_INIT_PARAM);
+    if(nbtk!=null) {
+      numberBackupsToKeep = Integer.parseInt(nbtk.toString());
+    } else {
+      numberBackupsToKeep = 0;
+    }
     NamedList slave = (NamedList) initArgs.get("slave");
     boolean enableSlave = isEnabled( slave );
     if (enableSlave) {
@@ -1155,5 +1177,7 @@ public class ReplicationHandler extends RequestHandlerBase implements SolrCoreAw
 
   public static final String NEXT_EXECUTION_AT = "nextExecutionAt";
   
-  public static final String NUMBER_BACKUPS_TO_KEEP = "numberToKeep";
+  public static final String NUMBER_BACKUPS_TO_KEEP_REQUEST_PARAM = "numberToKeep";
+  
+  public static final String NUMBER_BACKUPS_TO_KEEP_INIT_PARAM = "maxNumberOfBackups";
 }
