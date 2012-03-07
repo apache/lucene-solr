1242fb04371b7b4eeb4ccc5913a2f6ff31482b43
Dawid Weiss <dweiss@apache.org>
2012-03-07 22:10:00 +0000

    Fixing imports.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1298156 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/client/solrj/embedded/JettyWebappTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/embedded/JettyWebappTest.java
@@ -24,18 +24,15 @@ import java.util.Random;
 import org.apache.commons.io.IOUtils;
 import org.apache.lucene.util.LuceneTestCase;
 import org.apache.lucene.util.SystemPropertiesRestoreRule;
-import org.apache.solr.SolrTestCaseJ4;
 import org.apache.solr.util.ExternalPaths;
-import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.server.session.HashSessionIdManager;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.util.component.LifeCycle;
-import org.eclipse.jetty.util.log.Logger;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
 
 /**
  *
