8bd540f1dbb1425908b089ab158bb4265bfc8596
Robert Muir <rmuir@apache.org>
2012-03-23 12:14:29 +0000

    LUCENE-3904: fix Similarity javadocs when java7's javadoc is used
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1304302 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/search/similarities/TFIDFSimilarity.java
+++ lucene/core/src/java/org/apache/lucene/search/similarities/TFIDFSimilarity.java
@@ -81,7 +81,7 @@ import org.apache.lucene.util.SmallFloat;
  * of the weighted query vectors <i>V(q)</i> and <i>V(d)</i>:
  *
  *  <br>&nbsp;<br>
- *  <table cellpadding="2" cellspacing="2" border="0" align="center">
+ *  <table cellpadding="2" cellspacing="2" border="0" align="center" style="width:auto">
  *    <tr><td>
  *    <table cellpadding="1" cellspacing="0" border="1" align="center">
  *      <tr><td>
@@ -92,9 +92,9 @@ import org.apache.lucene.util.SmallFloat;
  *          </td>
  *          <td valign="middle" align="center">
  *            <table>
- *               <tr><td align="center"><small>V(q)&nbsp;&middot;&nbsp;V(d)</small></td></tr>
- *               <tr><td align="center">&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;</td></tr>
- *               <tr><td align="center"><small>|V(q)|&nbsp;|V(d)|</small></td></tr>
+ *               <tr><td align="center" style="text-align: center"><small>V(q)&nbsp;&middot;&nbsp;V(d)</small></td></tr>
+ *               <tr><td align="center" style="text-align: center">&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;</td></tr>
+ *               <tr><td align="center" style="text-align: center"><small>|V(q)|&nbsp;|V(d)|</small></td></tr>
  *            </table>
  *          </td>
  *        </tr>
@@ -166,7 +166,7 @@ import org.apache.lucene.util.SmallFloat;
  * we get <i>Lucene's Conceptual scoring formula</i>:
  *
  *  <br>&nbsp;<br>
- *  <table cellpadding="2" cellspacing="2" border="0" align="center">
+ *  <table cellpadding="2" cellspacing="2" border="0" align="center" style="width:auto">
  *    <tr><td>
  *    <table cellpadding="1" cellspacing="0" border="1" align="center">
  *      <tr><td>
@@ -179,9 +179,9 @@ import org.apache.lucene.util.SmallFloat;
  *          </td>
  *          <td valign="middle" align="center">
  *            <table>
- *               <tr><td align="center"><small><font color="#993399">V(q)&nbsp;&middot;&nbsp;V(d)</font></small></td></tr>
- *               <tr><td align="center">&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;</td></tr>
- *               <tr><td align="center"><small><font color="#FF33CC">|V(q)|</font></small></td></tr>
+ *               <tr><td align="center" style="text-align: center"><small><font color="#993399">V(q)&nbsp;&middot;&nbsp;V(d)</font></small></td></tr>
+ *               <tr><td align="center" style="text-align: center">&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;</td></tr>
+ *               <tr><td align="center" style="text-align: center"><small><font color="#FF33CC">|V(q)|</font></small></td></tr>
  *            </table>
  *          </td>
  *          <td valign="middle" align="right" rowspan="1">
@@ -258,7 +258,7 @@ import org.apache.lucene.util.SmallFloat;
  * to those of the <i>conceptual</i> formula:
  *
  * <P>
- * <table cellpadding="2" cellspacing="2" border="0" align="center">
+ * <table cellpadding="2" cellspacing="2" border="0" align="center" style="width:auto">
  *  <tr><td>
  *  <table cellpadding="" cellspacing="2" border="2" align="center">
  *  <tr><td>
@@ -269,7 +269,7 @@ import org.apache.lucene.util.SmallFloat;
  *       <A HREF="#formula_coord"><font color="#FF9933">coord(q,d)</font></A> &nbsp;&middot;&nbsp;
  *       <A HREF="#formula_queryNorm"><font color="#FF33CC">queryNorm(q)</font></A> &nbsp;&middot;&nbsp;
  *     </td>
- *     <td valign="bottom" align="center" rowspan="1">
+ *     <td valign="bottom" align="center" rowspan="1" style="text-align: center">
  *       <big><big><big>&sum;</big></big></big>
  *     </td>
  *     <td valign="middle" align="right" rowspan="1">
@@ -283,7 +283,7 @@ import org.apache.lucene.util.SmallFloat;
  *   </tr>
  *   <tr valigh="top">
  *    <td></td>
- *    <td align="center"><small>t in q</small></td>
+ *    <td align="center" style="text-align: center"><small>t in q</small></td>
  *    <td></td>
  *   </tr>
  *   </table>
@@ -311,7 +311,7 @@ import org.apache.lucene.util.SmallFloat;
  *      {@link org.apache.lucene.search.similarities.DefaultSimilarity#tf(float) DefaultSimilarity} is:
  *
  *      <br>&nbsp;<br>
- *      <table cellpadding="2" cellspacing="2" border="0" align="center">
+ *      <table cellpadding="2" cellspacing="2" border="0" align="center" style="width:auto">
  *        <tr>
  *          <td valign="middle" align="right" rowspan="1">
  *            {@link org.apache.lucene.search.similarities.DefaultSimilarity#tf(float) tf(t in d)} &nbsp; = &nbsp;
@@ -336,7 +336,7 @@ import org.apache.lucene.util.SmallFloat;
  *      {@link org.apache.lucene.search.similarities.DefaultSimilarity#idf(long, long) DefaultSimilarity} is:
  *
  *      <br>&nbsp;<br>
- *      <table cellpadding="2" cellspacing="2" border="0" align="center">
+ *      <table cellpadding="2" cellspacing="2" border="0" align="center" style="width:auto">
  *        <tr>
  *          <td valign="middle" align="right">
  *            {@link org.apache.lucene.search.similarities.DefaultSimilarity#idf(long, long) idf(t)}&nbsp; = &nbsp;
@@ -346,9 +346,9 @@ import org.apache.lucene.util.SmallFloat;
  *          </td>
  *          <td valign="middle" align="center">
  *            <table>
- *               <tr><td align="center"><small>numDocs</small></td></tr>
- *               <tr><td align="center">&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;</td></tr>
- *               <tr><td align="center"><small>docFreq+1</small></td></tr>
+ *               <tr><td align="center" style="text-align: center"><small>numDocs</small></td></tr>
+ *               <tr><td align="center" style="text-align: center">&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;</td></tr>
+ *               <tr><td align="center" style="text-align: center"><small>docFreq+1</small></td></tr>
  *            </table>
  *          </td>
  *          <td valign="middle" align="center">
@@ -384,7 +384,7 @@ import org.apache.lucene.util.SmallFloat;
  *      {@link org.apache.lucene.search.similarities.DefaultSimilarity#queryNorm(float) DefaultSimilarity}
  *      produces a <a href="http://en.wikipedia.org/wiki/Euclidean_norm#Euclidean_norm">Euclidean norm</a>:
  *      <br>&nbsp;<br>
- *      <table cellpadding="1" cellspacing="0" border="0" align="center">
+ *      <table cellpadding="1" cellspacing="0" border="0" align="center" style="width:auto">
  *        <tr>
  *          <td valign="middle" align="right" rowspan="1">
  *            queryNorm(q)  &nbsp; = &nbsp;
@@ -393,11 +393,11 @@ import org.apache.lucene.util.SmallFloat;
  *          </td>
  *          <td valign="middle" align="center" rowspan="1">
  *            <table>
- *               <tr><td align="center"><big>1</big></td></tr>
- *               <tr><td align="center"><big>
+ *               <tr><td align="center" style="text-align: center"><big>1</big></td></tr>
+ *               <tr><td align="center" style="text-align: center"><big>
  *                  &ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;&ndash;
  *               </big></td></tr>
- *               <tr><td align="center">sumOfSquaredWeights<sup><big>&frac12;</big></sup></td></tr>
+ *               <tr><td align="center" style="text-align: center">sumOfSquaredWeights<sup><big>&frac12;</big></sup></td></tr>
  *            </table>
  *          </td>
  *        </tr>
@@ -410,14 +410,14 @@ import org.apache.lucene.util.SmallFloat;
  *      computes this value as:
  *
  *      <br>&nbsp;<br>
- *      <table cellpadding="1" cellspacing="0" border="0"n align="center">
+ *      <table cellpadding="1" cellspacing="0" border="0"n align="center" style="width:auto">
  *        <tr>
  *          <td valign="middle" align="right" rowspan="1">
  *            {@link org.apache.lucene.search.Weight#getValueForNormalization() sumOfSquaredWeights} &nbsp; = &nbsp;
  *            {@link org.apache.lucene.search.Query#getBoost() q.getBoost()} <sup><big>2</big></sup>
  *            &nbsp;&middot;&nbsp;
  *          </td>
- *          <td valign="bottom" align="center" rowspan="1">
+ *          <td valign="bottom" align="center" rowspan="1" style="text-align: center">
  *            <big><big><big>&sum;</big></big></big>
  *          </td>
  *          <td valign="middle" align="right" rowspan="1">
@@ -429,7 +429,7 @@ import org.apache.lucene.util.SmallFloat;
  *        </tr>
  *        <tr valigh="top">
  *          <td></td>
- *          <td align="center"><small>t in q</small></td>
+ *          <td align="center" style="text-align: center"><small>t in q</small></td>
  *          <td></td>
  *        </tr>
  *      </table>
@@ -476,14 +476,14 @@ import org.apache.lucene.util.SmallFloat;
  *      If the document has multiple fields with the same name, all their boosts are multiplied together:
  *
  *      <br>&nbsp;<br>
- *      <table cellpadding="1" cellspacing="0" border="0"n align="center">
+ *      <table cellpadding="1" cellspacing="0" border="0"n align="center" style="width:auto">
  *        <tr>
  *          <td valign="middle" align="right" rowspan="1">
  *            norm(t,d) &nbsp; = &nbsp;
  *            lengthNorm
  *            &nbsp;&middot;&nbsp;
  *          </td>
- *          <td valign="bottom" align="center" rowspan="1">
+ *          <td valign="bottom" align="center" rowspan="1" style="text-align: center">
  *            <big><big><big>&prod;</big></big></big>
  *          </td>
  *          <td valign="middle" align="right" rowspan="1">
@@ -492,7 +492,7 @@ import org.apache.lucene.util.SmallFloat;
  *        </tr>
  *        <tr valigh="top">
  *          <td></td>
- *          <td align="center"><small>field <i><b>f</b></i> in <i>d</i> named as <i><b>t</b></i></small></td>
+ *          <td align="center" style="text-align: center"><small>field <i><b>f</b></i> in <i>d</i> named as <i><b>t</b></i></small></td>
  *          <td></td>
  *        </tr>
  *      </table>
