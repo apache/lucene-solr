11eb6a814ec2abc0f707c9a310f36f8ad78ef43a
Chris M. Hostetter <hossman@apache.org>
2012-03-27 17:50:36 +0000

    SOLR-3046: Fix whitespace typo in DIH response "Time taken" (merge r1305929 from trunk)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_3x@1305930 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/dataimporthandler/src/java/org/apache/solr/handler/dataimport/DocBuilder.java
+++ solr/contrib/dataimporthandler/src/java/org/apache/solr/handler/dataimport/DocBuilder.java
@@ -257,7 +257,7 @@ public class DocBuilder {
 	    if(importStatistics.failedDocCount.get() > 0)
 	      statusMessages.put(DataImporter.MSG.TOTAL_FAILED_DOCS, ""+ importStatistics.failedDocCount.get());
 	
-	    statusMessages.put("Time taken ", getTimeElapsedSince(startTime.get()));
+	    statusMessages.put("Time taken", getTimeElapsedSince(startTime.get()));
 	    LOG.info("Time taken = " + getTimeElapsedSince(startTime.get()));
 	  } catch(Exception e)
 		{
