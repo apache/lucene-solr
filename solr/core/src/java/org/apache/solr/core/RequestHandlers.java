a392193e5855d2f92606843abaeab4bfb81f7709
Mark Robert Miller <markrmiller@apache.org>
2012-03-28 13:38:05 +0000

    revert 	dsmiley commit  SOLR-3161
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1306318 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/core/RequestHandlers.java
+++ solr/core/src/java/org/apache/solr/core/RequestHandlers.java
@@ -41,6 +41,7 @@ import org.slf4j.LoggerFactory;
 public final class RequestHandlers {
   public static Logger log = LoggerFactory.getLogger(RequestHandlers.class);
 
+  public static final String DEFAULT_HANDLER_NAME="standard";
   protected final SolrCore core;
   // Use a synchronized map - since the handlers can be changed at runtime, 
   // the map implementation should be thread safe
@@ -181,7 +182,7 @@ public final class RequestHandlers {
       }
     }
 
-    if(get("") == null) register("", get("/select"));//defacto default handler
+    if(get("") == null) register("", get(DEFAULT_HANDLER_NAME));
   }
     
 
