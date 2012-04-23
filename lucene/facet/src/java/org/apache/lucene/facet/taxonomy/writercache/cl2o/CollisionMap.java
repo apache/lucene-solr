72a7a234ab5357379540bb43edb03ab6fe0e9d70
Robert Muir <rmuir@apache.org>
2012-04-23 02:41:27 +0000

    fix broken jdoc links: these ctors are already effectively pkg-private since CharBlockArray is pkg-private
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1329040 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/facet/src/java/org/apache/lucene/facet/taxonomy/writercache/cl2o/CollisionMap.java
+++ lucene/facet/src/java/org/apache/lucene/facet/taxonomy/writercache/cl2o/CollisionMap.java
@@ -54,11 +54,11 @@ public class CollisionMap {
 
   private Entry[] entries;
 
-  public CollisionMap(CharBlockArray labelRepository) {
+  CollisionMap(CharBlockArray labelRepository) {
     this(16 * 1024, 0.75f, labelRepository);
   }
 
-  public CollisionMap(int initialCapacity, CharBlockArray labelRepository) {
+  CollisionMap(int initialCapacity, CharBlockArray labelRepository) {
     this(initialCapacity, 0.75f, labelRepository);
   }
 
