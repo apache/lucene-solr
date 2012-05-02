84cebdea789566ba153578456c58361a2baa898b
Robert Muir <rmuir@apache.org>
2012-05-02 00:10:55 +0000

    LUCENE-3880: add backwards test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1332886 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/analysis/common/src/test/org/apache/lucene/analysis/core/TestUAX29URLEmailTokenizer.java
+++ lucene/analysis/common/src/test/org/apache/lucene/analysis/core/TestUAX29URLEmailTokenizer.java
@@ -470,6 +470,21 @@ public class TestUAX29URLEmailTokenizer extends BaseTokenStreamTestCase {
     checkOneTerm(a, "壹゙", "壹"); // ideographic Bug
     checkOneTerm(a, "아゙",  "아゙"); // hangul Works
   }
+  
+  // LUCENE-3880
+  /** @deprecated remove this and sophisticated backwards layer in 5.0 */
+  @Deprecated
+  public void testMailtoBackwards()  throws Exception {
+    Analyzer a = new Analyzer() {
+      @Override
+      protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
+        Tokenizer tokenizer = new UAX29URLEmailTokenizer(Version.LUCENE_34, reader);
+        return new TokenStreamComponents(tokenizer);
+      }
+    };
+    assertAnalyzesTo(a, "mailto:test@example.org",
+        new String[] { "mailto:test", "example.org" });
+  }
 
   /** blast some random strings through the analyzer */
   public void testRandomStrings() throws Exception {
