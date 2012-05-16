e542b9b3c76a8200c129633fd9bd5f4323ebb761
Erik Hatcher <ehatcher@apache.org>
2012-05-16 21:24:02 +0000

    Fix typo
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1339373 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/util/FileUtils.java
+++ solr/core/src/java/org/apache/solr/util/FileUtils.java
@@ -29,9 +29,9 @@ public class FileUtils {
    * Resolves a path relative a base directory.
    *
    * <p>
-   * This method does what "new File(base,path)" <b>Should</b> do, it wasn't
+   * This method does what "new File(base,path)" <b>Should</b> do, if it wasn't
    * completely lame: If path is absolute, then a File for that path is returned;
-   * if it's not absoluve, then a File is returnd using "path" as a child 
+   * if it's not absolute, then a File is returned using "path" as a child
    * of "base")
    * </p>
    */
