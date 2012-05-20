8945e4b2ce2a66b01c1016628d91c527478e642c
Michael McCandless <mikemccand@apache.org>
2012-05-20 13:41:33 +0000

    don't excessively call RateLimiter in tests
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1340713 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/store/MockIndexOutputWrapper.java
+++ lucene/test-framework/src/java/org/apache/lucene/store/MockIndexOutputWrapper.java
@@ -78,7 +78,7 @@ public class MockIndexOutputWrapper extends IndexOutput {
     long freeSpace = dir.maxSize == 0 ? 0 : dir.maxSize - dir.sizeInBytes();
     long realUsage = 0;
 
-    if (dir.rateLimiter != null) {
+    if (dir.rateLimiter != null && len >= 10) {
       dir.rateLimiter.pause(len);
     }
 
