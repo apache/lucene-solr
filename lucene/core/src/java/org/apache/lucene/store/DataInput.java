ef64958caa79f3be9f20d6c1de02cddccbe50e64
Robert Muir <rmuir@apache.org>
2012-05-28 21:10:08 +0000

    workaround javadocs bug in newer 1.6 versions which generates invalid html if you link to a method that takes generics
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1343391 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/store/DataInput.java
+++ lucene/core/src/java/org/apache/lucene/store/DataInput.java
@@ -204,7 +204,7 @@ public abstract class DataInput implements Cloneable {
   }
 
   /** Reads a Map&lt;String,String&gt; previously written
-   *  with {@link DataOutput#writeStringStringMap}. */
+   *  with {@link DataOutput#writeStringStringMap(Map)}. */
   public Map<String,String> readStringStringMap() throws IOException {
     final Map<String,String> map = new HashMap<String,String>();
     final int count = readInt();
@@ -218,7 +218,7 @@ public abstract class DataInput implements Cloneable {
   }
 
   /** Reads a Set&lt;String&gt; previously written
-   *  with {@link DataOutput#writeStringSet}. */
+   *  with {@link DataOutput#writeStringSet(Set)}. */
   public Set<String> readStringSet() throws IOException {
     final Set<String> set = new HashSet<String>();
     final int count = readInt();
