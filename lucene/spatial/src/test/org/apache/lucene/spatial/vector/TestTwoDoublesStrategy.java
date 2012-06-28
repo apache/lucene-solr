c4b08cf35c48ad706a8c8a573ea7509b4a260c01
Christopher John Male <chrism@apache.org>
2012-06-28 02:51:51 +0000

    LUCENE-4166: Fixed bad type checking of Circle shapes in TwoDoublesStrategy
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1354801 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/spatial/src/test/org/apache/lucene/spatial/vector/TestTwoDoublesStrategy.java
+++ lucene/spatial/src/test/org/apache/lucene/spatial/vector/TestTwoDoublesStrategy.java
@@ -18,7 +18,15 @@
 package org.apache.lucene.spatial.vector;
 
 import com.spatial4j.core.context.simple.SimpleSpatialContext;
+import com.spatial4j.core.exception.InvalidShapeException;
+import com.spatial4j.core.query.SpatialArgs;
+import com.spatial4j.core.query.SpatialOperation;
+import com.spatial4j.core.shape.Circle;
+import com.spatial4j.core.shape.Point;
+import com.spatial4j.core.shape.simple.CircleImpl;
+import com.spatial4j.core.shape.simple.PointImpl;
 import org.apache.lucene.search.FieldCache;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.spatial.SpatialMatchConcern;
 import org.apache.lucene.spatial.StrategyTestCase;
 import org.apache.lucene.spatial.util.NumericFieldInfo;
@@ -37,6 +45,22 @@ public class TestTwoDoublesStrategy extends StrategyTestCase<TwoDoublesFieldInfo
     this.strategy = new TwoDoublesStrategy(ctx,
         new NumericFieldInfo(), FieldCache.NUMERIC_UTILS_DOUBLE_PARSER);
     this.fieldInfo = new TwoDoublesFieldInfo(getClass().getSimpleName());
+  }
+
+  @Test
+  public void testCircleShapeSupport() {
+    Circle circle = new CircleImpl(new PointImpl(0, 0), 10, this.ctx);
+    SpatialArgs args = new SpatialArgs(SpatialOperation.Intersects, circle);
+    Query query = this.strategy.makeQuery(args, this.fieldInfo);
+
+    assertNotNull(query);
+  }
+
+  @Test(expected = InvalidShapeException.class)
+  public void testInvalidQueryShape() {
+    Point point = new PointImpl(0, 0);
+    SpatialArgs args = new SpatialArgs(SpatialOperation.Intersects, point);
+    this.strategy.makeQuery(args, this.fieldInfo);
   }
 
   @Test
