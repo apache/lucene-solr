cb90a00057255c8a7bdb96b3f0bb56eb86968ba1
Mark Robert Miller <markrmiller@apache.org>
2012-08-09 19:23:24 +0000

    fix test msg
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1371403 13f79535-47bb-0310-9956-ffa450edef68

--- solr/test-framework/src/java/org/apache/solr/cloud/AbstractFullDistribZkTestBase.java
+++ solr/test-framework/src/java/org/apache/solr/cloud/AbstractFullDistribZkTestBase.java
@@ -757,7 +757,7 @@ public abstract class AbstractFullDistribZkTestBase extends AbstractDistribZkTes
         zkStateReader.getClusterState().getSlice(DEFAULT_COLLECTION, shard)
             .getShards().size(), solrJetties.size());
 
-    SolrServer lastClient = null;
+    CloudJettyRunner lastJetty = null;
     for (CloudJettyRunner cjetty : solrJetties) {
       ZkNodeProps props = cjetty.info;
       if (verbose) System.err.println("client" + cnt++);
@@ -790,7 +790,7 @@ public abstract class AbstractFullDistribZkTestBase extends AbstractDistribZkTes
           ZkStateReader.ACTIVE);
       if (active && live) {
         if (lastNum > -1 && lastNum != num && failMessage == null) {
-          failMessage = shard + " is not consistent.  Got " + lastNum + " from " + lastClient + "lastClient"
+          failMessage = shard + " is not consistent.  Got " + lastNum + " from " + lastJetty.url + "lastClient"
               + " and got " + num + " from " + cjetty.url;
 
           if (verbose || true) {
@@ -801,15 +801,15 @@ public abstract class AbstractFullDistribZkTestBase extends AbstractDistribZkTes
             query.set("rows","1000");
             query.set("sort","id asc");
 
-            SolrDocumentList lst1 = lastClient.query(query).getResults();
+            SolrDocumentList lst1 = lastJetty.client.solrClient.query(query).getResults();
             SolrDocumentList lst2 = cjetty.client.solrClient.query(query).getResults();
 
-            showDiff(lst1, lst2, lastClient.toString(), cjetty.client.solrClient.toString());
+            showDiff(lst1, lst2, lastJetty.toString(), cjetty.client.solrClient.toString());
           }
 
         }
         lastNum = num;
-        lastClient = cjetty.client.solrClient;
+        lastJetty = cjetty;
       }
     }
     return failMessage;
