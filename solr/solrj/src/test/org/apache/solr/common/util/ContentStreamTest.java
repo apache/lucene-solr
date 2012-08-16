2a83b58506dd3fe139c7fa7a680ef025b9771e0d
Robert Muir <rmuir@apache.org>
2012-08-16 19:24:31 +0000

    use assertEquals so we will see whats going on when this fails
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1373994 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/common/util/ContentStreamTest.java
+++ solr/solrj/src/test/org/apache/solr/common/util/ContentStreamTest.java
@@ -24,6 +24,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.StringReader;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
@@ -110,6 +111,12 @@ public class ContentStreamTest extends LuceneTestCase
       charset = ContentStreamBase.DEFAULT_CHARSET;
     // Re-open the stream and this time use a reader
     stream = new ContentStreamBase.URLStream( url );
-    assertTrue( IOUtils.contentEquals( new StringReader(new String(content, charset)), stream.getReader() ) );
+    StringBuilder sb = new StringBuilder();
+    Reader reader = stream.getReader();
+    int ch;
+    while ((ch = reader.read()) > 0) {
+      sb.append((char)ch);
+    }
+    assertEquals(new String(content, charset), sb.toString());
   }
 }
