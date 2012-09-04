fb93d0e0a3ccb87f4c193391cad8597cce716591
Robert Muir <rmuir@apache.org>
2012-09-04 19:18:41 +0000

    LUCENE-4218: restore stringValue for numeric fields
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1380819 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/document/TestDocument.java
+++ lucene/core/src/test/org/apache/lucene/document/TestDocument.java
@@ -359,4 +359,25 @@ public class TestDocument extends LuceneTestCase {
     r.close();
     dir.close();
   }
+  
+  public void testNumericFieldAsString() throws Exception {
+    Document doc = new Document();
+    doc.add(new IntField("int", 5, Field.Store.YES));
+    assertEquals("5", doc.get("int"));
+    assertNull(doc.get("somethingElse"));
+    doc.add(new IntField("int", 4, Field.Store.YES));
+    assertArrayEquals(new String[] { "5", "4" }, doc.getValues("int"));
+    
+    Directory dir = newDirectory();
+    RandomIndexWriter iw = new RandomIndexWriter(random(), dir);
+    iw.addDocument(doc);
+    DirectoryReader ir = iw.getReader();
+    Document sdoc = ir.document(0);
+    assertEquals("5", sdoc.get("int"));
+    assertNull(sdoc.get("somethingElse"));
+    assertArrayEquals(new String[] { "5", "4" }, sdoc.getValues("int"));
+    ir.close();
+    iw.close();
+    dir.close();
+  }
 }
