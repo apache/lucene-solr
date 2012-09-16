9d7309c1349160dc897effab6f31510f6ad17a27
Michael McCandless <mikemccand@apache.org>
2012-09-16 12:11:09 +0000

    add missing comma
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1385249 13f79535-47bb-0310-9956-ffa450edef68

--- dev-tools/scripts/smokeTestRelease.py
+++ dev-tools/scripts/smokeTestRelease.py
@@ -441,7 +441,7 @@ def verifyUnpacked(project, artifact, unpackPath, version, tmpDir):
 
   if project == 'lucene':
     # TODO: clean this up to not be a list of modules that we must maintain
-    extras = ('analysis', 'benchmark', 'classification' 'codecs', 'core', 'demo', 'docs', 'facet', 'grouping', 'highlighter', 'join', 'memory', 'misc', 'queries', 'queryparser', 'sandbox', 'spatial', 'suggest', 'test-framework', 'licenses')
+    extras = ('analysis', 'benchmark', 'classification', 'codecs', 'core', 'demo', 'docs', 'facet', 'grouping', 'highlighter', 'join', 'memory', 'misc', 'queries', 'queryparser', 'sandbox', 'spatial', 'suggest', 'test-framework', 'licenses')
     if isSrc:
       extras += ('build.xml', 'common-build.xml', 'module-build.xml', 'ivy-settings.xml', 'backwards', 'tools', 'site')
   else:
