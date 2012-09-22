e1f27df522d3b419403f5dd2edd1c08b99f81785
Michael McCandless <mikemccand@apache.org>
2012-09-22 17:28:11 +0000

    port hack to 4x branch
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1388849 13f79535-47bb-0310-9956-ffa450edef68

--- dev-tools/scripts/smokeTestRelease.py
+++ dev-tools/scripts/smokeTestRelease.py
@@ -982,6 +982,9 @@ def verifyArtifactPerPOMtemplate(POMtemplates, artifacts, tmpDir, version):
   xpathSkipConfiguration = '{0}configuration/{0}skip'.format(namespace)
   for project in ('lucene', 'solr'):
     for POMtemplate in POMtemplates[project]:
+      if not POMtemplate.endswith('.xml.template'):
+        continue
+      print('      Checking POM template %s' % POMtemplate)
       treeRoot = ET.parse(POMtemplate).getroot()
       skipDeploy = False
       for plugin in treeRoot.findall(xpathPlugin):
