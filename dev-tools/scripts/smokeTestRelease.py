57add184378a7dc7a3cf658f84162f731f89ddd7
Michael McCandless <mikemccand@apache.org>
2012-09-25 21:38:20 +0000

    LUCENE-4429: smokeTestRelease requires version is format X.Y.Z[-ALPHA|-BETA]
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1390165 13f79535-47bb-0310-9956-ffa450edef68

--- dev-tools/scripts/smokeTestRelease.py
+++ dev-tools/scripts/smokeTestRelease.py
@@ -1106,6 +1106,8 @@ def crawl(downloadedFiles, urlString, targetDir, exclusions=set()):
         downloadedFiles.append(path)
         sys.stdout.write('.')
 
+reAllowedVersion = re.compile(r'^\d+\.\d+\.\d+(-ALPHA|-BETA)?$')
+
 def main():
 
   if len(sys.argv) < 4:
@@ -1116,6 +1118,10 @@ def main():
 
   baseURL = sys.argv[1]
   version = sys.argv[2]
+
+  if not reAllowedVersion.match(version):
+    raise RuntimeError('version "%s" does not match format X.Y.Z[-ALPHA|-BETA]' % version)
+  
   tmpDir = os.path.abspath(sys.argv[3])
   isSigned = True 
   if len(sys.argv) == 5:
