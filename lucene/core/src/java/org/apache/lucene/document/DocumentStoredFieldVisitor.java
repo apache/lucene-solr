7145779e4dc3cb9d9d73e57566eb98d5c177b2a7
Adrien Grand <jpountz@apache.org>
2012-09-25 13:07:05 +0000

    LUCENE-4423: DocumentStoredFieldVisitor.binaryField ignores offset and length.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1389850 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/document/DocumentStoredFieldVisitor.java
+++ lucene/core/src/java/org/apache/lucene/document/DocumentStoredFieldVisitor.java
@@ -59,7 +59,7 @@ public class DocumentStoredFieldVisitor extends StoredFieldVisitor {
 
   @Override
   public void binaryField(FieldInfo fieldInfo, byte[] value, int offset, int length) throws IOException {
-    doc.add(new StoredField(fieldInfo.name, value));
+    doc.add(new StoredField(fieldInfo.name, value, offset, length));
   }
 
   @Override
