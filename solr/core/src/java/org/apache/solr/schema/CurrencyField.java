11b4d43627d47d5c6e221659ede691f6912b6ab1
Jan Høydahl <janhoy@apache.org>
2012-09-25 00:03:27 +0000

    SOLR-3878: NPE in CurrencyValue.parse() while issuing wildcard range query on a CurrencyField
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1389659 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/schema/CurrencyField.java
+++ solr/core/src/java/org/apache/solr/schema/CurrencyField.java
@@ -227,7 +227,7 @@ public class CurrencyField extends FieldType implements SchemaAware, ResourceLoa
       final CurrencyValue p1 = CurrencyValue.parse(part1, defaultCurrency);
       final CurrencyValue p2 = CurrencyValue.parse(part2, defaultCurrency);
 
-      if (!p1.getCurrencyCode().equals(p2.getCurrencyCode())) {
+      if (p1 != null && p2 != null && !p1.getCurrencyCode().equals(p2.getCurrencyCode())) {
           throw new SolrException(SolrException.ErrorCode.BAD_REQUEST,
                   "Cannot parse range query " + part1 + " to " + part2 +
                           ": range queries only supported when upper and lower bound have same currency.");
@@ -237,11 +237,12 @@ public class CurrencyField extends FieldType implements SchemaAware, ResourceLoa
   }
 
   public Query getRangeQuery(QParser parser, SchemaField field, final CurrencyValue p1, final CurrencyValue p2, final boolean minInclusive, final boolean maxInclusive) {
-    String currencyCode = p1.getCurrencyCode();
+    String currencyCode = (p1 != null) ? p1.getCurrencyCode() :
+                          (p2 != null) ? p2.getCurrencyCode() : defaultCurrency;
     final CurrencyValueSource vs = new CurrencyValueSource(field, currencyCode, parser);
 
     return new SolrConstantScoreQuery(new ValueSourceRangeFilter(vs,
-            p1.getAmount() + "", p2.getAmount() + "", minInclusive, maxInclusive));
+            p1 == null ? null : p1.getAmount() + "" , p2 == null ? null : p2.getAmount() + "", minInclusive, maxInclusive));
   }
 
   @Override
@@ -659,6 +660,9 @@ class CurrencyValue {
    * @return The parsed CurrencyValue.
    */
   public static CurrencyValue parse(String externalVal, String defaultCurrency) {
+    if (externalVal == null) {
+      return null;
+    }
     String amount = externalVal;
     String code = defaultCurrency;
 
@@ -668,6 +672,10 @@ class CurrencyValue {
       code = amountAndCode[1];
     }
 
+    if (amount.equals("*")) {
+      return null;
+    }
+    
     Currency currency = java.util.Currency.getInstance(code);
 
     if (currency == null) {
