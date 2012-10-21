dc49949558d776f3059896438b936d182f8dba33
Michael McCandless <mikemccand@apache.org>
2012-10-21 12:59:25 +0000

    LUCENE-4481: add back some optos
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1400634 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/suggest/src/test/org/apache/lucene/search/suggest/analyzing/AnalyzingSuggesterTest.java
+++ lucene/suggest/src/test/org/apache/lucene/search/suggest/analyzing/AnalyzingSuggesterTest.java
@@ -17,6 +17,11 @@ package org.apache.lucene.search.suggest.analyzing;
  * limitations under the License.
  */
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
@@ -824,6 +829,29 @@ public class AnalyzingSuggesterTest extends LuceneTestCase {
     assertEquals(4, results.get(1).value);
     assertEquals("a b", results.get(2).key);
     assertEquals(3, results.get(2).value);
+
+    // Try again after save/load:
+    File tmpDir = _TestUtil.getTempDir("AnalyzingSuggesterTest");
+    tmpDir.mkdir();
+
+    File path = new File(tmpDir, "suggester");
+
+    OutputStream os = new FileOutputStream(path);
+    suggester.store(os);
+    os.close();
+
+    InputStream is = new FileInputStream(path);
+    suggester.load(is);
+    is.close();
+
+    results = suggester.lookup("a", false, 3);
+    assertEquals(3, results.size());
+    assertEquals("a", results.get(0).key);
+    assertEquals(5, results.get(0).value);
+    assertEquals("a c", results.get(1).key);
+    assertEquals(4, results.get(1).value);
+    assertEquals("a b", results.get(2).key);
+    assertEquals(3, results.get(2).value);
   }
 
   public void testDupSurfaceFormsMissingResults() throws Exception {
@@ -858,6 +886,27 @@ public class AnalyzingSuggesterTest extends LuceneTestCase {
         }));
 
     List<LookupResult> results = suggester.lookup("nellie", false, 2);
+    assertEquals(2, results.size());
+    assertEquals("hambone", results.get(0).key);
+    assertEquals(6, results.get(0).value);
+    assertEquals("nellie", results.get(1).key);
+    assertEquals(5, results.get(1).value);
+
+    // Try again after save/load:
+    File tmpDir = _TestUtil.getTempDir("AnalyzingSuggesterTest");
+    tmpDir.mkdir();
+
+    File path = new File(tmpDir, "suggester");
+
+    OutputStream os = new FileOutputStream(path);
+    suggester.store(os);
+    os.close();
+
+    InputStream is = new FileInputStream(path);
+    suggester.load(is);
+    is.close();
+
+    results = suggester.lookup("nellie", false, 2);
     assertEquals(2, results.size());
     assertEquals("hambone", results.get(0).key);
     assertEquals(6, results.get(0).value);
@@ -907,6 +956,27 @@ public class AnalyzingSuggesterTest extends LuceneTestCase {
         }));
 
     List<LookupResult> results = suggester.lookup("a", false, 2);
+    assertEquals(2, results.size());
+    assertEquals("a", results.get(0).key);
+    assertEquals(6, results.get(0).value);
+    assertEquals("b", results.get(1).key);
+    assertEquals(5, results.get(1).value);
+
+    // Try again after save/load:
+    File tmpDir = _TestUtil.getTempDir("AnalyzingSuggesterTest");
+    tmpDir.mkdir();
+
+    File path = new File(tmpDir, "suggester");
+
+    OutputStream os = new FileOutputStream(path);
+    suggester.store(os);
+    os.close();
+
+    InputStream is = new FileInputStream(path);
+    suggester.load(is);
+    is.close();
+
+    results = suggester.lookup("a", false, 2);
     assertEquals(2, results.size());
     assertEquals("a", results.get(0).key);
     assertEquals(6, results.get(0).value);
