a61c23fb95d4706f73cddc4caaad4b53bf381cc7
Mark Robert Miller <markrmiller@apache.org>
2012-10-27 15:54:23 +0000

    wait 30 seconds rather than 15 in this test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1402813 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/SyncSliceTest.java
+++ solr/core/src/test/org/apache/solr/cloud/SyncSliceTest.java
@@ -162,7 +162,7 @@ public class SyncSliceTest extends AbstractFullDistribZkTestBase {
     // to talk to a downed node causes grief
     waitToSeeDownInClusterState(leaderJetty, jetties);
 
-    waitForThingsToLevelOut(15);
+    waitForThingsToLevelOut(30);
     
     checkShardConsistency(false, true);
     
