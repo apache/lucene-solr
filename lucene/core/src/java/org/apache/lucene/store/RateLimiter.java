03f46fe4ede3e0bfe24723305b9b31af99ea80fb
Simon Willnauer <simonw@apache.org>
2012-11-08 21:49:53 +0000

    revert last commit
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1407281 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/store/RateLimiter.java
+++ lucene/core/src/java/org/apache/lucene/store/RateLimiter.java
@@ -19,102 +19,75 @@ package org.apache.lucene.store;
 
 import org.apache.lucene.util.ThreadInterruptedException;
 
-/** Abstract base class to rate limit IO.  Typically implementations are
- *  shared across multiple IndexInputs or IndexOutputs (for example
+/** Simple class to rate limit IO.  Typically it's shared
+ *  across multiple IndexInputs or IndexOutputs (for example
  *  those involved all merging).  Those IndexInputs and
  *  IndexOutputs would call {@link #pause} whenever they
  *  want to read bytes or write bytes. */
-public abstract class RateLimiter {
+
+public class RateLimiter {
+  private volatile double mbPerSec;
+  private volatile double nsPerByte;
+  private volatile long lastNS;
+
+  // TODO: we could also allow eg a sub class to dynamically
+  // determine the allowed rate, eg if an app wants to
+  // change the allowed rate over time or something
+
+  /** mbPerSec is the MB/sec max IO rate */
+  public RateLimiter(double mbPerSec) {
+    setMbPerSec(mbPerSec);
+  }
 
   /**
    * Sets an updated mb per second rate limit.
    */
-  public abstract void setMbPerSec(double mbPerSec);
+  public void setMbPerSec(double mbPerSec) {
+    this.mbPerSec = mbPerSec;
+    nsPerByte = 1000000000. / (1024*1024*mbPerSec);
+  }
+
   /**
    * The current mb per second rate limit.
    */
-  public abstract double getMbPerSec();
-  
+  public double getMbPerSec() {
+    return this.mbPerSec;
+  }
+
   /** Pauses, if necessary, to keep the instantaneous IO
-   *  rate at or below the target. 
-   *  <p>
-   *  Note: the implementation is thread-safe
-   *  </p>
-   *  @return the pause time in nano seconds 
-   * */
-  public abstract long pause(long bytes);
-  
-  /**
-   * Simple class to rate limit IO.
-   */
-  public static class SimpleRateLimiter extends RateLimiter {
-    private volatile double mbPerSec;
-    private volatile double nsPerByte;
-    private volatile long lastNS;
-
-    // TODO: we could also allow eg a sub class to dynamically
-    // determine the allowed rate, eg if an app wants to
-    // change the allowed rate over time or something
-
-    /** mbPerSec is the MB/sec max IO rate */
-    public SimpleRateLimiter(double mbPerSec) {
-      setMbPerSec(mbPerSec);
+   *  rate at or below the target. NOTE: multiple threads
+   *  may safely use this, however the implementation is
+   *  not perfectly thread safe but likely in practice this
+   *  is harmless (just means in some rare cases the rate
+   *  might exceed the target).  It's best to call this
+   *  with a biggish count, not one byte at a time. */
+  public void pause(long bytes) {
+    if (bytes == 1) {
+      return;
     }
 
-    /**
-     * Sets an updated mb per second rate limit.
-     */
-    public void setMbPerSec(double mbPerSec) {
-      this.mbPerSec = mbPerSec;
-      nsPerByte = 1000000000. / (1024*1024*mbPerSec);
-      
+    // TODO: this is purely instantaneous rate; maybe we
+    // should also offer decayed recent history one?
+    final long targetNS = lastNS = lastNS + ((long) (bytes * nsPerByte));
+    long curNS = System.nanoTime();
+    if (lastNS < curNS) {
+      lastNS = curNS;
     }
 
-    /**
-     * The current mb per second rate limit.
-     */
-    public double getMbPerSec() {
-      return this.mbPerSec;
-    }
-    
-    /** Pauses, if necessary, to keep the instantaneous IO
-     *  rate at or below the target. NOTE: multiple threads
-     *  may safely use this, however the implementation is
-     *  not perfectly thread safe but likely in practice this
-     *  is harmless (just means in some rare cases the rate
-     *  might exceed the target).  It's best to call this
-     *  with a biggish count, not one byte at a time.
-     *  @return the pause time in nano seconds 
-     * */
-    public long pause(long bytes) {
-      if (bytes == 1) {
-        return 0;
-      }
-
-      // TODO: this is purely instantaneous rate; maybe we
-      // should also offer decayed recent history one?
-      final long targetNS = lastNS = lastNS + ((long) (bytes * nsPerByte));
-      long curNS = System.nanoTime();
-      if (lastNS < curNS) {
-        lastNS = curNS;
-      }
-
-      // While loop because Thread.sleep doesn't always sleep
-      // enough:
-      while(true) {
-        final long pauseNS = targetNS - curNS;
-        if (pauseNS > 0) {
-          try {
-            Thread.sleep((int) (pauseNS/1000000), (int) (pauseNS % 1000000));
-          } catch (InterruptedException ie) {
-            throw new ThreadInterruptedException(ie);
-          }
-          curNS = System.nanoTime();
-          continue;
+    // While loop because Thread.sleep doesn't always sleep
+    // enough:
+    while(true) {
+      final long pauseNS = targetNS - curNS;
+      if (pauseNS > 0) {
+        try {
+          Thread.sleep((int) (pauseNS/1000000), (int) (pauseNS % 1000000));
+        } catch (InterruptedException ie) {
+          throw new ThreadInterruptedException(ie);
         }
-        break;
+        curNS = System.nanoTime();
+        continue;
       }
-      return targetNS;
+      break;
     }
   }
 }
