1cf528b713ea20cd50efc589b3da1bc6bd6ef481
Mark Robert Miller <markrmiller@apache.org>
2012-11-20 18:51:00 +0000

    add to error msg
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1411792 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/cloud/ZkController.java
+++ solr/core/src/java/org/apache/solr/cloud/ZkController.java
@@ -1018,7 +1018,7 @@ public final class ZkController {
     }
     
     throw new SolrException(ErrorCode.SERVER_ERROR,
-        "Could not get shard_id for core: " + coreName);
+        "Could not get shard_id for core: " + coreName + " coreNodeName:" + shardZkNodeName);
   }
   
   public static void uploadToZK(SolrZkClient zkClient, File dir, String zkPath) throws IOException, KeeperException, InterruptedException {
