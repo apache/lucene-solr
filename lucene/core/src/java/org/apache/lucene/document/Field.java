078623d375f081805cfe74f7f367a945e2575cf9
Michael McCandless <mikemccand@apache.org>
2012-12-12 20:29:59 +0000

    fix exc message
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1420952 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/document/Field.java
+++ lucene/core/src/java/org/apache/lucene/document/Field.java
@@ -347,7 +347,7 @@ public class Field implements IndexableField, StorableField {
       throw new IllegalArgumentException("cannot change value type from " + fieldsData.getClass().getSimpleName() + " to BytesRef");
     }
     if (type.indexed()) {
-      throw new IllegalArgumentException("cannot set a Reader value on an indexed field");
+      throw new IllegalArgumentException("cannot set a BytesRef value on an indexed field");
     }
     fieldsData = value;
   }
