d819b9c547595c2db8b640161e64e40e9805cd33
Robert Muir <rmuir@apache.org>
2012-12-13 22:58:00 +0000

    dont revert my changes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1421606 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/util/TestRuleSetupAndRestoreClassEnv.java
+++ lucene/test-framework/src/java/org/apache/lucene/util/TestRuleSetupAndRestoreClassEnv.java
@@ -200,6 +200,9 @@ final class TestRuleSetupAndRestoreClassEnv extends AbstractBeforeAfterRule {
           Arrays.toString(avoidCodecs.toArray()));
       throw e;
     }
+    if (Constants.JAVA_VENDOR.startsWith("IBM")) {
+      throw new AssumptionViolatedException("refusing to execute on broken jvm");
+    }
   }
 
   /**
