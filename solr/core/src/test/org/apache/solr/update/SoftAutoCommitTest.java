db517c4bb8c9734b1c07c886f5f0ba26da185d83
Mark Robert Miller <markrmiller@apache.org>
2012-12-21 16:27:40 +0000

    tests: more fudge
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1425004 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/update/SoftAutoCommitTest.java
+++ solr/core/src/test/org/apache/solr/update/SoftAutoCommitTest.java
@@ -235,7 +235,7 @@ public class SoftAutoCommitTest extends AbstractSolrTestCase {
     monitor.assertSaneOffers();
 
     // Wait for the soft commit with some fudge
-    soft529 = monitor.soft.poll(softCommitWaitMillis * 2, MILLISECONDS);
+    soft529 = monitor.soft.poll(softCommitWaitMillis * 3, MILLISECONDS);
     assertNotNull("soft529 wasn't fast enough", soft529);
     monitor.assertSaneOffers();
  
