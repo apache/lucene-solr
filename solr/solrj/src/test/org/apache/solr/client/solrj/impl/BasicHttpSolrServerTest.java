a500c91ca90385caae94021997e642dd4060d1dc
Robert Muir <rmuir@apache.org>
2012-12-27 00:49:44 +0000

    hack broken test to not waste hours of jenkins time
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1426097 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/client/solrj/impl/BasicHttpSolrServerTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/impl/BasicHttpSolrServerTest.java
@@ -124,20 +124,25 @@ public class BasicHttpSolrServerTest extends SolrJettyTestBase {
         .addServletWithMapping(DebugServlet.class, "/debug/*");
   }
   
+  // what is this actually testing? this test WILL randomly fail.
+  // not a good unit test!
   @Test
   public void testConnectionRefused() throws MalformedURLException {
     int unusedPort = findUnusedPort(); // XXX even if fwe found an unused port
                                        // it might not be unused anymore
     HttpSolrServer server = new HttpSolrServer("http://127.0.0.1:" + unusedPort
         + "/solr");
+    server.setConnectionTimeout(500);
     SolrQuery q = new SolrQuery("*:*");
     try {
       QueryResponse response = server.query(q);
       fail("Should have thrown an exception.");
     } catch (SolrServerException e) {
+      assumeFalse("blackholed!", e.getMessage().contains("IOException occured when talking to server"));
       assertTrue(e.getMessage().contains("refused"));
+    } finally {
+      server.shutdown();
     }
-    server.shutdown();
   }
   
   @Test
