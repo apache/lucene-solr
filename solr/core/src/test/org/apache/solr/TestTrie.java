ad11f0fde8e8042c0e696e552605fcc9fb04f26f
Uwe Schindler <uschindler@apache.org>
2013-01-05 22:45:10 +0000

    Merged revision(s) 1429410 from lucene/dev/trunk: SOLR-4275: Add test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1429411 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/TestTrie.java
+++ solr/core/src/test/org/apache/solr/TestTrie.java
@@ -16,6 +16,7 @@
  */
 package org.apache.solr;
 
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.schema.DateField;
 import org.apache.solr.schema.FieldType;
@@ -46,6 +47,38 @@ public class TestTrie extends SolrTestCaseJ4 {
   public void tearDown() throws Exception {
     clearIndex();
     super.tearDown();
+  }
+  
+  @Test
+  public void testTokenizer() throws Exception {
+    FieldType type = h.getCore().getSchema().getFieldType("tint");
+    assertTrue(type instanceof TrieField);
+    
+    String value = String.valueOf(random().nextInt());
+    TokenStream ts = type.getAnalyzer().tokenStream("dummy", new StringReader(value));
+    OffsetAttribute ofsAtt = ts.addAttribute(OffsetAttribute.class);
+    ts.reset();
+    int count = 0;
+    while (ts.incrementToken()) {
+      count++;
+      assertEquals(0, ofsAtt.startOffset());
+      assertEquals(value.length(), ofsAtt.endOffset());
+    }
+    final int precStep = ((TrieType) type).getPrecisionStep();
+    assertEquals( (32 + precStep - 1) / precStep, count);
+    ts.end();
+    assertEquals(value.length(), ofsAtt.startOffset());
+    assertEquals(value.length(), ofsAtt.endOffset());
+    ts.close();
+    
+    // Test empty one:
+    ts = type.getAnalyzer().tokenStream("dummy", new StringReader(""));
+    ts.reset();
+    assertFalse(ts.incrementToken());
+    ts.end();
+    assertEquals(0, ofsAtt.startOffset());
+    assertEquals(0, ofsAtt.endOffset());
+    ts.close();    
   }
 
   @Test
