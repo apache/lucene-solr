74ad1e8d37281eb7f10e6335a5b84e0cc2f82f15
Steven Rowe <sarowe@apache.org>
2013-01-16 00:19:55 +0000

    cleaned up class javadoc
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1433761 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/misc/src/java/org/apache/lucene/misc/HighFreqTerms.java
+++ lucene/misc/src/java/org/apache/lucene/misc/HighFreqTerms.java
@@ -40,11 +40,13 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
- * 
  * <code>HighFreqTerms</code> class extracts the top n most frequent terms
- * (by document frequency ) from an existing Lucene index and reports their document frequencey.
- * If the -t flag is  and reports both their document frequency and their total tf (total number of occurences) 
- * in order of highest total tf
+ * (by document frequency) from an existing Lucene index and reports their
+ * document frequency.
+ * <p>
+ * If the -t flag is given, both document frequency and total tf (total
+ * number of occurrences) are reported, ordered by descending total tf.
+ *
  */
 public class HighFreqTerms {
   
