7af430e0716aebbd625ab8e85a7c115890d50b4c
Robert Muir <rmuir@apache.org>
2013-01-31 03:18:58 +0000

    fix this to be explicit about which locale it is (or isnt) using...
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1440836 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/util/SimplePostTool.java
+++ solr/core/src/java/org/apache/solr/util/SimplePostTool.java
@@ -190,16 +190,18 @@ public class SimplePostTool {
    * @param millis the time in milliseconds
    */
   private void displayTiming(long millis) {
+    // TODO: if the intent is user-display: this should use SimpleDateFormat 
+    // or similar instead of homemade formatting.
     long hours = millis / 3600000;
     long minutes = (millis / 60000) % 60;
     long seconds = (millis / 1000) % 60;
     long milliseconds = millis % 1000;
     if (hours>0) {
-      System.out.println(String.format("Time taken: %02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds));
+      System.out.println(String.format(Locale.getDefault(), "Time taken: %02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds));
     } else if (minutes>0) {
-      System.out.println(String.format("Time taken: %02d:%02d.%03d", minutes, seconds, milliseconds));
+      System.out.println(String.format(Locale.getDefault(), "Time taken: %02d:%02d.%03d", minutes, seconds, milliseconds));
     } else {
-      System.out.println(String.format("Time taken: %d.%03ds", seconds, milliseconds));
+      System.out.println(String.format(Locale.getDefault(), "Time taken: %d.%03ds", seconds, milliseconds));
     }
  }
 
