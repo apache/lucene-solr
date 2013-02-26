06a3b97a65cfa40fb36752ede42fd15c7efed965
Chris M. Hostetter <hossman@apache.org>
2013-02-26 18:34:12 +0000

    LUCENE-4801: lingering doc remnants from pre-4.0 similarity API (merge r1450342)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1450344 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/misc/src/java/org/apache/lucene/misc/SweetSpotSimilarity.java
+++ lucene/misc/src/java/org/apache/lucene/misc/SweetSpotSimilarity.java
@@ -21,20 +21,16 @@ import org.apache.lucene.search.similarities.DefaultSimilarity;
 import org.apache.lucene.index.FieldInvertState;
 
 /**
+ * <p>
  * A similarity with a lengthNorm that provides for a "plateau" of
  * equally good lengths, and tf helper functions.
- *
+ * </p>
  * <p>
- * For lengthNorm, A global min/max can be specified to define the
+ * For lengthNorm, A min/max can be specified to define the
  * plateau of lengths that should all have a norm of 1.0.
  * Below the min, and above the max the lengthNorm drops off in a
  * sqrt function.
  * </p>
- * <p>
- * A per field min/max can be specified if different fields have
- * different sweet spots.
- * </p>
- *
  * <p>
  * For tf, baselineTf and hyperbolicTf functions are provided, which
  * subclasses can choose between.
@@ -105,7 +101,8 @@ public class SweetSpotSimilarity extends DefaultSimilarity {
    * computeLengthNorm(numTokens) </code> where
    * numTokens does not count overlap tokens if
    * discountOverlaps is true by default or true for this
-   * specific field. */
+   * specific field. 
+   */
   @Override
   public float lengthNorm(FieldInvertState state) {
     final int numTokens;
