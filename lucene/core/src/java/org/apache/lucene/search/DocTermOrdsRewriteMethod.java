527999b8676621942359941afa262a6fc2cbc2da
Robert Muir <rmuir@apache.org>
2013-03-01 08:43:15 +0000

    add missing hashcode/equals to these rewrite methods
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1451525 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/search/DocTermOrdsRewriteMethod.java
+++ lucene/core/src/java/org/apache/lucene/search/DocTermOrdsRewriteMethod.java
@@ -163,4 +163,20 @@ public final class DocTermOrdsRewriteMethod extends MultiTermQuery.RewriteMethod
       };
     }
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return 877;
+  }
 }
