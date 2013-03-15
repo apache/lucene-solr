5b6b0776ef109c8dd09f2437700bd4460e274c84
Stefan Matheis <steffkes@apache.org>
2013-03-15 20:36:01 +0000

    SOLR-4311: Admin UI - Optimize Caching Behaviour
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1457099 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/servlet/LoadAdminUiServlet.java
+++ solr/core/src/java/org/apache/solr/servlet/LoadAdminUiServlet.java
@@ -30,6 +30,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.solr.core.CoreContainer;
+import org.apache.solr.core.SolrCore;
 
 /**
  * A simple servlet to load the Solr Admin UI
@@ -53,14 +54,17 @@ public final class LoadAdminUiServlet extends HttpServlet {
         Writer out = new OutputStreamWriter(response.getOutputStream(), "UTF-8");
 
         String html = IOUtils.toString(in, "UTF-8");
+        Package package = SolrCore.class.getPackage();
 
         String[] search = new String[] { 
             "${contextPath}", 
-            "${adminPath}" 
+            "${adminPath}",
+            "${version}" 
         };
         String[] replace = new String[] {
             StringEscapeUtils.escapeJavaScript(request.getContextPath()),
-            StringEscapeUtils.escapeJavaScript(cores.getAdminPath())
+            StringEscapeUtils.escapeJavaScript(cores.getAdminPath()),
+            StringEscapeUtils.escapeJavaScript(package.getSpecificationVersion())
         };
         
         out.write( StringUtils.replaceEach(html, search, replace) );
