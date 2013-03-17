83ac5bb7d3c3571970a335ae48eec42f0bb66de7
Mark Robert Miller <markrmiller@apache.org>
2013-03-17 14:40:27 +0000

    tests: higher timeout, more specific fail msg, remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1457456 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/BasicDistributedZk2Test.java
+++ solr/core/src/test/org/apache/solr/cloud/BasicDistributedZk2Test.java
@@ -288,7 +288,7 @@ public class BasicDistributedZk2Test extends AbstractFullDistribZkTestBase {
   
     long numFound1 = cloudClient.query(new SolrQuery("*:*")).getResults().getNumFound();
     
-    cloudClient.getZkStateReader().getLeaderRetry(DEFAULT_COLLECTION, SHARD1, 45000);
+    cloudClient.getZkStateReader().getLeaderRetry(DEFAULT_COLLECTION, SHARD1, 60000);
     index_specific(shardToJetty.get(SHARD1).get(1).client.solrClient, id, 1000, i1, 108, t1,
         "specific doc!");
     
