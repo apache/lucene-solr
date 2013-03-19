9b5e3f881bde2484e9a7dbadad2c16b0138ec1c6
Mark Robert Miller <markrmiller@apache.org>
2013-03-19 02:52:28 +0000

    change runtime exception to service unavailable solrexception
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_4_2@1458097 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/DefaultSolrCoreState.java
+++ solr/core/src/java/org/apache/solr/update/DefaultSolrCoreState.java
@@ -25,6 +25,7 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.store.AlreadyClosedException;
 import org.apache.solr.cloud.RecoveryStrategy;
 import org.apache.solr.common.SolrException;
+import org.apache.solr.common.SolrException.ErrorCode;
 import org.apache.solr.core.CoreContainer;
 import org.apache.solr.core.CoreDescriptor;
 import org.apache.solr.core.DirectoryFactory;
@@ -81,7 +82,7 @@ public final class DefaultSolrCoreState extends SolrCoreState implements Recover
       throws IOException {
     
     if (closed) {
-      throw new RuntimeException("SolrCoreState already closed");
+      throw new SolrException(ErrorCode.SERVICE_UNAVAILABLE, "SolrCoreState already closed");
     }
     
     synchronized (writerPauseLock) {
@@ -98,7 +99,7 @@ public final class DefaultSolrCoreState extends SolrCoreState implements Recover
         } catch (InterruptedException e) {}
         
         if (closed) {
-          throw new RuntimeException("Already closed");
+          throw new SolrException(ErrorCode.SERVICE_UNAVAILABLE, "Already closed");
         }
       }
       
@@ -147,7 +148,7 @@ public final class DefaultSolrCoreState extends SolrCoreState implements Recover
         } catch (InterruptedException e) {}
         
         if (closed) {
-          throw new RuntimeException("SolrCoreState already closed");
+          throw new SolrException(ErrorCode.SERVICE_UNAVAILABLE, "SolrCoreState already closed");
         }
       }
 
