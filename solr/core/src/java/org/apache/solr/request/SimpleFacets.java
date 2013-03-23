ac518c03efe1875adc0dabff6ca8a0331261379d
Yonik Seeley <yonik@apache.org>
2013-03-23 13:42:23 +0000

    SOLR-3819: make grouped faceting respect filter exclusions
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1460142 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/request/SimpleFacets.java
+++ solr/core/src/java/org/apache/solr/request/SimpleFacets.java
@@ -315,7 +315,7 @@ public class SimpleFacets {
     }
     
     TermAllGroupsCollector collector = new TermAllGroupsCollector(groupField);
-    Filter mainQueryFilter = docsOrig.getTopFilter(); // This returns a filter that only matches documents matching with q param and fq params
+    Filter mainQueryFilter = docs.getTopFilter(); // This returns a filter that only matches documents matching with q param and fq params
     searcher.search(facetQuery, mainQueryFilter, collector);
     return collector.getGroupCount();
   }
