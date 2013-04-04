c18676ed4fbe8bca981ad2af86f2e4a4e8a5baf5
Michael McCandless <mikemccand@apache.org>
2013-04-04 11:04:38 +0000

    LUCENE-4898: openIfChange now allows targeting an IndexCommit from an NRT reader
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1464437 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/index/TestDirectoryReaderReopen.java
+++ lucene/core/src/test/org/apache/lucene/index/TestDirectoryReaderReopen.java
@@ -594,4 +594,28 @@ public class TestDirectoryReaderReopen extends LuceneTestCase {
     r.close();
     dir.close();
   }
+
+  public void testOpenIfChangedNRTToCommit() throws Exception {
+    Directory dir = newDirectory();
+
+    // Can't use RIW because it randomly commits:
+    IndexWriter w = new IndexWriter(dir, newIndexWriterConfig(TEST_VERSION_CURRENT, new MockAnalyzer(random())));
+    Document doc = new Document();
+    doc.add(newStringField("field", "value", Field.Store.NO));
+    w.addDocument(doc);
+    w.commit();
+    List<IndexCommit> commits = DirectoryReader.listCommits(dir);
+    assertEquals(1, commits.size());
+    w.addDocument(doc);
+    DirectoryReader r = DirectoryReader.open(w, true);
+
+    assertEquals(2, r.numDocs());
+    IndexReader r2 = DirectoryReader.openIfChanged(r, commits.get(0));
+    assertNotNull(r2);
+    r.close();
+    assertEquals(1, r2.numDocs());
+    w.close();
+    r2.close();
+    dir.close();
+  }
 }
