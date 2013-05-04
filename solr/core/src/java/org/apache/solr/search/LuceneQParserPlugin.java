b2756d7c39a0e57b83cfc4b78d7d01eb14536843
Jan Høydahl <janhoy@apache.org>
2013-05-04 07:48:31 +0000

    SOLR-4784: Make class LuceneQParser public (backport)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1479056 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/search/LuceneQParserPlugin.java
+++ solr/core/src/java/org/apache/solr/search/LuceneQParserPlugin.java
@@ -46,40 +46,6 @@ public class LuceneQParserPlugin extends QParserPlugin {
   }
 }
 
-class LuceneQParser extends QParser {
-  SolrQueryParser lparser;
-
-  public LuceneQParser(String qstr, SolrParams localParams, SolrParams params, SolrQueryRequest req) {
-    super(qstr, localParams, params, req);
-  }
-
-
-  @Override
-  public Query parse() throws SyntaxError {
-    String qstr = getString();
-    if (qstr == null || qstr.length()==0) return null;
-
-    String defaultField = getParam(CommonParams.DF);
-    if (defaultField==null) {
-      defaultField = getReq().getSchema().getDefaultSearchFieldName();
-    }
-    lparser = new SolrQueryParser(this, defaultField);
-
-    lparser.setDefaultOperator
-      (QueryParsing.getQueryParserDefaultOperator(getReq().getSchema(), 
-                                                  getParam(QueryParsing.OP)));
-
-    return lparser.parse(qstr);
-  }
-
-
-  @Override
-  public String[] getDefaultHighlightFields() {
-    return lparser == null ? new String[]{} : new String[]{lparser.getDefaultField()};
-  }
-  
-}
-
 
 class OldLuceneQParser extends LuceneQParser {
   String sortStr;
