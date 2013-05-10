faeb3442407a9ff21f260a96fa5e3d0ee474c13d
Shawn Heisey <elyograg@apache.org>
2013-05-10 06:27:04 +0000

    SOLR-4760: Include core name in logs when loading schema. (merge trunk r1480882)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_4_3@1480893 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/schema/IndexSchema.java
+++ solr/core/src/java/org/apache/solr/schema/IndexSchema.java
@@ -438,11 +438,20 @@ public class IndexSchema {
       final List<SchemaAware> schemaAware = new ArrayList<SchemaAware>();
       String expression = stepsToPath(SCHEMA, AT + NAME);
       Node nd = (Node) xpath.evaluate(expression, document, XPathConstants.NODE);
+      StringBuilder sb = new StringBuilder();
+      sb.append("[");
+      sb.append(loader.getCoreProperties().getProperty(NAME));
+      sb.append("] ");
       if (nd==null) {
-        log.warn("schema has no name!");
+        sb.append("schema has no name!");
+        log.warn(sb.toString());
       } else {
         name = nd.getNodeValue();
-        log.info("Schema " + NAME + "=" + name);
+        sb.append("Schema ");
+        sb.append(NAME);
+        sb.append("=");
+        sb.append(name);
+        log.info(sb.toString());
       }
 
       //                      /schema/@version
