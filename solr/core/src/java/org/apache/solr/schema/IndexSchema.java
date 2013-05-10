3f0814fb79b2b57f1b1563e63bf0b7df3282d3ae
Shawn Heisey <elyograg@apache.org>
2013-05-10 18:19:06 +0000

    SOLR-4760: fix typo in re-added bracket
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1481129 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/schema/IndexSchema.java
+++ solr/core/src/java/org/apache/solr/schema/IndexSchema.java
@@ -440,7 +440,7 @@ public class IndexSchema {
       } else {
         sb.append("null");
       }
-      sb.append("]est");
+      sb.append("] ");
       if (nd==null) {
         sb.append("schema has no name!");
         log.warn(sb.toString());
