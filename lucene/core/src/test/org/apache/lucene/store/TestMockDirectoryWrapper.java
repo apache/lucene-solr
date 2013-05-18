2e7b7dda6a810f08c6628596713bed7e8ed095dc
Adrien Grand <jpountz@apache.org>
2013-05-18 17:34:58 +0000

    Fix TestMockDirectoryWrapper.testDiskFull (broken by last commit, merged from r1484151).
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1484152 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/store/TestMockDirectoryWrapper.java
+++ lucene/core/src/test/org/apache/lucene/store/TestMockDirectoryWrapper.java
@@ -55,7 +55,7 @@ public class TestMockDirectoryWrapper extends LuceneTestCase {
   public void testDiskFull() throws IOException {
     // test writeBytes
     MockDirectoryWrapper dir = newMockDirectory();
-    dir.setMaxSizeInBytes(2);
+    dir.setMaxSizeInBytes(3);
     final byte[] bytes = new byte[] { 1, 2};
     IndexOutput out = dir.createOutput("foo", IOContext.DEFAULT);
     out.writeBytes(bytes, bytes.length); // first write should succeed
@@ -73,7 +73,7 @@ public class TestMockDirectoryWrapper extends LuceneTestCase {
     
     // test copyBytes
     dir = newMockDirectory();
-    dir.setMaxSizeInBytes(2);
+    dir.setMaxSizeInBytes(3);
     out = dir.createOutput("foo", IOContext.DEFAULT);
     out.copyBytes(new ByteArrayDataInput(bytes), bytes.length); // first copy should succeed
     // flush() to ensure the written bytes are not buffered and counted
