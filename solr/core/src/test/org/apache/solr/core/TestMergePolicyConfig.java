b3e02db3a977d48e78f17bdc5925581ee2038d83
Chris M. Hostetter <hossman@apache.org>
2013-06-20 01:36:34 +0000

    SOLR-4941: Fix handling of <mergePolicy> init arg "useCompoundFile" -> IndexWriterConfig
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1494837 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/core/TestMergePolicyConfig.java
+++ solr/core/src/test/org/apache/solr/core/TestMergePolicyConfig.java
@@ -17,13 +17,24 @@ package org.apache.solr.core;
  * limitations under the License.
  */
 
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.AtomicReader;
+import org.apache.lucene.index.AtomicReaderContext;
+import org.apache.lucene.index.SegmentReader;
+import org.apache.lucene.index.SegmentInfo;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.MergePolicy;
 import org.apache.lucene.index.TieredMergePolicy;
+import org.apache.solr.core.SolrCore;
+import org.apache.solr.util.RefCounted;
+import org.apache.solr.search.SolrIndexSearcher;
 import org.apache.solr.SolrTestCaseJ4;
 import org.junit.After;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestMergePolicyConfig extends SolrTestCaseJ4 {
+  
+  private static AtomicInteger docIdCounter = new AtomicInteger(42);
 
   @After
   public void after() throws Exception {
@@ -33,27 +44,39 @@ public class TestMergePolicyConfig extends SolrTestCaseJ4 {
   public void testDefaultMergePolicyConfig() throws Exception {
     initCore("solrconfig-mergepolicy-defaults.xml","schema-minimal.xml");
     IndexWriterConfig iwc = solrConfig.indexConfig.toIndexWriterConfig(h.getCore().getLatestSchema());
+    assertEquals(false, iwc.getUseCompoundFile());
 
     TieredMergePolicy tieredMP = assertAndCast(TieredMergePolicy.class,
                                                iwc.getMergePolicy());
     assertEquals(0.0D, tieredMP.getNoCFSRatio(), 0.0D);
 
+    assertCommitSomeNewDocs();
+    assertCompoundSegments(h.getCore(), false);
   }
 
   public void testLegacyMergePolicyConfig() throws Exception {
     initCore("solrconfig-mergepolicy-legacy.xml","schema-minimal.xml");
     IndexWriterConfig iwc = solrConfig.indexConfig.toIndexWriterConfig(h.getCore().getLatestSchema());
+    assertEquals(true, iwc.getUseCompoundFile());
+
+
     TieredMergePolicy tieredMP = assertAndCast(TieredMergePolicy.class,
                                                iwc.getMergePolicy());
 
     assertEquals(7, tieredMP.getMaxMergeAtOnce());
     assertEquals(7.0D, tieredMP.getSegmentsPerTier(), 0.0D);
     assertEquals(1.0D, tieredMP.getNoCFSRatio(), 0.0D);
+
+    assertCommitSomeNewDocs();
+    assertCompoundSegments(h.getCore(), true);
   }
   
   public void testTieredMergePolicyConfig() throws Exception {
     initCore("solrconfig-mergepolicy.xml","schema-minimal.xml");
     IndexWriterConfig iwc = solrConfig.indexConfig.toIndexWriterConfig(h.getCore().getLatestSchema());
+    assertEquals(true, iwc.getUseCompoundFile());
+
+
     TieredMergePolicy tieredMP = assertAndCast(TieredMergePolicy.class,
                                                iwc.getMergePolicy());
 
@@ -62,11 +85,25 @@ public class TestMergePolicyConfig extends SolrTestCaseJ4 {
     
     // mp-specific setters
     assertEquals(19, tieredMP.getMaxMergeAtOnceExplicit());
-    assertEquals(0.6D, tieredMP.getNoCFSRatio(), 0.001);
+    assertEquals(0.1D, tieredMP.getNoCFSRatio(), 0.0D);
     // make sure we overrode segmentsPerTier 
     // (split from maxMergeAtOnce out of mergeFactor)
     assertEquals(9D, tieredMP.getSegmentsPerTier(), 0.001);
     
+    assertCommitSomeNewDocs();
+    // even though we have a single segment (which is 100% of the size of 
+    // the index which is higher then our 0.6D threashold) the
+    // compound ratio doesn't matter because the segment was never merged
+    assertCompoundSegments(h.getCore(), true);
+
+    assertCommitSomeNewDocs();
+    assertNumSegments(h.getCore(), 2);
+    assertCompoundSegments(h.getCore(), true);
+
+    assertU(optimize());
+    assertNumSegments(h.getCore(), 1);
+    // we've now forced a merge, and the MP ratio should be in play
+    assertCompoundSegments(h.getCore(), false);
   }
 
   /**
@@ -79,6 +116,68 @@ public class TestMergePolicyConfig extends SolrTestCaseJ4 {
     assertNotNull(o);
     assertTrue(clazz.isInstance(o));
     return clazz.cast(o);
+  }
+
+  public static void assertCommitSomeNewDocs() {
+    for (int i = 0; i < 5; i++) {
+      int val = docIdCounter.getAndIncrement();
+      assertU(adoc("id", "" + val,
+                   "a_s", val + "_" + val + "_" + val + "_" + val,
+                   "b_s", val + "_" + val + "_" + val + "_" + val,
+                   "c_s", val + "_" + val + "_" + val + "_" + val,
+                   "d_s", val + "_" + val + "_" + val + "_" + val,
+                   "e_s", val + "_" + val + "_" + val + "_" + val,
+                   "f_s", val + "_" + val + "_" + val + "_" + val));
+    }
+    assertU(commit());
+  }
+
+  /**
+   * Given an SolrCore, asserts that the number of leave segments in 
+   * the index reader matches the expected value.
+   */
+  public static void assertNumSegments(SolrCore core, int expected) {
+    RefCounted<SolrIndexSearcher> searcherRef = core.getRegisteredSearcher();
+    try {
+      assertEquals(expected, searcherRef.get().getIndexReader().leaves().size());
+    } finally {
+      searcherRef.decref();
+    }
+  }
+
+  /**
+   * Given an SolrCore, asserts that each segment in the (searchable) index 
+   * has a compound file status that matches the expected input.
+   */
+  public static void assertCompoundSegments(SolrCore core, boolean compound) {
+    RefCounted<SolrIndexSearcher> searcherRef = core.getRegisteredSearcher();
+    try {
+      assertCompoundSegments(searcherRef.get().getIndexReader(), compound);
+    } finally {
+      searcherRef.decref();
+    }
+  }
+
+  /**
+   * Given an IndexReader, asserts that there is at least one AtomcReader leaf,
+   * and that all AtomicReader leaves are SegmentReader's that have a compound 
+   * file status that matches the expected input.
+   */
+  private static void assertCompoundSegments(IndexReader reader, 
+                                             boolean compound) {
+
+    assertNotNull("Null leaves", reader.leaves());
+    assertTrue("no leaves", 0 < reader.leaves().size());
+
+    for (AtomicReaderContext atomic : reader.leaves()) {
+      assertTrue("not a segment reader: " + atomic.reader().toString(), 
+                 atomic.reader() instanceof SegmentReader);
+      
+      assertEquals("Compound status incorrect for: " + 
+                   atomic.reader().toString(),
+                   compound,
+                   ((SegmentReader)atomic.reader()).getSegmentInfo().info.getUseCompoundFile());
+    }
   }
 
 }
