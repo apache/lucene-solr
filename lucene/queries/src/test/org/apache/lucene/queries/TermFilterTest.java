f6c8a999c84ad6cb4b49c8c7a2663e4101d5ff7d
Martijn van Groningen <mvg@apache.org>
2013-07-16 21:50:12 +0000

    value1 should always different than value2
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1503913 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/queries/src/test/org/apache/lucene/queries/TermFilterTest.java
+++ lucene/queries/src/test/org/apache/lucene/queries/TermFilterTest.java
@@ -123,7 +123,7 @@ public class TermFilterTest extends LuceneTestCase {
       String field1 = "field" + i;
       String field2 = "field" + i + num;
       String value1 = _TestUtil.randomRealisticUnicodeString(random());
-      String value2 = _TestUtil.randomRealisticUnicodeString(random()) + "x"; // this must be not equal to value1
+      String value2 = value1 + "x"; // this must be not equal to value1
 
       TermFilter filter1 = termFilter(field1, value1);
       TermFilter filter2 = termFilter(field1, value2);
