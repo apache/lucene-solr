8ac9d7a6f65334cb97a32bf09d2a921619549bef
Adrien Grand <jpountz@apache.org>
2013-07-19 13:02:14 +0000

    Fix test bug: IndexWriterConfig was not taken into account.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1504865 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/search/TestFieldCache.java
+++ lucene/core/src/test/org/apache/lucene/search/TestFieldCache.java
@@ -735,7 +735,7 @@ public class TestFieldCache extends LuceneTestCase {
     Directory dir = newDirectory();
     IndexWriterConfig cfg = newIndexWriterConfig(TEST_VERSION_CURRENT, new MockAnalyzer(random()));
     cfg.setMergePolicy(newLogMergePolicy());
-    RandomIndexWriter iw = new RandomIndexWriter(random(), dir);
+    RandomIndexWriter iw = new RandomIndexWriter(random(), dir, cfg);
     Document doc = new Document();
     IntField field = new IntField("f", 0, Store.YES);
     doc.add(field);
