c53362f38abe10811aded4250e4530ebffaee533
Chris M. Hostetter <hossman@apache.org>
2013-09-03 21:39:51 +0000

    SOLR-5206: Fixed OpenExchangeRatesOrgProvider to use refreshInterval correctly
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1519858 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/schema/OpenExchangeRatesOrgProviderTest.java
+++ solr/core/src/test/org/apache/solr/schema/OpenExchangeRatesOrgProviderTest.java
@@ -30,9 +30,12 @@ import java.util.Map;
  * Tests currency field type.
  */
 public class OpenExchangeRatesOrgProviderTest extends SolrTestCaseJ4 {
+  private final static long HARDCODED_TEST_TIMESTAMP = 1332070464L;
+
   OpenExchangeRatesOrgProvider oerp;
   ResourceLoader loader;
   private final Map<String,String> mockParams = new HashMap<String,String>();
+
 
   @Override
   @Before
@@ -50,18 +53,22 @@ public class OpenExchangeRatesOrgProviderTest extends SolrTestCaseJ4 {
   @Test
   public void testInit() throws Exception {
     oerp.init(mockParams);
+    // don't inform, we don't want to hit any of these URLs
+
     assertEquals("Wrong url", 
                  "open-exchange-rates.json", oerp.ratesFileLocation);
-    assertEquals("Wrong default interval", 1440, oerp.refreshInterval);
+    assertEquals("Wrong default interval", (1440*60), oerp.refreshIntervalSeconds);
 
     Map<String,String> params = new HashMap<String,String>();
     params.put(OpenExchangeRatesOrgProvider.PARAM_RATES_FILE_LOCATION, 
                "http://foo.bar/baz");
     params.put(OpenExchangeRatesOrgProvider.PARAM_REFRESH_INTERVAL, "100");
+
     oerp.init(params);
     assertEquals("Wrong param set url", 
                  "http://foo.bar/baz", oerp.ratesFileLocation);
-    assertEquals("Wrong param interval", 100, oerp.refreshInterval);
+    assertEquals("Wrong param interval", (100*60), oerp.refreshIntervalSeconds);
+
   }
 
   @Test
@@ -76,15 +83,31 @@ public class OpenExchangeRatesOrgProviderTest extends SolrTestCaseJ4 {
     oerp.init(mockParams);
     oerp.inform(loader);
     assertEquals(81.29D, oerp.getExchangeRate("USD", "JPY"), 0.0D);    
+    assertEquals("USD", oerp.rates.getBaseCurrency());
   }
 
   @Test
   public void testReload() {
+    // reminder: interval is in minutes
+    mockParams.put(OpenExchangeRatesOrgProvider.PARAM_REFRESH_INTERVAL, "100");
     oerp.init(mockParams);
     oerp.inform(loader);
-    assertTrue(oerp.reload());
-    assertEquals("USD", oerp.rates.getBaseCurrency());
-    assertEquals(new Long(1332070464L), new Long(oerp.rates.getTimestamp()));
+
+    // reminder: timestamp is in seconds
+    assertEquals(HARDCODED_TEST_TIMESTAMP, oerp.rates.getTimestamp());
+
+    // modify the timestamp to be "current" then fetch a rate and ensure no reload
+    final long currentTimestamp = (long) (System.currentTimeMillis() / 1000);
+    oerp.rates.setTimestamp(currentTimestamp);
+    assertEquals(81.29D, oerp.getExchangeRate("USD", "JPY"), 0.0D);    
+    assertEquals(currentTimestamp, oerp.rates.getTimestamp());
+
+    // roll back clock on timestamp and ensure rate fetch does reload
+    oerp.rates.setTimestamp(currentTimestamp - (101 * 60));
+    assertEquals(81.29D, oerp.getExchangeRate("USD", "JPY"), 0.0D);    
+    assertEquals("timestamp wasn't reset to hardcoded value, indicating no reload",
+                 HARDCODED_TEST_TIMESTAMP, oerp.rates.getTimestamp());
+
   }
 
   @Test(expected=SolrException.class)
