7e7cf0961f49595ff4b9f7ce9479c9870a60462a
Yonik Seeley <yonik@apache.org>
2013-09-04 14:57:44 +0000

    SOLR-5175: keep child order in block index
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1520042 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/AddUpdateCommand.java
+++ solr/core/src/java/org/apache/solr/update/AddUpdateCommand.java
@@ -194,15 +194,14 @@ public class AddUpdateCommand extends UpdateCommand implements Iterable<IndexDoc
   private List<SolrInputDocument> flatten(SolrInputDocument root) {
     List<SolrInputDocument> unwrappedDocs = new ArrayList<SolrInputDocument>();
     recUnwrapp(unwrappedDocs, root);
-    Collections.reverse(unwrappedDocs);
     return unwrappedDocs;
   }
 
   private void recUnwrapp(List<SolrInputDocument> unwrappedDocs, SolrInputDocument currentDoc) {
-    unwrappedDocs.add(currentDoc);
     for (SolrInputDocument child : currentDoc.getChildDocuments()) {
       recUnwrapp(unwrappedDocs, child);
     }
+    unwrappedDocs.add(currentDoc);
   }
 
 
