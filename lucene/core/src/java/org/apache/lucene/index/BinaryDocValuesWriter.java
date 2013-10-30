b34488950315a73f5391fa923f36767485e9e556
Michael McCandless <mikemccand@apache.org>
2013-10-30 14:32:19 +0000

    LUCENE-5296: add DirectDocValuesFormat
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1537108 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/index/BinaryDocValuesWriter.java
+++ lucene/core/src/java/org/apache/lucene/index/BinaryDocValuesWriter.java
@@ -24,11 +24,12 @@ import java.util.NoSuchElementException;
 import org.apache.lucene.codecs.DocValuesConsumer;
 import org.apache.lucene.store.DataInput;
 import org.apache.lucene.store.DataOutput;
+import org.apache.lucene.util.ArrayUtil;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.Counter;
 import org.apache.lucene.util.OpenBitSet;
-import org.apache.lucene.util.RamUsageEstimator;
 import org.apache.lucene.util.PagedBytes;
+import org.apache.lucene.util.RamUsageEstimator;
 import org.apache.lucene.util.packed.AppendingDeltaPackedLongBuffer;
 import org.apache.lucene.util.packed.PackedInts;
 
@@ -36,12 +37,8 @@ import org.apache.lucene.util.packed.PackedInts;
  *  segment flushes. */
 class BinaryDocValuesWriter extends DocValuesWriter {
 
-  /** Maximum length for a binary field; we set this to "a
-   *  bit" below Integer.MAX_VALUE because the exact max
-   *  allowed byte[] is JVM dependent, so we want to avoid
-   *  a case where a large value worked in one JVM but
-   *  failed later at search time with a different JVM.  */
-  private static final int MAX_LENGTH = Integer.MAX_VALUE-256;
+  /** Maximum length for a binary field. */
+  private static final int MAX_LENGTH = ArrayUtil.MAX_ARRAY_LENGTH;
 
   // 32 KB block sizes for PagedBytes storage:
   private final static int BLOCK_BITS = 15;
