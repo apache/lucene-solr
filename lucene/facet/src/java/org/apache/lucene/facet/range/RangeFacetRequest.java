55c8752b20d0a8b220f826cfdd1d9440fe532015
Shai Erera <shaie@apache.org>
2013-11-05 04:28:41 +0000

    LUCENE-5297: allow range faceting on any ValueSource
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1538863 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/facet/src/java/org/apache/lucene/facet/range/RangeFacetRequest.java
+++ lucene/facet/src/java/org/apache/lucene/facet/range/RangeFacetRequest.java
@@ -17,38 +17,91 @@ package org.apache.lucene.facet.range;
  * limitations under the License.
  */
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.lucene.facet.params.FacetIndexingParams;
 import org.apache.lucene.facet.search.FacetRequest;
+import org.apache.lucene.facet.search.FacetResultNode;
 import org.apache.lucene.facet.search.FacetsAggregator;
 import org.apache.lucene.facet.taxonomy.CategoryPath;
+import org.apache.lucene.index.AtomicReaderContext;
+import org.apache.lucene.index.NumericDocValues;
+import org.apache.lucene.queries.function.FunctionValues;
+import org.apache.lucene.queries.function.ValueSource;
+import org.apache.lucene.queries.function.valuesource.LongFieldSource;
 
 /**
- * Facet request for dynamic ranges based on a
- * NumericDocValues field.  This does not use the taxonomy
- * index nor any indexed facet values.
+ * A {@link FacetRequest} for dynamic ranges based on a {@link NumericDocValues}
+ * field or {@link ValueSource}. This does not use the taxonomy index nor any
+ * indexed facet values.
  * 
  * @lucene.experimental
  */
 public class RangeFacetRequest<T extends Range> extends FacetRequest {
 
   public final Range[] ranges;
-
+  public final String label;
+  
+  private final ValueSource valueSource;
+  
+  /**
+   * Create a request for the given ranges over the specified
+   * {@link NumericDocValues} field. The field will be used to as the root's
+   * {@link FacetResultNode} label.
+   */
   @SuppressWarnings("unchecked")
   public RangeFacetRequest(String field, T...ranges) {
-    super(new CategoryPath(field), 1);
-    this.ranges = ranges;
+    this(field, new LongFieldSource(field), ranges);
   }
 
+  /**
+   * Create a request for the given ranges over the specified
+   * {@link NumericDocValues} field. The field will be used to as the root's
+   * {@link FacetResultNode} label.
+   */
   @SuppressWarnings("unchecked")
   public RangeFacetRequest(String field, List<T> ranges) {
     this(field, (T[]) ranges.toArray(new Range[ranges.size()]));
   }
+  
+  /**
+   * Create a request for the given ranges over the specified
+   * {@link ValueSource}. The label will be used to as the root's
+   * {@link FacetResultNode} label.
+   */
+  @SuppressWarnings("unchecked")
+  public RangeFacetRequest(String label, ValueSource valueSource, T...ranges) {
+    super(new CategoryPath(label), 1);
+    this.ranges = ranges;
+    this.valueSource = valueSource;
+    this.label = label;
+  }
+  
+  /**
+   * Create a request for the given ranges over the specified
+   * {@link ValueSource}. The label will be used to as the root's
+   * {@link FacetResultNode} label.
+   */
+  @SuppressWarnings("unchecked")
+  public RangeFacetRequest(String label, ValueSource valueSource, List<T> ranges) {
+    this(label, valueSource, (T[]) ranges.toArray(new Range[ranges.size()]));
+  }
 
+  /**
+   * Returns the {@link FunctionValues} for the given
+   * {@link AtomicReaderContext}. If the request was created over a
+   * {@link NumericDocValues} field, the respective {@link NumericDocValues} is
+   * returned.
+   */
+  public FunctionValues getValues(AtomicReaderContext context) throws IOException {
+    return valueSource.getValues(Collections.emptyMap(), context);
+  }
+  
   @Override
   public FacetsAggregator createFacetsAggregator(FacetIndexingParams fip) {
-    return null;
+    throw new UnsupportedOperationException("this FacetRequest does not support categories aggregation and only works with RangeAccumulator");
   }
   
 }
