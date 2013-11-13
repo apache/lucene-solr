99255ff0da685e6727d08833ed1c7feca18525ce
Erick Erickson <erick@apache.org>
2013-11-13 00:56:37 +0000

    LUCENE-5337 Add Payload support to FileDictionary (Suggest) and make it more configurable
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1541341 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/suggest/src/java/org/apache/lucene/search/suggest/FileDictionary.java
+++ lucene/suggest/src/java/org/apache/lucene/search/suggest/FileDictionary.java
@@ -28,46 +28,126 @@ import org.apache.lucene.util.IOUtils;
 /**
  * Dictionary represented by a text file.
  * 
- * <p/>Format allowed: 1 string per line, optionally with a tab-separated integer value:<br/>
- * word1 TAB 100<br/>
- * word2 word3 TAB 101<br/>
- * word4 word5 TAB 102<br/>
+ * <p/>Format allowed: 1 entry per line:<br/>
+ * An entry can be: <br/>
+ * <ul>
+ * <li>suggestion</li>
+ * <li>suggestion <code>fieldDelimiter</code> weight</li>
+ * <li>suggestion <code>fieldDelimiter</code> weight <code>fieldDelimiter</code> payload</li>
+ * </ul>
+ * where the default <code>fieldDelimiter</code> is {@value #DEFAULT_FIELD_DELIMITER}<br/>
+ * <p/>
+ * <b>NOTE:</b> 
+ * <ul>
+ * <li>In order to have payload enabled, the first entry has to have a payload</li>
+ * <li>If the weight for an entry is not specified then a value of 1 is used</li>
+ * <li>A payload cannot be specified without having the weight specified for an entry</li>
+ * <li>If the payload for an entry is not specified (assuming payload is enabled) 
+ *  then an empty payload is returned</li>
+ * <li>An entry cannot have more than two <code>fieldDelimiter</code></li>
+ * </ul>
+ * <p/>
+ * <b>Example:</b><br/>
+ * word1 word2 TAB 100 TAB payload1<br/>
+ * word3 TAB 101<br/>
+ * word4 word3 TAB 102<br/>
  */
 public class FileDictionary implements Dictionary {
 
+  /**
+   * Tab-delimited fields are most common thus the default, but one can override this via the constructor
+   */
+  public final static String DEFAULT_FIELD_DELIMITER = "\t";
   private BufferedReader in;
   private String line;
   private boolean done = false;
+  private final String fieldDelimiter;
 
   /**
    * Creates a dictionary based on an inputstream.
+   * Using {@link #DEFAULT_FIELD_DELIMITER} as the 
+   * field seperator in a line.
    * <p>
    * NOTE: content is treated as UTF-8
    */
   public FileDictionary(InputStream dictFile) {
-    in = new BufferedReader(IOUtils.getDecodingReader(dictFile, IOUtils.CHARSET_UTF_8));
+    this(dictFile, DEFAULT_FIELD_DELIMITER);
   }
 
   /**
    * Creates a dictionary based on a reader.
+   * Using {@link #DEFAULT_FIELD_DELIMITER} as the 
+   * field seperator in a line.
    */
   public FileDictionary(Reader reader) {
+    this(reader, DEFAULT_FIELD_DELIMITER);
+  }
+  
+  /**
+   * Creates a dictionary based on a reader. 
+   * Using <code>fieldDelimiter</code> to seperate out the
+   * fields in a line.
+   */
+  public FileDictionary(Reader reader, String fieldDelimiter) {
     in = new BufferedReader(reader);
+    this.fieldDelimiter = fieldDelimiter;
+  }
+  
+  /**
+   * Creates a dictionary based on an inputstream.
+   * Using <code>fieldDelimiter</code> to seperate out the
+   * fields in a line.
+   * <p>
+   * NOTE: content is treated as UTF-8
+   */
+  public FileDictionary(InputStream dictFile, String fieldDelimiter) {
+    in = new BufferedReader(IOUtils.getDecodingReader(dictFile, IOUtils.CHARSET_UTF_8));
+    this.fieldDelimiter = fieldDelimiter;
   }
 
   @Override
   public InputIterator getWordsIterator() {
-    return new FileIterator();
+    try {
+      return new FileIterator();
+    } catch (IOException e) {
+      throw new RuntimeException();
+    }
   }
 
   final class FileIterator implements InputIterator {
-    private long curFreq;
+    private long curWeight;
     private final BytesRef spare = new BytesRef();
+    private BytesRef curPayload = new BytesRef();
+    private boolean isFirstLine = true;
+    private boolean hasPayloads = false;
     
-   
+    private FileIterator() throws IOException {
+      line = in.readLine();
+      if (line == null) {
+        done = true;
+        IOUtils.close(in);
+      } else {
+        String[] fields = line.split(fieldDelimiter);
+        if (fields.length > 3) {
+          throw new IllegalArgumentException("More than 3 fields in one line");
+        } else if (fields.length == 3) { // term, weight, payload
+          hasPayloads = true;
+          spare.copyChars(fields[0]);
+          readWeight(fields[1]);
+          curPayload.copyChars(fields[2]);
+        } else if (fields.length == 2) { // term, weight
+          spare.copyChars(fields[0]);
+          readWeight(fields[1]);
+        } else { // only term
+          spare.copyChars(fields[0]);
+          curWeight = 1;
+        }
+      }
+    }
+    
     @Override
     public long weight() {
-      return curFreq;
+      return curWeight;
     }
 
     @Override
@@ -75,20 +155,33 @@ public class FileDictionary implements Dictionary {
       if (done) {
         return null;
       }
+      if (isFirstLine) {
+        isFirstLine = false;
+        return spare;
+      }
       line = in.readLine();
       if (line != null) {
-        String[] fields = line.split("\t");
-        if (fields.length > 1) {
-          // keep reading floats for bw compat
-          try {
-            curFreq = Long.parseLong(fields[1]);
-          } catch (NumberFormatException e) {
-            curFreq = (long)Double.parseDouble(fields[1]);
-          }
+        String[] fields = line.split(fieldDelimiter);
+        if (fields.length > 3) {
+          throw new IllegalArgumentException("More than 3 fields in one line");
+        } else if (fields.length == 3) { // term, weight and payload
           spare.copyChars(fields[0]);
-        } else {
-          spare.copyChars(line);
-          curFreq = 1;
+          readWeight(fields[1]);
+          if (hasPayloads) {
+            curPayload.copyChars(fields[2]);
+          }
+        } else if (fields.length == 2) { // term, weight
+          spare.copyChars(fields[0]);
+          readWeight(fields[1]);
+          if (hasPayloads) { // have an empty payload
+            curPayload = new BytesRef();
+          }
+        } else { // only term
+          spare.copyChars(fields[0]);
+          curWeight = 1;
+          if (hasPayloads) {
+            curPayload = new BytesRef();
+          }
         }
         return spare;
       } else {
@@ -100,12 +193,21 @@ public class FileDictionary implements Dictionary {
 
     @Override
     public BytesRef payload() {
-      return null;
+      return (hasPayloads) ? curPayload : null;
     }
 
     @Override
     public boolean hasPayloads() {
-      return false;
+      return hasPayloads;
+    }
+    
+    private void readWeight(String weight) {
+      // keep reading floats for bw compat
+      try {
+        curWeight = Long.parseLong(weight);
+      } catch (NumberFormatException e) {
+        curWeight = (long)Double.parseDouble(weight);
+      }
     }
   }
 }
