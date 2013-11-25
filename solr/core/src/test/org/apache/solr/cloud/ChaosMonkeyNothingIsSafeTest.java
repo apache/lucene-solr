56a116aae85741e0bbba99f8835d091f38aac8a7
Mark Robert Miller <markrmiller@apache.org>
2013-11-25 01:11:51 +0000

    SOLR-5417: fail if the chaosmonkey runs for over 20 seconds and doesn't kill a jetty
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1545125 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/ChaosMonkeyNothingIsSafeTest.java
+++ solr/core/src/test/org/apache/solr/cloud/ChaosMonkeyNothingIsSafeTest.java
@@ -144,7 +144,7 @@ public class ChaosMonkeyNothingIsSafeTest extends AbstractFullDistribZkTestBase 
       if (RUN_LENGTH != -1) {
         runLength = RUN_LENGTH;
       } else {
-        int[] runTimes = new int[] {5000,6000,10000,15000,15000,30000,30000,45000,90000,120000};
+        int[] runTimes = new int[] {5000,6000,10000,15000,25000,30000,30000,45000,90000,120000};
         runLength = runTimes[random().nextInt(runTimes.length - 1)];
       }
       
