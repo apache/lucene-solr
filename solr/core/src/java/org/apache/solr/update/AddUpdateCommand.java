e54b08e2abff578bb565def6b9361489a1ce28d9
Mark Robert Miller <markrmiller@apache.org>
2013-11-27 20:43:35 +0000

    SOLR-5509: fix possible npe
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1546189 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/AddUpdateCommand.java
+++ solr/core/src/java/org/apache/solr/update/AddUpdateCommand.java
@@ -109,14 +109,16 @@ public class AddUpdateCommand extends UpdateCommand implements Iterable<IndexDoc
    }
 
    public String getPrintableId() {
-     IndexSchema schema = req.getSchema();
-     SchemaField sf = schema.getUniqueKeyField();
-     if (solrDoc != null && sf != null) {
-       SolrInputField field = solrDoc.getField(sf.getName());
-       if (field != null) {
-         return field.getFirstValue().toString();
-       }
-     }
+    if (req != null) {
+      IndexSchema schema = req.getSchema();
+      SchemaField sf = schema.getUniqueKeyField();
+      if (solrDoc != null && sf != null) {
+        SolrInputField field = solrDoc.getField(sf.getName());
+        if (field != null) {
+          return field.getFirstValue().toString();
+        }
+      }
+    }
      return "(null)";
    }
 
