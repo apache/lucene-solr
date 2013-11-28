60d331aaf62168f1884c3eff66581b77708dee21
Mark Robert Miller <markrmiller@apache.org>
2013-11-28 04:10:56 +0000

    SOLR-5509: Always add a param for 'from' node.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1546279 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/servlet/SolrDispatchFilter.java
+++ solr/core/src/java/org/apache/solr/servlet/SolrDispatchFilter.java
@@ -52,7 +52,6 @@ import org.apache.solr.response.QueryResponseWriter;
 import org.apache.solr.response.SolrQueryResponse;
 import org.apache.solr.servlet.cache.HttpCacheHeaderUtil;
 import org.apache.solr.servlet.cache.Method;
-import org.apache.solr.update.processor.DistributedUpdateProcessor;
 import org.apache.solr.update.processor.DistributingUpdateProcessorFactory;
 import org.apache.solr.util.FastWriter;
 import org.slf4j.Logger;
@@ -319,8 +318,7 @@ public class SolrDispatchFilter implements Filter
             SolrParams queryParams = SolrRequestParsers.parseQueryString(req.getQueryString());
             if (coreUrl != null
                 && queryParams
-                    .get(DistributingUpdateProcessorFactory.DISTRIB_UPDATE_PARAM) == null
-                && queryParams.get(DistributedUpdateProcessor.DISTRIB_FROM) == null) {
+                    .get(DistributingUpdateProcessorFactory.DISTRIB_UPDATE_PARAM) == null) {
               path = path.substring(idx);
               remoteQuery(coreUrl + path, req, solrReq, resp);
               return;
