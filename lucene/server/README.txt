8141861fed5140caf404ce068b8ae2497ce0641a
Robert Muir <rmuir@apache.org>
2013-12-24 16:03:03 +0000

    LUCENE-5376: fix documentation-lint, rat-sources, remove python build, remove plugin (it can be a separate module?)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene5376@1553294 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/server/README.txt
+++
@@ -1,7 +0,0 @@
-Simple server wrapping Lucene 4.x.
-
-To run all tests:
-
-  python3 -u build.py test
-
-
--- lucene/server/plugins/BinaryDocument/test.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, '../../src/python')
-import os
-import util
-
-util.run('ant compile-test', '../..')
-util.run('ant jar test')
-
