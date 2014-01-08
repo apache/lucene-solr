4f72365ba27b4126e49619a86e3f1f665e4e4ed0
Mark Robert Miller <markrmiller@apache.org>
2014-01-08 16:35:17 +0000

    SOLR-5615: Deadlock while trying to recover after a ZK session expiration.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1556572 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/cloud/ElectionContext.java
+++ solr/core/src/java/org/apache/solr/cloud/ElectionContext.java
@@ -458,6 +458,11 @@ final class OverseerElectionContext extends ElectionContext {
     overseer.start(id);
   }
   
+  public void cancelElection() throws InterruptedException, KeeperException {
+    super.cancelElection();
+    overseer.close();
+  }
+  
   @Override
   public void joinedElectionFired() {
     overseer.close();
