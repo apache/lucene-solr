679c3c8de8d933fb5edb551af93bceb53a456a53
Robert Muir <rmuir@apache.org>
2014-01-15 19:44:06 +0000

    LUCENE-5399: remove code duplication
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene539399@1558552 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/search/Sorting.java
+++ solr/core/src/java/org/apache/solr/search/Sorting.java
@@ -40,18 +40,25 @@ public class Sorting {
    * @return SortField
    */
   public static SortField getStringSortField(String fieldName, boolean reverse, boolean nullLast, boolean nullFirst) {
-    if (nullLast) {
-      if (!reverse) return new SortField(fieldName, nullStringLastComparatorSource);
-      else return new SortField(fieldName, SortField.Type.STRING, true);
-    } else if (nullFirst) {
-      if (reverse) return new SortField(fieldName, nullStringLastComparatorSource, true);
-      else return new SortField(fieldName, SortField.Type.STRING, false);
-    } else {
-      return new SortField(fieldName, SortField.Type.STRING, reverse);
+    if (nullFirst && nullLast) {
+      throw new IllegalArgumentException("Cannot specify missing values as both first and last");
     }
+
+    SortField sortField = new SortField(fieldName, SortField.Type.STRING, reverse);
+
+    // 4 cases:
+    // missingFirst / forward: default lucene behavior
+    // missingFirst / reverse: set sortMissingLast
+    // missingLast  / forward: set sortMissingLast
+    // missingLast  / reverse: default lucene behavior
+    
+    if (nullFirst && reverse) {
+      sortField.setMissingValue(SortField.STRING_LAST);
+    } else if (nullLast && !reverse) {
+      sortField.setMissingValue(SortField.STRING_LAST);
+    }
+
+    return sortField;
   }
-
-
-  static final FieldComparatorSource nullStringLastComparatorSource = new MissingStringLastComparatorSource(null);
 }
 
