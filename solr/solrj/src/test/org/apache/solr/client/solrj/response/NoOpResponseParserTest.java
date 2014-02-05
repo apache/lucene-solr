315263e31f1b0261fd8abbcb86345f31e54218c2
Shalin Shekhar Mangar <shalin@apache.org>
2014-02-05 11:41:07 +0000

    SOLR-5530: Fix forbidden-api-check failure
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1564712 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/client/solrj/response/NoOpResponseParserTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/response/NoOpResponseParserTest.java
@@ -85,9 +85,9 @@ public class NoOpResponseParserTest extends SolrJettyTestBase {
     assertResponse(responseString);
   }
 
-  private void assertResponse(String responseString) {
+  private void assertResponse(String responseString) throws IOException {
     ResponseParser xmlResponseParser = new XMLResponseParser();
-    NamedList expectedResponse = xmlResponseParser.processResponse(IOUtils.toInputStream(responseString), "UTF-8");
+    NamedList expectedResponse = xmlResponseParser.processResponse(IOUtils.toInputStream(responseString, "UTF-8"), "UTF-8");
     List<SolrDocument> documentList = (List<SolrDocument>) expectedResponse.getAll("response").get(0);
     assertEquals(1, documentList.size());
     SolrDocument solrDocument = documentList.get(0);
