f9ea3f81011aa70d2366c5bd65b839936dfad18d
Michael McCandless <mikemccand@apache.org>
2014-02-07 10:52:17 +0000

    LUCENE-5434: fix test bug: this test case uses a non-NRT reader too
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1565617 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/index/TestIndexWriterReader.java
+++ lucene/core/src/test/org/apache/lucene/index/TestIndexWriterReader.java
@@ -67,7 +67,9 @@ public class TestIndexWriterReader extends LuceneTestCase {
   }
   
   public void testAddCloseOpen() throws IOException {
-    Directory dir1 = getAssertNoDeletesDirectory(newDirectory());
+    // Can't use assertNoDeletes: this test pulls a non-NRT
+    // reader in the end:
+    Directory dir1 = newDirectory();
     IndexWriterConfig iwc = newIndexWriterConfig(TEST_VERSION_CURRENT, new MockAnalyzer(random()));
     
     IndexWriter writer = new IndexWriter(dir1, iwc);
