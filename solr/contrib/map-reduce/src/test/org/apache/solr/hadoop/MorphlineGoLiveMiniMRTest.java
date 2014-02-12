e77313fbd8347feee5d276aca412dc27fc3e3a01
Mark Robert Miller <markrmiller@apache.org>
2014-02-12 17:46:50 +0000

    SOLR-3854: Update MorphlineGoLiveMiniMRTest for SSL. Disable SSL for now .
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1567701 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/map-reduce/src/test/org/apache/solr/hadoop/MorphlineGoLiveMiniMRTest.java
+++ solr/contrib/map-reduce/src/test/org/apache/solr/hadoop/MorphlineGoLiveMiniMRTest.java
@@ -91,6 +91,10 @@ import com.carrotsearch.randomizedtesting.annotations.ThreadLeakZombies.Conseque
 @Slow
 public class MorphlineGoLiveMiniMRTest extends AbstractFullDistribZkTestBase {
   
+  static {
+    ALLOW_SSL = false;
+  }
+  
   private static final int RECORD_COUNT = 2104;
   private static final String RESOURCES_DIR = ExternalPaths.SOURCE_HOME + "/contrib/map-reduce/src/test-files";  
   private static final String DOCUMENTS_DIR = RESOURCES_DIR + "/test-documents";
@@ -690,7 +694,7 @@ public class MorphlineGoLiveMiniMRTest extends AbstractFullDistribZkTestBase {
       throws Exception {
     
     JettySolrRunner jetty = new JettySolrRunner(solrHome.getAbsolutePath(),
-        context, 0, solrConfigOverride, schemaOverride);
+        context, 0, solrConfigOverride, schemaOverride, true, null, sslConfig);
 
     jetty.setShards(shardList);
     
