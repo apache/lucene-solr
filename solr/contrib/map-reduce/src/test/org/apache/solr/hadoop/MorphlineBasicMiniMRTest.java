efbd2b9d82f73f3d5650b5aae7d6f47b79733a42
Uwe Schindler <uschindler@apache.org>
2014-02-23 22:50:44 +0000

    Disable 2 tests not working on windows (they are not run from ANT because of another assume, but eclipse tries to run them - no security manager).
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1571111 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/map-reduce/src/test/org/apache/solr/hadoop/MorphlineBasicMiniMRTest.java
+++ solr/contrib/map-reduce/src/test/org/apache/solr/hadoop/MorphlineBasicMiniMRTest.java
@@ -115,6 +115,7 @@ public class MorphlineBasicMiniMRTest extends SolrTestCaseJ4 {
     assumeFalse("HDFS tests were disabled by -Dtests.disableHdfs",
         Boolean.parseBoolean(System.getProperty("tests.disableHdfs", "false")));
     
+    assumeFalse("FIXME: This test does not work with Windows because of native library requirements", Constants.WINDOWS);
     assumeFalse("FIXME: This test fails under Java 8 due to the Saxon dependency - see SOLR-1301", Constants.JRE_IS_MINIMUM_JAVA8);
     assumeFalse("FIXME: This test fails under J9 due to the Saxon dependency - see SOLR-1301", System.getProperty("java.vm.info", "<?>").contains("IBM J9"));
     
