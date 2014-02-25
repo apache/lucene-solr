c0cc5946b5372a68231f22864421309172355956
Noble Paul <noble@apache.org>
2014-02-25 14:08:39 +0000

    SOLR-5069 fixing test failure
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1571702 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/CollectionsAPIDistributedZkTest.java
+++ solr/core/src/test/org/apache/solr/cloud/CollectionsAPIDistributedZkTest.java
@@ -1189,8 +1189,6 @@ public class CollectionsAPIDistributedZkTest extends AbstractFullDistribZkTestBa
     CloudSolrServer client = createCloudClient(null);
     try {
       createCollection(collectionName, client,2,2);
-
-      waitForRecoveriesToFinish(collectionName, false);
       String newReplicaName = Assign.assignNode(collectionName , client.getZkStateReader().getClusterState() );
       ArrayList<String> nodeList = new ArrayList<String>(client.getZkStateReader().getClusterState().getLiveNodes());
       Collections.shuffle(nodeList);
@@ -1270,7 +1268,8 @@ public class CollectionsAPIDistributedZkTest extends AbstractFullDistribZkTestBa
         MAX_SHARDS_PER_NODE, maxShardsPerNode,
         NUM_SLICES, numShards);
     Map<String,List<Integer>> collectionInfos = new HashMap<String,List<Integer>>();
-    createCollection(collectionInfos, COLL_NAME, props, client);
+    createCollection(collectionInfos, COLL_NAME, props, client,"conf1");
+    waitForRecoveriesToFinish(COLL_NAME, false);
   }
   
   @Override
