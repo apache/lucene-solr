8a083e46a5e8be21d6e0f7f6d54c47cbb35b19cf
Uwe Schindler <uschindler@apache.org>
2014-03-12 16:37:02 +0000

    LUCENE-5514: Update bytecode version
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1576812 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/expressions/src/java/org/apache/lucene/expressions/js/JavascriptCompiler.java
+++ lucene/expressions/src/java/org/apache/lucene/expressions/js/JavascriptCompiler.java
@@ -83,7 +83,7 @@ public class JavascriptCompiler {
     }
   }
   
-  private static final int CLASSFILE_VERSION = Opcodes.V1_6;
+  private static final int CLASSFILE_VERSION = Opcodes.V1_7;
   
   // We use the same class name for all generated classes as they all have their own class loader.
   // The source code is displayed as "source file name" in stack trace.
