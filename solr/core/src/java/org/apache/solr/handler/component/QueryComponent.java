67bc019bd9b1f042de7a1fbb9ced3afb1248cb83
Steven Rowe <sarowe@apache.org>
2014-03-17 15:24:54 +0000

    SOLR-5875: QueryComponent.mergeIds() unmarshals all docs' sort field values once per doc instead of once per shard.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1578434 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/handler/component/QueryComponent.java
+++ solr/core/src/java/org/apache/solr/handler/component/QueryComponent.java
@@ -904,6 +904,7 @@ public class QueryComponent extends SearchComponent
         numFound += docs.getNumFound();
 
         NamedList sortFieldValues = (NamedList)(srsp.getSolrResponse().getResponse().get("sort_values"));
+        NamedList unmarshalledSortFieldValues = unmarshalSortValues(ss, sortFieldValues, schema);
 
         // go through every doc in this response, construct a ShardDoc, and
         // put it in the priority queue so it can be ordered.
@@ -940,7 +941,7 @@ public class QueryComponent extends SearchComponent
             }
           }
 
-          shardDoc.sortFieldValues = unmarshalSortValues(ss, sortFieldValues, schema);
+          shardDoc.sortFieldValues = unmarshalledSortFieldValues;
 
           queue.insertWithOverflow(shardDoc);
         } // end for-each-doc-in-response
