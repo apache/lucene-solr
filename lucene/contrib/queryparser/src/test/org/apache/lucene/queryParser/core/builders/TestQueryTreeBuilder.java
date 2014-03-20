83437e422e75c474d06748ae691e2034473e542c
Michael McCandless <mikemccand@apache.org>
2014-03-20 19:16:30 +0000

    LUCENE-4890: get this test passing again
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_3_6@1579717 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/contrib/queryparser/src/test/org/apache/lucene/queryParser/core/builders/TestQueryTreeBuilder.java
+++ lucene/contrib/queryparser/src/test/org/apache/lucene/queryParser/core/builders/TestQueryTreeBuilder.java
@@ -1,4 +1,4 @@
-package org.apache.lucene.queryparser.flexible.core.builders;
+package org.apache.lucene.queryParser.core.builders;
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
