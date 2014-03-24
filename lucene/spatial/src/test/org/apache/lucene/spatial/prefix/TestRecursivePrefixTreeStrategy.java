f480ce33f70fad5e2ba6608cbb0837248058c355
David Wayne Smiley <dsmiley@apache.org>
2014-03-24 19:40:31 +0000

    LUCENE-4978: spatial grid false-negatives at edge
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_4_7@1581017 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/spatial/src/test/org/apache/lucene/spatial/prefix/TestRecursivePrefixTreeStrategy.java
+++ lucene/spatial/src/test/org/apache/lucene/spatial/prefix/TestRecursivePrefixTreeStrategy.java
@@ -19,9 +19,7 @@ package org.apache.lucene.spatial.prefix;
 
 import com.spatial4j.core.context.SpatialContext;
 import com.spatial4j.core.distance.DistanceUtils;
-import com.spatial4j.core.io.GeohashUtils;
 import com.spatial4j.core.shape.Point;
-import com.spatial4j.core.shape.Rectangle;
 import com.spatial4j.core.shape.Shape;
 import org.apache.lucene.spatial.SpatialMatchConcern;
 import org.apache.lucene.spatial.StrategyTestCase;
@@ -31,10 +29,7 @@ import org.apache.lucene.spatial.query.SpatialOperation;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class TestRecursivePrefixTreeStrategy extends StrategyTestCase {
@@ -104,75 +99,6 @@ public class TestRecursivePrefixTreeStrategy extends StrategyTestCase {
     checkHits(q(qPt, 34 * KM2DEG, distErrPct), 0, null);
   }
 
-  @Test
-  public void geohashRecursiveRandom() throws IOException {
-    init(12);
-
-    //1. Iterate test with the cluster at some worldly point of interest
-    Point[] clusterCenters = new Point[]{ctx.makePoint(-180,0), ctx.makePoint(0,90), ctx.makePoint(0,-90)};
-    for (Point clusterCenter : clusterCenters) {
-      //2. Iterate on size of cluster (a really small one and a large one)
-      String hashCenter = GeohashUtils.encodeLatLon(clusterCenter.getY(), clusterCenter.getX(), maxLength);
-      //calculate the number of degrees in the smallest grid box size (use for both lat & lon)
-      String smallBox = hashCenter.substring(0,hashCenter.length()-1);//chop off leaf precision
-      Rectangle clusterDims = GeohashUtils.decodeBoundary(smallBox,ctx);
-      double smallRadius = Math.max(clusterDims.getMaxX()-clusterDims.getMinX(),clusterDims.getMaxY()-clusterDims.getMinY());
-      assert smallRadius < 1;
-      double largeRadius = 20d;//good large size; don't use >=45 for this test code to work
-      double[] radiusDegs = {largeRadius,smallRadius};
-      for (double radiusDeg : radiusDegs) {
-        //3. Index random points in this cluster circle
-        deleteAll();
-        List<Point> points = new ArrayList<Point>();
-        for(int i = 0; i < 20; i++) {
-          //Note that this will not result in randomly distributed points in the
-          // circle, they will be concentrated towards the center a little. But
-          // it's good enough.
-          Point pt = ctx.getDistCalc().pointOnBearing(clusterCenter,
-              random().nextDouble() * radiusDeg, random().nextInt() * 360, ctx, null);
-          pt = alignGeohash(pt);
-          points.add(pt);
-          addDocument(newDoc("" + i, pt));
-        }
-        commit();
-
-        //3. Use some query centers. Each is twice the cluster's radius away.
-        for(int ri = 0; ri < 4; ri++) {
-          Point queryCenter = ctx.getDistCalc().pointOnBearing(clusterCenter,
-              radiusDeg*2, random().nextInt(360), ctx, null);
-          queryCenter = alignGeohash(queryCenter);
-          //4.1 Query a small box getting nothing
-          checkHits(q(queryCenter, radiusDeg - smallRadius/2), 0, null);
-          //4.2 Query a large box enclosing the cluster, getting everything
-          checkHits(q(queryCenter, radiusDeg*3 + smallRadius/2), points.size(), null);
-          //4.3 Query a medium box getting some (calculate the correct solution and verify)
-          double queryDist = radiusDeg * 2;
-
-          //Find matching points.  Put into int[] of doc ids which is the same thing as the index into points list.
-          int[] ids = new int[points.size()];
-          int ids_sz = 0;
-          for (int i = 0; i < points.size(); i++) {
-            Point point = points.get(i);
-            if (ctx.getDistCalc().distance(queryCenter, point) <= queryDist)
-              ids[ids_sz++] = i;
-          }
-          ids = Arrays.copyOf(ids, ids_sz);
-          //assert ids_sz > 0 (can't because randomness keeps us from being able to)
-
-          checkHits(q(queryCenter, queryDist), ids.length, ids);
-        }
-
-      }//for radiusDeg
-
-    }//for clusterCenter
-
-  }//randomTest()
-
-  /** Query point-distance (in degrees) with zero error percent. */
-  private SpatialArgs q(Point pt, double distDEG) {
-    return q(pt, distDEG, 0.0);
-  }
-
   private SpatialArgs q(Point pt, double distDEG, double distErrPct) {
     Shape shape = ctx.makeCircle(pt, distDEG);
     SpatialArgs args = new SpatialArgs(SpatialOperation.Intersects,shape);
@@ -194,8 +120,4 @@ public class TestRecursivePrefixTreeStrategy extends StrategyTestCase {
     }
   }
 
-  /** NGeohash round-trip for given precision. */
-  private Point alignGeohash(Point p) {
-    return GeohashUtils.decode(GeohashUtils.encodeLatLon(p.getY(), p.getX(), maxLength), ctx);
-  }
 }
