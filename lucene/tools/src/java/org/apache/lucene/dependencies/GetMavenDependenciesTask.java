71336c5f3c321b5ae40c9a3e8050f1d65173977d
Steven Rowe <sarowe@apache.org>
2014-04-03 05:37:27 +0000

    SOLR-5920: Maven config: restrict conversion of module names containing 'analysis-' to 'analyzers-' to those not preceded by 'solr-' (merged trunk r1584260)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_4_7@1584262 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/tools/src/java/org/apache/lucene/dependencies/GetMavenDependenciesTask.java
+++ lucene/tools/src/java/org/apache/lucene/dependencies/GetMavenDependenciesTask.java
@@ -151,7 +151,7 @@ public class GetMavenDependenciesTask extends Task {
   }
 
   public void setVerbose(boolean verbose) {
-    verboseLevel = (verbose ? Project.MSG_INFO : Project.MSG_VERBOSE);
+    verboseLevel = (verbose ? Project.MSG_VERBOSE : Project.MSG_INFO);
   }
 
   public void setCentralizedVersionsFile(File file) {
@@ -615,7 +615,7 @@ public class GetMavenDependenciesTask extends Task {
       // Pattern.compile("(lucene|solr)/build/(.*)/classes/java");
       String artifact = matcher.group(2);
       artifact = artifact.replace('/', '-');
-      artifact = artifact.replace("analysis-", "analyzers-");
+      artifact = artifact.replace("(?<!solr-)analysis-", "analyzers-");
       if ("lucene".equals(matcher.group(1))) {
         artifactId.append("lucene-");
       }
