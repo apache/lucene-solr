05bb3e8673d60708a160c1ffa4eab173194df61c
Uwe Schindler <uschindler@apache.org>
2014-04-11 05:39:28 +0000

    Move to Lucene 4.9 on stable branch
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1586576 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -96,6 +96,13 @@ public enum Version {
   @Deprecated
   LUCENE_48,
 
+  /**
+   * Match settings and bugs in Lucene's 4.9 release.
+   * @deprecated (5.0) Use latest
+   */
+  @Deprecated
+  LUCENE_49,
+
   /** Match settings and bugs in Lucene's 5.0 release.
    *  <p>
    *  Use this to get the latest &amp; greatest settings, bug
