d3a80e52c109480b8f26145ec3600d0c1e1721a3
Robert Muir <rmuir@apache.org>
2014-05-02 02:54:53 +0000

    LUCENE-5611: lazy-init the stored fields writer like before to prevent leaks
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1591808 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/index/TestIndexWriter.java
+++ lucene/core/src/test/org/apache/lucene/index/TestIndexWriter.java
@@ -2254,7 +2254,6 @@ public class TestIndexWriter extends LuceneTestCase {
     IOUtils.close(reader, w, dir);
   }
   
-  @AwaitsFix(bugUrl = "https://issues.apache.org/jira/browse/LUCENE-5611")
   public void testIterableThrowsException2() throws IOException {
     Directory dir = newDirectory();
     IndexWriter w = new IndexWriter(dir, newIndexWriterConfig(
