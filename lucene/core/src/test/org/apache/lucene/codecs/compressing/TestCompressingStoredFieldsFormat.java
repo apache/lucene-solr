ee1bfe16cd64cf1e6ab96052310c4a72df0a423c
Michael McCandless <mikemccand@apache.org>
2014-05-08 21:57:28 +0000

    fix test bug
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1593431 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/codecs/compressing/TestCompressingStoredFieldsFormat.java
+++ lucene/core/src/test/org/apache/lucene/codecs/compressing/TestCompressingStoredFieldsFormat.java
@@ -27,6 +27,7 @@ import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.IntField;
 import org.apache.lucene.index.BaseStoredFieldsFormatTestCase;
+import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.RandomIndexWriter;
 import org.apache.lucene.store.Directory;
@@ -52,7 +53,9 @@ public class TestCompressingStoredFieldsFormat extends BaseStoredFieldsFormatTes
     // disable CFS because this test checks file names
     iwConf.setMergePolicy(newLogMergePolicy(false));
     iwConf.setUseCompoundFile(false);
-    RandomIndexWriter iw = new RandomIndexWriter(random(), dir, iwConf);
+
+    // Cannot use RIW because this test wants CFS to stay off:
+    IndexWriter iw = new IndexWriter(dir, iwConf);
 
     final Document validDoc = new Document();
     validDoc.add(new IntField("id", 0, Store.YES));
@@ -67,6 +70,8 @@ public class TestCompressingStoredFieldsFormat extends BaseStoredFieldsFormatTes
       
       @Override
       public String stringValue() {
+        // TODO: really bad & scary that this causes IW to
+        // abort the segment!!  We should fix this.
         return null;
       }
       
