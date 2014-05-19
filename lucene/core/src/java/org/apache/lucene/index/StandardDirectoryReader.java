d01f0838e409ba12d6d7ac652a8142562af036f5
Michael McCandless <mikemccand@apache.org>
2014-05-19 20:16:52 +0000

    woops, revert
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1596043 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/index/StandardDirectoryReader.java
+++ lucene/core/src/java/org/apache/lucene/index/StandardDirectoryReader.java
@@ -148,9 +148,7 @@ final class StandardDirectoryReader extends DirectoryReader {
     // remember which readers are shared between the old and the re-opened
     // DirectoryReader - we have to incRef those readers
     boolean[] readerShared = new boolean[infos.size()];
-
-    System.out.println("CYCLE infos=" + infos.size());
-
+    
     for (int i = infos.size() - 1; i>=0; i--) {
       // find SegmentReader for this segment
       Integer oldReaderIndex = segmentReaders.get(infos.info(i).info.name);
@@ -161,7 +159,6 @@ final class StandardDirectoryReader extends DirectoryReader {
         // there is an old reader for this segment - we'll try to reopen it
         newReaders[i] = (SegmentReader) oldReaders.get(oldReaderIndex.intValue());
       }
-      System.out.println("  i=" + i + " newReaders[i]=" + newReaders[i]);
 
       boolean success = false;
       Throwable prior = null;
@@ -174,7 +171,6 @@ final class StandardDirectoryReader extends DirectoryReader {
           readerShared[i] = false;
           newReaders[i] = newReader;
         } else {
-          System.out.println("    now in else");
           if (newReaders[i].getSegmentInfo().getDelGen() == infos.info(i).getDelGen()
               && newReaders[i].getSegmentInfo().getFieldInfosGen() == infos.info(i).getFieldInfosGen()) {
             // No change; this reader will be shared between
@@ -183,7 +179,6 @@ final class StandardDirectoryReader extends DirectoryReader {
             readerShared[i] = true;
             newReaders[i].incRef();
           } else {
-            System.out.println("    has changes");
             // there are changes to the reader, either liveDocs or DV updates
             readerShared[i] = false;
             // Steal the ref returned by SegmentReader ctor:
@@ -196,7 +191,6 @@ final class StandardDirectoryReader extends DirectoryReader {
               // both DV and liveDocs have changed
               newReaders[i] = new SegmentReader(infos.info(i), newReaders[i]);
             }
-            System.out.println("    done open");
           }
         }
         success = true;
@@ -206,7 +200,6 @@ final class StandardDirectoryReader extends DirectoryReader {
         if (!success) {
           for (i++; i < infos.size(); i++) {
             if (newReaders[i] != null) {
-              System.out.println("  decRef i=" + i);
               try {
                 if (!readerShared[i]) {
                   // this is a new subReader that is not used by the old one,
