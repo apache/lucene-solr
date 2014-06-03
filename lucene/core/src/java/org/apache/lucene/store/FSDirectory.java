0b14562930c7e2e8999f24dcf77c7a3ce388a03d
Robert Muir <rmuir@apache.org>
2014-06-03 18:32:18 +0000

    LUCENE-5730: FSDirectory.open should return mmap for 64-bit OS X
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1599732 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/store/FSDirectory.java
+++ lucene/core/src/java/org/apache/lucene/store/FSDirectory.java
@@ -176,8 +176,7 @@ public abstract class FSDirectory extends BaseDirectory {
   /** Just like {@link #open(File)}, but allows you to
    *  also specify a custom {@link LockFactory}. */
   public static FSDirectory open(File path, LockFactory lockFactory) throws IOException {
-    if ((Constants.WINDOWS || Constants.SUN_OS || Constants.LINUX)
-          && Constants.JRE_IS_64BIT && MMapDirectory.UNMAP_SUPPORTED) {
+    if (Constants.JRE_IS_64BIT && MMapDirectory.UNMAP_SUPPORTED) {
       return new MMapDirectory(path, lockFactory);
     } else if (Constants.WINDOWS) {
       return new SimpleFSDirectory(path, lockFactory);
