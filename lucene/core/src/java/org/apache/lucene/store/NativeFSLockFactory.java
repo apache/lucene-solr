2887b927a33d8b9bd74a6d01b2856f47bcd91add
Simon Willnauer <simonw@apache.org>
2014-06-06 09:09:06 +0000

    LUCENE-5738: Ensure NativeFSLock prevents opening the file channel twice if lock is held
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1600831 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/store/NativeFSLockFactory.java
+++ lucene/core/src/java/org/apache/lucene/store/NativeFSLockFactory.java
@@ -23,6 +23,9 @@ import java.nio.channels.OverlappingFileLockException;
 import java.nio.file.StandardOpenOption;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.lucene.util.IOUtils;
 
@@ -117,11 +120,14 @@ class NativeFSLock extends Lock {
   private FileLock lock;
   private File path;
   private File lockDir;
+  private static final Set<String> LOCK_HELD = Collections.synchronizedSet(new HashSet<String>());
+
 
   public NativeFSLock(File lockDir, String lockFileName) {
     this.lockDir = lockDir;
     path = new File(lockDir, lockFileName);
   }
+
 
   @Override
   public synchronized boolean obtain() throws IOException {
@@ -141,48 +147,71 @@ class NativeFSLock extends Lock {
       throw new IOException("Found regular file where directory expected: " + 
           lockDir.getAbsolutePath());
     }
-    
-    channel = FileChannel.open(path.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-    boolean success = false;
-    try {
-      lock = channel.tryLock();
-      success = lock != null;
-    } catch (IOException | OverlappingFileLockException e) {
-      // At least on OS X, we will sometimes get an
-      // intermittent "Permission Denied" IOException,
-      // which seems to simply mean "you failed to get
-      // the lock".  But other IOExceptions could be
-      // "permanent" (eg, locking is not supported via
-      // the filesystem).  So, we record the failure
-      // reason here; the timeout obtain (usually the
-      // one calling us) will use this as "root cause"
-      // if it fails to get the lock.
-      failureReason = e;
-    } finally {
-      if (!success) {
+    final String canonicalPath = path.getCanonicalPath();
+    // Make sure nobody else in-process has this lock held
+    // already, and, mark it held if not:
+    // This is a pretty crazy workaround for some documented
+    // but yet awkward JVM behavior:
+    //
+    //   On some systems, closing a channel releases all locks held by the Java virtual machine on the underlying file
+    //   regardless of whether the locks were acquired via that channel or via another channel open on the same file.
+    //   It is strongly recommended that, within a program, a unique channel be used to acquire all locks on any given
+    //   file.
+    //
+    // This essentially means if we close "A" channel for a given file all locks might be released... the odd part
+    // is that we can't re-obtain the lock in the same JVM but from a different process if that happens. Nevertheless
+    // this is super trappy. See LUCENE-5738
+    boolean obtained = false;
+    if (LOCK_HELD.add(canonicalPath)) {
+      try {
+        channel = FileChannel.open(path.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
         try {
-          IOUtils.closeWhileHandlingException(channel);
-        } finally {
+          lock = channel.tryLock();
+          obtained = lock != null;
+        } catch (IOException | OverlappingFileLockException e) {
+          // At least on OS X, we will sometimes get an
+          // intermittent "Permission Denied" IOException,
+          // which seems to simply mean "you failed to get
+          // the lock".  But other IOExceptions could be
+          // "permanent" (eg, locking is not supported via
+          // the filesystem).  So, we record the failure
+          // reason here; the timeout obtain (usually the
+          // one calling us) will use this as "root cause"
+          // if it fails to get the lock.
+          failureReason = e;
+        }
+      } finally {
+        if (obtained == false) { // not successful - clear up and move out
+          clearLockHeld(path);
+          final FileChannel toClose = channel;
           channel = null;
+          IOUtils.closeWhileHandlingException(toClose);
         }
       }
     }
-    return lock != null;
+    return obtained;
   }
 
   @Override
   public synchronized void close() throws IOException {
     try {
       if (lock != null) {
-        lock.release();
-        lock = null;
+        try {
+          lock.release();
+          lock = null;
+        } finally {
+          clearLockHeld(path);
+        }
       }
     } finally {
-      if (channel != null) {
-        channel.close();
-        channel = null;
-      }
+      IOUtils.close(channel);
+      channel = null;
     }
+  }
+
+  private static final void clearLockHeld(File path) throws IOException {
+    boolean remove = LOCK_HELD.remove(path.getCanonicalPath());
+    assert remove : "Lock was cleared but never marked as held";
   }
 
   @Override
