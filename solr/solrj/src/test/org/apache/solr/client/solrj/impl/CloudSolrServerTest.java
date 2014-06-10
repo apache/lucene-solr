386d11c5633643b35c9be10b7c138b8497ca62cc
Shalin Shekhar Mangar <shalin@apache.org>
2014-06-10 12:37:41 +0000

    SOLR-6146: Incorrect configuration such as wrong chroot in zk server address can cause CloudSolrServer to leak resources
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1601621 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/client/solrj/impl/CloudSolrServerTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/impl/CloudSolrServerTest.java
@@ -57,6 +57,7 @@ import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.params.ShardParams;
 import org.apache.solr.common.util.NamedList;
+import org.apache.zookeeper.KeeperException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -359,4 +360,20 @@ public class CloudSolrServerTest extends AbstractFullDistribZkTestBase {
     }
   }
 
+  public void testWrongZkChrootTest() throws MalformedURLException {
+    CloudSolrServer server = null;
+    try {
+      server = new CloudSolrServer(zkServer.getZkAddress() + "/xyz/foo");
+      server.setDefaultCollection(DEFAULT_COLLECTION);
+      server.setZkClientTimeout(1000*60);
+      server.connect();
+      fail("Expected exception");
+    } catch(SolrException e) {
+      assertTrue(e.getCause() instanceof KeeperException);
+    } finally {
+      server.shutdown();
+    }
+    // see SOLR-6146 - this test will fail by virtue of the zkClient tracking performed
+    // in the afterClass method of the base class
+  }
 }
