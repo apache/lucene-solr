983d1d4e3a21e50d9c1dbe09f1a1f0efb8ef68a2
Shalin Shekhar Mangar <shalin@apache.org>
2014-06-10 12:38:39 +0000

    SOLR-6146: Incorrect configuration such as wrong chroot in zk server address can cause CloudSolrServer to leak resources
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1601622 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrServer.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrServer.java
@@ -236,24 +236,33 @@ public class CloudSolrServer extends SolrServer {
     if (zkStateReader == null) {
       synchronized (this) {
         if (zkStateReader == null) {
+          ZkStateReader zk = null;
           try {
-            ZkStateReader zk = new ZkStateReader(zkHost, zkClientTimeout,
+            zk = new ZkStateReader(zkHost, zkClientTimeout,
                 zkConnectTimeout);
             zk.createClusterStateWatchersAndUpdate();
             zkStateReader = zk;
           } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
           } catch (KeeperException e) {
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
           } catch (IOException e) {
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
           } catch (TimeoutException e) {
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
+          } catch (Exception e) {
+            if (zk != null) zk.close();
+            // do not wrap because clients may be relying on the underlying exception being thrown
+            throw e;
           }
         }
       }
