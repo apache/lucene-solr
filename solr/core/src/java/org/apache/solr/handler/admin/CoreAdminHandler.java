1a67cb58d90401c513c12668aff2d74510477ea8
Alan Woodward <romseygeek@apache.org>
2014-06-16 09:40:39 +0000

    SOLR-6169: Remove broken CoreAdminHandler handleAlias action
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1602830 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/handler/admin/CoreAdminHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/CoreAdminHandler.java
@@ -629,20 +629,7 @@ public class CoreAdminHandler extends RequestHandlerBase {
    */
   @Deprecated
   protected void handleAliasAction(SolrQueryRequest req, SolrQueryResponse rsp) {
-    SolrParams params = req.getParams();
-
-    String name = params.get(CoreAdminParams.OTHER);
-    String cname = params.get(CoreAdminParams.CORE);
-    boolean doPersist = false;
-    if (cname.equals(name)) return;
-
-    SolrCore core = coreContainer.getCore(cname);
-    if (core != null) {
-      doPersist = coreContainer.isPersistent();
-      coreContainer.register(name, core, false);
-      // no core.close() since each entry in the cores map should increase the ref
-    }
-    return;
+    throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, "The ALIAS action is no longer supported");
   }
 
 
