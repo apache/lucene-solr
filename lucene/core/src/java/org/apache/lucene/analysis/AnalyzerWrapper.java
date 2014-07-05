afe033878a1a6870acd7da3a3d429ba8121ecf30
Uwe Schindler <uschindler@apache.org>
2014-07-05 12:19:51 +0000

    LUCENE-5803: Add more Javadocs to AnalyzerWrapper to encourage people to use DelegatingAnalyzerWrapper (if possible). Add changes entry for Solr.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1608005 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/analysis/AnalyzerWrapper.java
+++ lucene/core/src/java/org/apache/lucene/analysis/AnalyzerWrapper.java
@@ -22,13 +22,24 @@ import java.io.Reader;
 /**
  * Extension to {@link Analyzer} suitable for Analyzers which wrap
  * other Analyzers.
- * <p/>
- * {@link #getWrappedAnalyzer(String)} allows the Analyzer
+ * 
+ * <p>{@link #getWrappedAnalyzer(String)} allows the Analyzer
  * to wrap multiple Analyzers which are selected on a per field basis.
- * <p/>
- * {@link #wrapComponents(String, Analyzer.TokenStreamComponents)} allows the
+ * 
+ * <p>{@link #wrapComponents(String, Analyzer.TokenStreamComponents)} allows the
  * TokenStreamComponents of the wrapped Analyzer to then be wrapped
  * (such as adding a new {@link TokenFilter} to form new TokenStreamComponents.
+ *
+ * <p>{@link #wrapReader(String, Reader)} allows the Reader of the wrapped
+ * Analyzer to then be wrapped (such as adding a new {@link CharFilter}.
+ *
+ * <p><b>Important:</b> If you do not want to wrap the TokenStream
+ * using {@link #wrapComponents(String, Analyzer.TokenStreamComponents)}
+ * or the Reader using {@link #wrapReader(String, Reader)} and just delegate
+ * to other analyzers (like by field name), use {@link DelegatingAnalyzerWrapper}
+ * as superclass!
+ *
+ * @see DelegatingAnalyzerWrapper
  */
 public abstract class AnalyzerWrapper extends Analyzer {
 
