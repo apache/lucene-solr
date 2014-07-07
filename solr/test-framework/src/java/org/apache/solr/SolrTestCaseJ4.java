0f8e3fd9731ef1d96a89d6c9825c7a425e4a065e
Shalin Shekhar Mangar <shalin@apache.org>
2014-07-07 19:01:46 +0000

    SOLR-5596: Set system property zookeeper.forceSync=no for Solr test cases
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1608555 13f79535-47bb-0310-9956-ffa450edef68

--- solr/test-framework/src/java/org/apache/solr/SolrTestCaseJ4.java
+++ solr/test-framework/src/java/org/apache/solr/SolrTestCaseJ4.java
@@ -155,7 +155,8 @@ public abstract class SolrTestCaseJ4 extends LuceneTestCase {
     initCoreDataDir = createTempDir("init-core-data");
 
     System.err.println("Creating dataDir: " + initCoreDataDir.getAbsolutePath());
-    
+
+    System.setProperty("zookeeper.forceSync", "no");
     System.setProperty("jetty.testMode", "true");
     System.setProperty("enable.update.log", usually() ? "true" : "false");
     System.setProperty("tests.shardhandler.randomSeed", Long.toString(random().nextLong()));
@@ -187,6 +188,7 @@ public abstract class SolrTestCaseJ4 extends LuceneTestCase {
       coreName = ConfigSolrXmlOld.DEFAULT_DEFAULT_CORE_NAME;
     } finally {
       initCoreDataDir = null;
+      System.clearProperty("zookeeper.forceSync");
       System.clearProperty("jetty.testMode");
       System.clearProperty("tests.shardhandler.randomSeed");
       System.clearProperty("enable.update.log");
