9461d29da2580c717298b99833985f393bde9fac
Robert Muir <rmuir@apache.org>
2014-07-14 10:46:26 +0000

    LUCENE-5810: fix MDW logic around fsync, now that NRTCachingDirectory extends FilterDirectory
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1610381 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/store/MockDirectoryWrapper.java
+++ lucene/test-framework/src/java/org/apache/lucene/store/MockDirectoryWrapper.java
@@ -192,6 +192,9 @@ public class MockDirectoryWrapper extends BaseDirectoryWrapper {
   private boolean mustSync() {
     Directory delegate = in;
     while (delegate instanceof FilterDirectory) {
+      if (delegate instanceof NRTCachingDirectory) {
+        return true;
+      }
       delegate = ((FilterDirectory) delegate).getDelegate();
     }
     return delegate instanceof NRTCachingDirectory;
