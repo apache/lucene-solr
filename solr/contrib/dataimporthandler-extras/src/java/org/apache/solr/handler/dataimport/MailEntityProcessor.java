899d57f418200444cb9efeaf328fb6bfdba7773b
Timothy Potter <thelabdude@apache.org>
2014-07-16 02:51:54 +0000

    SOLR-2245: fix for issues found by precommit checks
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1610905 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/dataimporthandler-extras/src/java/org/apache/solr/handler/dataimport/MailEntityProcessor.java
+++ solr/contrib/dataimporthandler-extras/src/java/org/apache/solr/handler/dataimport/MailEntityProcessor.java
@@ -52,9 +52,9 @@ import com.sun.mail.gimap.GmailRawSearchTerm;
 public class MailEntityProcessor extends EntityProcessorBase {
   
   private static final SimpleDateFormat sinceDateParser = 
-      new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ROOT);
   private static final SimpleDateFormat afterFmt = 
-      new SimpleDateFormat("yyyy/MM/dd");
+      new SimpleDateFormat("yyyy/MM/dd", Locale.ROOT);
   
   public static interface CustomFilter {
     public SearchTerm getCustomSearch(Folder folder);
@@ -226,7 +226,7 @@ public class MailEntityProcessor extends EntityProcessorBase {
       addEnvelopeToDocument(part, row);
     }
     
-    String ct = part.getContentType().toLowerCase();
+    String ct = part.getContentType().toLowerCase(Locale.ROOT);
     ContentType ctype = new ContentType(ct);
     if (part.isMimeType("multipart/*")) {
       Object content = part.getContent();
