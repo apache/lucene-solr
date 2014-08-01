57b947d183657ad45d55bc2d89daca9f90e18eb3
Shalin Shekhar Mangar <shalin@apache.org>
2014-08-01 06:27:52 +0000

    Fix message in stop.sh
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1615040 13f79535-47bb-0310-9956-ffa450edef68

--- solr/cloud-dev/stop.sh
+++ solr/cloud-dev/stop.sh
@@ -17,7 +17,7 @@ java -DSTOP.PORT=7983 -DSTOP.KEY=key -jar start.jar --stop
 
 for (( i=2; i <= $numServers; i++ ))
 do
-  echo "starting example$i"
+  echo "stopping example$i"
   cd ../example$i
   stopPort=`expr $baseStopPort + $i`
   java -DSTOP.PORT=$stopPort -DSTOP.KEY=key -jar start.jar --stop
