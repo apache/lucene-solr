a73cdfaf07436162496a95d638a2f989ae1d9de0
Michael McCandless <mikemccand@apache.org>
2014-08-11 16:15:09 +0000

    LUCENE-5875: reduce page size of backing packed array used by NodeHash when building an FST from 1B to 128M values
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1617318 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/fst/NodeHash.java
+++ lucene/core/src/java/org/apache/lucene/util/fst/NodeHash.java
@@ -33,7 +33,7 @@ final class NodeHash<T> {
   private final FST.BytesReader in;
 
   public NodeHash(FST<T> fst, FST.BytesReader in) {
-    table = new PagedGrowableWriter(16, 1<<30, 8, PackedInts.COMPACT);
+    table = new PagedGrowableWriter(16, 1<<27, 8, PackedInts.COMPACT);
     mask = 15;
     this.fst = fst;
     this.in = in;
