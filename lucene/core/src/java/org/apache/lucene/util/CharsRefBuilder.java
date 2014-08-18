b08f57d7c2e3bec40c6e672d0142dfb385c5ff34
Adrien Grand <jpountz@apache.org>
2014-08-18 15:04:10 +0000

    LUCENE-5892: Missed the covariant return type.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1618628 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/CharsRefBuilder.java
+++ lucene/core/src/java/org/apache/lucene/util/CharsRefBuilder.java
@@ -68,7 +68,7 @@ public class CharsRefBuilder implements Appendable {
   }
 
   @Override
-  public Appendable append(CharSequence csq) {
+  public CharsRefBuilder append(CharSequence csq) {
     if (csq == null) {
       return append(NULL_STRING);
     }
@@ -76,7 +76,7 @@ public class CharsRefBuilder implements Appendable {
   }
 
   @Override
-  public Appendable append(CharSequence csq, int start, int end) {
+  public CharsRefBuilder append(CharSequence csq, int start, int end) {
     if (csq == null) {
       return append(NULL_STRING);
     }
@@ -88,7 +88,7 @@ public class CharsRefBuilder implements Appendable {
   }
 
   @Override
-  public Appendable append(char c) {
+  public CharsRefBuilder append(char c) {
     grow(ref.length + 1);
     setCharAt(ref.length++, c);
     return this;
