c01e7ea99cc1c0a694f670883342e8d920e89a4f
Simon Willnauer <simonw@apache.org>
2014-08-20 19:36:20 +0000

    LUCENE-5896: Use local random instance rather than global in LuceneTestCase
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1619211 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/util/LuceneTestCase.java
+++ lucene/test-framework/src/java/org/apache/lucene/util/LuceneTestCase.java
@@ -892,8 +892,8 @@ public abstract class LuceneTestCase extends Assert {
     if (r.nextBoolean()) {
       c.setMergeScheduler(new SerialMergeScheduler());
     } else if (rarely(r)) {
-      int maxThreadCount = TestUtil.nextInt(random(), 1, 4);
-      int maxMergeCount = TestUtil.nextInt(random(), maxThreadCount, maxThreadCount + 4);
+      int maxThreadCount = TestUtil.nextInt(r, 1, 4);
+      int maxMergeCount = TestUtil.nextInt(r, maxThreadCount, maxThreadCount + 4);
       ConcurrentMergeScheduler cms = new ConcurrentMergeScheduler();
       cms.setMaxMergesAndThreads(maxMergeCount, maxThreadCount);
       c.setMergeScheduler(cms);
@@ -975,7 +975,7 @@ public abstract class LuceneTestCase extends Assert {
       mergePolicy.setNoCFSRatio(r.nextBoolean() ? 1.0 : 0.0);
     }
     
-    if (rarely()) {
+    if (rarely(r)) {
       mergePolicy.setMaxCFSSegmentSizeMB(0.2 + r.nextDouble() * 2.0);
     } else {
       mergePolicy.setMaxCFSSegmentSizeMB(Double.POSITIVE_INFINITY);
@@ -1045,7 +1045,7 @@ public abstract class LuceneTestCase extends Assert {
           flushByRAM = false;
           break;
         case EITHER:
-          flushByRAM = random().nextBoolean();
+          flushByRAM = r.nextBoolean();
           break;
         default:
           throw new AssertionError();
