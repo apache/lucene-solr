44661b026031772796c0529a97e2be82eba3896c
Mark Robert Miller <markrmiller@apache.org>
2014-08-24 17:26:51 +0000

    SOLR-6418: ChaosMonkeySafeLeaderTest is too slow on non nightly runs sometimes.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1620164 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/ChaosMonkeySafeLeaderTest.java
+++ solr/core/src/test/org/apache/solr/cloud/ChaosMonkeySafeLeaderTest.java
@@ -117,8 +117,13 @@ public class ChaosMonkeySafeLeaderTest extends AbstractFullDistribZkTestBase {
       if (RUN_LENGTH != -1) {
         runLength = RUN_LENGTH;
       } else {
-        int[] runTimes = new int[] {5000, 6000, 10000, 25000, 27000, 30000,
-            30000, 45000, 90000, 120000};
+        int[] runTimes;
+        if (TEST_NIGHTLY) {
+          runTimes = new int[] {5000, 6000, 10000, 15000, 25000, 30000,
+              30000, 45000, 90000, 120000};
+        } else {
+          runTimes = new int[] {5000, 7000, 15000};
+        }
         runLength = runTimes[random().nextInt(runTimes.length - 1)];
       }
       
