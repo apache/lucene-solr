5460fa9008ebaec3fc1d11248db0e118079cb1f5
Robert Muir <rmuir@apache.org>
2014-09-02 17:27:34 +0000

    fix test bug
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1622068 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/index/TestCompoundFile.java
+++ lucene/core/src/test/org/apache/lucene/index/TestCompoundFile.java
@@ -809,6 +809,11 @@ public class TestCompoundFile extends LuceneTestCase
   
   public void testListAll() throws Exception {
     Directory dir = newDirectory();
+    if (dir instanceof MockDirectoryWrapper) {
+      // test lists files manually and tries to verify every .cfs it finds,
+      // but a virus scanner could leave some trash.
+      ((MockDirectoryWrapper)dir).setEnableVirusScanner(false);
+    }
     // riw should sometimes create docvalues fields, etc
     RandomIndexWriter riw = new RandomIndexWriter(random(), dir);
     Document doc = new Document();
