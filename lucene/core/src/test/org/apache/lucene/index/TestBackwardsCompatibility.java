df680a78f7c0eb98e389234370ef88dd9afd518c
Robert Muir <rmuir@apache.org>
2014-09-10 15:25:28 +0000

    LUCENE-5863: add 4.8
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1624030 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
+++ lucene/core/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
@@ -185,6 +185,8 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
                                     "45.nocfs",
                                     "461.cfs",
                                     "461.nocfs",
+                                    "48.cfs",
+                                    "48.nocfs",
                                     "481.cfs",
                                     "481.nocfs",
                                     "49.cfs",
