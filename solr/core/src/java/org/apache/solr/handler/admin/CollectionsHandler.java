451744546759e6c8bc89ec3122e467d296759c2b
Chris M. Hostetter <hossman@apache.org>
2014-09-12 21:12:20 +0000

    SOLR-5473, SOLR-5474, SOLR-5810 ... NO SVN KEYWORDS! ! !
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1624650 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/handler/admin/CollectionsHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/CollectionsHandler.java
@@ -674,8 +674,4 @@ public class CollectionsHandler extends RequestHandlerBase {
     return "Manage SolrCloud Collections";
   }
 
-  @Override
-  public String getSource() {
-    return "$URL: https://svn.apache.org/repos/asf/lucene/dev/trunk/solr/core/src/java/org/apache/solr/handler/admin/CollectionHandler.java $";
-  }
 }
