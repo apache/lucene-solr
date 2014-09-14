3bb1cfb449efd0b9db8e565fb791f327b9f2cdce
Uwe Schindler <uschindler@apache.org>
2014-09-14 23:26:17 +0000

    Don't use a SimpleFSLockFactory for RAMDirectoryFactory in Solr (fails tests in trunk)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1624924 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/core/RAMDirectoryFactoryTest.java
+++ solr/core/src/test/org/apache/solr/core/RAMDirectoryFactoryTest.java
@@ -43,8 +43,8 @@ public class RAMDirectoryFactoryTest extends LuceneTestCase {
       }
     };
     String path = "/fake/path";
-    Directory dir1 = factory.get(path, DirContext.DEFAULT, null);
-    Directory dir2 = factory.get(path, DirContext.DEFAULT, null);
+    Directory dir1 = factory.get(path, DirContext.DEFAULT, "single");
+    Directory dir2 = factory.get(path, DirContext.DEFAULT, "single");
     assertEquals("RAMDirectoryFactory should not create new instance of RefCntRamDirectory " +
         "every time open() is called for the same path", dir1, dir2);
 
@@ -54,7 +54,7 @@ public class RAMDirectoryFactoryTest extends LuceneTestCase {
 
   private void dotestOpenSucceedForEmptyDir() throws IOException {
     RAMDirectoryFactory factory = new RAMDirectoryFactory();
-    Directory dir = factory.get("/fake/path", DirContext.DEFAULT, null);
+    Directory dir = factory.get("/fake/path", DirContext.DEFAULT, "single");
     assertNotNull("RAMDirectoryFactory should create RefCntRamDirectory even if the path doen't lead " +
         "to index directory on the file system", dir);
     factory.release(dir);
