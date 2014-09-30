2edd352cdd3abc46d75100c3903aac0b44effa0f
Shai Erera <shaie@apache.org>
2014-09-30 11:56:21 +0000

    init new example class before each example is run
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1628404 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/demo/src/java/org/apache/lucene/demo/facet/SimpleFacetsExample.java
+++ lucene/demo/src/java/org/apache/lucene/demo/facet/SimpleFacetsExample.java
@@ -228,24 +228,24 @@ public class SimpleFacetsExample {
   public static void main(String[] args) throws Exception {
     System.out.println("Facet counting example:");
     System.out.println("-----------------------");
-    SimpleFacetsExample example1 = new SimpleFacetsExample();
-    List<FacetResult> results1 = example1.runFacetOnly();
+    SimpleFacetsExample example = new SimpleFacetsExample();
+    List<FacetResult> results1 = example.runFacetOnly();
     System.out.println("Author: " + results1.get(0));
     System.out.println("Publish Date: " + results1.get(1));
     
     System.out.println("Facet counting example (combined facets and search):");
     System.out.println("-----------------------");
-    SimpleFacetsExample example = new SimpleFacetsExample();
+    example = new SimpleFacetsExample();
     List<FacetResult> results = example.runSearch();
     System.out.println("Author: " + results.get(0));
     System.out.println("Publish Date: " + results.get(1));
     
-    System.out.println("\n");
+    example = new SimpleFacetsExample();
     System.out.println("Facet drill-down example (Publish Date/2010):");
     System.out.println("---------------------------------------------");
     System.out.println("Author: " + example.runDrillDown());
 
-    System.out.println("\n");
+    example = new SimpleFacetsExample();
     System.out.println("Facet drill-sideways example (Publish Date/2010):");
     System.out.println("---------------------------------------------");
     for(FacetResult result : example.runDrillSideways()) {
