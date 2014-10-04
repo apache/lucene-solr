1d2c51b089d55368fb881080c8dcb1129dbfd5dd
Michael McCandless <mikemccand@apache.org>
2014-10-04 08:54:15 +0000

    allow this monster test to print stuff to stdout
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1629362 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/index/Test2BTerms.java
+++ lucene/core/src/test/org/apache/lucene/index/Test2BTerms.java
@@ -42,23 +42,22 @@ import org.apache.lucene.util.AttributeImpl;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.LuceneTestCase.Monster;
 import org.apache.lucene.util.LuceneTestCase.SuppressCodecs;
+import org.apache.lucene.util.LuceneTestCase.SuppressSysoutChecks;
 import org.apache.lucene.util.LuceneTestCase;
 import org.apache.lucene.util.TestUtil;
 import org.apache.lucene.util.TimeUnits;
-
 import com.carrotsearch.randomizedtesting.annotations.TimeoutSuite;
 
 // NOTE: SimpleText codec will consume very large amounts of
 // disk (but, should run successfully).  Best to run w/
 // -Dtests.codec=<current codec>, and w/ plenty of RAM, eg:
 //
-//   ant test -Dtests.monster=true -Dtests.heapsize=8g
-//
-//   java -server -Xmx8g -d64 -cp .:lib/junit-4.10.jar:./build/classes/test:./build/classes/test-framework:./build/classes/java -Dlucene.version=4.0-dev -Dtests.directory=MMapDirectory -DtempDir=build -ea org.junit.runner.JUnitCore org.apache.lucene.index.Test2BTerms
+//   ant test -Dtests.monster=true -Dtests.heapsize=8g -Dtests.codec=Lucene50 -Dtestcase=Test2BTerms
 //
 @SuppressCodecs({ "SimpleText", "Memory", "Direct" })
 @Monster("very slow, use 8g heap")
 @TimeoutSuite(millis = 6 * TimeUnits.HOUR)
+@SuppressSysoutChecks(bugUrl="We.print.lots.o.stuff.on.purpose")
 public class Test2BTerms extends LuceneTestCase {
 
   private final static int TOKEN_LEN = 5;
