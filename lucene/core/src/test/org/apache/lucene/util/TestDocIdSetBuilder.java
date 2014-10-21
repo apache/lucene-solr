fc3b1b07da55dce84fcdb3bbeff814ac002f4428
Adrien Grand <jpountz@apache.org>
2014-10-21 08:19:46 +0000

    LUCENE-6015: Fix TestDocIdSetBuilder.testDense expectations.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1633309 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/util/TestDocIdSetBuilder.java
+++ lucene/core/src/test/org/apache/lucene/util/TestDocIdSetBuilder.java
@@ -76,7 +76,7 @@ public class TestDocIdSetBuilder extends LuceneTestCase {
       builder.or(new RoaringDocIdSet.Builder(maxDoc).add(doc).build().iterator());
     }
     for (int i = 0; i < numIterators; ++i) {
-      final int baseInc = 2 + random().nextInt(10000);
+      final int baseInc = 2 + random().nextInt(10);
       RoaringDocIdSet.Builder b = new RoaringDocIdSet.Builder(maxDoc);
       for (int doc = random().nextInt(10000); doc < maxDoc; doc += baseInc + random().nextInt(2000)) {
         b.add(doc);
