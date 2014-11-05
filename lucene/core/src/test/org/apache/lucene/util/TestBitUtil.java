3d55d593ab5a678300c147f20faade92c1c0b5b5
Adrien Grand <jpountz@apache.org>
2014-11-05 16:35:13 +0000

    LUCENE-6040: Speedup broadword bit selection.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1636914 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/util/TestBitUtil.java
+++ lucene/core/src/test/org/apache/lucene/util/TestBitUtil.java
@@ -18,23 +18,18 @@ package org.apache.lucene.util;
  */
 
 
-public class TestBroadWord extends LuceneTestCase {
-  private void tstRank(long x) {
-    assertEquals("rank(" + x + ")", Long.bitCount(x), BroadWord.bitCount(x));
-  }
-
-  public void testRank1() {
-    tstRank(0L);
-    tstRank(1L);
-    tstRank(3L);
-    tstRank(0x100L);
-    tstRank(0x300L);
-    tstRank(0x8000000000000001L);
-  }
+public class TestBitUtil extends LuceneTestCase {
 
   private void tstSelect(long x, int r, int exp) {
-    assertEquals("selectNaive(" + x + "," + r + ")", exp, BroadWord.selectNaive(x, r));
-    assertEquals("select(" + x + "," + r + ")", exp, BroadWord.select(x, r));
+    if ((0 <= exp) && (exp <= 63)) {
+      assertEquals("selectNaive(" + x + "," + r + ")", exp, BitUtil.selectNaive(x, r));
+      assertEquals("select(" + x + "," + r + ")", exp, BitUtil.select(x, r));
+    } else {
+      int act = BitUtil.selectNaive(x, r);
+      assertTrue("selectNaive(" + x + "," + r + ")", act < 0 || act > 63);
+      act = BitUtil.select(x, r);
+      assertTrue("select(" + x + "," + r + ")", act < 0 || act > 63);
+    }
   }
 
   public void testSelectFromZero() {
@@ -74,53 +69,18 @@ public class TestBroadWord extends LuceneTestCase {
       tstSelect(0xFFFFFFFFFFFFFFFFL,i+1,i);
     }
   }
-  public void testPerfSelectAllBitsBroad() {
+  public void testPerfSelectAllBits() {
     for (int j = 0; j < 100000; j++) { // 1000000 for real perf test
       for (int i = 0; i < 64; i++) {
-        assertEquals(i, BroadWord.select(0xFFFFFFFFFFFFFFFFL, i+1));
+        assertEquals(i, BitUtil.select(0xFFFFFFFFFFFFFFFFL, i+1));
       }
     }
   }
   public void testPerfSelectAllBitsNaive() {
     for (int j = 0; j < 10000; j++) { // real perftest: 1000000
       for (int i = 0; i < 64; i++) {
-        assertEquals(i, BroadWord.selectNaive(0xFFFFFFFFFFFFFFFFL, i+1));
+        assertEquals(i, BitUtil.selectNaive(0xFFFFFFFFFFFFFFFFL, i+1));
       }
-    }
-  }
-  public void testSmalleru_87_01() {
-    // 0 <= arguments < 2 ** (k-1), k=8, see paper
-    for (long i = 0x0L; i <= 0x7FL; i++) {
-      for (long j = 0x0L; i <= 0x7FL; i++) {
-        long ii = i * BroadWord.L8_L;
-        long jj = j * BroadWord.L8_L;
-        assertEquals(ToStringUtils.longHex(ii) + " < " + ToStringUtils.longHex(jj),
-            ToStringUtils.longHex((i<j) ? (0x80L * BroadWord.L8_L) : 0x0L),
-            ToStringUtils.longHex(BroadWord.smallerUpTo7_8(ii,jj)));
-      }
-    }
-  }
-
-  public void testSmalleru_8_01() {
-    // 0 <= arguments < 2 ** k, k=8, see paper
-    for (long i = 0x0L; i <= 0xFFL; i++) {
-      for (long j = 0x0L; i <= 0xFFL; i++) {
-        long ii = i * BroadWord.L8_L;
-        long jj = j * BroadWord.L8_L;
-        assertEquals(ToStringUtils.longHex(ii) + " < " + ToStringUtils.longHex(jj),
-            ToStringUtils.longHex((i<j) ? (0x80L * BroadWord.L8_L): 0x0L),
-            ToStringUtils.longHex(BroadWord.smalleru_8(ii,jj)));
-      }
-    }
-  }
-
-  public void testNotEquals0_8() {
-    // 0 <= arguments < 2 ** k, k=8, see paper
-    for (long i = 0x0L; i <= 0xFFL; i++) {
-      long ii = i * BroadWord.L8_L;
-      assertEquals(ToStringUtils.longHex(ii) + " <> 0",
-          ToStringUtils.longHex((i != 0L) ? (0x80L * BroadWord.L8_L) : 0x0L),
-          ToStringUtils.longHex(BroadWord.notEquals0_8(ii)));
     }
   }
 }
