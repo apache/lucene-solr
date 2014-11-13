b5e95399547344068724d440f23f4002f19b00ea
Stefan Matheis <steffkes@apache.org>
2014-11-13 20:31:13 +0000

    SOLR-6740: Admin UI - improve Files View
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1639511 13f79535-47bb-0310-9956-ffa450edef68

--- solr/webapp/web/js/scripts/files.js
+++ solr/webapp/web/js/scripts/files.js
@@ -103,6 +103,7 @@ sammy.get
                           data: {
                             title : file,
                             attr : {
+                              title : file,
                               href : '#/' + current_core + '/files?file=' + prefix + file
                             }
                           },
@@ -161,7 +162,7 @@ sammy.get
 
           var endpoint = file_endpoint + '?file=' + selected_file;
 
-          var content_type_map = { xml : 'text/xml', html : 'text/html', js : 'text/javascript' };
+          var content_type_map = { xml : 'text/xml', html : 'text/html', js : 'text/javascript', json : 'application/json', 'css' : 'text/css' };
           var file_ext = selected_file.match( /\.(\w+)$/  );
           endpoint += '&contentType=' + ( content_type_map[ file_ext[1] || '' ] || 'text/plain' ) + ';charset=utf-8';
 
@@ -208,6 +209,14 @@ sammy.get
                   else if( 0 === content_type.indexOf( 'text/javascript' ) )
                   {
                     highlight = 'javascript';
+                  }
+                  else if( 0 === content_type.indexOf( 'text/css' ) )
+                  {
+                    highlight = 'css';
+                  }
+                  else if( 0 === content_type.indexOf( 'application/json' ) )
+                  {
+                    highlight = 'json';
                   }
 
                   var code = $(
