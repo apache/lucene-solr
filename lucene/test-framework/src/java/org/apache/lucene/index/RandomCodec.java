673c2bde45830105cc9186a302881045b2f9cef0
Robert Muir <rmuir@apache.org>
2014-11-27 18:58:15 +0000

    less simpletext, less simplefsdir, put directdv in rotation
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1642209 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/test-framework/src/java/org/apache/lucene/index/RandomCodec.java
+++ lucene/test-framework/src/java/org/apache/lucene/index/RandomCodec.java
@@ -37,6 +37,7 @@ import org.apache.lucene.codecs.blockterms.LuceneVarGapDocFreqInterval;
 import org.apache.lucene.codecs.blockterms.LuceneVarGapFixedInterval;
 import org.apache.lucene.codecs.blocktreeords.BlockTreeOrdsPostingsFormat;
 import org.apache.lucene.codecs.bloom.TestBloomFilteredLucenePostings;
+import org.apache.lucene.codecs.memory.DirectDocValuesFormat;
 import org.apache.lucene.codecs.memory.DirectPostingsFormat;
 import org.apache.lucene.codecs.memory.FSTOrdPostingsFormat;
 import org.apache.lucene.codecs.memory.FSTPostingsFormat;
@@ -136,15 +137,16 @@ public class RandomCodec extends AssertingCodec {
         new LuceneFixedGap(TestUtil.nextInt(random, 1, 1000)),
         new LuceneVarGapFixedInterval(TestUtil.nextInt(random, 1, 1000)),
         new LuceneVarGapDocFreqInterval(TestUtil.nextInt(random, 1, 100), TestUtil.nextInt(random, 1, 1000)),
-        new SimpleTextPostingsFormat(),
+        random.nextInt(10) == 0 ? new SimpleTextPostingsFormat() : TestUtil.getDefaultPostingsFormat(),
         new AssertingPostingsFormat(),
         new MemoryPostingsFormat(true, random.nextFloat()),
         new MemoryPostingsFormat(false, random.nextFloat()));
     
     addDocValues(avoidCodecs,
         TestUtil.getDefaultDocValuesFormat(),
+        new DirectDocValuesFormat(), // maybe not a great idea...
         new MemoryDocValuesFormat(),
-        new SimpleTextDocValuesFormat(),
+        random.nextInt(10) == 0 ? new SimpleTextDocValuesFormat() : TestUtil.getDefaultDocValuesFormat(),
         new AssertingDocValuesFormat());
 
     Collections.shuffle(formats, random);
