c89927890c7ac4e9ba92a54d47b1290f78d35306
Robert Muir <rmuir@apache.org>
2014-11-28 01:38:04 +0000

    fix test bug
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1642254 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/misc/src/test/org/apache/lucene/uninverting/TestFieldCacheVsDocValues.java
+++ lucene/misc/src/test/org/apache/lucene/uninverting/TestFieldCacheVsDocValues.java
@@ -577,6 +577,6 @@ public class TestFieldCacheVsDocValues extends LuceneTestCase {
   
   protected boolean codecAcceptsHugeBinaryValues(String field) {
     String name = TestUtil.getDocValuesFormat(field);
-    return !(name.equals("Memory") || name.equals("Direct"));
+    return !(name.equals("Memory")); // Direct has a different type of limit
   }
 }
