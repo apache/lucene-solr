ce9926814e891840e855ca14f48b0638bb109818
Robert Muir <rmuir@apache.org>
2014-11-28 03:54:06 +0000

    fix lint problems at private level (not enforced, jflex has some issues with generated code)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1642262 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/suggest/src/java/org/apache/lucene/search/suggest/analyzing/FreeTextSuggester.java
+++ lucene/suggest/src/java/org/apache/lucene/search/suggest/analyzing/FreeTextSuggester.java
@@ -734,12 +734,12 @@ public class FreeTextSuggester extends Lookup {
     }
   }
 
-  /** weight -> cost */
+  /** weight -&gt; cost */
   private long encodeWeight(long ngramCount) {
     return Long.MAX_VALUE - ngramCount;
   }
 
-  /** cost -> weight */
+  /** cost -&gt; weight */
   //private long decodeWeight(Pair<Long,BytesRef> output) {
   private long decodeWeight(Long output) {
     assert output != null;
