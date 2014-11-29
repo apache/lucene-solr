9d35397ad56831c52b21aab59aa6c6baa5f24d07
Michael McCandless <mikemccand@apache.org>
2014-11-29 10:37:55 +0000

    fix test bug
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1642415 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/util/TestArrayUtil.java
+++ lucene/core/src/test/org/apache/lucene/util/TestArrayUtil.java
@@ -66,7 +66,7 @@ public class TestArrayUtil extends LuceneTestCase {
     final Random rnd = random();
     final int num = atLeast(10000);
     for (int iter = 0; iter < num; iter++) {
-      final int minTargetSize = rnd.nextInt(Integer.MAX_VALUE);
+      final int minTargetSize = rnd.nextInt(ArrayUtil.MAX_ARRAY_LENGTH);
       final int elemSize = rnd.nextInt(11);
       final int v = ArrayUtil.oversize(minTargetSize, elemSize);
       assertTrue(v >= minTargetSize);
