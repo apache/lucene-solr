3193ae8e2ea4cb88fda4f23057ddc57a7e21539f
Mark Robert Miller <markrmiller@apache.org>
2014-12-02 17:44:27 +0000

    SOLR-6756: A first pass at making the cloud-dev scripts work again.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1642964 13f79535-47bb-0310-9956-ffa450edef68

--- solr/cloud-dev/solrcloud-extzk-start.sh
+++ solr/cloud-dev/solrcloud-extzk-start.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 
+# TODO: !OUT OF DATE!
+
 zkaddress = localhost:2181/solr
 
 cd ..
 
-rm -r -f example2
-rm -r -f example3
-rm -r -f example4
-rm -r -f example5
-rm -r -f example6
+rm -r -f server2
+rm -r -f server3
+rm -r -f server4
+rm -r -f server5
+rm -r -f server6
 
 rm -r -f dist
 rm -r -f build
-rm -r -f example/solr/zoo_data
-rm -r -f example/solr/collection1/data
-rm -f example/example.log
+rm -r -f server/solr/zoo_data
+rm -r -f server/solr/collection1/data
+rm -f server/server.log
 
 ant example dist
 
-cp -r -f example example2
-cp -r -f example example3
-cp -r -f example example4
-cp -r -f example example5
-cp -r -f example example6
+cp -r -f server server2
+cp -r -f server server3
+cp -r -f server server4
+cp -r -f server server5
+cp -r -f server server6
 
-java -classpath "example/solr-webapp/webapp/WEB-INF/lib/*:example/lib/ext/" org.apache.solr.cloud.ZkController "$zkaddress" 8983 example/solr/conf conf1
+java -classpath "server/solr-webapp/webapp/WEB-INF/lib/*:server/lib/ext/" org.apache.solr.cloud.ZkController "$zkaddress" 8983 server/solr/conf conf1
 
-cd example
-java -DzkHost="$zkaddress" -DnumShards=2 -DSTOP.PORT=7983 -DSTOP.KEY=key -jar start.jar 1>example.log 2>&1 &
+cd server
+java -DzkHost="$zkaddress" -DnumShards=2 -DSTOP.PORT=7983 -DSTOP.KEY=key -jar start.jar 1>server.log 2>&1 &
 
-cd ../example2
-java -Djetty.port=7574 -DzkHost="$zkaddress" -DSTOP.PORT=6574 -DSTOP.KEY=key -jar start.jar 1>example2.log 2>&1 &
+cd ../server2
+java -Djetty.port=7574 -DzkHost="$zkaddress" -DSTOP.PORT=6574 -DSTOP.KEY=key -jar start.jar 1>server2.log 2>&1 &
 
-cd ../example3
-java -Djetty.port=7575 -DzkHost="$zkaddress" -DSTOP.PORT=6575 -DSTOP.KEY=key -jar start.jar 1>example3.log 2>&1 &
+cd ../server3
+java -Djetty.port=7575 -DzkHost="$zkaddress" -DSTOP.PORT=6575 -DSTOP.KEY=key -jar start.jar 1>server3.log 2>&1 &
 
-cd ../example4
-java -Djetty.port=7576 -DzkHost="$zkaddress" -DSTOP.PORT=6576 -DSTOP.KEY=key -jar start.jar 1>example4.log 2>&1 &
+cd ../server4
+java -Djetty.port=7576 -DzkHost="$zkaddress" -DSTOP.PORT=6576 -DSTOP.KEY=key -jar start.jar 1>server4.log 2>&1 &
 
-cd ../example5
-java -Djetty.port=7577 -DzkHost="$zkaddress" -DSTOP.PORT=6577 -DSTOP.KEY=key -jar start.jar 1>example5.log 2>&1 &
+cd ../server5
+java -Djetty.port=7577 -DzkHost="$zkaddress" -DSTOP.PORT=6577 -DSTOP.KEY=key -jar start.jar 1>server5.log 2>&1 &
 
-cd ../example6
-java -Djetty.port=7578 -DzkHost="$zkaddress" -DSTOP.PORT=6578 -DSTOP.KEY=key -jar start.jar 1>example6.log 2>&1 &
+cd ../server6
+java -Djetty.port=7578 -DzkHost="$zkaddress" -DSTOP.PORT=6578 -DSTOP.KEY=key -jar start.jar 1>server6.log 2>&1 &
