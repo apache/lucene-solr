622bc6ec8de4b98f0b79469948e6d2b36b42cf53
Alan Woodward <romseygeek@apache.org>
2014-12-15 18:26:34 +0000

    SOLR-6849: Fix another @Slow test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1645713 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/TestModifyConfFiles.java
+++ solr/core/src/test/org/apache/solr/cloud/TestModifyConfFiles.java
@@ -28,6 +28,8 @@ import org.apache.solr.common.util.SimpleOrderedMap;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 
+import static org.junit.internal.matchers.StringContains.containsString;
+
 public class TestModifyConfFiles extends AbstractFullDistribZkTestBase {
 
   public TestModifyConfFiles() {
@@ -48,7 +50,7 @@ public class TestModifyConfFiles extends AbstractFullDistribZkTestBase {
       client.request(request);
       fail("Should have caught exception");
     } catch (Exception e) {
-      assertEquals(e.getMessage(), "Input stream list was null for admin file write operation.");
+      assertThat(e.getMessage(), containsString("Input stream list was null for admin file write operation."));
     }
 
     params.remove("file");
