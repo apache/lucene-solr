c36b12731c308787954eed62ae8c8aad048e76ef
Alan Woodward <romseygeek@apache.org>
2014-12-17 14:46:45 +0000

    LUCENE-2878: Remove dead code from TermScorer
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene2878@1646271 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/search/TermScorer.java
+++ lucene/core/src/java/org/apache/lucene/search/TermScorer.java
@@ -17,27 +17,27 @@ package org.apache.lucene.search;
  * limitations under the License.
  */
 
+import java.io.IOException;
+
 import org.apache.lucene.index.DocsEnum;
 import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.util.BytesRef;
-
-import java.io.IOException;
 
 /** Expert: A <code>Scorer</code> for documents matching a <code>Term</code>.
  */
 final class TermScorer extends Scorer {
   private final DocsEnum docsEnum;
   private final Similarity.SimScorer docScorer;
-  
+
   /**
    * Construct a <code>TermScorer</code>.
-   * 
+   *
    * @param weight
    *          The weight of the <code>Term</code> in the query.
    * @param td
    *          An iterator over the documents matching the <code>Term</code>.
    * @param docScorer
-   *          The </code>Similarity.SimScorer</code> implementation 
+   *          The </code>Similarity.SimScorer</code> implementation
    *          to be used for score computations.
    */
   TermScorer(Weight weight, DocsEnum td, Similarity.SimScorer docScorer) {
@@ -54,16 +54,6 @@ final class TermScorer extends Scorer {
   @Override
   public int freq() throws IOException {
     return docsEnum.freq();
-  }
-
-  /**
-   * Advances to the next document matching the query. <br>
-   * 
-   * @return the document matching the query or NO_MORE_DOCS if there are no more documents.
-   */
-  @Override
-  public int nextDoc() throws IOException {
-    return docsEnum.nextDoc();
   }
 
   @Override
@@ -95,18 +85,28 @@ final class TermScorer extends Scorer {
   public BytesRef getPayload() throws IOException {
     return docsEnum.getPayload();
   }
-  
+
+  /**
+   * Advances to the next document matching the query. <br>
+   *
+   * @return the document matching the query or NO_MORE_DOCS if there are no more documents.
+   */
+  @Override
+  public int nextDoc() throws IOException {
+    return docsEnum.nextDoc();
+  }
+
   @Override
   public float score() throws IOException {
     assert docID() != NO_MORE_DOCS;
-    return docScorer.score(docsEnum.docID(), docsEnum.freq());  
+    return docScorer.score(docsEnum.docID(), docsEnum.freq());
   }
 
   /**
    * Advances to the first match beyond the current whose document number is
    * greater than or equal to a given target. <br>
    * The implementation uses {@link DocsEnum#advance(int)}.
-   * 
+   *
    * @param target
    *          The target document number.
    * @return the matching document or NO_MORE_DOCS if none exist.
@@ -115,7 +115,7 @@ final class TermScorer extends Scorer {
   public int advance(int target) throws IOException {
     return docsEnum.advance(target);
   }
-  
+
   @Override
   public long cost() {
     return docsEnum.cost();
@@ -123,16 +123,5 @@ final class TermScorer extends Scorer {
 
   /** Returns a string representation of this <code>TermScorer</code>. */
   @Override
-  public String toString() {
-    return "scorer(" + weight + ")[" + super.toString() + "]";
-  }
-  
-  // TODO: benchmark if the specialized conjunction really benefits
-  // from this, or if instead its from sorting by docFreq, or both
-
-  DocsEnum getDocsEnum() {
-    return docsEnum;
-  }
-
-
+  public String toString() { return "scorer(" + weight + ")[" + super.toString() + "]"; }
 }
