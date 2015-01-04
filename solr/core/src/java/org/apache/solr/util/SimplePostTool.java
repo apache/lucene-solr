b5a484fe1f757dd4998defce1763dfcc6fed4f32
Erik Hatcher <ehatcher@apache.org>
2015-01-04 22:31:13 +0000

    SOLR-6908: Minor corrections to SimplePostTool -help
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1649442 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/util/SimplePostTool.java
+++ solr/core/src/java/org/apache/solr/util/SimplePostTool.java
@@ -404,7 +404,7 @@ public class SimplePostTool {
      "  java -Ddata=web -Dc=gettingstarted -jar post.jar http://example.com/\n"+
      "  java -Dtype=text/csv -Dc=gettingstarted -jar post.jar *.csv\n"+
      "  java -Dtype=application/json -Dc=gettingstarted -jar post.jar *.json\n"+
-     "  java -Durl=http://localhost:8983/solr/techproducts/update/extract -Dparams=literal.id=a -jar post.jar solr-word.pdf\n"+
+     "  java -Durl=http://localhost:8983/solr/techproducts/update/extract -Dparams=literal.id=pdf1 -jar post.jar solr-word.pdf\n"+
      "  java -Dauto -Dc=gettingstarted -jar post.jar *\n"+
      "  java -Dauto -Dc=gettingstarted -Drecursive -jar post.jar afolder\n"+
      "  java -Dauto -Dc=gettingstarted -Dfiletypes=ppt,html -jar post.jar afolder\n"+
