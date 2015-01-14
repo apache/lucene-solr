231b7c5b9438dcf8fbf7c20004eed52099ba9ddb
Anshum Gupta <anshum@apache.org>
2015-01-14 02:26:29 +0000

    Bumping up version to 5.1 for the 5.0 release
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1651555 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -204,8 +204,15 @@ public final class Version {
    *  <p>
    *  Use this to get the latest &amp; greatest settings, bug
    *  fixes, etc, for Lucene.
+   * @deprecated (5.1.0) Use latest
    */
+  @Deprecated
   public static final Version LUCENE_5_0_0 = new Version(5, 0, 0);
+
+  /**
+   * Match settings and bugs in Lucene's 5.1.0 release.
+   */
+  public static final Version LUCENE_5_1_0 = new Version(5, 1, 0);
 
   // To add a new version:
   //  * Only add above this comment
@@ -226,7 +233,7 @@ public final class Version {
    * some defaults may have changed and may break functionality 
    * in your application.
    */
-  public static final Version LATEST = LUCENE_5_0_0;
+  public static final Version LATEST = LUCENE_5_1_0;
 
   /**
    * Constant for backwards compatibility.
