19905bf336ad638abcb1c7b22876d494492f3a0a
Timothy Potter <thelabdude@apache.org>
2015-01-15 18:29:59 +0000

    SOLR-6982: remove bad search/replace issue
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1652210 13f79535-47bb-0310-9956-ffa450edef68

--- solr/bin/solr.cmd
+++ solr/bin/solr.cmd
@@ -6,7 +6,7 @@
 @REM  (the "License"); you may not use this file except in compliance with
 @REM  the License.  You may obtain a copy of the License at
 @REM
-@REM      !SOLR_URL_SCHEME!://www.apache.org/licenses/LICENSE-2.0
+@REM      http://www.apache.org/licenses/LICENSE-2.0
 @REM
 @REM  Unless required by applicable law or agreed to in writing, software
 @REM  distributed under the License is distributed on an "AS IS" BASIS,
