eea6b5d2589303588613834032b93b7f4fbde9b6
Noble Paul <noble@apache.org>
2015-01-26 18:44:13 +0000

    fixing NPE http://jenkins.thetaphi.de/job/Lucene-Solr-trunk-Linux/11688/
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_5_0@1654865 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/schema/TestBulkSchemaConcurrent.java
+++ solr/core/src/test/org/apache/solr/schema/TestBulkSchemaConcurrent.java
@@ -115,7 +115,7 @@ public class TestBulkSchemaConcurrent  extends AbstractFullDistribZkTestBase {
     boolean success = true;
 
     for (List e : collectErrors) {
-      if(!e.isEmpty()){
+      if(e != null &&  !e.isEmpty()){
         success = false;
         log.error(e.toString());
       }
