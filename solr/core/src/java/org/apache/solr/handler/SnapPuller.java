8708c2f8ebdc36438651c554f53ae918850b31f1
Anshum Gupta <anshum@apache.org>
2015-01-28 09:43:10 +0000

    SOLR-6640: Fixing the comment (merge from trunk)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1655250 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/handler/SnapPuller.java
+++ solr/core/src/java/org/apache/solr/handler/SnapPuller.java
@@ -407,7 +407,7 @@ public class SnapPuller {
         }
         
         if (!isFullCopyNeeded) {
-          // a searcher might be using some flushed but committed segments
+          // a searcher might be using some flushed but not committed segments
           // because of soft commits (which open a searcher on IW's data)
           // so we need to close the existing searcher on the last commit
           // and wait until we are able to clean up all unused lucene files
