669e9cf617c532442de87a36b21258898b669c42
Michael McCandless <mikemccand@apache.org>
2015-01-30 14:39:05 +0000

    LUCENE-6209: IndexWriter now logs (to infoStream) how much time flushing threads were stalled because of > 2X IW's RAM buffer in flush backlog
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1656029 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/index/DocumentsWriterStallControl.java
+++ lucene/core/src/java/org/apache/lucene/index/DocumentsWriterStallControl.java
@@ -20,6 +20,7 @@ import java.util.IdentityHashMap;
 import java.util.Map;
 
 import org.apache.lucene.index.DocumentsWriterPerThreadPool.ThreadState;
+import org.apache.lucene.util.InfoStream;
 import org.apache.lucene.util.ThreadInterruptedException;
 
 /**
@@ -43,6 +44,11 @@ final class DocumentsWriterStallControl {
   private int numWaiting; // only with assert
   private boolean wasStalled; // only with assert
   private final Map<Thread, Boolean> waiting = new IdentityHashMap<>(); // only with assert
+  private final InfoStream infoStream;
+
+  DocumentsWriterStallControl(LiveIndexWriterConfig iwc) {
+    infoStream = iwc.getInfoStream();
+  }
   
   /**
    * Update the stalled flag status. This method will set the stalled flag to
@@ -85,8 +91,13 @@ final class DocumentsWriterStallControl {
     return stalled;
   }
   
-  
+  long stallStartNS;
+
   private void incWaiters() {
+    stallStartNS = System.nanoTime();
+    if (infoStream.isEnabled("DW") && numWaiting == 0) {
+      infoStream.message("DW", "now stalling flushes");
+    }
     numWaiting++;
     assert waiting.put(Thread.currentThread(), Boolean.TRUE) == null;
     assert numWaiting > 0;
@@ -96,6 +107,10 @@ final class DocumentsWriterStallControl {
     numWaiting--;
     assert waiting.remove(Thread.currentThread()) != null;
     assert numWaiting >= 0;
+    if (infoStream.isEnabled("DW") && numWaiting == 0) {
+      long stallEndNS = System.nanoTime();
+      infoStream.message("DW", "done stalling flushes for " + ((stallEndNS - stallStartNS)/1000000.0) + " ms");
+    }
   }
   
   synchronized boolean hasBlocked() { // for tests
