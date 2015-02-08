f2bf0e2235b4b5e0b18dc8755716fad10df78ac2
Mark Robert Miller <markrmiller@apache.org>
2015-02-08 18:31:07 +0000

    SOLR-7033, SOLR-5961: RecoveryStrategy should not publish any state when closed / cancelled and there should always be a pause between recoveries even when recoveries are rapidly stopped and started as well as when a node attempts to become the leader for a shard.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1658236 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/SolrCoreState.java
+++ solr/core/src/java/org/apache/solr/update/SolrCoreState.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.util.concurrent.locks.Lock;
 
 import org.apache.lucene.index.IndexWriter;
+import org.apache.solr.cloud.ActionThrottle;
 import org.apache.solr.core.CoreContainer;
 import org.apache.solr.core.CoreDescriptor;
 import org.apache.solr.core.DirectoryFactory;
@@ -139,5 +140,10 @@ public abstract class SolrCoreState {
   public abstract void cancelRecovery();
 
   public abstract void close(IndexWriterCloser closer);
+
+  /**
+   * @return throttle to limit how fast a core attempts to become leader
+   */
+  public abstract ActionThrottle getLeaderThrottle();
 
 }
