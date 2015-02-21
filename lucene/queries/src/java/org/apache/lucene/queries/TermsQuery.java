163c0bc0b72019d00df12e16038684416428a656
Adrien Grand <jpountz@apache.org>
2015-02-21 16:52:50 +0000

    LUCENE-6270: Fix merge issues with LUCENE-6272.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1661396 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/queries/src/java/org/apache/lucene/queries/TermsQuery.java
+++ lucene/queries/src/java/org/apache/lucene/queries/TermsQuery.java
@@ -416,26 +416,6 @@ public class TermsQuery extends Query implements Accountable {
           }
 
           @Override
-          public int nextPosition() throws IOException {
-            return -1;
-          }
-
-          @Override
-          public int startOffset() throws IOException {
-            return -1;
-          }
-
-          @Override
-          public int endOffset() throws IOException {
-            return -1;
-          }
-
-          @Override
-          public BytesRef getPayload() throws IOException {
-            return null;
-          }
-
-          @Override
           public int docID() {
             return disi.docID();
           }
