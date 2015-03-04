4d6df6a4806876802f78f08cf541f4e7ac212a68
Adrien Grand <jpountz@apache.org>
2015-03-04 09:23:48 +0000

    LUCENE-6304: Add MatchNoDocsQuery.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1663899 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/search/Query.java
+++ lucene/core/src/java/org/apache/lucene/search/Query.java
@@ -112,10 +112,7 @@ public abstract class Query implements Cloneable {
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + Float.floatToIntBits(boost);
-    return result;
+    return Float.floatToIntBits(getBoost()) ^ getClass().hashCode();
   }
 
   @Override
