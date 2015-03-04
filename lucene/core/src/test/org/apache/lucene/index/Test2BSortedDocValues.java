823f134f020e16b9b2a49beffb7438caf1a6ee48
Michael McCandless <mikemccand@apache.org>
2015-03-04 22:17:01 +0000

    backport test fix
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_4_10@1664167 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/index/Test2BSortedDocValues.java
+++ lucene/core/src/test/org/apache/lucene/index/Test2BSortedDocValues.java
@@ -37,7 +37,7 @@ import com.carrotsearch.randomizedtesting.annotations.TimeoutSuite;
 @Monster("very slow")
 public class Test2BSortedDocValues extends LuceneTestCase {
   
-  // indexes Integer.MAX_VALUE docs with a fixed binary field
+  // indexes IndexWriter.MAX_DOCS docs with a fixed binary field
   public void testFixedSorted() throws Exception {
     BaseDirectoryWrapper dir = newFSDirectory(createTempDir("2BFixedSorted"));
     if (dir instanceof MockDirectoryWrapper) {
@@ -58,7 +58,7 @@ public class Test2BSortedDocValues extends LuceneTestCase {
     SortedDocValuesField dvField = new SortedDocValuesField("dv", data);
     doc.add(dvField);
     
-    for (int i = 0; i < Integer.MAX_VALUE; i++) {
+    for (int i = 0; i < IndexWriter.MAX_DOCS; i++) {
       bytes[0] = (byte)(i >> 8);
       bytes[1] = (byte) i;
       w.addDocument(doc);
@@ -92,7 +92,7 @@ public class Test2BSortedDocValues extends LuceneTestCase {
     dir.close();
   }
   
-  // indexes Integer.MAX_VALUE docs with a fixed binary field
+  // indexes IndexWriter.MAX_DOCS docs with a fixed binary field
   public void test2BOrds() throws Exception {
     BaseDirectoryWrapper dir = newFSDirectory(createTempDir("2BOrds"));
     if (dir instanceof MockDirectoryWrapper) {
@@ -113,7 +113,7 @@ public class Test2BSortedDocValues extends LuceneTestCase {
     SortedDocValuesField dvField = new SortedDocValuesField("dv", data);
     doc.add(dvField);
     
-    for (int i = 0; i < Integer.MAX_VALUE; i++) {
+    for (int i = 0; i < IndexWriter.MAX_DOCS; i++) {
       bytes[0] = (byte)(i >> 24);
       bytes[1] = (byte)(i >> 16);
       bytes[2] = (byte)(i >> 8);
