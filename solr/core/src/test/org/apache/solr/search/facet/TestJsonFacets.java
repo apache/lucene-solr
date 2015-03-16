c1fe594019a3ec8d132b9dfb6cd79c5f23dd66bc
Yonik Seeley <yonik@apache.org>
2015-03-16 15:21:00 +0000

    tests: clean up static fields
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1667035 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/search/facet/TestJsonFacets.java
+++ solr/core/src/test/org/apache/solr/search/facet/TestJsonFacets.java
@@ -56,6 +56,7 @@ public class TestJsonFacets extends SolrTestCaseHS {
     JSONTestUtil.failRepeatedKeys = false;
     if (servers != null) {
       servers.stop();
+      servers = null;
     }
   }
 
