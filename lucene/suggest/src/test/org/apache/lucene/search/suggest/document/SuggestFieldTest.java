409f09394997eedd5fec77a30e874727f9b616cc
Adrien Grand <jpountz@apache.org>
2015-04-01 10:33:33 +0000

    Fix SuggestFieldTest to not always expect IndexWriter.tryDelete to succeed.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1670601 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/suggest/src/test/org/apache/lucene/search/suggest/document/SuggestFieldTest.java
+++ lucene/suggest/src/test/org/apache/lucene/search/suggest/document/SuggestFieldTest.java
@@ -44,6 +44,7 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.RandomIndexWriter;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queries.TermsQuery;
 import org.apache.lucene.search.DocIdSet;
 import org.apache.lucene.search.DocIdSetIterator;
@@ -55,9 +56,11 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.util.Bits;
 import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.util.BytesRefBuilder;
 import org.apache.lucene.util.FixedBitSet;
 import org.apache.lucene.util.LineFileDocs;
 import org.apache.lucene.util.LuceneTestCase;
+import org.apache.lucene.util.NumericUtils;
 import org.apache.lucene.util.TestUtil;
 import org.junit.After;
 import org.junit.Before;
@@ -198,22 +201,9 @@ public class SuggestFieldTest extends LuceneTestCase {
       document = new Document();
     }
 
-    // get docIDs to delete
+    iw.deleteDocuments(new Term("str_field", "delete"));
+
     DirectoryReader reader = DirectoryReader.open(iw, false);
-    List<Integer> docIdsToDelete = new ArrayList<>();
-    for (int i = 0; i < reader.maxDoc(); i++) {
-      Document doc = reader.document(i);
-      if ("delete".equals(doc.get("str_field"))) {
-        docIdsToDelete.add(i);
-      }
-    }
-
-    for (Integer docID : docIdsToDelete) {
-      assertTrue(iw.tryDeleteDocument(reader, docID));
-    }
-    reader.close();
-
-    reader = DirectoryReader.open(iw, false);
     SuggestIndexSearcher indexSearcher = new SuggestIndexSearcher(reader, analyzer);
     TopSuggestDocs suggest = indexSearcher.suggest("suggest_field", "abc_", numLive);
     assertSuggestions(suggest, expectedEntries.toArray(new Entry[expectedEntries.size()]));
@@ -254,16 +244,13 @@ public class SuggestFieldTest extends LuceneTestCase {
     for (int i = 0; i < num; i++) {
       Document document = new Document();
       document.add(newSuggestField("suggest_field", "abc_" + i, i));
+      document.add(newStringField("delete", "delete", Field.Store.NO));
       iw.addDocument(document);
     }
 
-    DirectoryReader reader = DirectoryReader.open(iw, false);
-    for (int docID = 0; docID < reader.maxDoc(); docID++) {
-      assertTrue(iw.tryDeleteDocument(reader, docID));
-    }
-    reader.close();
+    iw.deleteDocuments(new Term("delete", "delete"));
 
-    reader = DirectoryReader.open(iw, false);
+    DirectoryReader reader = DirectoryReader.open(iw, false);
     SuggestIndexSearcher indexSearcher = new SuggestIndexSearcher(reader, analyzer);
     TopSuggestDocs suggest = indexSearcher.suggest("suggest_field", "abc_", num);
     assertThat(suggest.totalHits, equalTo(0));
@@ -285,20 +272,9 @@ public class SuggestFieldTest extends LuceneTestCase {
       iw.addDocument(document);
     }
 
-    DirectoryReader reader = DirectoryReader.open(iw, false);
-    // delete all but the lowest scored suggestion
-    for (int docID = 0; docID < reader.maxDoc(); docID++) {
-      Document doc = reader.document(docID);
-      IndexableField[] weights = doc.getFields("weight_fld");
-      assertThat(weights.length, equalTo(1));
-      int weight = (int) weights[0].numericValue();
-      if (weight != 1) {
-        assertTrue(iw.tryDeleteDocument(reader, docID));
-      }
-    }
-    reader.close();
+    iw.deleteDocuments(NumericRangeQuery.newIntRange("weight_fld", 2, null, true, false));
 
-    reader = DirectoryReader.open(iw, false);
+    DirectoryReader reader = DirectoryReader.open(iw, true);
     SuggestIndexSearcher indexSearcher = new SuggestIndexSearcher(reader, analyzer);
     TopSuggestDocs suggest = indexSearcher.suggest("suggest_field", "abc_", 1);
     assertSuggestions(suggest, new Entry("abc_1", 1));
