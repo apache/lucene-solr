1614df8195f367580342bd29f57564be54f11dc2
Robert Muir <rmuir@apache.org>
2015-04-01 15:22:26 +0000

    ignore eclipse IDE files in this script too
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1670671 13f79535-47bb-0310-9956-ffa450edef68

--- dev-tools/scripts/createPatch.py
+++ dev-tools/scripts/createPatch.py
@@ -90,7 +90,7 @@ def run_diff(from_dir, to_dir, skip_whitespace):
     flags += 'bBw'
 
   args = ['diff', flags]
-  for ignore in ('.svn', '.git', 'build', '.caches', '.idea', 'idea-build'):
+  for ignore in ('.svn', '.git', 'build', '.caches', '.idea', 'idea-build', 'eclipse-build'):
     args.append('-x')
     args.append(ignore)
   args.append(from_dir)
