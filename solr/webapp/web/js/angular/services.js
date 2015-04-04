57b2e0d6a8e741213a96220b3b69da47b3055a29
Erick Erickson <erick@apache.org>
2015-04-04 17:12:31 +0000

    SOLR-7241: Add document tab support to AngularJS adminUI
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1671280 13f79535-47bb-0310-9956-ffa450edef68

--- solr/webapp/web/js/angular/services.js
+++ solr/webapp/web/js/angular/services.js
@@ -77,11 +77,32 @@ solrAdminServices.factory('System',
   }])
 .factory('Update',
   ['$resource', function($resource) {
-    return $resource('/solr/:core/update', {core: '@core', wt:'json', _:Date.now()}, {
+    return $resource('/solr/:core/:handler', {core: '@core', wt:'json', _:Date.now(), handler:'/update'}, {
       "optimize": {params: { optimize: "true"}},
-      "commit": {params: {commit: "true"}}
+      "commit": {params: {commit: "true"}},
+      "post": {method: "POST", params: {handler: '@handler'}}
     });
   }])
+.service('FileUpload', function ($http) {
+    this.upload = function(params, file, success, error){
+        var url = "/solr/" + params.core + "/" + params.handler + "?";
+        raw = params.raw;
+        delete params.core;
+        delete params.handler;
+        delete params.raw;
+        url += $.param(params);
+        if (raw && raw.length>0) {
+            if (raw[0] != "&") raw = "&" + raw;
+            url += raw;
+        }
+        var fd = new FormData();
+        fd.append('file', file);
+        $http.post(url, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        }).success(success).error(error);
+    }
+})
 .factory('Luke',
   ['$resource', function($resource) {
     return $resource('/solr/:core/admin/luke', {core: '@core', wt:'json', _:Date.now()}, {
