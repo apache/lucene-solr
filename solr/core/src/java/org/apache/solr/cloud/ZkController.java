c7ddad1c20027ecf28fa15b6d36d9724e6b24dfa
Timothy Potter <thelabdude@apache.org>
2015-04-06 15:56:56 +0000

    SOLR-7338: A reloaded core will never register itself as active after a ZK session expiration, also fixes SOLR-6583
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1671562 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/cloud/ZkController.java
+++ solr/core/src/java/org/apache/solr/cloud/ZkController.java
@@ -883,7 +883,9 @@ public final class ZkController {
         // leader election perhaps?
 
         UpdateLog ulog = core.getUpdateHandler().getUpdateLog();
-        if (!core.isReloaded() && ulog != null) {
+        
+        // we will call register again after zk expiration and on reload
+        if (!afterExpiration && !core.isReloaded() && ulog != null) {
           // disable recovery in case shard is in construction state (for shard splits)
           Slice slice = getClusterState().getSlice(collection, shardId);
           if (slice.getState() != Slice.State.CONSTRUCTION || !isLeader) {
@@ -898,11 +900,11 @@ public final class ZkController {
               log.info("No LogReplay needed for core=" + core.getName() + " baseURL=" + baseUrl);
             }
           }
-          boolean didRecovery = checkRecovery(coreName, desc, recoverReloadedCores, isLeader, cloudDesc,
-              collection, coreZkNodeName, shardId, leaderProps, core, cc);
-          if (!didRecovery) {
-            publish(desc, Replica.State.ACTIVE);
-          }
+        }
+        boolean didRecovery = checkRecovery(coreName, desc, recoverReloadedCores, isLeader, cloudDesc,
+            collection, coreZkNodeName, shardId, leaderProps, core, cc, afterExpiration);
+        if (!didRecovery) {
+          publish(desc, Replica.State.ACTIVE);
         }
       }
 
@@ -1048,10 +1050,10 @@ public final class ZkController {
    * Returns whether or not a recovery was started
    */
   private boolean checkRecovery(String coreName, final CoreDescriptor desc,
-      boolean recoverReloadedCores, final boolean isLeader,
-      final CloudDescriptor cloudDesc, final String collection,
-      final String shardZkNodeName, String shardId, ZkNodeProps leaderProps,
-      SolrCore core, CoreContainer cc) {
+                                boolean recoverReloadedCores, final boolean isLeader,
+                                final CloudDescriptor cloudDesc, final String collection,
+                                final String shardZkNodeName, String shardId, ZkNodeProps leaderProps,
+                                SolrCore core, CoreContainer cc, boolean afterExpiration) {
     if (SKIP_AUTO_RECOVERY) {
       log.warn("Skipping recovery according to sys prop solrcloud.skip.autorecovery");
       return false;
@@ -1059,7 +1061,7 @@ public final class ZkController {
     boolean doRecovery = true;
     if (!isLeader) {
       
-      if (core.isReloaded() && !recoverReloadedCores) {
+      if (!afterExpiration && core.isReloaded() && !recoverReloadedCores) {
         doRecovery = false;
       }
       
