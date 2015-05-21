ab6f9a1280b81bcd7a59ba4b098363bdebeb83c2
Dawid Weiss <dweiss@apache.org>
2015-05-21 07:53:05 +0000

    JavaDoc correction (FixedBitSet->BitSet).
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1680756 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/BitDocIdSet.java
+++ lucene/core/src/java/org/apache/lucene/util/BitDocIdSet.java
@@ -34,9 +34,8 @@ public class BitDocIdSet extends DocIdSet {
   private final long cost;
 
   /**
-   * Wrap the given {@link FixedBitSet} as a {@link DocIdSet}. The provided
-   * {@link FixedBitSet} should not be modified after having wrapped as a
-   * {@link DocIdSet}.
+   * Wrap the given {@link BitSet} as a {@link DocIdSet}. The provided
+   * {@link BitSet} must not be modified afterwards.
    */
   public BitDocIdSet(BitSet set, long cost) {
     this.set = set;
