11bf206e0dc250410b21c2d226ce5746054e76ae
Timothy Potter <thelabdude@apache.org>
2015-05-22 17:44:57 +0000

    SOLR-6820: Increase the default number of buckets to 65536 instead of 256
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1681169 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/update/UpdateLog.java
+++ solr/core/src/java/org/apache/solr/update/UpdateLog.java
@@ -244,7 +244,7 @@ public class UpdateLog implements PluginInfoInitialized {
 
     numRecordsToKeep = objToInt(info.initArgs.get("numRecordsToKeep"), 100);
     maxNumLogsToKeep = objToInt(info.initArgs.get("maxNumLogsToKeep"), 10);
-    numVersionBuckets = objToInt(info.initArgs.get("numVersionBuckets"), 256);
+    numVersionBuckets = objToInt(info.initArgs.get("numVersionBuckets"), 65536);
     if (numVersionBuckets <= 0)
       throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,
           "Number of version buckets must be greater than 0!");
