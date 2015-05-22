387ddaf56ad99590a1160444fce57319451dbf91
Erik Hatcher <ehatcher@apache.org>
2015-05-22 14:52:05 +0000

    oops, back out previous commit
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1681134 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/handler/NestedRequestHandler.java
+++ solr/core/src/java/org/apache/solr/handler/NestedRequestHandler.java
@@ -23,7 +23,7 @@ import org.apache.solr.request.SolrRequestHandler;
 /**An interface for RequestHandlers need to handle all paths under its registered path
  */
 public interface NestedRequestHandler {
-  /** Return a SolrRequestHandler to handle a subpath from the path this handler is registered.
+  /** Return a Requestandler to handle a subpath from the path this handler is registered.
    */
   public SolrRequestHandler getSubHandler(String subPath);
 }
