6cd18fa6453243e2999fce5519d1328f94f9cd86
Mark Robert Miller <markrmiller@apache.org>
2015-05-29 14:22:23 +0000

    SOLR-7118: Raise fail tolerance.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1682483 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/cloud/ChaosMonkeyNothingIsSafeTest.java
+++ solr/core/src/test/org/apache/solr/cloud/ChaosMonkeyNothingIsSafeTest.java
@@ -49,7 +49,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @SuppressSSL(bugUrl = "https://issues.apache.org/jira/browse/SOLR-5776")
 @ThreadLeakLingering(linger = 60000)
 public class ChaosMonkeyNothingIsSafeTest extends AbstractFullDistribZkTestBase {
-  private static final int FAIL_TOLERANCE = 40;
+  private static final int FAIL_TOLERANCE = 60;
 
   public static Logger log = LoggerFactory.getLogger(ChaosMonkeyNothingIsSafeTest.class);
   
