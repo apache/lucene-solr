2cb14e923cd3d53cd08b2835964a31d5604e4d9f
Michael McCandless <mikemccand@apache.org>
2015-06-05 18:47:16 +0000

    LUCENE-5805: QueryNodeImpl.removeFromParent was doing nothing, in a costly way
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1683839 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/queryparser/src/test/org/apache/lucene/queryparser/flexible/core/nodes/TestQueryNode.java
+++ lucene/queryparser/src/test/org/apache/lucene/queryparser/flexible/core/nodes/TestQueryNode.java
@@ -44,23 +44,38 @@ public class TestQueryNode extends LuceneTestCase {
     assertTrue(node.getTag("tAg") != null);
     
   }
-  
+
+
   /* LUCENE-5099 - QueryNodeProcessorImpl should set parent to null before returning on processing */
   public void testRemoveFromParent() throws Exception {
     BooleanQueryNode booleanNode = new BooleanQueryNode(Collections.<QueryNode>emptyList());
     FieldQueryNode fieldNode = new FieldQueryNode("foo", "A", 0, 1);
     assertNull(fieldNode.getParent());
-    
+
     booleanNode.add(fieldNode);
     assertNotNull(fieldNode.getParent());
 
     fieldNode.removeFromParent();
     assertNull(fieldNode.getParent());
+    /* LUCENE-5805 - QueryNodeImpl.removeFromParent does a lot of work without any effect */
+    assertFalse(booleanNode.getChildren().contains(fieldNode));
 
     booleanNode.add(fieldNode);
     assertNotNull(fieldNode.getParent());
-    
+
     booleanNode.set(Collections.<QueryNode>emptyList());
+    assertNull(fieldNode.getParent());
+  }
+
+  public void testRemoveChildren() throws Exception{
+    BooleanQueryNode booleanNode = new BooleanQueryNode(Collections.<QueryNode>emptyList());
+    FieldQueryNode fieldNode = new FieldQueryNode("foo", "A", 0, 1);
+
+    booleanNode.add(fieldNode);
+    assertTrue(booleanNode.getChildren().size() == 1);
+
+    booleanNode.removeChildren(fieldNode);
+    assertTrue(booleanNode.getChildren().size()==0);
     assertNull(fieldNode.getParent());
   }
   
