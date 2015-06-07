867c75a5d97998c0b781c094c362b4377649231d
Erik Hatcher <ehatcher@apache.org>
2015-06-07 22:31:26 +0000

    Remove explicitly defined request handlers from example and test solrconfig's that are already defined implicitly
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1684080 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/handler/admin/AdminHandlers.java
+++ solr/core/src/java/org/apache/solr/handler/admin/AdminHandlers.java
@@ -35,8 +35,8 @@ import org.slf4j.LoggerFactory;
  * A special Handler that registers all standard admin handlers
  * 
  * @since solr 1.3
- * @deprecated No need to register this requesthandler . All
- * the plugins registered by this class are iplicitly registered by the system
+ * @deprecated No need to register this request handler. All
+ * the plugins registered by this class are implicitly registered by the system
  */
 @Deprecated
 public class AdminHandlers extends RequestHandlerBase implements SolrCoreAware
