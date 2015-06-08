517d362a7ff096e1abd5d431b954fc4767e16a29
Anshum Gupta <anshum@apache.org>
2015-06-08 01:26:36 +0000

    Add 5.2.0 back compat test indexes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1684092 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
@@ -220,7 +220,9 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
       "5.0.0-cfs",
       "5.0.0-nocfs",
       "5.1.0-cfs",
-      "5.1.0-nocfs"
+      "5.1.0-nocfs",
+      "5.2.0-cfs",
+      "5.2.0-nocfs"
   };
   
   final String[] unsupportedNames = {
