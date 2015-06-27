87c8d8dd44540fd9b89a331736720602b99a7a0d
Shalin Shekhar Mangar <shalin@apache.org>
2015-06-27 10:56:09 +0000

    Fix compiler warning by removing empty javadoc section
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1687911 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/test/org/apache/solr/client/solrj/impl/LBHttpSolrClientTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/impl/LBHttpSolrClientTest.java
@@ -1,18 +1,3 @@
-/**
- * 
- */
-package org.apache.solr.client.solrj.impl;
-
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.solr.client.solrj.ResponseParser;
-import org.apache.solr.common.params.ModifiableSolrParams;
-import org.junit.Test;
-
-import java.io.IOException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -29,6 +14,18 @@ import static org.junit.Assert.assertNull;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.solr.client.solrj.impl;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.solr.client.solrj.ResponseParser;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * Test the LBHttpSolrClient.
