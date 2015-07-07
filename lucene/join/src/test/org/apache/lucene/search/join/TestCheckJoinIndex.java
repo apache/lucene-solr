0fe4cb474a087fbf26168b3318262fc0dfd41b4d
Adrien Grand <jpountz@apache.org>
2015-07-07 12:49:32 +0000

    LUCENE-6589: Fix test bug.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1689649 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/join/src/test/org/apache/lucene/search/join/TestCheckJoinIndex.java
+++ lucene/join/src/test/org/apache/lucene/search/join/TestCheckJoinIndex.java
@@ -107,7 +107,7 @@ public class TestCheckJoinIndex extends LuceneTestCase {
     final RandomIndexWriter w = new RandomIndexWriter(random(), dir, iwc);
 
     List<Document> block = new ArrayList<>();
-    final int numChildren = TestUtil.nextInt(random(), 0, 3);
+    final int numChildren = TestUtil.nextInt(random(), 1, 3);
     for (int i = 0; i < numChildren; ++i) {
       Document doc = new Document();
       doc.add(new StringField("child", Integer.toString(i), Store.NO));
