d50f1e3703bb6f70a7f814f02a837629089c4f43
Gregory Chanan <gchanan@apache.org>
2015-07-22 18:18:54 +0000

    SOLR-7742: Avoid stacktrace in info logging
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1692309 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/core/ConfigSetProperties.java
+++ solr/core/src/java/org/apache/solr/core/ConfigSetProperties.java
@@ -48,7 +48,7 @@ public class ConfigSetProperties {
     try {
       reader = new InputStreamReader(loader.openResource(name), StandardCharsets.UTF_8);
     } catch (SolrResourceNotFoundException ex) {
-      log.info("Did not find ConfigSet properties", ex);
+      log.info("Did not find ConfigSet properties, assuming default properties: " + ex.getMessage());
       return null;
     } catch (Exception ex) {
       throw new SolrException(ErrorCode.SERVER_ERROR, "Unable to load reader for ConfigSet properties: " + name, ex);
