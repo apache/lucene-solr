a5d3bd5645e465df6389bcd7ad24ebf47fe0a2cd
Shalin Shekhar Mangar <shalin@apache.org>
2015-07-29 08:56:28 +0000

    SOLR-7842: ZK connection loss or session expiry events should not fire config directory listeners
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1693218 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/cloud/ZkController.java
+++ solr/core/src/java/org/apache/solr/cloud/ZkController.java
@@ -2330,6 +2330,10 @@ public final class ZkController {
 
     @Override
     public void process(WatchedEvent event) {
+      if (event.getState() == Event.KeeperState.Disconnected || event.getState() == Event.KeeperState.Expired)  {
+        return;
+      }
+
       Stat stat = null;
       try {
         stat = zkClient.exists(zkDir, null, true);
