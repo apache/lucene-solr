12fb9eda87508affa3a601e1ad955951798c25f1
Alan Woodward <romseygeek@apache.org>
2015-08-03 16:53:55 +0000

    LUCENE-6706: Commit from the right directory this time
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1693933 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/queryparser/src/java/org/apache/lucene/queryparser/xml/builders/BoostingTermBuilder.java
+++ lucene/queryparser/src/java/org/apache/lucene/queryparser/xml/builders/BoostingTermBuilder.java
@@ -1,11 +1,12 @@
 package org.apache.lucene.queryparser.xml.builders;
 
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.payloads.AveragePayloadFunction;
-import org.apache.lucene.search.payloads.PayloadTermQuery;
-import org.apache.lucene.search.spans.SpanQuery;
 import org.apache.lucene.queryparser.xml.DOMUtils;
 import org.apache.lucene.queryparser.xml.ParserException;
+import org.apache.lucene.search.payloads.AveragePayloadFunction;
+import org.apache.lucene.search.payloads.PayloadScoreQuery;
+import org.apache.lucene.search.spans.SpanQuery;
+import org.apache.lucene.search.spans.SpanTermQuery;
 import org.w3c.dom.Element;
 
 /*
@@ -26,7 +27,7 @@ import org.w3c.dom.Element;
  */
 
 /**
- * Builder for {@link PayloadTermQuery}
+ * Builder for {@link PayloadScoreQuery}
  */
 public class BoostingTermBuilder extends SpanBuilderBase {
 
@@ -35,7 +36,8 @@ public class BoostingTermBuilder extends SpanBuilderBase {
     String fieldName = DOMUtils.getAttributeWithInheritanceOrFail(e, "fieldName");
     String value = DOMUtils.getNonBlankTextOrFail(e);
 
-    PayloadTermQuery btq = new PayloadTermQuery(new Term(fieldName, value), new AveragePayloadFunction());
+    PayloadScoreQuery btq = new PayloadScoreQuery(new SpanTermQuery(new Term(fieldName, value)),
+        new AveragePayloadFunction());
     btq.setBoost(DOMUtils.getAttribute(e, "boost", 1.0f));
     return btq;
   }
