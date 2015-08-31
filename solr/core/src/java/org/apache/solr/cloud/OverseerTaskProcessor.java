0043146fe303029e4fb012af91b1c7effcac5c01
Mark Robert Miller <markrmiller@apache.org>
2015-08-31 17:20:01 +0000

    SOLR-7956: Missed an interrupt shutdown on 5x backport.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1700279 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/cloud/OverseerTaskProcessor.java
+++ solr/core/src/java/org/apache/solr/cloud/OverseerTaskProcessor.java
@@ -298,16 +298,7 @@ public class OverseerTaskProcessor implements Runnable, Closeable {
     isClosed = true;
     if(tpe != null) {
       if (!tpe.isShutdown()) {
-        tpe.shutdown();
-        try {
-          tpe.awaitTermination(60, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-          log.warn("Thread interrupted while waiting for OCP threadpool close.");
-          Thread.currentThread().interrupt();
-        } finally {
-          if (!tpe.isShutdown())
-            tpe.shutdownNow();
-        }
+        ExecutorUtil.shutdownAndAwaitTermination(tpe);
       }
     }
   }
