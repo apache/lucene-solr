00cbe8a7e3d8bd0e5ee9ff7cd12466fb3b33cf19
Yonik Seeley <yonik@apache.org>
2015-09-04 16:45:06 +0000

    SOLR-8007: tests: fix TestSearcherReuse by avoiding background merges
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1701292 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/test/org/apache/solr/search/TestSearcherReuse.java
+++ solr/core/src/test/org/apache/solr/search/TestSearcherReuse.java
@@ -89,7 +89,10 @@ public class TestSearcherReuse extends SolrTestCaseJ4 {
         assertU(commit());
       }
     }
-    assertU(commit());
+
+    // with random merge policies, a regular commit can cause a segment to be flushed that can kick off a background merge
+    // that can cause a later commit to actually see changes and open a new searcher.  This should not be possible with optimize
+    assertU(optimize());
 
     // seed a single query into the cache
     assertQ(req("*:*"), "//*[@numFound='"+numDocs+"']");
