aeb41ee7e48d16068e80354b131becdeb5d86ba6
Uwe Schindler <uschindler@apache.org>
2015-09-09 21:48:38 +0000

    LUCENE-6775: Improved MorfologikFilterFactory to allow loading of custom dictionaries from ResourceLoader
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1702118 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/analysis/morfologik/src/test/org/apache/lucene/analysis/morfologik/TestMorfologikFilterFactory.java
+++ lucene/analysis/morfologik/src/test/org/apache/lucene/analysis/morfologik/TestMorfologikFilterFactory.java
@@ -20,17 +20,57 @@ package org.apache.lucene.analysis.morfologik;
 import java.io.StringReader;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.lucene.analysis.BaseTokenStreamTestCase;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.util.ClasspathResourceLoader;
+import org.apache.lucene.analysis.util.ResourceLoader;
 
 /**
  * Test for {@link MorfologikFilterFactory}.
  */
 public class TestMorfologikFilterFactory extends BaseTokenStreamTestCase {
-  public void testCreateDictionary() throws Exception {
+  final ResourceLoader loader = new ClasspathResourceLoader(getClass());
+
+  public void testDefaultDictionary() throws Exception {
     StringReader reader = new StringReader("rowery bilety");
     MorfologikFilterFactory factory = new MorfologikFilterFactory(Collections.<String,String>emptyMap());
+    factory.inform(loader);
+    TokenStream stream = whitespaceMockTokenizer(reader);
+    stream = factory.create(stream);
+    assertTokenStreamContents(stream, new String[] {"rower", "bilet"});
+  }
+  
+  public void testResourceDictionary() throws Exception {
+    StringReader reader = new StringReader("rowery bilety");
+    Map<String,String> params = new HashMap<>();
+    params.put(MorfologikFilterFactory.DICTIONARY_RESOURCE_ATTRIBUTE, MorfologikFilterFactory.DEFAULT_DICTIONARY_RESOURCE);
+    MorfologikFilterFactory factory = new MorfologikFilterFactory(params);
+    factory.inform(loader);
+    TokenStream stream = whitespaceMockTokenizer(reader);
+    stream = factory.create(stream);
+    assertTokenStreamContents(stream, new String[] {"rower", "bilet"});
+  }
+  
+  public void testResourceLoaderDictionary1() throws Exception {
+    StringReader reader = new StringReader("rowery bilety");
+    Map<String,String> params = new HashMap<>();
+    params.put(MorfologikFilterFactory.DICTIONARY_FSA_FILE_ATTRIBUTE, "/morfologik/dictionaries/pl.dict");
+    MorfologikFilterFactory factory = new MorfologikFilterFactory(params);
+    factory.inform(loader);
+    TokenStream stream = whitespaceMockTokenizer(reader);
+    stream = factory.create(stream);
+    assertTokenStreamContents(stream, new String[] {"rower", "bilet"});
+  }
+  
+  public void testResourceLoaderDictionary2() throws Exception {
+    StringReader reader = new StringReader("rowery bilety");
+    Map<String,String> params = new HashMap<>();
+    params.put(MorfologikFilterFactory.DICTIONARY_FSA_FILE_ATTRIBUTE, "/morfologik/dictionaries/pl.dict");
+    params.put(MorfologikFilterFactory.DICTIONARY_FEATURES_FILE_ATTRIBUTE, "/morfologik/dictionaries/pl.info");
+    MorfologikFilterFactory factory = new MorfologikFilterFactory(params);
+    factory.inform(loader);
     TokenStream stream = whitespaceMockTokenizer(reader);
     stream = factory.create(stream);
     assertTokenStreamContents(stream, new String[] {"rower", "bilet"});
@@ -39,12 +79,48 @@ public class TestMorfologikFilterFactory extends BaseTokenStreamTestCase {
   /** Test that bogus arguments result in exception */
   public void testBogusArguments() throws Exception {
     try {
-      HashMap<String,String> map = new HashMap<String,String>();
-      map.put("bogusArg", "bogusValue");
-      new MorfologikFilterFactory(map);
+      HashMap<String,String> params = new HashMap<String,String>();
+      params.put("bogusArg", "bogusValue");
+      new MorfologikFilterFactory(params);
       fail();
     } catch (IllegalArgumentException expected) {
       assertTrue(expected.getMessage().contains("Unknown parameters"));
+    }
+  }
+  
+  public void testIncompatibleArgs1() throws Exception {
+    try {
+      HashMap<String,String> params = new HashMap<String,String>();
+      params.put(MorfologikFilterFactory.DICTIONARY_RESOURCE_ATTRIBUTE, MorfologikFilterFactory.DEFAULT_DICTIONARY_RESOURCE);
+      params.put(MorfologikFilterFactory.DICTIONARY_FSA_FILE_ATTRIBUTE, "/morfologik/dictionaries/pl.dict");
+      new MorfologikFilterFactory(params);
+      fail();
+    } catch (IllegalArgumentException expected) {
+      assertTrue(expected.getMessage().contains("at the same time"));
+    }
+  }
+  
+  public void testIncompatibleArgs2() throws Exception {
+    try {
+      HashMap<String,String> params = new HashMap<String,String>();
+      params.put(MorfologikFilterFactory.DICTIONARY_RESOURCE_ATTRIBUTE, MorfologikFilterFactory.DEFAULT_DICTIONARY_RESOURCE);
+      params.put(MorfologikFilterFactory.DICTIONARY_FSA_FILE_ATTRIBUTE, "/morfologik/dictionaries/pl.dict");
+      params.put(MorfologikFilterFactory.DICTIONARY_FEATURES_FILE_ATTRIBUTE, "/morfologik/dictionaries/pl.info");
+      new MorfologikFilterFactory(params);
+      fail();
+    } catch (IllegalArgumentException expected) {
+      assertTrue(expected.getMessage().contains("at the same time"));
+    }
+  }
+  
+  public void testMissingArgs1() throws Exception {
+    try {
+      HashMap<String,String> params = new HashMap<String,String>();
+      params.put(MorfologikFilterFactory.DICTIONARY_FEATURES_FILE_ATTRIBUTE, "/morfologik/dictionaries/pl.info");
+      new MorfologikFilterFactory(params);
+      fail();
+    } catch (IllegalArgumentException expected) {
+      assertTrue(expected.getMessage().contains("Missing"));
     }
   }
 }
