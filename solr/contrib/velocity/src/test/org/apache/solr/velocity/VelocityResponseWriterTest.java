426252627f9ed6ed25ecff5d462abaae5eeb5943
Erik Hatcher <ehatcher@apache.org>
2015-09-11 16:51:55 +0000

    SOLR-8041: Fix VelocityResponseWriter's .get(key,baseName,locale) to use specified locale (merged from trunk r1702516)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1702517 13f79535-47bb-0310-9956-ffa450edef68

--- solr/contrib/velocity/src/test/org/apache/solr/velocity/VelocityResponseWriterTest.java
+++ solr/contrib/velocity/src/test/org/apache/solr/velocity/VelocityResponseWriterTest.java
@@ -148,6 +148,11 @@ public class VelocityResponseWriterTest extends SolrTestCaseJ4 {
         VelocityResponseWriter.LOCALE,"en_US")));
     assertEquals("Colour", h.query(req("q", "*:*", "wt", "velocity", VelocityResponseWriter.TEMPLATE, "locale",
         VelocityResponseWriter.LOCALE,"en_UK")));
+
+    // Test that $resource.get(key,baseName,locale) works with specified locale
+    assertEquals("Colour", h.query(req("q","*:*", "wt","velocityWithCustomTools",VelocityResponseWriter.TEMPLATE,"t",
+        SolrParamResourceLoader.TEMPLATE_PARAM_PREFIX+"t", "$resource.get(\"color\",\"resources\",\"en_UK\")")));
+
   }
 
   @Test
