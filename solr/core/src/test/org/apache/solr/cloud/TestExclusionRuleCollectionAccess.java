a0000750623ed31e05a8a77fdf388c3a991ae6c9
Anshum Gupta <anshum@apache.org>
2015-09-16 19:05:12 +0000

    SOLR-8058: Fix the exclusion filter so that collections that start with js, css, img, tpl can be accessed.(merge from branch_5x)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_5_3@1703447 13f79535-47bb-0310-9956-ffa450edef68

---
+++ solr/core/src/test/org/apache/solr/cloud/TestExclusionRuleCollectionAccess.java
@@ -0,0 +1,50 @@
+package org.apache.solr.cloud;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.lucene.util.LuceneTestCase;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.request.CollectionAdminRequest;
+import org.junit.Test;
+
+@LuceneTestCase.Slow
+public class TestExclusionRuleCollectionAccess extends AbstractFullDistribZkTestBase {
+
+  public TestExclusionRuleCollectionAccess() {
+    schemaString = "schema15.xml";      // we need a string id
+    sliceCount = 1;
+  }
+
+  @Test
+  public void doTest() throws Exception {
+    CollectionAdminRequest.Create req = new CollectionAdminRequest.Create();
+    req.setCollectionName("css33");
+    req.setNumShards(1);
+    req.process(cloudClient);
+    
+    waitForRecoveriesToFinish("css33", false);
+    
+    try (SolrClient c = createCloudClient("css33")) {
+      c.add(getDoc("id", "1"));
+      c.commit();
+
+      assertEquals("Should have returned 1 result", 1, c.query(params("q", "*:*", "collection", "css33")).getResults().getNumFound());
+    }
+  }
+  
+}
