b952a9a48c9c0f6b7954326ce4d02ad0a97ec0e7
Mark Robert Miller <markrmiller@apache.org>
2015-10-01 18:43:49 +0000

    SOLR-8095: Allow disabling HDFS Locality Metrics and disable by default as it may have performance implications on rapidly changing indexes.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1706307 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/core/HdfsDirectoryFactory.java
+++ solr/core/src/java/org/apache/solr/core/HdfsDirectoryFactory.java
@@ -81,7 +81,9 @@ public class HdfsDirectoryFactory extends CachingDirectoryFactory implements Sol
   public static final String NRTCACHINGDIRECTORY_MAXMERGESIZEMB = "solr.hdfs.nrtcachingdirectory.maxmergesizemb";
   public static final String NRTCACHINGDIRECTORY_MAXCACHEMB = "solr.hdfs.nrtcachingdirectory.maxcachedmb";
   public static final String NUMBEROFBLOCKSPERBANK = "solr.hdfs.blockcache.blocksperbank";
-  
+
+  public static final String LOCALITYMETRICS_ENABLED = "solr.hdfs.locality.metrics.enabled";
+
   public static final String KERBEROS_ENABLED = "solr.hdfs.security.kerberos.enabled";
   public static final String KERBEROS_KEYTAB = "solr.hdfs.security.kerberos.keytabfile";
   public static final String KERBEROS_PRINCIPAL = "solr.hdfs.security.kerberos.principal";
@@ -219,8 +221,9 @@ public class HdfsDirectoryFactory extends CachingDirectoryFactory implements Sol
       hdfsDir = new HdfsDirectory(new Path(path), lockFactory, conf);
       dir = hdfsDir;
     }
-    
-    LocalityHolder.reporter.registerDirectory(hdfsDir);
+    if (params.getBool(LOCALITYMETRICS_ENABLED, false)) {
+      LocalityHolder.reporter.registerDirectory(hdfsDir);
+    }
 
     boolean nrtCachingDirectory = getConfig(NRTCACHINGDIRECTORY_ENABLE, true);
     if (nrtCachingDirectory) {
