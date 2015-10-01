85c3dec6234df40b7bbfde25f41380c3e88b5ab5
Christine Poerschke <cpoerschke@apache.org>
2015-10-01 21:31:29 +0000

    SOLR-8025: remove unnecessary ResponseBuilder.getQueryCommand() calls (merge in revision 1706313 from trunk)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1706322 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/search/grouping/distributed/requestfactory/TopGroupsShardRequestFactory.java
+++ solr/core/src/java/org/apache/solr/search/grouping/distributed/requestfactory/TopGroupsShardRequestFactory.java
@@ -56,7 +56,7 @@ public class TopGroupsShardRequestFactory implements ShardRequestFactory {
     // If we have a group.query we need to query all shards... Or we move this to the group first phase queries
     boolean containsGroupByQuery = rb.getGroupingSpec().getQueries().length > 0;
     // TODO: If groups.truncate=true we only have to query the specific shards even faceting and statistics are enabled
-    if ((rb.getQueryCommand().getFlags() & SolrIndexSearcher.GET_DOCSET) != 0 || containsGroupByQuery) {
+    if (rb.isNeedDocSet() || containsGroupByQuery) {
       // In case we need more results such as faceting and statistics we have to query all shards
       return createRequestForAllShards(rb);
     } else {
