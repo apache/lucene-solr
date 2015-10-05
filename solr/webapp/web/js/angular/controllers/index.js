8a430f3065e12a03b0a2d7897e5ef63a569fdc0f
Upayavira <upayavira@apache.org>
2015-10-05 11:03:14 +0000

    SOLR-7686 Fix dashboard on Windows - don't show load average
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1706799 13f79535-47bb-0310-9956-ffa450edef68

--- solr/webapp/web/js/angular/controllers/index.js
+++ solr/webapp/web/js/angular/controllers/index.js
@@ -22,11 +22,13 @@ solrAdminApp.controller('IndexController', function($scope, System, Cores, Const
       $scope.system = data;
 
       // load average
-      load_average = ( data.system.uptime || '' ).match( /load averages?: (\d+[.,]\d\d),? (\d+[.,]\d\d),? (\d+[.,]\d\d)/ );
-      for (var i=0;i<2;i++) {
-        load_average[i]=load_average[i].replace(",","."); // for European users
+      var load_average = ( data.system.uptime || '' ).match( /load averages?: (\d+[.,]\d\d),? (\d+[.,]\d\d),? (\d+[.,]\d\d)/ );
+      if (load_average) {
+        for (var i=0;i<2;i++) {
+          load_average[i]=load_average[i].replace(",","."); // for European users
+        }
+        $scope.load_average = load_average.slice(1);
       }
-      $scope.load_average = load_average.slice(1);
 
       // physical memory
       var memoryMax = parse_memory_value(data.system.totalPhysicalMemorySize);
