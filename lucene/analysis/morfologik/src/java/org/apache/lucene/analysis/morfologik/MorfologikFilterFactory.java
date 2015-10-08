3845dec5bd93e70e06370ca8aa0a58f343ec8229
Dawid Weiss <dweiss@apache.org>
2015-10-08 09:21:35 +0000

    LUCENE-6833: Upgraded Morfologik to version 2.0.1. The 'dictionary' attribute has been reverted back and now points at the dictionary resource to be loaded instead of the default Polish dictionary.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1707458 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/analysis/morfologik/src/java/org/apache/lucene/analysis/morfologik/MorfologikFilterFactory.java
+++ lucene/analysis/morfologik/src/java/org/apache/lucene/analysis/morfologik/MorfologikFilterFactory.java
@@ -19,11 +19,12 @@ package org.apache.lucene.analysis.morfologik;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
 import morfologik.stemming.Dictionary;
+import morfologik.stemming.DictionaryMetadata;
+import morfologik.stemming.polish.PolishStemmer;
 
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.util.ResourceLoader;
@@ -31,87 +32,47 @@ import org.apache.lucene.analysis.util.ResourceLoaderAware;
 import org.apache.lucene.analysis.util.TokenFilterFactory;
 
 /**
- * Filter factory for {@link MorfologikFilter}. For backward compatibility polish
- * dictionary is used as default. You can change dictionary resource 
- * by dictionary-resource parameter:
+ * Filter factory for {@link MorfologikFilter}. 
+ * 
+ * <p>An explicit resource name of the dictionary ({@code ".dict"}) can be 
+ * provided via the <code>dictionary</code> attribute, as the example below demonstrates:
  * <pre class="prettyprint">
- * &lt;fieldType name="text_polish" class="solr.TextField" positionIncrementGap="100"&gt;
+ * &lt;fieldType name="text_mylang" class="solr.TextField" positionIncrementGap="100"&gt;
  *   &lt;analyzer&gt;
  *     &lt;tokenizer class="solr.WhitespaceTokenizerFactory"/&gt;
- *     &lt;filter class="solr.MorfologikFilterFactory" dictionary-resource="pl" /&gt;
+ *     &lt;filter class="solr.MorfologikFilterFactory" dictionary="mylang.dict" /&gt;
  *   &lt;/analyzer&gt;
  * &lt;/fieldType&gt;</pre>
  * 
- * <p>Alternatively, you can pass in the filenames of FSA ({@code ".dict"} and features "{@code ".info"}" file
- * (if the features file is not given, its name is derived from the FSA file):
- * <pre class="prettyprint">
- * &lt;fieldType name="text_polish" class="solr.TextField" positionIncrementGap="100"&gt;
- *   &lt;analyzer&gt;
- *     &lt;tokenizer class="solr.WhitespaceTokenizerFactory"/&gt;
- *     &lt;filter class="solr.MorfologikFilterFactory" dictionary-fsa-file="mylang.dict" dictionary-features-file="mylang.info" /&gt;
- *   &lt;/analyzer&gt;
- * &lt;/fieldType&gt;</pre>
+ * <p>If the dictionary attribute is not provided, the Polish dictionary is loaded
+ * and used by default. 
  * 
  * @see <a href="http://morfologik.blogspot.com/">Morfologik web site</a>
  */
 public class MorfologikFilterFactory extends TokenFilterFactory implements ResourceLoaderAware {
-  /**
-   * The default dictionary resource (for Polish). 
-   */
-  public static final String DEFAULT_DICTIONARY_RESOURCE = "pl";
+  /** Dictionary resource attribute (should have {@code ".dict"} suffix), loaded from {@link ResourceLoader}. */
+  public static final String DICTIONARY_ATTRIBUTE = "dictionary";
 
-  /** Schema attribute. */
-  @Deprecated
-  public static final String DICTIONARY_SCHEMA_ATTRIBUTE = "dictionary";
+  /** {@link #DICTIONARY_ATTRIBUTE} value passed to {@link #inform}. */
+  private String resourceName;
 
-  /** Dictionary resource */
-  public static final String DICTIONARY_RESOURCE_ATTRIBUTE = "dictionary-resource";
-
-  /** Dictionary FSA file (should have {@code ".dict"} suffix), loaded from {@link ResourceLoader}. */
-  public static final String DICTIONARY_FSA_FILE_ATTRIBUTE = "dictionary-fsa-file";
-
-  /** Dictionary features/properties file, loaded from {@link ResourceLoader}. If not given, this
-   * loads the file with same name like {@link #DICTIONARY_FSA_FILE_ATTRIBUTE}, but with
-   * {@code ".info"} suffix.
-   */
-  public static final String DICTIONARY_FEATURES_FILE_ATTRIBUTE = "dictionary-features-file";
-
-  private final String dictionaryFsaFile, dictionaryFeaturesFile, dictionaryResource;
-  private Dictionary dictionary; // initialized on inform()
+  /** Loaded {@link Dictionary}, initialized on {@link #inform(ResourceLoader)}. */
+  private Dictionary dictionary;
 
   /** Creates a new MorfologikFilterFactory */
   public MorfologikFilterFactory(Map<String,String> args) {
     super(args);
 
     // Be specific about no-longer-supported dictionary attribute.
-    String dictionaryName = get(args, DICTIONARY_SCHEMA_ATTRIBUTE);
-    if (dictionaryName != null && !dictionaryName.isEmpty()) {
-      throw new IllegalArgumentException("The " + DICTIONARY_SCHEMA_ATTRIBUTE + " attribute is no "
-          + "longer supported (Morfologik now offers one unified Polish dictionary): " + dictionaryName
-          + ". Perhaps you wanted to use 'dictionary-resource' attribute instead?");
+    final String DICTIONARY_RESOURCE_ATTRIBUTE = "dictionary-resource";
+    String dictionaryResource = get(args, DICTIONARY_RESOURCE_ATTRIBUTE);
+    if (dictionaryResource != null && !dictionaryResource.isEmpty()) {
+      throw new IllegalArgumentException("The " + DICTIONARY_RESOURCE_ATTRIBUTE + " attribute is no "
+          + "longer supported. Use the '" + DICTIONARY_ATTRIBUTE + "' attribute instead (see LUCENE-6833).");
     }
 
-    // first check FSA and features (at least FSA must be given, features name is guessed):
-    dictionaryFsaFile = get(args, DICTIONARY_FSA_FILE_ATTRIBUTE);
-    dictionaryFeaturesFile = get(args, DICTIONARY_FEATURES_FILE_ATTRIBUTE,
-        (dictionaryFsaFile == null) ? null : Dictionary.getExpectedFeaturesName(dictionaryFsaFile));
-    
-    if (dictionaryFsaFile == null && dictionaryFeaturesFile == null) {
-      // if we have no FSA/features combination, we resolve the classpath resource:
-      dictionaryResource = get(args, DICTIONARY_RESOURCE_ATTRIBUTE, DEFAULT_DICTIONARY_RESOURCE);
-    } else if (dictionaryFsaFile == null || dictionaryFeaturesFile == null) {
-      // if we have incomplete FSA/features tuple in args
-      throw new IllegalArgumentException(String.format(Locale.ENGLISH, "Missing '%s' or '%s' attribute.",
-          DICTIONARY_FSA_FILE_ATTRIBUTE, DICTIONARY_FEATURES_FILE_ATTRIBUTE));      
-    } else {
-      dictionaryResource = null;
-      if (get(args, DICTIONARY_RESOURCE_ATTRIBUTE) != null) {
-        // fail if both is given: FSA/features files + classpath resource
-        throw new IllegalArgumentException(String.format(Locale.ENGLISH, "Cannot give '%s' and '%s'/'%s' at the same time.",
-            DICTIONARY_RESOURCE_ATTRIBUTE, DICTIONARY_FSA_FILE_ATTRIBUTE, DICTIONARY_FEATURES_FILE_ATTRIBUTE));
-      }
-    }
-    
+    resourceName = get(args, DICTIONARY_ATTRIBUTE);
+
     if (!args.isEmpty()) {
       throw new IllegalArgumentException("Unknown parameters: " + args);
     }
@@ -119,16 +80,14 @@ public class MorfologikFilterFactory extends TokenFilterFactory implements Resou
 
   @Override
   public void inform(ResourceLoader loader) throws IOException {
-    if (dictionaryFsaFile != null) {
-      assert dictionaryFeaturesFile != null;
-      assert dictionaryResource == null;
-      try (final InputStream dictIn = loader.openResource(dictionaryFsaFile);
-          final InputStream metaIn = loader.openResource(dictionaryFeaturesFile)) {
-        this.dictionary = Dictionary.readAndClose(dictIn, metaIn);
-      }
+    if (resourceName == null) {
+      // Get the dictionary lazily, does not hold up memory.
+      this.dictionary = new PolishStemmer().getDictionary();
     } else {
-      assert dictionaryResource != null;
-      this.dictionary = MorfologikFilter.loadDictionaryResource(dictionaryResource);
+      try (InputStream dict = loader.openResource(resourceName);
+           InputStream meta = loader.openResource(DictionaryMetadata.getExpectedMetadataFileName(resourceName))) {
+        this.dictionary = Dictionary.read(dict, meta);
+      }
     }
   }
 
