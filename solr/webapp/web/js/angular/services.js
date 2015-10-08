bb1b9060acfe03af6089cc3648b20b03b592d42e
Upayavira <upayavira@apache.org>
2015-10-08 09:37:53 +0000

    SOLR-7666 Resolve exceptions and tidy HTML label name
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1707464 13f79535-47bb-0310-9956-ffa450edef68

--- solr/webapp/web/js/angular/services.js
+++ solr/webapp/web/js/angular/services.js
@@ -136,7 +136,7 @@ solrAdminServices.factory('System',
 .factory('Luke',
   ['$resource', function($resource) {
     return $resource('/solr/:core/admin/luke', {core: '@core', wt:'json', _:Date.now()}, {
-      "index":  {params: {numTerms: 0}},
+      "index":  {params: {numTerms: 0, show: 'index'}},
       "schema": {params: {show:'schema'}},
       "field": {},
       "fields": {params: {show:'schema'}, interceptor: {
@@ -178,8 +178,8 @@ solrAdminServices.factory('System',
   ['$resource', function($resource) {
     return $resource('/solr/:core/admin/ping', {wt:'json', core: '@core', ts:Date.now(), _:Date.now()}, {
      "ping": {},
-     "status": {params:{action:"status"}}
-    });
+     "status": {params:{action:"status"}, headers: {doNotIntercept: "true"}
+    }});
   }])
 .factory('Mbeans',
   ['$resource', function($resource) {
