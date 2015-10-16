6b36732a9b8442600b1e7e2ba5ed15a5aca42420
Uwe Schindler <uschindler@apache.org>
2015-10-16 09:43:56 +0000

    LUCENE-6830: Upgrade ANTLR used in expressions module to version 4.5.1-1
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1708935 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/expressions/src/java/org/apache/lucene/expressions/js/JavascriptLexer.java
+++ lucene/expressions/src/java/org/apache/lucene/expressions/js/JavascriptLexer.java
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 class JavascriptLexer extends Lexer {
-  static { RuntimeMetaData.checkVersion("4.5", RuntimeMetaData.VERSION); }
+  static { RuntimeMetaData.checkVersion("4.5.1", RuntimeMetaData.VERSION); }
 
   protected static final DFA[] _decisionToDFA;
   protected static final PredictionContextCache _sharedContextCache =
