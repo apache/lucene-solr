bd4811e230957cbdf038defe2ce0b1565d0f327a
Uwe Schindler <uschindler@apache.org>
2015-10-16 09:45:57 +0000

    Merged revision(s) 1708935 from lucene/dev/trunk: LUCENE-6830: Upgrade ANTLR used in expressions module to version 4.5.1-1
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1708936 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/expressions/src/java/org/apache/lucene/expressions/js/JavascriptParser.java
+++ lucene/expressions/src/java/org/apache/lucene/expressions/js/JavascriptParser.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 class JavascriptParser extends Parser {
-  static { RuntimeMetaData.checkVersion("4.5", RuntimeMetaData.VERSION); }
+  static { RuntimeMetaData.checkVersion("4.5.1", RuntimeMetaData.VERSION); }
 
   protected static final DFA[] _decisionToDFA;
   protected static final PredictionContextCache _sharedContextCache =
