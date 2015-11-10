e1ca34b179daefb0e4dde6984d8cf16b95a3d3f5
Jan Høydahl <janhoy@apache.org>
2015-11-10 22:19:52 +0000

    LUCENE-6888: Explain output of map() function now also prints default value (backport)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1713738 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/queries/src/test/org/apache/lucene/queries/function/TestFunctionQueryExplanations.java
+++ lucene/queries/src/test/org/apache/lucene/queries/function/TestFunctionQueryExplanations.java
@@ -18,6 +18,7 @@ package org.apache.lucene.queries.function;
  */
 
 import org.apache.lucene.queries.function.valuesource.ConstValueSource;
+import org.apache.lucene.queries.function.valuesource.RangeMapFloatFunction;
 import org.apache.lucene.search.BaseExplanationTestCase;
 import org.apache.lucene.search.BoostQuery;
 import org.apache.lucene.search.Query;
@@ -32,5 +33,18 @@ public class TestFunctionQueryExplanations extends BaseExplanationTestCase {
   public void testBoost() throws Exception {
     Query q = new BoostQuery(new FunctionQuery(new ConstValueSource(5)), 2);
     qtest(q, new int[] { 0,1,2,3 });
+  }
+
+  public void testMapFunction() throws Exception {
+    ValueSource rff = new RangeMapFloatFunction(new ConstValueSource(3), 0, 1, 2, new Float(4));
+    Query q = new FunctionQuery(rff);
+    qtest(q, new int[] { 0,1,2,3 });
+    assertEquals("map(const(3.0),0.0,1.0,const(2.0),const(4.0))", rff.description());
+    assertEquals("map(const(3.0),min=0.0,max=1.0,target=const(2.0),defaultVal=const(4.0))", rff.getValues(null, null).toString(123));
+
+    // DefaultValue is null -> defaults to source value
+    rff = new RangeMapFloatFunction(new ConstValueSource(3), 0, 1, 2, null);
+    assertEquals("map(const(3.0),0.0,1.0,const(2.0),null)", rff.description());
+    assertEquals("map(const(3.0),min=0.0,max=1.0,target=const(2.0),defaultVal=null)", rff.getValues(null, null).toString(123));
   }
 }
