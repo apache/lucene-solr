7307bb7ebaa05d0b70d9f578d07a2960eff39239
Michael McCandless <mikemccand@apache.org>
2015-11-12 11:39:51 +0000

    LUCENE-6895: fix test bug
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1714021 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/spatial3d/src/test/org/apache/lucene/geo3d/TestGeo3DPointField.java
+++ lucene/spatial3d/src/test/org/apache/lucene/geo3d/TestGeo3DPointField.java
@@ -75,7 +75,7 @@ public class TestGeo3DPointField extends LuceneTestCase {
   private static Codec getCodec() {
     if (Codec.getDefault().getName().equals("Lucene60")) {
       int maxPointsInLeafNode = TestUtil.nextInt(random(), 16, 2048);
-      double maxMBSortInHeap = 0.1 + (3*random().nextDouble());
+      double maxMBSortInHeap = 3.0 + (3*random().nextDouble());
       if (VERBOSE) {
         System.out.println("TEST: using Lucene60DimensionalFormat with maxPointsInLeafNode=" + maxPointsInLeafNode + " and maxMBSortInHeap=" + maxMBSortInHeap);
       }
