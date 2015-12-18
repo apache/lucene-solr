4acfcd061a0c447fcfcfd501dada9b46b36d2b85
Noble Paul <noble@apache.org>
2015-12-18 09:13:58 +0000

    SOLR-8429 precommit error
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1720732 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/security/BasicAuthPlugin.java
+++ solr/core/src/java/org/apache/solr/security/BasicAuthPlugin.java
@@ -26,6 +26,7 @@ import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.lang.invoke.MethodHandles;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
@@ -43,7 +44,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BasicAuthPlugin extends AuthenticationPlugin implements ConfigEditablePlugin {
-  private static final Logger log = LoggerFactory.getLogger(BasicAuthPlugin.class);
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   private AuthenticationProvider zkAuthentication;
   private final static ThreadLocal<Header> authHeader = new ThreadLocal<>();
   private boolean blockUnknown = false;
