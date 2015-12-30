10cdcf6f9baccf5e4da3340e9dd8b2cf9375b904
Michael McCandless <mikemccand@apache.org>
2015-12-30 12:22:16 +0000

    fix test bug
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1722325 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/test/org/apache/lucene/search/TestDimensionalRangeQuery.java
+++ lucene/core/src/test/org/apache/lucene/search/TestDimensionalRangeQuery.java
@@ -1028,7 +1028,7 @@ public class TestDimensionalRangeQuery extends LuceneTestCase {
   private static Codec getCodec() {
     if (Codec.getDefault().getName().equals("Lucene60")) {
       int maxPointsInLeafNode = TestUtil.nextInt(random(), 16, 2048);
-      double maxMBSortInHeap = 2.0 + (3*random().nextDouble());
+      double maxMBSortInHeap = 3.0 + (3*random().nextDouble());
       if (VERBOSE) {
         System.out.println("TEST: using Lucene60DimensionalFormat with maxPointsInLeafNode=" + maxPointsInLeafNode + " and maxMBSortInHeap=" + maxMBSortInHeap);
       }
