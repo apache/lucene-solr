c87b01af027b4c4f1a62de21d5c46e831a1d92d1
Tomas Eduardo Fernandez Lobbe <tflobbe@apache.org>
2016-01-06 22:37:51 +0000

    SOLR-8477: Let users choose compression mode in SchemaCodecFactory
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1723427 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/core/SchemaCodecFactory.java
+++ solr/core/src/java/org/apache/solr/core/SchemaCodecFactory.java
@@ -1,12 +1,21 @@
 package org.apache.solr.core;
 
+import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
+import java.util.Locale;
+
 import org.apache.lucene.codecs.Codec;
 import org.apache.lucene.codecs.DocValuesFormat;
 import org.apache.lucene.codecs.PostingsFormat;
+import org.apache.lucene.codecs.lucene50.Lucene50StoredFieldsFormat.Mode;
 import org.apache.lucene.codecs.lucene60.Lucene60Codec;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.SolrException.ErrorCode;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.schema.SchemaField;
 import org.apache.solr.util.plugin.SolrCoreAware;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
@@ -29,11 +38,27 @@ import org.apache.solr.util.plugin.SolrCoreAware;
  * Per-field CodecFactory implementation, extends Lucene's 
  * and returns postings format implementations according to the 
  * schema configuration.
+ * <br>
+ * Also, a string argument with name <code>compressionMode</code> can be
+ * provided to chose between the different compression options for
+ * stored fields
+ * 
  * @lucene.experimental
  */
 public class SchemaCodecFactory extends CodecFactory implements SolrCoreAware {
+  
+  /**
+   * Key to use in init arguments to set the compression mode in the codec.
+   */
+  public static final String COMPRESSION_MODE = "compressionMode";
+  
+  public static final Mode SOLR_DEFAULT_COMPRESSION_MODE = Mode.BEST_SPEED;
+  
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  
   private Codec codec;
   private volatile SolrCore core;
+  
   
   // TODO: we need to change how solr does this?
   // rather than a string like "Direct" you need to be able to pass parameters
@@ -51,7 +76,23 @@ public class SchemaCodecFactory extends CodecFactory implements SolrCoreAware {
   @Override
   public void init(NamedList args) {
     super.init(args);
-    codec = new Lucene60Codec() {
+    assert codec == null;
+    String compressionModeStr = (String)args.get(COMPRESSION_MODE);
+    Mode compressionMode;
+    if (compressionModeStr != null) {
+      try {
+        compressionMode = Mode.valueOf(compressionModeStr.toUpperCase(Locale.ROOT));
+      } catch (IllegalArgumentException e) {
+        throw new SolrException(ErrorCode.SERVER_ERROR, 
+            "Invalid compressionMode: '" + compressionModeStr + 
+            "'. Value must be one of " + Arrays.toString(Mode.values()));
+      }
+      log.info("Using compressionMode: " + compressionMode);
+    } else {
+      compressionMode = SOLR_DEFAULT_COMPRESSION_MODE;
+      log.info("Using default compressionMode: " + compressionMode);
+    }
+    codec = new Lucene60Codec(compressionMode) {
       @Override
       public PostingsFormat getPostingsFormatForField(String field) {
         final SchemaField schemaField = core.getLatestSchema().getFieldOrNull(field);
