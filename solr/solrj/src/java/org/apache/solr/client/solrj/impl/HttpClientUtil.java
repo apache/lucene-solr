098e12e21e758aca7664a8afda144c16448fc58b
Mark Robert Miller <markrmiller@apache.org>
2016-01-07 19:43:09 +0000

    SOLR-8450: Our HttpClient retry policy is too permissive.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1723616 13f79535-47bb-0310-9956-ffa450edef68

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/HttpClientUtil.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/HttpClientUtil.java
@@ -263,12 +263,7 @@ public class HttpClientUtil {
     } else {
       // if the request is not fully sent, we retry
       // streaming updates are not a problem, because they are not retryable
-      httpClient.setHttpRequestRetryHandler(new DefaultHttpRequestRetryHandler(){
-        @Override
-        protected boolean handleAsIdempotent(final HttpRequest request) {
-          return false; // we can't tell if a Solr request is idempotent
-        }
-      });
+      httpClient.setHttpRequestRetryHandler(new SolrHttpRequestRetryHandler(3));
     }
   }
 
