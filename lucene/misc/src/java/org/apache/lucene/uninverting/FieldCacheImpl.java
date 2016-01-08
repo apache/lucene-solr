96deb5e27329e400a66ea43c5c997771f1137ac5
Christine Poerschke <cpoerschke@apache.org>
2016-01-08 22:16:10 +0000

    LUCENE-6948: Fix ArrayIndexOutOfBoundsException in PagedBytes$Reader.fill by removing an unnecessary long-to-int cast. Also, unrelated, 2 ArrayList<>(initialCapacity) tweaks in getChildResources methods. (merge in revision 1723787 from trunk)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1723810 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/misc/src/java/org/apache/lucene/uninverting/FieldCacheImpl.java
+++ lucene/misc/src/java/org/apache/lucene/uninverting/FieldCacheImpl.java
@@ -615,7 +615,7 @@ class FieldCacheImpl implements FieldCache {
     
     @Override
     public Collection<Accountable> getChildResources() {
-      List<Accountable> resources = new ArrayList<>();
+      List<Accountable> resources = new ArrayList<>(3);
       resources.add(Accountables.namedAccountable("term bytes", bytes));
       resources.add(Accountables.namedAccountable("ord -> term", termOrdToBytesOffset));
       resources.add(Accountables.namedAccountable("doc -> ord", docToTermOrd));
@@ -740,7 +740,7 @@ class FieldCacheImpl implements FieldCache {
       return new BinaryDocValues() {
         @Override
         public BytesRef get(int docID) {
-          final int pointer = (int) docToOffset.get(docID);
+          final long pointer = docToOffset.get(docID);
           if (pointer == 0) {
             term.length = 0;
           } else {
@@ -758,7 +758,7 @@ class FieldCacheImpl implements FieldCache {
 
     @Override
     public Collection<Accountable> getChildResources() {
-      List<Accountable> resources = new ArrayList<>();
+      List<Accountable> resources = new ArrayList<>(2);
       resources.add(Accountables.namedAccountable("term bytes", bytes));
       resources.add(Accountables.namedAccountable("addresses", docToOffset));
       return Collections.unmodifiableList(resources);
