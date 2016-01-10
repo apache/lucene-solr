5db6df0ca2c5545b67c3ffc32c07684a0c71cf2d
Steven Rowe <sarowe@apache.org>
2016-01-10 19:42:37 +0000

    LUCENE-6955: The release smoke tester inappropriately requires back compat index testing for versions not less than the one being smoke tested
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1723956 13f79535-47bb-0310-9956-ffa450edef68

--- dev-tools/scripts/smokeTestRelease.py
+++ dev-tools/scripts/smokeTestRelease.py
@@ -730,7 +730,7 @@ def verifyUnpacked(java, project, artifact, unpackPath, svnRevision, version, te
 
   if project == 'lucene' and isSrc:
     print('  confirm all releases have coverage in TestBackwardsCompatibility')
-    confirmAllReleasesAreTestedForBackCompat(unpackPath)
+    confirmAllReleasesAreTestedForBackCompat(version, unpackPath)
     
 
 def testNotice(unpackPath):
@@ -1281,7 +1281,7 @@ def getAllLuceneReleases():
   l.sort()
   return l
 
-def confirmAllReleasesAreTestedForBackCompat(unpackPath):
+def confirmAllReleasesAreTestedForBackCompat(smokeVersion, unpackPath):
 
   print('    find all past Lucene releases...')
   allReleases = getAllLuceneReleases()
@@ -1345,8 +1345,14 @@ def confirmAllReleasesAreTestedForBackCompat(unpackPath):
   notTested = []
   for x in allReleases:
     if x not in testedIndices:
-      if '.'.join(str(y) for y in x) in ('1.4.3', '1.9.1', '2.3.1', '2.3.2'):
+      releaseVersion = '.'.join(str(y) for y in x)
+      if releaseVersion in ('1.4.3', '1.9.1', '2.3.1', '2.3.2'):
         # Exempt the dark ages indices
+        continue
+      if x >= tuple(int(y) for y in smokeVersion.split('.')):
+        # Exempt versions not less than the one being smoke tested
+        print('      Backcompat testing not required for release %s because it\'s not less than %s'
+              % (releaseVersion, smokeVersion))
         continue
       notTested.append(x)
 
