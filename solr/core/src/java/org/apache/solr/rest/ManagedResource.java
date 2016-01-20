edae06bd170f572211201bb2449b463b75efcd83
Christine Poerschke <cpoerschke@apache.org>
2016-01-20 17:00:28 +0000

    SOLR-8566: ManagedResource.buildMapToStore initialCapacity tweak. ManagedResource.storeManagedData log.error tweak ("load data" instead of "load stop words"). (merge in revision 1725752 from trunk)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_5x@1725757 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/rest/ManagedResource.java
+++ solr/core/src/java/org/apache/solr/rest/ManagedResource.java
@@ -267,7 +267,7 @@ public abstract class ManagedResource {
           // note: the data we're managing now remains in a dubious state
           // however the text analysis component remains unaffected 
           // (at least until core reload)
-          log.error("Failed to load stop words from storage due to: "+reloadExc);
+          log.error("Failed to load data from storage due to: "+reloadExc);
         }
       }
       
@@ -324,7 +324,7 @@ public abstract class ManagedResource {
    * Builds the JSON object to be stored, containing initArgs and managed data fields. 
    */
   protected Map<String,Object> buildMapToStore(Object managedData) {
-    Map<String,Object> toStore = new LinkedHashMap<>();
+    Map<String,Object> toStore = new LinkedHashMap<>(4, 1.0f);
     toStore.put(INIT_ARGS_JSON_FIELD, convertNamedListToMap(managedInitArgs));
     
     // report important dates when data was init'd / updated
