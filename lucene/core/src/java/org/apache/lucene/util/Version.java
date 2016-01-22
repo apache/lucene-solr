9cf3a8dd85e6da0518599b1c6fb6bac4089d7a2e
Anshum Gupta <anshum@apache.org>
2016-01-22 19:36:46 +0000

    Add version 5.3.3
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene_solr_5_3@1726295 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -253,8 +253,15 @@ public final class Version {
 
   /**
    * Match settings and bugs in Lucene's 5.3.2 release.
+   * @deprecated (5.3.3) Use latest
    */
+  @Deprecated
   public static final Version LUCENE_5_3_2 = new Version(5, 3, 2);
+
+  /**
+   * Match settings and bugs in Lucene's 5.3.3 release.
+   */
+  public static final Version LUCENE_5_3_3 = new Version(5, 3, 3);
 
   // To add a new version:
   //  * Only add above this comment
@@ -275,7 +282,7 @@ public final class Version {
    * some defaults may have changed and may break functionality 
    * in your application.
    */
-  public static final Version LATEST = LUCENE_5_3_2;
+  public static final Version LATEST = LUCENE_5_3_3;
 
   /**
    * Constant for backwards compatibility.
