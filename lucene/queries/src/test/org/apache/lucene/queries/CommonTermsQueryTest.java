fe34e968ce9d8740797d9ca52738bcf4093b584b
Robert Muir <rmuir@apache.org>
2016-01-26 04:15:29 -0500

    fix test bug (fails with DFI similarity)

--- lucene/queries/src/test/org/apache/lucene/queries/CommonTermsQueryTest.java
+++ lucene/queries/src/test/org/apache/lucene/queries/CommonTermsQueryTest.java
@@ -265,7 +265,7 @@ public class CommonTermsQueryTest extends LuceneTestCase {
       assertEquals("0", r.document(search.scoreDocs[0].doc).get("id"));
       assertEquals("2", r.document(search.scoreDocs[1].doc).get("id"));
       assertEquals("3", r.document(search.scoreDocs[2].doc).get("id"));
-      assertTrue(search.scoreDocs[1].score > search.scoreDocs[2].score);
+      assertTrue(search.scoreDocs[1].score >= search.scoreDocs[2].score);
     }
     
     {
