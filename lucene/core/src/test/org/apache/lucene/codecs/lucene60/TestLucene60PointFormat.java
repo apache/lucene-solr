c260c9d25190e2a446508631e932a83cbec628f3
Mike McCandless <mikemccand@apache.org>
2016-01-29 05:02:32 -0500

    fix test bug

--- lucene/core/src/test/org/apache/lucene/codecs/lucene60/TestLucene60PointFormat.java
+++ lucene/core/src/test/org/apache/lucene/codecs/lucene60/TestLucene60PointFormat.java
@@ -41,7 +41,7 @@ public class TestLucene60PointFormat extends BasePointFormatTestCase {
     if (random().nextBoolean()) {
       // randomize parameters
       int maxPointsInLeafNode = TestUtil.nextInt(random(), 50, 500);
-      double maxMBSortInHeap = 0.1 + (3*random().nextDouble());
+      double maxMBSortInHeap = 3.0 + (3*random().nextDouble());
       if (VERBOSE) {
         System.out.println("TEST: using Lucene60PointFormat with maxPointsInLeafNode=" + maxPointsInLeafNode + " and maxMBSortInHeap=" + maxMBSortInHeap);
       }
