f38b5f745dfe157c2cc52fefe75585be2c82845c
Mike McCandless <mikemccand@apache.org>
2016-02-18 16:17:44 -0500

    do not create a 5.6.0 version now since 5.5.0 is the last 5.x feature release

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -274,15 +274,8 @@ public final class Version {
 
   /**
    * Match settings and bugs in Lucene's 5.5.0 release.
-   * @deprecated (5.6.0) Use latest
    */
-  @Deprecated
   public static final Version LUCENE_5_5_0 = new Version(5, 5, 0);
-
-  /**
-   * Match settings and bugs in Lucene's 5.6.0 release.
-   */
-  public static final Version LUCENE_5_6_0 = new Version(5, 6, 0);
 
   // To add a new version:
   //  * Only add above this comment
@@ -303,7 +296,7 @@ public final class Version {
    * some defaults may have changed and may break functionality 
    * in your application.
    */
-  public static final Version LATEST = LUCENE_5_6_0;
+  public static final Version LATEST = LUCENE_5_5_0;
 
   /**
    * Constant for backwards compatibility.
