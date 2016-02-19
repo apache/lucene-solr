250446a88184e0c0c123c620b5a3cfcd715ca820
markrmiller <markrmiller@apache.org>
2016-02-19 14:36:53 -0500

    SOLR-8695: Ensure ZK watchers are not triggering our watch logic on connection events and make this handling more consistent. Removed CDCR changes for the 5.x branches, where CDCR was never committed: Conflicts: 	solr/CHANGES.txt 	solr/core/src/java/org/apache/solr/handler/CdcrBufferStateManager.java 	solr/core/src/java/org/apache/solr/handler/CdcrLeaderStateManager.java 	solr/core/src/java/org/apache/solr/handler/CdcrProcessStateManager.java

--- solr/core/src/java/org/apache/solr/cloud/ZkController.java
+++ solr/core/src/java/org/apache/solr/cloud/ZkController.java
@@ -2432,7 +2432,8 @@ public final class ZkController {
 
     @Override
     public void process(WatchedEvent event) {
-      if (event.getState() == Event.KeeperState.Disconnected || event.getState() == Event.KeeperState.Expired)  {
+      // session events are not change events, and do not remove the watcher
+      if (Event.EventType.None.equals(event.getType())) {
         return;
       }
 
