8abd5a415b4f234c335332a4116e01aed3a0a4e0
Mike McCandless <mikemccand@apache.org>
2016-02-24 07:52:22 -0500

    fix test bug: let BKD use enough heap in this test

--- lucene/core/src/test/org/apache/lucene/search/TestPointQueries.java
+++ lucene/core/src/test/org/apache/lucene/search/TestPointQueries.java
@@ -1017,7 +1017,7 @@ public class TestPointQueries extends LuceneTestCase {
   private static Codec getCodec() {
     if (Codec.getDefault().getName().equals("Lucene60")) {
       int maxPointsInLeafNode = TestUtil.nextInt(random(), 16, 2048);
-      double maxMBSortInHeap = 3.0 + (3*random().nextDouble());
+      double maxMBSortInHeap = 4.0 + (3*random().nextDouble());
       if (VERBOSE) {
         System.out.println("TEST: using Lucene60PointFormat with maxPointsInLeafNode=" + maxPointsInLeafNode + " and maxMBSortInHeap=" + maxMBSortInHeap);
       }
