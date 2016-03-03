a2120730a617b41354b83af9434abead18c4c1c4
Shalin Shekhar Mangar <shalin@apache.org>
2016-03-03 15:39:08 +0530

    Adding version 6.1.0

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -42,6 +42,13 @@ public final class Version {
   public static final Version LUCENE_6_0_0 = new Version(6, 0, 0);
 
   /**
+   * Match settings and bugs in Lucene's 6.1.0 release.
+   * @deprecated Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_6_1_0 = new Version(6, 1, 0);
+
+  /**
    * Match settings and bugs in Lucene's 7.0.0 release.
    */
   public static final Version LUCENE_7_0_0 = new Version(7, 0, 0);
