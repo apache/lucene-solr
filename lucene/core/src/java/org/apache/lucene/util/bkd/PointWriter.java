0e189bca2d2af8bbf3dacaa27212dc9824a3b845
Mike McCandless <mikemccand@apache.org>
2016-03-21 05:49:47 -0400

    LUCENE-7120: re-use readers while recursing in BKDWriter so we get more thorough checksum verification on its temp files

--- lucene/core/src/java/org/apache/lucene/util/bkd/PointWriter.java
+++ lucene/core/src/java/org/apache/lucene/util/bkd/PointWriter.java
@@ -19,6 +19,7 @@ package org.apache.lucene.util.bkd;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.List;
 
 /** Appends many points, and then at the end provides a {@link PointReader} to iterate
  *  those points.  This abstracts away whether we write to disk, or use simple arrays
@@ -29,6 +30,9 @@ interface PointWriter extends Closeable {
 
   /** Returns a {@link PointReader} iterator to step through all previously added points */
   PointReader getReader(long startPoint, long length) throws IOException;
+
+  /** Returns the single shared reader, used at multiple times during the recursion, to read previously added points */
+  PointReader getSharedReader(long startPoint, long length, List<Closeable> toCloseHeroically) throws IOException;
 
   /** Removes any temp files behind this writer */
   void destroy() throws IOException;
