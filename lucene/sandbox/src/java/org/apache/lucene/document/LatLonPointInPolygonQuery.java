81d5ce884ba100592d3dad49e00158587fc03523
Robert Muir <rmuir@apache.org>
2016-04-24 18:47:50 -0400

    implement grow() for polygon queries too: easy speedup.

--- lucene/sandbox/src/java/org/apache/lucene/document/LatLonPointInPolygonQuery.java
+++ lucene/sandbox/src/java/org/apache/lucene/document/LatLonPointInPolygonQuery.java
@@ -121,6 +121,11 @@ final class LatLonPointInPolygonQuery extends Query {
         values.intersect(field, 
                          new IntersectVisitor() {
                            @Override
+                           public void grow(int count) {
+                             result.grow(count);
+                           }
+
+                           @Override
                            public void visit(int docID) {
                              result.add(docID);
                            }
