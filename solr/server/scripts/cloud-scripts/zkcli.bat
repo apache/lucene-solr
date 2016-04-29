ad152d23d5e70121f5e6ddc4bae5dabb288b96c2
Gregory Chanan <gchanan@cloudera.com>
2016-04-29 14:58:54 -0700

    SOLR-9047: fix windows script

--- solr/server/scripts/cloud-scripts/zkcli.bat
+++ solr/server/scripts/cloud-scripts/zkcli.bat
@@ -15,4 +15,4 @@ if defined LOG4J_PROPS (
   set "LOG4J_CONFIG=file:%SDIR%\log4j.properties"
 )
 
-"%JVM%" -Dlog4j.configuration="%LOG4J_PROPS%" -classpath "%SDIR%\..\..\solr-webapp\webapp\WEB-INF\lib\*;%SDIR%\..\..\lib\ext\*" org.apache.solr.cloud.ZkCLI %*
+"%JVM%" -Dlog4j.configuration="%LOG4J_CONFIG%" -classpath "%SDIR%\..\..\solr-webapp\webapp\WEB-INF\lib\*;%SDIR%\..\..\lib\ext\*" org.apache.solr.cloud.ZkCLI %*
