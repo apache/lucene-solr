9f5bcc5b3cd977d8b3c59bd7122ba3c1f78e3d42
jbernste <jbernste@apache.org>
2016-05-03 13:03:57 -0400

    SOLR-9059: NPE in SolrClientCache following collection reload
    Conflicts:
    	solr/core/src/test/org/apache/solr/handler/TestSQLHandler.java

--- solr/solrj/src/java/org/apache/solr/client/solrj/io/SolrClientCache.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/io/SolrClientCache.java
@@ -63,7 +63,7 @@ public class SolrClientCache implements Serializable {
     return client;
   }
 
-  public void close() {
+  public synchronized void close() {
     for(Map.Entry<String, SolrClient> entry : solrClients.entrySet()) {
       try {
         entry.getValue().close();
@@ -71,5 +71,6 @@ public class SolrClientCache implements Serializable {
         log.error("Error closing SolrClient for " + entry.getKey(), e);
       }
     }
+    solrClients.clear();
   }
 }
