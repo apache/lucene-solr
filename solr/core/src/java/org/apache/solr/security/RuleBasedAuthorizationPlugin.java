cf16f5d13d41f61a748e6376c85eb4941cc4a293
Noble Paul <noble@apache.org>
2016-05-03 15:56:25 +0530

    SOLR-9049: RuleBasedAuthorizationPlugin supports regex in param values eg: "command" : "REGEX:(i?)create"

--- solr/core/src/java/org/apache/solr/security/RuleBasedAuthorizationPlugin.java
+++ solr/core/src/java/org/apache/solr/security/RuleBasedAuthorizationPlugin.java
@@ -26,12 +26,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Function;
 
 import org.apache.solr.util.CommandOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyIterator;
 import static java.util.Collections.unmodifiableMap;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toMap;
@@ -129,12 +131,9 @@ public class RuleBasedAuthorizationPlugin implements AuthorizationPlugin, Config
           continue;
         }
         if (permission.params != null) {
-          for (Map.Entry<String, Object> e : permission.params.entrySet()) {
-            String paramVal = context.getParams().get(e.getKey());
-            Object val = e.getValue();
-            if (val instanceof List) {
-              if (!((List) val).contains(paramVal)) continue loopPermissions;
-            } else if (!Objects.equals(val, paramVal)) continue loopPermissions;
+          for (Map.Entry<String, Function<String[], Boolean>> e : permission.params.entrySet()) {
+            String[] paramVal = context.getParams().getParams(e.getKey());
+            if(!e.getValue().apply(paramVal)) continue loopPermissions;
           }
         }
       }
