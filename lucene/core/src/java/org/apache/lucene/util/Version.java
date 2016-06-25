ec0ab0d61702ebf4eb3a75b68ab47c4750e4f4e5
Steve Rowe <sarowe@apache.org>
2016-06-25 10:59:59 -0400

    Add version 5.5.3

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -288,8 +288,18 @@ public final class Version {
 
   /**
    * Match settings and bugs in Lucene's 5.5.2 release.
+   * @deprecated (5.5.3) Use latest
    */
+  @Deprecated
   public static final Version LUCENE_5_5_2 = new Version(5, 5, 2);
+
+  /**
+   * Match settings and bugs in Lucene's 5.5.3 release.
+   * <p>
+   * Use this to get the latest &amp; greatest settings, bug
+   * fixes, etc, for Lucene.
+   */
+  public static final Version LUCENE_5_5_3 = new Version(5, 5, 3);
 
   // To add a new version:
   //  * Only add above this comment
@@ -310,7 +320,7 @@ public final class Version {
    * some defaults may have changed and may break functionality 
    * in your application.
    */
-  public static final Version LATEST = LUCENE_5_5_2;
+  public static final Version LATEST = LUCENE_5_5_3;
 
   /**
    * Constant for backwards compatibility.
