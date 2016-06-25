62dff546ed40455f0bb2c6751153e67f895492db
Steve Rowe <sarowe@apache.org>
2016-06-25 13:17:08 -0400

    Add 5.5.2 back compat test indexes

--- lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
@@ -238,6 +238,8 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
       "5.5.0-nocfs",
       "5.5.1-cfs",
       "5.5.1-nocfs",
+      "5.5.2-cfs",
+      "5.5.2-nocfs",
       "6.0.0-cfs",
       "6.0.0-nocfs",
       "6.0.1-cfs",
