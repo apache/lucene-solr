eb071436331595cc453cd9c7d9c82d3269bb5e40
Varun Thacker <varunthacker1989@gmail.com>
2016-06-27 12:08:45 +0530

    SOLR-7374: Fixing test failures like build #3366. Index a minimum of 1 doc

--- solr/core/src/test/org/apache/solr/handler/BackupRestoreUtils.java
+++ solr/core/src/test/org/apache/solr/handler/BackupRestoreUtils.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Random;
 
 import org.apache.lucene.util.LuceneTestCase;
+import org.apache.lucene.util.TestUtil;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse;
@@ -40,11 +41,8 @@ public class BackupRestoreUtils extends LuceneTestCase {
     masterClient.deleteByQuery(collectionName, "*:*");
 
     Random random = new Random(docsSeed);// use a constant seed for the whole test run so that we can easily re-index.
-    int nDocs = random.nextInt(100);
+    int nDocs = TestUtil.nextInt(random, 1, 100);
     log.info("Indexing {} test docs", nDocs);
-    if (nDocs == 0) {
-      return 0;
-    }
 
     List<SolrInputDocument> docs = new ArrayList<>(nDocs);
     for (int i = 0; i < nDocs; i++) {
