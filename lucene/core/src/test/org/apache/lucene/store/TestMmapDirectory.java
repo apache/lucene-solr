4ae13ef31a0f18f119c119997687b4f6e6bfd8f0
Uwe Schindler <uschindler@apache.org>
2016-08-12 19:57:17 +0200

    LUCENE-7409: Disable test on stable branch

--- lucene/core/src/test/org/apache/lucene/store/TestMmapDirectory.java
+++ lucene/core/src/test/org/apache/lucene/store/TestMmapDirectory.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.concurrent.CountDownLatch;
 
-// import org.junit.Ignore;
+import org.junit.Ignore;
 
 /**
  * Tests MMapDirectory
@@ -42,7 +42,7 @@ public class TestMmapDirectory extends BaseDirectoryTestCase {
         MMapDirectory.UNMAP_SUPPORTED);
   }
   
-  // TODO: @Ignore("This test is for JVM testing purposes. There are no guarantees that it may not fail with SIGSEGV!")
+  @Ignore("This test is for JVM testing purposes. There are no guarantees that it may not fail with SIGSEGV!")
   public void testAceWithThreads() throws Exception {
     for (int iter = 0; iter < 10; iter++) {
       Directory dir = getDirectory(createTempDir("testAceWithThreads"));
