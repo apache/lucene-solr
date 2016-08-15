7df2a9e2667c91aac74aac843049724b2690afac
Robert Muir <rmuir@apache.org>
2016-08-15 08:32:08 -0400

    LUCENE-7161: disable flaky test

--- lucene/queries/src/test/org/apache/lucene/queries/mlt/TestMoreLikeThis.java
+++ lucene/queries/src/test/org/apache/lucene/queries/mlt/TestMoreLikeThis.java
@@ -41,6 +41,7 @@ import org.apache.lucene.search.QueryUtils;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.util.LuceneTestCase;
+import org.apache.lucene.util.LuceneTestCase.AwaitsFix;
 
 public class TestMoreLikeThis extends LuceneTestCase {
 
@@ -265,6 +266,7 @@ public class TestMoreLikeThis extends LuceneTestCase {
     return writer.numDocs() - 1;
   }
 
+  @AwaitsFix(bugUrl = "https://issues.apache.org/jira/browse/LUCENE-7161")
   public void testMultiFieldShouldReturnPerFieldBooleanQuery() throws Exception {
     IndexReader reader = null;
     Directory dir = newDirectory();
