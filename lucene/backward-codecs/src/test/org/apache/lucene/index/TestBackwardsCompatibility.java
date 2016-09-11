f3447f48b6b654fb74c118822999467a7f60c3c9
Anshum Gupta <anshum@apache.org>
2016-09-11 14:40:16 -0700

    Add 5.5.3 back compat test indexes

--- lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
@@ -290,7 +290,9 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
       "5.5.1-cfs",
       "5.5.1-nocfs",
       "5.5.2-cfs",
-      "5.5.2-nocfs"
+      "5.5.2-nocfs",
+      "5.5.3-cfs",
+      "5.5.3-nocfs"
   };
   
   final String[] unsupportedNames = {
