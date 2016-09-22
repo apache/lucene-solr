09a39e134809ae1057975ab268d3c382b0603820
Noble Paul <noble@apache.org>
2016-09-22 16:43:01 +0530

    SOLR-8995: precommit errors

--- solr/core/src/java/org/apache/solr/update/processor/MaxFieldValueUpdateProcessorFactory.java
+++ solr/core/src/java/org/apache/solr/update/processor/MaxFieldValueUpdateProcessorFactory.java
@@ -60,7 +60,7 @@ public final class MaxFieldValueUpdateProcessorFactory extends FieldValueSubsetU
     try {
       // NOTE: the extra cast to Object is needed to prevent compile
       // errors on Eclipse Compiler (ecj) used for javadoc lint
-      result = Collections.singletonList(Collections.max(values));
+      result = Collections.singletonList((Object) Collections.max(values));
     } catch (ClassCastException e) {
       throw new SolrException
         (BAD_REQUEST, 
