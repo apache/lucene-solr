3f35cadd44a0515018bed079124731bc159d54d0
Noble Paul <noble@apache.org>
2016-09-26 21:59:31 +0530

    SOLR-9557: reverting an optimization

--- solr/solrj/src/java/org/apache/solr/common/util/StrUtils.java
+++ solr/solrj/src/java/org/apache/solr/common/util/StrUtils.java
@@ -26,8 +26,6 @@ import java.util.Locale;
 
 import org.apache.solr.common.SolrException;
 
-import static java.util.Collections.singletonList;
-
 /**
  *
  */
@@ -41,7 +39,6 @@ public class StrUtils {
    * outside strings.
    */
   public static List<String> splitSmart(String s, char separator) {
-    if(s.indexOf(separator) == -1) return singletonList(s);
     ArrayList<String> lst = new ArrayList<>(4);
     int pos=0, start=0, end=s.length();
     char inString=0;
