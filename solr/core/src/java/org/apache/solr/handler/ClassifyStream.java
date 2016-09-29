568b54687a938ed0f6cd8b29100eda2c0b547975
Joel Bernstein <jbernste@apache.org>
2016-09-29 18:03:03 -0400

    SOLR-9258: Fix precommit

--- solr/core/src/java/org/apache/solr/handler/ClassifyStream.java
+++ solr/core/src/java/org/apache/solr/handler/ClassifyStream.java
@@ -19,7 +19,6 @@ package org.apache.solr.handler;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Locale;
