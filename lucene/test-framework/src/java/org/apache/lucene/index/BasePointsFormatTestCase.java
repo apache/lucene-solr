1b7a88f61ea44ecc873d7c7d135ce5c6ab88bb0a
Mike McCandless <mikemccand@apache.org>
2016-10-12 09:00:26 -0400

    LUCENE-7491: fix merge exception if the same field has points in some segments but not in others

--- lucene/test-framework/src/java/org/apache/lucene/index/BasePointsFormatTestCase.java
+++ lucene/test-framework/src/java/org/apache/lucene/index/BasePointsFormatTestCase.java
@@ -997,4 +997,27 @@ public abstract class BasePointsFormatTestCase extends BaseIndexFileFormatTestCa
     // structure than the tree created by adding points separately
     return false;
   }
+
+  // LUCENE-7491
+  public void testMixedSchema() throws Exception {
+    Directory dir = newDirectory();
+    IndexWriterConfig iwc = newIndexWriterConfig();
+    RandomIndexWriter w = new RandomIndexWriter(random(), dir, iwc);
+    iwc.setMaxBufferedDocs(2);
+    for(int i=0;i<2;i++) {
+      Document doc = new Document();
+      doc.add(new StringField("id", Integer.toString(i), Field.Store.NO));
+      doc.add(new IntPoint("int", i));
+      w.addDocument(doc);
+    }
+    // index has 1 segment now (with 2 docs) and that segment does have points, but the "id" field in particular does NOT
+
+    Document doc = new Document();
+    doc.add(new IntPoint("id", 0));
+    w.addDocument(doc);
+    // now we write another segment where the id field does have points:
+    
+    w.forceMerge(1);
+    IOUtils.close(w, dir);
+  }
 }
