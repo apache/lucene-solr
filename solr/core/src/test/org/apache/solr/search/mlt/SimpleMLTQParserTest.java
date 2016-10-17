e10b9385df5c3183e80ed084fa48b0c4da9dd476
Ere Maijala <ere.maijala@helsinki.fi>
2016-10-17 10:30:36 +0300

    Improved the SimpleMLTQParserTest test case so that it actually shows a difference in results when boosting is used.

--- solr/core/src/test/org/apache/solr/search/mlt/SimpleMLTQParserTest.java
+++ solr/core/src/test/org/apache/solr/search/mlt/SimpleMLTQParserTest.java
@@ -108,33 +108,35 @@ public class SimpleMLTQParserTest extends SolrTestCaseJ4 {
     );
 
     params = new ModifiableSolrParams();
-    params.set(CommonParams.Q, "{!mlt qf=lowerfilt,lowerfilt1^1000 boost=false mintf=0}30");
+    params.set(CommonParams.Q, "{!mlt qf=lowerfilt,lowerfilt1^1000 boost=false mintf=0 mindf=0}30");
+    params.set(CommonParams.DEBUG, "true");
     assertQ(req(params), 
-        "//result/doc[1]/int[@name='id'][.='13']",
-        "//result/doc[2]/int[@name='id'][.='14']",
-        "//result/doc[3]/int[@name='id'][.='18']",
-        "//result/doc[4]/int[@name='id'][.='20']",
-        "//result/doc[5]/int[@name='id'][.='22']",
-        "//result/doc[6]/int[@name='id'][.='23']",
-        "//result/doc[7]/int[@name='id'][.='15']",
-        "//result/doc[8]/int[@name='id'][.='16']",
-        "//result/doc[9]/int[@name='id'][.='19']",
-        "//result/doc[10]/int[@name='id'][.='21']"
+        "//result/doc[1]/int[@name='id'][.='31']",
+        "//result/doc[2]/int[@name='id'][.='13']",
+        "//result/doc[3]/int[@name='id'][.='14']",
+        "//result/doc[4]/int[@name='id'][.='18']",
+        "//result/doc[5]/int[@name='id'][.='20']",
+        "//result/doc[6]/int[@name='id'][.='22']",
+        "//result/doc[7]/int[@name='id'][.='23']",
+        "//result/doc[8]/int[@name='id'][.='32']",
+        "//result/doc[9]/int[@name='id'][.='15']",
+        "//result/doc[10]/int[@name='id'][.='16']"
     );
 
     params = new ModifiableSolrParams();
-    params.set(CommonParams.Q, "{!mlt qf=lowerfilt,lowerfilt1^1000 boost=true mintf=0}30");
+    params.set(CommonParams.Q, "{!mlt qf=lowerfilt,lowerfilt1^1000 boost=true mintf=0 mindf=0}30");
+    params.set(CommonParams.DEBUG, "true");
     assertQ(req(params), 
-        "//result/doc[1]/int[@name='id'][.='13']",
-        "//result/doc[2]/int[@name='id'][.='14']",
-        "//result/doc[3]/int[@name='id'][.='18']",
-        "//result/doc[4]/int[@name='id'][.='20']",
-        "//result/doc[5]/int[@name='id'][.='22']",
-        "//result/doc[6]/int[@name='id'][.='23']",
-        "//result/doc[7]/int[@name='id'][.='15']",
-        "//result/doc[8]/int[@name='id'][.='16']",
-        "//result/doc[9]/int[@name='id'][.='19']",
-        "//result/doc[10]/int[@name='id'][.='21']"
+        "//result/doc[1]/int[@name='id'][.='29']",
+        "//result/doc[2]/int[@name='id'][.='31']",
+        "//result/doc[3]/int[@name='id'][.='32']",
+        "//result/doc[4]/int[@name='id'][.='13']",
+        "//result/doc[5]/int[@name='id'][.='14']",
+        "//result/doc[6]/int[@name='id'][.='18']",
+        "//result/doc[7]/int[@name='id'][.='20']",
+        "//result/doc[8]/int[@name='id'][.='22']",
+        "//result/doc[9]/int[@name='id'][.='23']",
+        "//result/doc[10]/int[@name='id'][.='15']"
     );
     
     params = new ModifiableSolrParams();
