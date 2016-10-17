30c9f4311a950d9387e5032b164a6a84ca1816c6
Christine Poerschke <cpoerschke@apache.org>
2016-10-17 17:13:32 -0400

    SOLR-9627: Add QParser.getSortSpec, deprecate misleadingly named QParser.getSort (Judith Silverman, Christine Poerschke)

--- solr/core/src/java/org/apache/solr/handler/MoreLikeThisHandler.java
+++ solr/core/src/java/org/apache/solr/handler/MoreLikeThisHandler.java
@@ -117,7 +117,7 @@ public class MoreLikeThisHandler extends RequestHandlerBase
           if (q != null) {
             QParser parser = QParser.getParser(q, defType, req);
             query = parser.getQuery();
-            sortSpec = parser.getSort(true);
+            sortSpec = parser.getSortSpec(true);
           }
 
           String[] fqs = req.getParams().getParams(CommonParams.FQ);
