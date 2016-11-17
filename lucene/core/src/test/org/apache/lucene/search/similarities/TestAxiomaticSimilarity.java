4d0fd8ef50c18f15f3e2bbe545f8da39f5d8c7f4
Peilin Yang <yangpeilyn@gmail.com>
2016-11-16 21:20:15 -0500

    update test cases

--- lucene/core/src/test/org/apache/lucene/search/similarities/TestAxiomaticSimilarity.java
+++ lucene/core/src/test/org/apache/lucene/search/similarities/TestAxiomaticSimilarity.java
@@ -21,19 +21,6 @@ import org.apache.lucene.util.LuceneTestCase;
 
 public class TestAxiomaticSimilarity extends LuceneTestCase {
   
-  public void testSaneNormValues() {
-    BM25Similarity sim = new BM25Similarity();
-    for (int i = 0; i < 256; i++) {
-      float len = sim.decodeNormValue((byte) i);
-      assertFalse("negative len: " + len + ", byte=" + i, len < 0.0f);
-      assertFalse("inf len: " + len + ", byte=" + i, Float.isInfinite(len));
-      assertFalse("nan len for byte=" + i, Float.isNaN(len));
-      if (i > 0) {
-        assertTrue("len is not decreasing: " + len + ",byte=" + i, len < sim.decodeNormValue((byte)(i-1)));
-      }
-    }
-  }
-  
   public void testIllegalS() {
     IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
       new AxiomaticF2EXP(Float.POSITIVE_INFINITY, 0.1f);
@@ -77,11 +64,11 @@ public class TestAxiomaticSimilarity extends LuceneTestCase {
     IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
       new AxiomaticF3EXP(0.35f, -1);
     });
-    assertTrue(expected.getMessage().contains("illegal query length value"));
+    assertTrue(expected.getMessage().contains("illegal queryLen value"));
 
     expected = expectThrows(IllegalArgumentException.class, () -> {
-      new AxiomaticF2EXP(0.35f, Integer.MAX_VALUE+1);
+      new AxiomaticF3EXP(0.35f, Integer.MAX_VALUE+1);
     });
-    assertTrue(expected.getMessage().contains("illegal query length value"));
+    assertTrue(expected.getMessage().contains("illegal queryLen value"));
   }
 }
