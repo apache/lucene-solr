2ec97165204a9e4b55419816fc10eaaab6c9095f
Noble Paul <noble@apache.org>
2016-11-26 12:39:50 +0530

    SOLR-9784: deprecated uploadconfig

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrClient.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrClient.java
@@ -649,6 +649,7 @@ public class CloudSolrClient extends SolrClient {
    * @param configName the name of the config
    * @throws IOException if an IO error occurs
    */
+  @Deprecated
   public void uploadConfig(Path configPath, String configName) throws IOException {
     stateProvider.connect();
     assertZKStateProvider().uploadConfig(configPath, configName);
@@ -668,6 +669,7 @@ public class CloudSolrClient extends SolrClient {
    * @param downloadPath  the path to write config files to
    * @throws IOException  if an I/O exception occurs
    */
+  @Deprecated
   public void downloadConfig(String configName, Path downloadPath) throws IOException {
     assertZKStateProvider().downloadConfig(configName, downloadPath);
   }
@@ -1530,6 +1532,10 @@ public class CloudSolrClient extends SolrClient {
   
   public void setConnectionTimeout(int timeout) {
     this.lbClient.setConnectionTimeout(timeout); 
+  }
+
+  public ClusterStateProvider getClusterStateProvider(){
+    return stateProvider;
   }
 
   private static boolean hasInfoToFindLeaders(UpdateRequest updateRequest, String idField) {
