687f03661d726d70d5718db8b45990b3fbc98183
Andrzej Bialecki <ab@apache.org>
2016-12-23 23:01:47 +0100

    SOLR-9805 Fix NPE.

--- solr/core/src/test/org/apache/solr/metrics/JvmMetricsTest.java
+++ solr/core/src/test/org/apache/solr/metrics/JvmMetricsTest.java
@@ -44,7 +44,7 @@ public class JvmMetricsTest extends SolrJettyTestBase {
     assertTrue(metrics.size() > 0);
     for (String metric : OperatingSystemMetricSet.METRICS) {
       Gauge<?> gauge = (Gauge<?>)metrics.get(metric);
-      if (gauge == null) { // some are optional depending on OS
+      if (gauge == null || gauge.getValue() == null) { // some are optional depending on OS
         continue;
       }
       double value = ((Number)gauge.getValue()).doubleValue();
