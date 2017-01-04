a308ecf6a675dbd6e26f3b79ef26814bd1d02efe
Hrishikesh Gadre <hgadre@cloudera.com>
2017-01-04 10:53:24 -0800

    [SOLR-9926] Add an environment variable to zkcli to pass java system properties

--- solr/server/scripts/cloud-scripts/zkcli.sh
+++ solr/server/scripts/cloud-scripts/zkcli.sh
@@ -21,6 +21,6 @@ fi
 #  -DzkDigestUsername=admin-user -DzkDigestPassword=CHANGEME-ADMIN-PASSWORD \
 #  -DzkDigestReadonlyUsername=readonly-user -DzkDigestReadonlyPassword=CHANGEME-READONLY-PASSWORD"
 
-PATH=$JAVA_HOME/bin:$PATH $JVM $SOLR_ZK_CREDS_AND_ACLS  -Dlog4j.configuration=$log4j_config \
+PATH=$JAVA_HOME/bin:$PATH $JVM $SOLR_ZK_CREDS_AND_ACLS $ZKCLI_JVM_FLAGS -Dlog4j.configuration=$log4j_config \
 -classpath "$sdir/../../solr-webapp/webapp/WEB-INF/lib/*:$sdir/../../lib/ext/*" org.apache.solr.cloud.ZkCLI ${1+"$@"}
 
