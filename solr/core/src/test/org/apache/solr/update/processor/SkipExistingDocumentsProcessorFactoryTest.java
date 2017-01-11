2f721048d4e9e35ba81ad574d3927cdba798ee24
Shalin Shekhar Mangar <shalin@apache.org>
2017-01-11 12:20:42 +0530

    SOLR-9918: Remove unused import to make precommit happy
    (cherry picked from commit 2437204)

--- solr/core/src/test/org/apache/solr/update/processor/SkipExistingDocumentsProcessorFactoryTest.java
+++ solr/core/src/test/org/apache/solr/update/processor/SkipExistingDocumentsProcessorFactoryTest.java
@@ -20,7 +20,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
