9821ac7329804dba9834c590762a2b13f3d95ce8
Alan Woodward <romseygeek@apache.org>
2017-01-22 08:54:26 +0000

    Fix precommit

--- lucene/core/src/java/org/apache/lucene/document/SortedSetDocValuesRangeQuery.java
+++ lucene/core/src/java/org/apache/lucene/document/SortedSetDocValuesRangeQuery.java
@@ -19,7 +19,6 @@ package org.apache.lucene.document;
 import java.io.IOException;
 import java.util.Objects;
 
-import org.apache.lucene.index.DocValues;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
