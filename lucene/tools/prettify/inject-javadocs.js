cffa82062c5be766db6bc87bc232a39a413600ec
Uwe Schindler <uschindler@apache.org>
2017-01-22 18:29:01 +0100

    LUCENE-7651: Fix Javadocs build for Java 8u121 by injecting "Google Code Prettify" without adding Javascript to Javadocs's -bottom parameter. Also update Prettify to latest version to fix Google Chrome issue.
    # Conflicts:
    #	lucene/CHANGES.txt

--- lucene/tools/prettify/inject-javadocs.js
+++ lucene/tools/prettify/inject-javadocs.js
@@ -14,5 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-PR.registerLangHandler(PR.createSimpleLexer([["pln",/^[\t-\r ]+/,null,"\t\n\r "],["str",/^"(?:[^\n\f\r"\\]|\\[\S\s])*(?:"|$)/,null,'"'],["str",/^'(?:[^\n\f\r'\\]|\\[^&])'?/,null,"'"],["lit",/^(?:0o[0-7]+|0x[\da-f]+|\d+(?:\.\d+)?(?:e[+-]?\d+)?)/i,null,"0123456789"]],[["com",/^(?:--+[^\n\f\r]*|{-(?:[^-]|-+[^}-])*-})/],["kwd",/^(?:case|class|data|default|deriving|do|else|if|import|in|infix|infixl|infixr|instance|let|module|newtype|of|then|type|where|_)(?=[^\d'A-Za-z]|$)/,
-null],["pln",/^(?:[A-Z][\w']*\.)*[A-Za-z][\w']*/],["pun",/^[^\d\t-\r "'A-Za-z]+/]]),["hs"]);
+!function(){
+  var oldonload = window.onload;
+  if (typeof oldonload != 'function') {
+    window.onload = prettyPrint;
+  } else {
+    window.onload = function() {
+      oldonload();
+      prettyPrint();
+    }
+  }
+}();
