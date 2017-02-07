0d760aeddbc95f2de977dcaed781547f2cbf715f
Ishan Chattopadhyaya <ishan@apache.org>
2017-02-07 04:22:35 -0800

    SOLR-5944: Reverting the previous fix for SolrCmdDistributor

--- solr/core/src/java/org/apache/solr/update/processor/DistributedUpdateProcessor.java
+++ solr/core/src/java/org/apache/solr/update/processor/DistributedUpdateProcessor.java
@@ -799,16 +799,8 @@ public class DistributedUpdateProcessor extends UpdateRequestProcessor {
 
       if (cmd.isInPlaceUpdate()) {
         params.set(DISTRIB_INPLACE_PREVVERSION, String.valueOf(cmd.prevVersion));
-        // Use synchronous=true so that a new connection is used, instead
-        // of the update being streamed through an existing streaming client.
-        // When using a streaming client, the previous update
-        // and the current in-place update (that depends on the previous update), if reordered
-        // in the stream, can result in the current update being bottled up behind the previous
-        // update in the stream and can lead to degraded performance.
-        cmdDistrib.distribAdd(cmd, nodes, params, true, replicationTracker);
-      } else {
-        cmdDistrib.distribAdd(cmd, nodes, params, false, replicationTracker);
       }
+      cmdDistrib.distribAdd(cmd, nodes, params, false, replicationTracker);
     }
     
     // TODO: what to do when no idField?
