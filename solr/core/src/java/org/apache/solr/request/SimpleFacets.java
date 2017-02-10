d89ffe23f1bb24bbf8882d11cae8444b37af26f1
Adrien Grand <jpountz@gmail.com>
2017-02-10 09:39:53 +0100

    SOLR-9912: Remove unused imports.

--- solr/core/src/java/org/apache/solr/request/SimpleFacets.java
+++ solr/core/src/java/org/apache/solr/request/SimpleFacets.java
@@ -23,7 +23,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -38,7 +37,6 @@ import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.lucene.index.Fields;
 import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
