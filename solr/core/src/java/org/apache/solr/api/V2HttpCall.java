2470fbaa0ee4d1a211957bc47242bd081a31234d
Noble Paul <noble.paul@gmail.com>
2017-02-14 17:47:12 -0800

    SOLR-8029: introspect was not showing for certain collection apis

--- solr/core/src/java/org/apache/solr/api/V2HttpCall.java
+++ solr/core/src/java/org/apache/solr/api/V2HttpCall.java
@@ -136,7 +136,7 @@ public class V2HttpCall extends HttpSolrCall {
       if (isCompositeApi && apiInfo instanceof CompositeApi) {
         ((CompositeApi) this.api).add(apiInfo);
       } else {
-        api = apiInfo;
+        api = apiInfo == null ? api : apiInfo;
       }
       MDCLoggingContext.setCore(core);
       parseRequest();
