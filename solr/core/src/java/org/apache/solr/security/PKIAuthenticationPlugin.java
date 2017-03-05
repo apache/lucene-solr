8379e3d22afe2a5970c4a3828cce69b2f7030b00
Noble Paul <noble@apache.org>
2017-03-06 10:09:01 +1030

    SOLR-10230: default TTL of PKIAuthenticationPlugin increased to 10secs

--- solr/core/src/java/org/apache/solr/security/PKIAuthenticationPlugin.java
+++ solr/core/src/java/org/apache/solr/security/PKIAuthenticationPlugin.java
@@ -64,7 +64,7 @@ public class PKIAuthenticationPlugin extends AuthenticationPlugin implements Htt
   private final Map<String, PublicKey> keyCache = new ConcurrentHashMap<>();
   private final CryptoKeys.RSAKeyPair keyPair = new CryptoKeys.RSAKeyPair();
   private final CoreContainer cores;
-  private final int MAX_VALIDITY = Integer.parseInt(System.getProperty("pkiauth.ttl", "5000"));
+  private final int MAX_VALIDITY = Integer.parseInt(System.getProperty("pkiauth.ttl", "10000"));
   private final String myNodeName;
 
   private boolean interceptorRegistered = false;
