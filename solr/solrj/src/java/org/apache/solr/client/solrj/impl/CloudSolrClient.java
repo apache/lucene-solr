feadf725ea9611631830d3f97270e6af3fee94cb
Boris Naguet <boris.naguet@worldline.com>
2017-03-09 14:19:24 +0100

    SOLR-10250: clean formatting

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrClient.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrClient.java
@@ -903,17 +903,17 @@ public class CloudSolrClient extends SolrClient {
     // TolerantUpdateProcessor
     List<SimpleOrderedMap<String>> toleratedErrors = null; 
     int maxToleratedErrors = Integer.MAX_VALUE;
-    
+
     NamedList adds = new NamedList();
-    
+
     for(int i=0; i<response.size(); i++) {
       NamedList shardResponse = (NamedList)response.getVal(i);
-      
+
       Object addsObject = shardResponse.get("adds");
       if(addsObject instanceof NamedList) {
-    	  adds.addAll((NamedList)addsObject);
+        adds.addAll((NamedList)addsObject);
       }
-      
+
       NamedList header = (NamedList)shardResponse.get("responseHeader");
       Integer shardStatus = (Integer)header.get("status");
       int s = shardStatus.intValue();
@@ -948,9 +948,9 @@ public class CloudSolrClient extends SolrClient {
     }
 
     if(adds != null && adds.size() > 0) {
-    	condensed.add("adds", adds);
+      condensed.add("adds", adds);
     }
-    
+
     NamedList cheader = new NamedList();
     cheader.add("status", status);
     cheader.add("QTime", timeMillis);
