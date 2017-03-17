3f6a15fd797081a6f6383a623ae2a794bcd6f800
Tomas Fernandez Lobbe <tflobbe@apache.org>
2017-03-17 11:55:15 -0700

    SOLR-10237: Poly-Fields should work with subfield that have docValues=true

--- solr/core/src/java/org/apache/solr/schema/LatLonType.java
+++ solr/core/src/java/org/apache/solr/schema/LatLonType.java
@@ -75,10 +75,10 @@ public class LatLonType extends AbstractSubTypeFieldType implements SpatialQuery
       Point point = SpatialUtils.parsePointSolrException(externalVal, SpatialContext.GEO);
       //latitude
       SchemaField subLatSF = subField(field, LAT, schema);
-      f.add(subLatSF.createField(String.valueOf(point.getY()), subLatSF.indexed() && !subLatSF.omitNorms() ? boost : 1f));
+      f.addAll(subLatSF.createFields(String.valueOf(point.getY()), subLatSF.indexed() && !subLatSF.omitNorms() ? boost : 1f));
       //longitude
       SchemaField subLonSF = subField(field, LON, schema);
-      f.add(subLonSF.createField(String.valueOf(point.getX()), subLonSF.indexed() && !subLonSF.omitNorms() ? boost : 1f));
+      f.addAll(subLonSF.createFields(String.valueOf(point.getX()), subLonSF.indexed() && !subLonSF.omitNorms() ? boost : 1f));
     }
 
     if (field.stored()) {
@@ -87,6 +87,14 @@ public class LatLonType extends AbstractSubTypeFieldType implements SpatialQuery
       f.add(createField(field.getName(), externalVal, customType, 1f));
     }
     return f;
+  }
+  
+  @Override
+  protected void checkSupportsDocValues() {
+    // DocValues supported only when enabled at the fieldType 
+    if (!hasProperty(DOC_VALUES)) {
+      throw new UnsupportedOperationException("LatLonType can't have docValues=true in the field definition, use docValues=true in the fieldType definition, or in subFieldType/subFieldSuffix");
+    }
   }
 
 
