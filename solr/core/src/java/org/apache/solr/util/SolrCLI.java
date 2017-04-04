8b87a474cbf6873935975302dbd856c3cbef53ec
Mikhail Khludnev <mkhl@apache.org>
2017-04-04 11:01:19 +0300

    SOLR-9745: check exit code only if process has finished

--- solr/core/src/java/org/apache/solr/util/SolrCLI.java
+++ solr/core/src/java/org/apache/solr/util/SolrCLI.java
@@ -2940,7 +2940,7 @@ public class SolrCLI {
           // safe to ignore ...
           Thread.interrupted();
         }
-        if (handler.getExitValue() != 0) {
+        if (handler.hasResult() && handler.getExitValue() != 0) {
           throw new Exception("Failed to start Solr using command: "+startCmd+" Exception : "+handler.getException());
         }
       } else {
