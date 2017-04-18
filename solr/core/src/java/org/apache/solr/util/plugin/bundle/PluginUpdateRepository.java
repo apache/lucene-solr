31b59e5a2e7ad23c90fd37b3916be44b4f3c08ea
Jan Høydahl <janhoy@apache.org>
2017-04-18 15:26:38 +0200

    Simplify UpdateRepo based on new pf4j code

--- solr/core/src/java/org/apache/solr/util/plugin/bundle/PluginUpdateRepository.java
+++ solr/core/src/java/org/apache/solr/util/plugin/bundle/PluginUpdateRepository.java
@@ -17,16 +17,9 @@
 
 package org.apache.solr.util.plugin.bundle;
 
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.lang.invoke.MethodHandles;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ro.fortsoft.pf4j.update.DefaultUpdateRepository;
@@ -40,7 +33,6 @@ public class PluginUpdateRepository extends DefaultUpdateRepository {
 
   private String url;
   private boolean locationResolved = false;
-  private String pluginsJson;
   private Map<String, PluginInfo> plugins;
 
   public PluginUpdateRepository(String id) {
@@ -48,12 +40,11 @@ public class PluginUpdateRepository extends DefaultUpdateRepository {
   }
 
   public PluginUpdateRepository(String id, String location) {
-    this(id, location, "plugins.json");
+    super(id, location);
   }
 
   public PluginUpdateRepository(String id, String location, String pluginsJson) {
-    super(id, location);
-    this.pluginsJson = pluginsJson;
+    super(id, location, pluginsJson);
   }
 
   @Override
@@ -72,48 +63,4 @@ public class PluginUpdateRepository extends DefaultUpdateRepository {
   protected String resolveUrl() {
     return super.getUrl();
   }
-
-  @Override
-  public void refresh() {
-    plugins = null;
-  }
-
-  @Override
-  public Map<String, PluginInfo> getPlugins() {
-    if (plugins == null) {
-      initPlugins();
-    }
-    return plugins;
-  }
-
-  protected void initPlugins() {
-    Reader pluginsJsonReader;
-    plugins = new HashMap<>();
-    URL pluginsUrl = null;
-    try {
-        pluginsUrl = new URL(new URL(getUrl()), pluginsJson);
-        log.debug("Read plugins of '{}' repository from '{}'", getId(), pluginsUrl);
-        pluginsJsonReader = new InputStreamReader(pluginsUrl.openStream());
-    } catch (Exception e) {
-        log.error("Failed to find {}", pluginsUrl);
-        return;
-    }
-
-    Gson gson = new GsonBuilder().create();
-    PluginInfo[] items = gson.fromJson(pluginsJsonReader, PluginInfo[].class);
-    plugins = new HashMap<>(items.length);
-    for (PluginInfo p : items) {
-      for (PluginInfo.PluginRelease r : p.releases) {
-        try {
-          r.url = new URL(new URL(getUrl()), r.url).toString();
-        } catch (MalformedURLException e) {
-          log.warn("Skipping release {} of plugin {} due to failure to build valid absolute URL. Url was {}{}", r.version, p.id, getUrl(), r.url);
-        }
-      }
-      plugins.put(p.id, p);
-    }
-    log.debug("Found {} plugins in repository '{}'", plugins.size(), getId());
-  }
-
-
 }
