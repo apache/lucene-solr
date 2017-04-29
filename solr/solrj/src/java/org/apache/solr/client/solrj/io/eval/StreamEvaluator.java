460b3b36e9544b4be75cf1d25c89d8bde8ec5c74
Dennis Gove <dpgove@gmail.com>
2017-04-28 21:45:56 -0400

    SOLR-10559: Updates TupStream and enhances evaluators to work over values in the SteamContext

--- solr/solrj/src/java/org/apache/solr/client/solrj/io/eval/StreamEvaluator.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/io/eval/StreamEvaluator.java
@@ -27,6 +27,30 @@ import org.apache.solr.client.solrj.io.stream.StreamContext;
 import org.apache.solr.client.solrj.io.stream.expr.Expressible;
 
 public interface StreamEvaluator extends Expressible, Serializable {
-  Object evaluate(final Tuple tuple) throws IOException;
   void setStreamContext(StreamContext streamContext);
+  StreamContext getStreamContext();
+  
+  Object evaluate(final Tuple tuple) throws IOException;
+  
+  /**
+   * Execute the evaluator over lets stored within the StreamContext. This allows 
+   * evaluators to be executed over values calculated elsewhere in the pipeline
+   * and stored in the {@link StreamContext#getLets() streamContext.lets}
+   * 
+   * Default implementation just creates a tuple out of all values in the context 
+   * and passes that to {@link StreamEvaluator#evaluate(Tuple)}.
+   * 
+   * @return Evaluated value
+   * @throws IOException throw on error during evaluation
+   */
+  default Object evaluateOverContext() throws IOException{
+    StreamContext context = getStreamContext();
+    if(null != context){
+      Tuple contextTuple = new Tuple(context.getLets());
+      return evaluate(contextTuple);
+    }
+    
+    return null;
+  }
+  
 }
