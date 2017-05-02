6017202d6ebcbcc1c6c8d9328530acb68b858d7d
Mark Miller <markrmiller@apache.org>
2017-05-02 13:38:44 -0300

    SOLR-10430: Add ls command to ZkCLI for listing only sub-directories.

--- solr/core/src/test/org/apache/solr/cloud/ZkCLITest.java
+++ solr/core/src/test/org/apache/solr/cloud/ZkCLITest.java
@@ -16,10 +16,12 @@
  */
 package org.apache.solr.cloud;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.lang.invoke.MethodHandles;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
@@ -191,7 +193,33 @@ public class ZkCLITest extends SolrTestCaseJ4 {
     zkClient.makePath("/test", true);
     String[] args = new String[] {"-zkhost", zkServer.getZkAddress(), "-cmd",
         "list"};
+
+    ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+    final PrintStream myOut = new PrintStream(byteStream, false, StandardCharsets.UTF_8.name());
+    ZkCLI.setStdout(myOut);
+
     ZkCLI.main(args);
+
+    final String standardOutput = byteStream.toString(StandardCharsets.UTF_8.name());
+    String separator = System.lineSeparator();
+    assertEquals("/ (1)" + separator + " /test (0)" + separator + separator, standardOutput);
+  }
+
+  @Test
+  public void testLs() throws Exception {
+    zkClient.makePath("/test/path", true);
+    String[] args = new String[] {"-zkhost", zkServer.getZkAddress(), "-cmd",
+        "ls", "/test"};
+
+    ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+    final PrintStream myOut = new PrintStream(byteStream, false, StandardCharsets.UTF_8.name());
+    ZkCLI.setStdout(myOut);
+
+    ZkCLI.main(args);
+    
+    final String standardOutput = byteStream.toString(StandardCharsets.UTF_8.name());
+    String separator = System.lineSeparator();
+    assertEquals("/test (1)" + separator + " /test/path (0)" + separator + separator, standardOutput);
   }
 
   @Test
