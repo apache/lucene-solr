30f1422ba955a38c37b1ec97244ebb7ede9ee7be
Mikhail Khludnev <mkhl@apache.org>
2017-05-04 15:45:14 +0300

    SOLR-9867: make sure cores are assigned in the end of SolrDispatchFilter.createCoreContainer() only

--- solr/core/src/java/org/apache/solr/servlet/SolrDispatchFilter.java
+++ solr/core/src/java/org/apache/solr/servlet/SolrDispatchFilter.java
@@ -247,9 +247,9 @@ public class SolrDispatchFilter extends BaseSolrFilter {
    */
   protected CoreContainer createCoreContainer(Path solrHome, Properties extraProperties) {
     NodeConfig nodeConfig = loadNodeConfig(solrHome, extraProperties);
-    cores = new CoreContainer(nodeConfig, extraProperties, true);
-    cores.load();
-    return cores;
+    CoreContainer coreContainer = new CoreContainer(nodeConfig, extraProperties, true);
+    coreContainer.load();
+    return coreContainer;
   }
 
   /**
