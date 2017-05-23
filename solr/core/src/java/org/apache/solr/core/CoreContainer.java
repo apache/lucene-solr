28b8696d771d6cccdbab673918cbf04d34b998ad
Erick <erick@apache.org>
2017-05-23 12:20:58 -0700

    SOLR-10721: Provide a way to know when Core Discovery is finished and when all async cores are done loading

--- solr/core/src/java/org/apache/solr/core/CoreContainer.java
+++ solr/core/src/java/org/apache/solr/core/CoreContainer.java
@@ -186,6 +186,13 @@ public class CoreContainer {
 
   protected MetricsCollectorHandler metricsCollectorHandler;
 
+
+  // Bits for the state variable.
+  public final static long LOAD_COMPLETE = 0x1L;
+  public final static long CORE_DISCOVERY_COMPLETE = 0x2L;
+  public final static long INITIAL_CORE_LOAD_COMPLETE = 0x4L;
+  private volatile long status = 0L;
+
   private enum CoreInitFailedAction { fromleader, none }
 
   /**
@@ -579,7 +586,8 @@ public class CoreContainer {
         Collections.sort(cds, coreComparator::compare);
       }
       checkForDuplicateCoreNames(cds);
-
+      status |= CORE_DISCOVERY_COMPLETE;
+      
       for (final CoreDescriptor cd : cds) {
         if (cd.isTransient() || !cd.isLoadOnStartup()) {
           getTransientCacheHandler().addTransientDescriptor(cd.getName(), cd);
@@ -641,7 +649,10 @@ public class CoreContainer {
     if (isZooKeeperAware()) {
       zkSys.getZkController().checkOverseerDesignate();
     }
+    // This is a bit redundant but these are two distinct concepts for all they're accomplished at the same time.
+    status |= LOAD_COMPLETE | INITIAL_CORE_LOAD_COMPLETE;
   }
+
 
   public TransientSolrCoreCache getTransientCacheHandler() {
 
@@ -1531,6 +1542,10 @@ public class CoreContainer {
 
   public NodeConfig getNodeConfig() {
     return cfg;
+  }
+
+  public long getStatus() {
+    return status;
   }
 
 }
