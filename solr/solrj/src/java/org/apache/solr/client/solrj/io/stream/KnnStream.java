f4872c9296b509951a30e9d9618a3730f44bbefb
Joel Bernstein <jbernste@apache.org>
2017-05-24 08:06:01 -0400

    SOLR-10731: Fix precommit

--- solr/solrj/src/java/org/apache/solr/client/solrj/io/stream/KnnStream.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/io/stream/KnnStream.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
