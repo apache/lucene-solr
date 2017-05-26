732e8331cf984c0ebe02e20f70daacd82817bd4e
Ishan Chattopadhyaya <ishan@apache.org>
2017-05-27 03:30:27 +0530

    SOLR-10735: Fixing windows space directory issue

--- solr/core/src/java/org/apache/solr/util/SolrCLI.java
+++ solr/core/src/java/org/apache/solr/util/SolrCLI.java
@@ -2929,7 +2929,7 @@ public class SolrCLI {
         solrHome = solrHome.substring(cwdPath.length()+1);
 
       String startCmd =
-          String.format(Locale.ROOT, "%s start %s -p %d -s \"%s\" %s %s %s %s %s",
+          String.format(Locale.ROOT, "\"%s\" start %s -p %d -s \"%s\" %s %s %s %s %s",
               callScript, cloudModeArg, port, solrHome, hostArg, zkHostArg, memArg, extraArgs, addlOptsArg);
       startCmd = startCmd.replaceAll("\\s+", " ").trim(); // for pretty printing
 
