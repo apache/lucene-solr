f02fbead0b3700cc2f5c15e8d6872587f29afc15
Vivek Narang <vivek.narang10@gmail.com>
2017-06-01 18:51:41 -0400

    removing uncommented code ...

--- dev-tools/SolrNightBenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/SolrNightlyBenchmarks.java
+++ dev-tools/SolrNightBenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/SolrNightlyBenchmarks.java
@@ -1,21 +1,23 @@
 package org.apache.solr.tests.nightlybenchmarks;
 import java.io.IOException;
 
+import org.apache.solr.tests.nightlybenchmarks.QueryClient.QueryType;
+
 public class SolrNightlyBenchmarks {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
 		Util.init(args);
 		
-		//Tests.indexingTests(Util.COMMIT_ID, 10000);
+		Tests.indexingTests(Util.COMMIT_ID, 10000);
 		
 		Tests.setUpCloudForFeatureTests(Util.COMMIT_ID, 50000, 1, "2", "2");
-//		BenchmarkReportData.numericQueryTNQMetric = Tests.numericQueryTests(Util.COMMIT_ID, QueryType.TERM_NUMERIC_QUERY, 1, 180, 120);
-//		BenchmarkReportData.numericQueryRNQMetric =	Tests.numericQueryTests(Util.COMMIT_ID, QueryType.RANGE_NUMERIC_QUERY, 1, 180, 120);
-//		BenchmarkReportData.numericQueryLNQMetric =	Tests.numericQueryTests(Util.COMMIT_ID, QueryType.LESS_THAN_NUMERIC_QUERY, 1, 180, 120);
-//		BenchmarkReportData.numericQueryGNQMetric =	Tests.numericQueryTests(Util.COMMIT_ID, QueryType.GREATER_THAN_NUMERIC_QUERY, 1, 180, 120);
+		BenchmarkReportData.numericQueryTNQMetric = Tests.numericQueryTests(Util.COMMIT_ID, QueryType.TERM_NUMERIC_QUERY, 1, 180, 120);
+		BenchmarkReportData.numericQueryRNQMetric =	Tests.numericQueryTests(Util.COMMIT_ID, QueryType.RANGE_NUMERIC_QUERY, 1, 180, 120);
+		BenchmarkReportData.numericQueryLNQMetric =	Tests.numericQueryTests(Util.COMMIT_ID, QueryType.LESS_THAN_NUMERIC_QUERY, 1, 180, 120);
+		BenchmarkReportData.numericQueryGNQMetric =	Tests.numericQueryTests(Util.COMMIT_ID, QueryType.GREATER_THAN_NUMERIC_QUERY, 1, 180, 120);
 		Tests.shutDown();
-//		BenchmarkAppConnector.publishDataForWebApp();
+		BenchmarkAppConnector.publishDataForWebApp();
 
 		
 		Util.destroy();
