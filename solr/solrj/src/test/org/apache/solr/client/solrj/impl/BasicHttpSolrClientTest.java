fb3d3f1c9296734576a70b80f61b497eccc8ebd5
Chris Hostetter <hossman@apache.org>
2017-06-01 10:50:58 -0700

    SOLR-10791: Remove deprecated options in SSLTestConfig

--- solr/solrj/src/test/org/apache/solr/client/solrj/impl/BasicHttpSolrClientTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/impl/BasicHttpSolrClientTest.java
@@ -71,7 +71,6 @@ import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.common.util.SuppressForbidden;
-import org.apache.solr.util.SSLTestConfig;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -620,21 +619,6 @@ public class BasicHttpSolrClientTest extends SolrJettyTestBase {
     } finally {
       HttpClientUtil.close(client);;
     }
-  }
-
-  /**
-   * A trivial test that verifies the example keystore used for SSL testing can be 
-   * found using the base class. this helps future-proof against the possibility of 
-   * something moving/breaking the keystore path in a way that results in the SSL 
-   * randomization logic being forced to silently never use SSL.  (We can't enforce 
-   * this type of check in the base class because then it would not be usable by client 
-   * code depending on the test framework
-   */
-  public void testExampleKeystorePath() {
-    assertNotNull("Example keystore is null, meaning that something has changed in the " +
-                  "structure of the example configs and/or ExternalPaths.java - " + 
-                  "SSL randomization is broken",
-                  SSLTestConfig.TEST_KEYSTORE);
   }
 
   /**
