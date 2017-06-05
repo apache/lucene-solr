b1b9a94b1d8c0f418d0c7408a17147e8cca2d2e1
Christine Poerschke <cpoerschke@apache.org>
2017-06-05 10:35:53 +0100

    SOLR-10744: remove unused import

--- solr/core/src/test/org/apache/solr/util/TestUtils.java
+++ solr/core/src/test/org/apache/solr/util/TestUtils.java
@@ -18,7 +18,6 @@ package org.apache.solr.util;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
