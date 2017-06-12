406fa718504c26224b383237e3d67c78aabb25ab
Vivek Narang <vivek.narang10@gmail.com>
2017-06-11 21:28:25 -0400

    updating test plans uncommenting tests ...

--- dev-tools/SolrNightBenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/TestPlans.java
+++ dev-tools/SolrNightBenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/TestPlans.java
@@ -27,13 +27,13 @@ public class TestPlans {
 		
 		Util.COMMIT_ID = commitID;
 		
-		//Tests.indexingTestsStandalone(Util.COMMIT_ID, 10000);
+		Tests.indexingTestsStandalone(Util.COMMIT_ID, 10000);
 		
-		//Tests.createCollectionTestStandalone(Util.COMMIT_ID);
+		Tests.createCollectionTestStandalone(Util.COMMIT_ID);
 
-		//Tests.indexingTestsStandaloneConcurrent(Util.COMMIT_ID, 10000);
+		Tests.indexingTestsStandaloneConcurrent(Util.COMMIT_ID, 10000);
 
-		//Tests.runNumericQueryTestsStandalone();
+		Tests.runNumericQueryTestsStandalone();
 
 		Tests.indexingTestsCloudSerial(Util.COMMIT_ID, 10000, 2, "1", "2");
 		Tests.indexingTestsCloudSerial(Util.COMMIT_ID, 10000, 2, "2", "1");
@@ -45,7 +45,7 @@ public class TestPlans {
 		Tests.indexingTestsCloudConcurrent(Util.COMMIT_ID, 10000, 3, "1", "3");
 		Tests.indexingTestsCloudConcurrent(Util.COMMIT_ID, 10000, 4, "2", "2");
 		
-		//Tests.runNumericTestsCloud();
+		Tests.runNumericTestsCloud();
 		
 		Util.postMessage("** Executing the benchmark test plan [COMPLETE]...", MessageType.BLUE_TEXT, false);
 		
