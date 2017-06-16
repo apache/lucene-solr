d953488e930197a518600ee71f734abb58f30916
Noble Paul <noble@apache.org>
2017-06-16 15:21:30 +0930

    unused imports removed

--- solr/solrj/src/java/org/apache/solr/client/solrj/request/CollectionApiMapping.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/request/CollectionApiMapping.java
@@ -23,7 +23,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiConsumer;
 
 import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.common.params.CollectionParams.CollectionAction;
