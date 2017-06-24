59c16fc26593e77e2dd1ea07c1b1cba2e987dcdb
Vivek Narang <vivek.narang@lucidworks.com>
2017-06-24 15:40:12 -0400

    Update QueryClient.java

--- dev-tools/SolrNightBenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/QueryClient.java
+++ dev-tools/SolrNightBenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/QueryClient.java
@@ -1,7 +1,5 @@
 package org.apache.solr.tests.nightlybenchmarks;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
@@ -20,6 +18,8 @@ import java.io.FileReader;
  * limitations under the License.
  */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
@@ -31,23 +31,6 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
-
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with this
- * work for additional information regarding copyright ownership. The ASF
- * licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 
 public class QueryClient implements Runnable {
 
