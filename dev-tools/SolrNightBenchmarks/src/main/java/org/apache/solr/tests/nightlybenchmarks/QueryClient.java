cee50cea9928e221c5e27b431dcb4a9e7091cd4b
Vivek Narang <vivek.narang@lucidworks.com>
2017-06-24 15:40:40 -0400

    Update QueryClient.java

--- dev-tools/SolrNightBenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/QueryClient.java
+++ dev-tools/SolrNightBenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/QueryClient.java
@@ -1,6 +1,5 @@
 package org.apache.solr.tests.nightlybenchmarks;
 
-
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
