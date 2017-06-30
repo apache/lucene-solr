3b6d741016acc4a2f25a816fc56ba7486759968b
Anshum Gupta <anshumg@apple.com>
2017-06-30 10:40:27 -0700

    Solr 7.0 release, bumping up version to 8 on master

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -124,11 +124,18 @@ public final class Version {
 
   /**
    * Match settings and bugs in Lucene's 7.0.0 release.
-   *  <p>
-   *  Use this to get the latest &amp; greatest settings, bug
-   *  fixes, etc, for Lucene.
+   * @deprecated (8.0.0) Use latest
    */
+  @Deprecated
   public static final Version LUCENE_7_0_0 = new Version(7, 0, 0);
+
+  /**
+   * Match settings and bugs in Lucene's 8.0.0 release.
+   * <p>
+   * Use this to get the latest &amp; greatest settings, bug
+   * fixes, etc, for Lucene.
+   */
+  public static final Version LUCENE_8_0_0 = new Version(8, 0, 0);
 
   // To add a new version:
   //  * Only add above this comment
@@ -149,7 +156,7 @@ public final class Version {
    * some defaults may have changed and may break functionality 
    * in your application.
    */
-  public static final Version LATEST = LUCENE_7_0_0;
+  public static final Version LATEST = LUCENE_8_0_0;
 
   /**
    * Constant for backwards compatibility.
