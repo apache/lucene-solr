ddfa074214dc1e1a3aa53fdcb387796aadbcb914
Andrzej Bialecki <ab@apache.org>
2017-07-04 19:46:10 +0200

    SOLR-10878: Fix precommit.

--- solr/core/src/java/org/apache/solr/cloud/MoveReplicaCmd.java
+++ solr/core/src/java/org/apache/solr/cloud/MoveReplicaCmd.java
@@ -31,7 +31,6 @@ import org.apache.solr.common.cloud.DocCollection;
 import org.apache.solr.common.cloud.Replica;
 import org.apache.solr.common.cloud.Slice;
 import org.apache.solr.common.cloud.ZkNodeProps;
-import org.apache.solr.common.cloud.ZkStateReader;
 import org.apache.solr.common.params.CoreAdminParams;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.common.util.Utils;
