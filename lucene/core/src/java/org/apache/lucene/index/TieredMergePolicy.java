9c154ffbb8cbc67e8284e8971c84689f5810a977
Erick Erickson <erick@apache.org>
2017-08-01 14:10:10 -0700

    LUCENE-7915: fix precommit problem

--- lucene/core/src/java/org/apache/lucene/index/TieredMergePolicy.java
+++ lucene/core/src/java/org/apache/lucene/index/TieredMergePolicy.java
@@ -20,7 +20,6 @@ package org.apache.lucene.index;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
