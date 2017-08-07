e3f5aed562b2e398a40ea85ee8264d28fc0e023b
Karl Wright <DaddyWri@gmail.com>
2017-08-07 07:01:19 -0400

    LUCENE-7918: Revamp the API for composites so that it's generic and useful for many kinds of shapes.  Committed (as was LUCENE-7906) on behalf of Ignacio Vera.

---
+++ lucene/spatial3d/src/java/org/apache/lucene/spatial3d/geom/GeoBaseCompositeShape.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.lucene.spatial3d.geom;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Base class to create a composite of GeoShapes.
+ *
+ * @param <T> is the type of GeoShapes of the composite.
+ * @lucene.experimental
+ */
+public abstract class GeoBaseCompositeShape<T extends GeoShape> implements GeoShape {
+
+  /**
+   * Shape's container
+   */
+  protected final List<T> shapes = new ArrayList<>();
+
+  /**
+   * Constructor.
+   */
+  public GeoBaseCompositeShape() {
+  }
+
+  /**
+   * Add a shape to the composite.
+   *
+   * @param shape is the shape to add.
+   */
+  public void addShape(final T shape) {
+    shapes.add(shape);
+  }
+
+  /**
+   * Get the number of shapes in the composite
+   *
+   * @return the number of shapes
+   */
+  public int size() {
+    return shapes.size();
+  }
+
+  /**
+   * Get shape at index
+   *
+   * @return the shape at given index
+   */
+  public T getShape(int index) {
+    return shapes.get(index);
+  }
+
+  @Override
+  public boolean isWithin(final Vector point) {
+    return isWithin(point.x, point.y, point.z);
+  }
+
+  @Override
+  public boolean isWithin(final double x, final double y, final double z) {
+    for (GeoShape shape : shapes) {
+      if (shape.isWithin(x, y, z))
+        return true;
+    }
+    return false;
+  }
+
+  @Override
+  public GeoPoint[] getEdgePoints() {
+    List<GeoPoint> edgePoints = new ArrayList<>();
+    for (GeoShape shape : shapes) {
+      edgePoints.addAll(Arrays.asList(shape.getEdgePoints()));
+    }
+    return edgePoints.toArray(new GeoPoint[edgePoints.size()]);
+  }
+
+  @Override
+  public boolean intersects(final Plane p, final GeoPoint[] notablePoints, final Membership... bounds) {
+    for (GeoShape shape : shapes) {
+      if (shape.intersects(p, notablePoints, bounds))
+        return true;
+    }
+    return false;
+  }
+
+  @Override
+  public void getBounds(Bounds bounds) {
+    for (GeoShape shape : shapes) {
+      shape.getBounds(bounds);
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode() * 31 + shapes.hashCode();//TODO cache
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof GeoBaseCompositeShape<?>))
+      return false;
+    GeoBaseCompositeShape<?> other = (GeoBaseCompositeShape<?>) o;
+    return super.equals(o) && shapes.equals(other.shapes);
+  }
+}
--- lucene/spatial3d/src/java/org/apache/lucene/spatial3d/geom/GeoCompositeAreaShape.java
+++ lucene/spatial3d/src/java/org/apache/lucene/spatial3d/geom/GeoCompositeAreaShape.java
@@ -22,126 +22,17 @@ package org.apache.lucene.spatial3d.geom;
  *
  * @lucene.experimental
  */
-public class GeoCompositeAreaShape extends GeoCompositeMembershipShape implements GeoAreaShape {
+public class GeoCompositeAreaShape extends GeoBaseCompositeAreaShape<GeoAreaShape> {
 
-  /** Constructor.
+  /**
+   * Constructor.
    */
   public GeoCompositeAreaShape() {
   }
 
-  /**
-   * Add a shape to the composite. It throw an IllegalArgumentException
-   * if the shape is not a GeoAreaShape
-   *@param shape is the shape to add.
-   */
   @Override
-  public void addShape(final GeoMembershipShape shape) {
-    if (!(shape instanceof GeoAreaShape)){
-      throw new IllegalArgumentException("GeoCompositeAreaShape must be composed of GeoAreaShapes");
-    }
-    shapes.add(shape);
-  }
-
-  @Override
-  public boolean intersects(GeoShape geoShape){
-    for(GeoShape inShape : shapes){
-      if (((GeoAreaShape)inShape).intersects(geoShape)){
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /** All edgepoints inside shape */
-  protected final static int ALL_INSIDE = 0;
-  /** Some edgepoints inside shape */
-  protected final static int SOME_INSIDE = 1;
-  /** No edgepoints inside shape */
-  protected final static int NONE_INSIDE = 2;
-
-  /** Determine the relationship between the GeoAreShape and the
-   * shape's edgepoints.
-   *@param geoShape is the shape.
-   *@return the relationship.
-   */
-  protected  int isShapeInsideGeoAreaShape(final GeoShape geoShape) {
-    boolean foundOutside = false;
-    boolean foundInside = false;
-    for (GeoPoint p : geoShape.getEdgePoints()) {
-      if (isWithin(p)) {
-        foundInside = true;
-      } else {
-        foundOutside = true;
-      }
-      if (foundInside && foundOutside) {
-        return SOME_INSIDE;
-      }
-    }
-    if (!foundInside && !foundOutside)
-      return NONE_INSIDE;
-    if (foundInside && !foundOutside)
-      return ALL_INSIDE;
-    if (foundOutside && !foundInside)
-      return NONE_INSIDE;
-    return SOME_INSIDE;
-  }
-
-  /** Determine the relationship between the GeoAreShape's edgepoints and the
-   * provided shape.
-   *@param geoshape is the shape.
-   *@return the relationship.
-   */
-  protected int isGeoAreaShapeInsideShape(final GeoShape geoshape)  {
-    boolean foundOutside = false;
-    boolean foundInside = false;
-    for (GeoPoint p : getEdgePoints()) {
-      if (geoshape.isWithin(p)) {
-        foundInside = true;
-      } else {
-        foundOutside = true;
-      }
-      if (foundInside && foundOutside) {
-        return SOME_INSIDE;
-      }
-    }
-    if (!foundInside && !foundOutside)
-      return NONE_INSIDE;
-    if (foundInside && !foundOutside)
-      return ALL_INSIDE;
-    if (foundOutside && !foundInside)
-      return NONE_INSIDE;
-    return SOME_INSIDE;
-  }
-
-  @Override
-  public int getRelationship(GeoShape geoShape) {
-    final int insideGeoAreaShape = isShapeInsideGeoAreaShape(geoShape);
-    if (insideGeoAreaShape == SOME_INSIDE) {
-      return GeoArea.OVERLAPS;
-    }
-
-    final int insideShape = isGeoAreaShapeInsideShape(geoShape);
-    if (insideShape == SOME_INSIDE) {
-      return GeoArea.OVERLAPS;
-    }
-
-    if (insideGeoAreaShape == ALL_INSIDE && insideShape==ALL_INSIDE) {
-      return GeoArea.OVERLAPS;
-    }
-
-    if (intersects(geoShape)){
-      return  GeoArea.OVERLAPS;
-    }
-
-    if (insideGeoAreaShape == ALL_INSIDE) {
-      return GeoArea.WITHIN;
-    }
-
-    if (insideShape==ALL_INSIDE) {
-      return GeoArea.CONTAINS;
-    }
-
-    return GeoArea.DISJOINT;
+  public String toString() {
+    return "GeoCompositeAreaShape: {" + shapes + '}';
   }
 
 }
