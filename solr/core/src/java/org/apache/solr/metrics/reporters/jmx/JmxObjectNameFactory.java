2711dbed8b4b72c37fa929be5167fb6a823c1134
Andrzej Bialecki <ab@apache.org>
2017-08-14 13:46:14 +0200

    SOLR-11221: SolrJmxReporter broken on core reload.

--- solr/core/src/java/org/apache/solr/metrics/reporters/jmx/JmxObjectNameFactory.java
+++ solr/core/src/java/org/apache/solr/metrics/reporters/jmx/JmxObjectNameFactory.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.solr.metrics.reporters;
+package org.apache.solr.metrics.reporters.jmx;
 
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
