e441b3ac4ea61d800e59260ef2c01876ef5c5745
Noble Paul <noble@apache.org>
2017-10-18 16:58:59 +1030

    SOLR-11359: refactored replica name parsing into Utils

--- solr/solrj/src/java/org/apache/solr/common/util/Utils.java
+++ solr/solrj/src/java/org/apache/solr/common/util/Utils.java
@@ -394,4 +394,23 @@ public class Utils {
       return ValidatingJsonMap.parse(CommonParams.APISPEC_LOCATION + name + ".json", CommonParams.APISPEC_LOCATION);
     };
   }
+
+  public static String parseMetricsReplicaName(String collectionName, String coreName) {
+    if (collectionName == null || !coreName.startsWith(collectionName)) {
+      return null;
+    } else {
+      // split "collection1_shard1_1_replica1" into parts
+      if (coreName.length() > collectionName.length()) {
+        String str = coreName.substring(collectionName.length() + 1);
+        int pos = str.lastIndexOf("_replica");
+        if (pos == -1) { // ?? no _replicaN part ??
+          return str;
+        } else {
+          return str.substring(pos + 1);
+        }
+      } else {
+        return null;
+      }
+    }
+  }
 }
