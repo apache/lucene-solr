8e19a1ac9994713b6f22739de3cf8c00f54f8967
Cassandra Targett <ctargett@apache.org>
2017-11-14 16:47:14 -0600

    SOLR-11584: add JS and CSS to support tabbed content; add "column" style TOC support

--- solr/solr-ref-guide/src/js/customscripts.js
+++ solr/solr-ref-guide/src/js/customscripts.js
@@ -22,6 +22,53 @@ $( document ).ready(function() {
      */
     anchors.add('h2,h3,h4,h5');
 
+    // "Bootstrap Bootstrap"
+    // NOTE: by default, we use "dynamic-tabs" in our wrapper instead of "tab-content"
+    // so that if javascript is disabled, bootstrap's CSS doesn't hide all non-active "tab-pane" divs
+    $(".dynamic-tabs").each(function(ignored) {
+    $(this).addClass("tab-content");
+    var nav_ul = $("<ul>", { "class": "nav nav-pills" });
+    $(".tab-pane", this).each(function(tab_index) {
+        var pill_li = $("<li>");
+        // force the first tab to always be the active tab
+        if (0 == tab_index) {
+        $(this).addClass("active");
+        pill_li.addClass("active");
+        } else {
+        // our validator should have prevented this, but remove them just in case
+        $(this).removeClass("active");
+        }
+
+        var pill_a = $("<a>", { "data-toggle" : "pill" } );
+        if ($(this)[0].hasAttribute("id")) {
+        pill_a.attr("href", "#" + $(this).attr("id"));
+        } else {
+        // our validator will complain if a tab-pane has no id, but if the user
+        // views the resultin HTML, draw attention to it...
+        pill_a.append( " BAD TAB-PANE HAS NO ID ");
+        }
+
+        var label = $(".tab-label", this);
+        if (0==label.length) {
+        // our validator will complain if a tab-pane has no tab-label, but if the user
+        // views the resultin HTML, draw attention to it...
+        pill_a.append( " BAD TAB-PANE HAS NO TAB-LABEL ");
+        } else {
+        // NOTE: using the "inner" HTML of the label...
+        // so by default we can use "bold" (or whatever) in our asciidoc and have that
+        // be what people see in the PDF or when javascript is disabled,
+        // but when the pills+tabs get active, the pills won't all be bold (or whatever)
+        pill_a.append( label.html() );
+        // NOTE: Removing the label isn't strictly neccessary, but makes the pills/tabs less redundent
+        label.remove();
+        }
+
+        pill_li.append(pill_a);
+        nav_ul.append(pill_li);
+    });
+    $(this).before(nav_ul);
+    });
+
 });
 
 // needed for nav tabs on pages. See Formatting > Nav tabs for more details.
