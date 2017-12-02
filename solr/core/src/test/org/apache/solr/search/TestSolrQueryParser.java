f92bb90b76f02d50e999c678a98f32296c09162f
Doug Turnbull <softwaredoug@gmail.com>
2017-12-01 20:47:29 -0500

    Target syntax of lucene qparser

--- solr/core/src/test/org/apache/solr/search/TestSolrQueryParser.java
+++ solr/core/src/test/org/apache/solr/search/TestSolrQueryParser.java
@@ -1060,29 +1060,19 @@ public class TestSolrQueryParser extends SolrTestCaseJ4 {
 
 
   public void testSynonymQueryStyle() throws Exception {
-    ModifiableSolrParams edismaxParams = params("qf", "t_pick_best_foo");
 
-    QParser qParser = QParser.getParser("tabby", "edismax", req(edismaxParams));
-    Query q = qParser.getQuery();
-    assertEquals("+((t_pick_best_foo:tabbi | t_pick_best_foo:cat | t_pick_best_foo:felin | t_pick_best_foo:anim))", q.toString());
+    Query q = QParser.getParser("tabby", req(params("df", "t_pick_best_foo"))).getQuery();
+    assertEquals("(t_pick_best_foo:tabbi | t_pick_best_foo:cat | t_pick_best_foo:felin | t_pick_best_foo:anim)", q.toString());
 
-    edismaxParams = params("qf", "t_as_distinct_foo");
-    qParser = QParser.getParser("tabby", "edismax", req(edismaxParams));
-    q = qParser.getQuery();
-    assertEquals("+((t_as_distinct_foo:tabbi t_as_distinct_foo:cat t_as_distinct_foo:felin t_as_distinct_foo:anim))", q.toString());
+    q = QParser.getParser("tabby", req(params("df", "t_as_distinct_foo"))).getQuery();
+    assertEquals("t_as_distinct_foo:tabbi t_as_distinct_foo:cat t_as_distinct_foo:felin t_as_distinct_foo:anim", q.toString());
 
     /*confirm autoGeneratePhraseQueries always builds OR queries*/
-    edismaxParams = params("qf", "t_as_distinct_foo",
-        "sow", "false");
-    qParser = QParser.getParser("jeans", "edismax", req(edismaxParams));
-    q = qParser.getQuery();
-    assertEquals("+(((t_as_distinct_foo:\"denim pant\" t_as_distinct_foo:jean)))", q.toString());
+    q = QParser.getParser("jeans",  req(params("df", "t_as_distinct_foo", "sow", "false"))).getQuery();
+    assertEquals("(t_as_distinct_foo:\"denim pant\" t_as_distinct_foo:jean)", q.toString());
 
-    edismaxParams = params("qf", "t_pick_best_foo",
-        "sow", "false");
-    qParser = QParser.getParser("jeans", "edismax", req(edismaxParams));
-    q = qParser.getQuery();
-    assertEquals("+(((t_pick_best_foo:\"denim pant\" t_pick_best_foo:jean)))", q.toString());
+    q = QParser.getParser("jeans",  req(params("df", "t_pick_best_foo", "sow", "false"))).getQuery();
+    assertEquals("(t_pick_best_foo:\"denim pant\" t_pick_best_foo:jean)", q.toString());
 
   }
 
