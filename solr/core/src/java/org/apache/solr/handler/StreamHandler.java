c775a80d5a4f9485f7d7c1430c4a2f9f411d6adc
Joel Bernstein <jbernste@apache.org>
2018-01-03 13:56:56 -0500

    Add chiSquareDataSet Stream Evaluator

--- solr/core/src/java/org/apache/solr/handler/StreamHandler.java
+++ solr/core/src/java/org/apache/solr/handler/StreamHandler.java
@@ -290,10 +290,11 @@ public class StreamHandler extends RequestHandlerBase implements SolrCoreAware, 
         .withFunctionName("sumSq", SumSqEvaluator.class)
         .withFunctionName("akima", AkimaEvaluator.class)
         .withFunctionName("lerp", LerpEvaluator.class)
+        .withFunctionName("chiSquareDataSet", ChiSquareDataSetEvaluator.class)
 
         // Boolean Stream Evaluators
 
-            .withFunctionName("and", AndEvaluator.class)
+        .withFunctionName("and", AndEvaluator.class)
         .withFunctionName("eor", ExclusiveOrEvaluator.class)
         .withFunctionName("eq", EqualToEvaluator.class)
         .withFunctionName("gt", GreaterThanEvaluator.class)
