48c472dca7fbdd18fb664a0cff77bdc9eb1d137b
Thomas B. Marshment-Howell <TBM@SHSW.WIWHS.US>
2018-01-04 15:12:09 -0600

    Rearrange functions for readability.

--- solr/webapp/web/js/angular/controllers/logging.js
+++ solr/webapp/web/js/angular/controllers/logging.js
@@ -25,15 +25,6 @@ var format_time_content = function( time, timeZone ) {
 
 solrAdminApp.controller('LoggingController',
   function($scope, $timeout, $cookies, Logging, Constants){
-    $scope.toggleRefresh = function() {
-      if(!$scope.stopped) {
-        $scope.stopped = true;
-        $timeout.cancel($scope.timeout);
-      } else {
-        $scope.stopped = false;
-        $scope.timeout = $timeout($scope.refresh, 10000);
-      }
-    };
     $scope.resetMenu("logging", Constants.IS_ROOT_PAGE);
     $scope.timezone = $cookies.logging_timezone || "Local";
     $scope.refresh = function() {
@@ -87,7 +78,15 @@ solrAdminApp.controller('LoggingController',
       });
     };
     $scope.refresh();
-
+    $scope.toggleRefresh = function() {
+      if(!$scope.stopped) {
+        $scope.stopped = true;
+        $timeout.cancel($scope.timeout);
+      } else {
+        $scope.stopped = false;
+        $scope.timeout = $timeout($scope.refresh, 10000);
+      }
+    };
     $scope.toggleTimezone = function() {
       $scope.timezone = ($scope.timezone=="Local") ? "UTC":"Local";
       $cookies.logging_timezone = $scope.timezone;
