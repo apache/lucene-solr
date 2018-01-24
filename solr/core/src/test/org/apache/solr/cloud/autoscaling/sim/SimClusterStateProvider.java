4a7dcdc4a452d87a66ebfae7016bc0112f675580
Andrzej Bialecki <ab@apache.org>
2018-01-24 18:40:45 +0100

    SOLR-11889: Reset disk space when deleting collections. Use constants.

--- solr/core/src/test/org/apache/solr/cloud/autoscaling/sim/SimClusterStateProvider.java
+++ solr/core/src/test/org/apache/solr/cloud/autoscaling/sim/SimClusterStateProvider.java
@@ -397,7 +397,7 @@ public class SimClusterStateProvider implements ClusterStateProvider {
       // mark replica as active
       replicaInfo.getVariables().put(ZkStateReader.STATE_PROP, Replica.State.ACTIVE.toString());
       // add a property expected in tests
-      replicaInfo.getVariables().put(Suggestion.coreidxsize, 123450000);
+      replicaInfo.getVariables().put(Suggestion.coreidxsize, SimCloudManager.DEFAULT_IDX_SIZE_BYTES);
 
       replicas.add(replicaInfo);
       // at this point nuke our cached DocCollection state
@@ -414,9 +414,9 @@ public class SimClusterStateProvider implements ClusterStateProvider {
       cloudManager.getSimNodeStateProvider().simSetNodeValue(nodeId, ImplicitSnitch.CORES, cores + 1);
       Integer disk = (Integer)values.get(ImplicitSnitch.DISK);
       if (disk == null) {
-        disk = 1000;
+        disk = SimCloudManager.DEFAULT_DISK;
       }
-      cloudManager.getSimNodeStateProvider().simSetNodeValue(nodeId, ImplicitSnitch.DISK, disk - 10);
+      cloudManager.getSimNodeStateProvider().simSetNodeValue(nodeId, ImplicitSnitch.DISK, disk - 1);
       if (runLeaderElection) {
         simRunLeaderElection(Collections.singleton(replicaInfo.getCollection()), true);
       }
@@ -452,7 +452,7 @@ public class SimClusterStateProvider implements ClusterStateProvider {
             if (disk == null || disk == 0) {
               throw new Exception("Unexpected value of 'freedisk' (" + disk + ") on node: " + nodeId);
             }
-            cloudManager.getSimNodeStateProvider().simSetNodeValue(nodeId, ImplicitSnitch.DISK, disk + 10);
+            cloudManager.getSimNodeStateProvider().simSetNodeValue(nodeId, ImplicitSnitch.DISK, disk + 1);
           }
           LOG.trace("-- simRemoveReplica {}", ri);
           simRunLeaderElection(Collections.singleton(ri.getCollection()), true);
@@ -725,7 +725,10 @@ public class SimClusterStateProvider implements ClusterStateProvider {
       collProperties.clear();
       sliceProperties.clear();
       leaderThrottles.clear();
-      cloudManager.getSimNodeStateProvider().simGetAllNodeValues().forEach((n, values) -> values.put("cores", 0));
+      cloudManager.getSimNodeStateProvider().simGetAllNodeValues().forEach((n, values) -> {
+        values.put(ImplicitSnitch.CORES, 0);
+        values.put(ImplicitSnitch.DISK, 1000);
+      });
       collectionsStatesRef.set(null);
     } finally {
       lock.unlock();
