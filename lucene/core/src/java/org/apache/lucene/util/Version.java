0031150fcaa7c223524cc0893ec5dbdf32a5a58e
Alan Woodward <romseygeek@apache.org>
2018-03-12 10:35:08 +0000

    Add missing 7.2.1 version

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -61,6 +61,14 @@ public final class Version {
   public static final Version LUCENE_7_2_0 = new Version(7, 2, 0);
 
   /**
+   * Match settings and bugs in Lucene's 7.2.1 release.
+   * @deprecated Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_7_2_1 = new Version(7, 2, 1);
+
+
+  /**
    * Match settings and bugs in Lucene's 7.3.0 release.
    * @deprecated Use latest
    */
