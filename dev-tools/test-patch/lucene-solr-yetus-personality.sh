bd20e36d2dd58cb291b0cb3b59bd9e240136a5d2
Steve Rowe <sarowe@apache.org>
2018-03-15 15:38:05 -0400

    SOLR-10912: attempted personality plugins fix: trying just junit/javac instead of unit/compile (which didn't actually do anything at all)

--- dev-tools/test-patch/lucene-solr-yetus-personality.sh
+++ dev-tools/test-patch/lucene-solr-yetus-personality.sh
@@ -28,7 +28,7 @@
 #   4) Add a "<plugin>_rebuild" function to call out to ant to perform the validation method.
 # See examples of the above-described function types ^^ below.
    
-PLUGIN_LIST="ant,jira,compile,unit,test4tests"
+PLUGIN_LIST="ant,jira,javac,junit,test4tests"
 PLUGIN_LIST+=",testoutput,checkluceneversion,ratsources,checkforbiddenapis,checklicenses"
 PLUGIN_LIST+=",validatesourcepatterns,validaterefguide"
 personality_plugins "${PLUGIN_LIST}"
