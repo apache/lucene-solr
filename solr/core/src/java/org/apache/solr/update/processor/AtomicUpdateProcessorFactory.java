e340cef68ddfd4b8a5166ddb17da47e8ebe7fa76
Noble Paul <noble@apache.org>
2018-03-23 13:48:28 +1100

    SOLR-12015: Add support "add-distinct" in AtomicURP so that we can use the 'add-distict' as a request parameter e.g: atomic.add-distict=<multival-field-name>

--- solr/core/src/java/org/apache/solr/update/processor/AtomicUpdateProcessorFactory.java
+++ solr/core/src/java/org/apache/solr/update/processor/AtomicUpdateProcessorFactory.java
@@ -16,9 +16,6 @@
  */
 package org.apache.solr.update.processor;
 
-import static java.util.Collections.singletonMap;
-import static org.apache.solr.common.SolrException.ErrorCode.SERVER_ERROR;
-
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
@@ -40,6 +37,9 @@ import org.apache.solr.util.plugin.SolrCoreAware;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static java.util.Collections.singletonMap;
+import static org.apache.solr.common.SolrException.ErrorCode.SERVER_ERROR;
+
 /**
  * An update processor that will convert conventional field-value document to atomic update document
  * <p>
@@ -59,7 +59,8 @@ public class AtomicUpdateProcessorFactory extends UpdateRequestProcessorFactory 
   private final static String REMOVE = "remove";
   private final static String SET = "set";
   private final static String REMOVEREGEX = "removeregex";
-  private final static Set<String> VALID_OPS = new HashSet<>(Arrays.asList(ADD, INC, REMOVE, SET, REMOVEREGEX));
+  private final static String ADDDISTINCT = "add-distinct";
+  private final static Set<String> VALID_OPS = new HashSet<>(Arrays.asList(ADD, INC, REMOVE, SET, REMOVEREGEX, ADDDISTINCT));
 
   private final static String VERSION = "_version_";
   public static final String NAME = "atomic";
