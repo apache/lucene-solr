43796e516932881da7abbc8cc379ec2661020f7e
Robert Muir <rmuir@apache.org>
2018-03-26 19:02:21 -0400

    LUCENE-8175: upgrade icu4j to 61.1 which fixes concurrency issue

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/util/UnicodeProps.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/util/UnicodeProps.java
@@ -24,7 +24,7 @@ import org.apache.lucene.util.SparseFixedBitSet;
 
 /**
  * This file contains unicode properties used by various {@link CharTokenizer}s.
- * The data was created using ICU4J v60.2.0.0
+ * The data was created using ICU4J v61.1.0.0
  * <p>
  * Unicode version: 10.0.0.0
  */
