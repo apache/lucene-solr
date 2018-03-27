b3cf209f6faaedd70e8846bcf1ef369942a1acb6
Tommaso Teofili <tommaso@apache.org>
2018-03-27 10:14:55 +0200

    LUCENE-8223 - remove time dependent checks in performance test

--- lucene/classification/src/test/org/apache/lucene/classification/KNearestFuzzyClassifierTest.java
+++ lucene/classification/src/test/org/apache/lucene/classification/KNearestFuzzyClassifierTest.java
@@ -69,21 +69,15 @@ public class KNearestFuzzyClassifierTest extends ClassificationTestBase<BytesRef
     MockAnalyzer analyzer = new MockAnalyzer(random());
     LeafReader leafReader = getRandomIndex(analyzer, 100);
     try {
-      long trainStart = System.currentTimeMillis();
       Classifier<BytesRef> classifier = new KNearestFuzzyClassifier(leafReader, null, analyzer, null, 3, categoryFieldName, textFieldName);
-      long trainEnd = System.currentTimeMillis();
-      long trainTime = trainEnd - trainStart;
-      assertTrue("training took more than 10s: " + trainTime / 1000 + "s", trainTime < 10000);
 
-      long evaluationStart = System.currentTimeMillis();
       ConfusionMatrixGenerator.ConfusionMatrix confusionMatrix = ConfusionMatrixGenerator.getConfusionMatrix(leafReader,
           classifier, categoryFieldName, textFieldName, -1);
       assertNotNull(confusionMatrix);
-      long evaluationEnd = System.currentTimeMillis();
-      long evaluationTime = evaluationEnd - evaluationStart;
-      assertTrue("evaluation took more than 2m: " + evaluationTime / 1000 + "s", evaluationTime < 120000);
+
       double avgClassificationTime = confusionMatrix.getAvgClassificationTime();
-      assertTrue(5000 > avgClassificationTime);
+      assertTrue(avgClassificationTime >= 0);
+
       double accuracy = confusionMatrix.getAccuracy();
       assertTrue(accuracy >= 0d);
       assertTrue(accuracy <= 1d);
