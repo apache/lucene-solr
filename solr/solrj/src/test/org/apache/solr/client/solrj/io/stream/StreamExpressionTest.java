eddcb9894a05ad531588d88cabdab69e8048bcb9
Joel Bernstein <jbernste@apache.org>
2018-04-05 13:24:52 -0400

    SOLR-12183: Fix precommit

--- solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/StreamExpressionTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/StreamExpressionTest.java
@@ -22,7 +22,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import org.apache.lucene.util.LuceneTestCase;
