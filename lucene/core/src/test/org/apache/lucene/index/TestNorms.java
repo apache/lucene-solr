6262b7757b843dd3bb41e86e334c832cec72d161
Simon Willnauer <simonw@apache.org>
2018-05-09 10:04:18 +0200

    [TEST] Fix TestNorms to ensure that max token lenght is at least 3 to have predictable norms

--- lucene/core/src/test/org/apache/lucene/index/TestNorms.java
+++ lucene/core/src/test/org/apache/lucene/index/TestNorms.java
@@ -69,7 +69,8 @@ public class TestNorms extends LuceneTestCase {
   public void buildIndex(Directory dir) throws IOException {
     Random random = random();
     MockAnalyzer analyzer = new MockAnalyzer(random());
-    analyzer.setMaxTokenLength(TestUtil.nextInt(random(), 1, IndexWriter.MAX_TERM_LENGTH));
+    // we need at least 3 for maxTokenLength otherwise norms are messed up
+    analyzer.setMaxTokenLength(TestUtil.nextInt(random(), 3, IndexWriter.MAX_TERM_LENGTH));
     IndexWriterConfig config = newIndexWriterConfig(analyzer);
     Similarity provider = new MySimProvider();
     config.setSimilarity(provider);
