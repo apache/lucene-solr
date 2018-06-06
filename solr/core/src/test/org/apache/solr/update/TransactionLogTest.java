0358fcb1759038f76d91c124703c5d3244125f75
Cao Manh Dat <datcm@apache.org>
2018-06-06 15:52:15 +0700

    SOLR-12250: Fix precommit

--- solr/core/src/test/org/apache/solr/update/TransactionLogTest.java
+++ solr/core/src/test/org/apache/solr/update/TransactionLogTest.java
@@ -18,8 +18,6 @@
 package org.apache.solr.update;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Locale;
