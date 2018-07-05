2684e745937c3fa05af467f0594eb4177cbb4cdc
Steve Rowe <sarowe@apache.org>
2018-07-05 14:58:10 -0400

    First uninstall 'asciidoctor' gem before installing it, to remove unwanted versions

--- dev-tools/scripts/jenkins.build.ref.guide.sh
+++ dev-tools/scripts/jenkins.build.ref.guide.sh
@@ -60,6 +60,7 @@ echoRun "rvm $RUBY_VERSION@$GEMSET"      # Activate this project's gemset
 
 # Install gems in the gemset.  Param --force disables dependency conflict detection.
 echoRun "gem install --force --version 3.5.0 jekyll"
+echoRun "gem uninstall asciidoctor"      # Get rid of all versions
 echoRun "gem install --force --version 1.5.6.2 asciidoctor"
 echoRun "gem install --force --version 2.1.0 jekyll-asciidoc"
 echoRun "gem install --force --version 1.1.2 pygments.rb"
