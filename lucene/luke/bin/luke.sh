47f1b740e213a321224165319999bbfbb96078f1
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-07-15 01:03:43 +0900

    Packaging luke

--- lucene/luke/bin/luke.sh
+++ lucene/luke/bin/luke.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
+LUKE_PATH=$(cd `dirname $0`/.. && pwd)
 JAVA_OPTS="$JAVA_OPTS -Xmx512m -Xms512m"
 
-if [[ -d `echo $LUKE_PATH` ]]; then
-  nohup java ${JAVA_OPTS} -classpath "${LUKE_PATH}/lib/*" org.apache.lucene.luke.app.desktop.LukeMain > /dev/null 2&>1 &
-else
-  echo "Unable to find the LUKE_PATH environnement variable..."
-  echo "Assuming you're running from the root folder of luke..."
-  nohup java ${JAVA_OPTS} -classpath "./lib/*" org.apache.lucene.luke.app.desktop.LukeMain > /dev/null 2&>1 &
-fi
+nohup java ${JAVA_OPTS} -classpath "${LUKE_PATH}/lib/*" org.apache.lucene.luke.app.desktop.LukeMain > /dev/null 2>&1 &
+
