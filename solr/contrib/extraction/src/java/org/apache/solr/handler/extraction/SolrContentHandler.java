96010e1f7e9fbba75bc637e2bfce8cca2030e9c8
Bar Rotstein <barrotstein@gmail.com>
2018-08-21 22:21:32 +0300

    replace extraction date util in SolrContentHandler with URP and update test config

--- solr/contrib/extraction/src/java/org/apache/solr/handler/extraction/SolrContentHandler.java
+++ solr/contrib/extraction/src/java/org/apache/solr/handler/extraction/SolrContentHandler.java
@@ -20,7 +20,6 @@ import java.lang.invoke.MethodHandles;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -32,7 +31,6 @@ import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.schema.IndexSchema;
 import org.apache.solr.schema.SchemaField;
-import org.apache.solr.schema.NumberType;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.TikaMetadataKeys;
 import org.slf4j.Logger;
@@ -253,12 +251,12 @@ public class SolrContentHandler extends DefaultHandler implements ExtractingPara
     }
 
     if (fval != null) {
-      document.addField(name, transformValue(fval, sf));
+      document.addField(name, fval);
     }
 
     if (vals != null) {
       for (String val : vals) {
-        document.addField(name, transformValue(val, sf));
+        document.addField(name, val);
       }
     }
 
@@ -308,30 +306,6 @@ public class SolrContentHandler extends DefaultHandler implements ExtractingPara
   @Override
   public void ignorableWhitespace(char[] chars, int offset, int length) throws SAXException {
     characters(chars, offset, length);
-  }
-
-  /**
-   * Can be used to transform input values based on their {@link org.apache.solr.schema.SchemaField}
-   * <p>
-   * This implementation only formats dates using the {@link ExtractionDateUtil}.
-   *
-   * @param val    The value to transform
-   * @param schFld The {@link org.apache.solr.schema.SchemaField}
-   * @return The potentially new value.
-   */
-  protected String transformValue(String val, SchemaField schFld) {
-    String result = val;
-    if (schFld != null && NumberType.DATE.equals(schFld.getType().getNumberType())) {
-      //try to transform the date
-      try {
-        Date date = ExtractionDateUtil.parseDate(val, dateFormats); // may throw
-        result = date.toInstant().toString();//ISO format
-      } catch (Exception e) {
-        // Let the specific fieldType handle errors
-        // throw new SolrException(SolrException.ErrorCode.SERVER_ERROR, "Invalid value: " + val + " for field: " + schFld, e);
-      }
-    }
-    return result;
   }
 
   /**
