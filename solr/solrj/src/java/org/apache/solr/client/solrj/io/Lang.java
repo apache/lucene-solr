fbf2885ae96fcaa499a3018158b5c94a25c048be
Joel Bernstein <jbernste@apache.org>
2018-09-06 14:00:38 -0400

    SOLR-11943: Add machine learning functions for location data

--- solr/solrj/src/java/org/apache/solr/client/solrj/io/Lang.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/io/Lang.java
@@ -255,6 +255,7 @@ public class Lang {
         .withFunctionName("removeCache", RemoveCacheEvaluator.class)
         .withFunctionName("listCache", ListCacheEvaluator.class)
         .withFunctionName("zscores", NormalizeEvaluator.class)
+        .withFunctionName("locationVectors", LocationVectorsEvaluator.class)
 
         // Boolean Stream Evaluators
 
