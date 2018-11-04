9730819a3ff993299e0888162931e5fb100ab665
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-11-04 15:51:49 +0900

    Fix typo.

--- lucene/luke/bin/luke.sh
+++ lucene/luke/bin/luke.sh
@@ -3,7 +3,7 @@
 JAVA_OPTIONS="-Xmx1024m -Xms512m -XX:MaxMetaspaceSize=256m"
 if [[ ! -d `echo $LUKE_PATH` ]]; then
   LUKE_PATH=$(cd $(dirname $0) && pwd)
-  echo "Unable to find the LUCENE_PATH environnement variable."
+  echo "Unable to find the LUCENE_PATH environment variable."
   echo "Set LUKE_PATH to $LUKE_PATH"
 fi
 
