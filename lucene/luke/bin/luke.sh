678f24f498425a80bfbb2af9e7e5381cd2bdee5d
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-11-23 21:24:00 +0900

    Add jvm option '--add-opens' to allow deep reflection (for guice/cglib)

--- lucene/luke/bin/luke.sh
+++ lucene/luke/bin/luke.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-JAVA_OPTIONS="-Xmx1024m -Xms512m -XX:MaxMetaspaceSize=256m"
-if [[ ! -d `echo $LUKE_PATH` ]]; then
-  LUKE_PATH=$(cd $(dirname $0) && pwd)
-  echo "Unable to find the LUCENE_PATH environment variable."
-  echo "Set LUKE_PATH to $LUKE_PATH"
+ADD_OPENS_OPTION="--add-opens java.base/java.lang=ALL-UNNAMED"
+java ${ADD_OPENS_OPTION} -version > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+  # running on jdk9+
+  JAVA_OPTIONS="${ADD_OPENS_OPTION}"
+else
+  # running on jdk8
+  JAVA_OPTIONS=""
 fi
 
-cd ${LUKE_PATH}
+LUKE_HOME=$(cd $(dirname $0) && pwd)
+cd ${LUKE_HOME}
+
+JAVA_OPTIONS="${JAVA_OPTIONS} -Xmx1024m -Xms512m -XX:MaxMetaspaceSize=256m"
 
 CLASSPATHS="./*:./lib/*:../core/*:../codecs/*:../backward-codecs/*:../queries/*:../queryparser/*:../misc/*"
 for dir in `ls ../analysis`; do
   CLASSPATHS="${CLASSPATHS}:../analysis/${dir}/*:../analysis/${dir}/lib/*"
 done
 
-java -cp ${CLASSPATHS} ${JAVA_OPTIONS} org.apache.lucene.luke.app.desktop.LukeMain > ${HOME}/.luke.d/luke_out.log 2>&1 &
+nohup java -cp ${CLASSPATHS} ${JAVA_OPTIONS} org.apache.lucene.luke.app.desktop.LukeMain > ${HOME}/.luke.d/luke_out.log 2>&1 &
