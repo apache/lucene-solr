480b1fc4dab21d5a5c49b06c45e411158caffd17
markrmiller <markrmiller@apache.org>
2018-12-09 10:29:05 -0600

    SOLR-12801: tweak shard count specificiation

--- solr/core/src/test/org/apache/solr/cloud/MultiThreadedOCPTest.java
+++ solr/core/src/test/org/apache/solr/cloud/MultiThreadedOCPTest.java
@@ -55,12 +55,13 @@ public class MultiThreadedOCPTest extends AbstractFullDistribZkTestBase {
 
   public MultiThreadedOCPTest() {
     sliceCount = 2;
+    
+    fixShardCount(3);
   }
 
   @Test
 // commented 20-July-2018   @BadApple(bugUrl="https://issues.apache.org/jira/browse/SOLR-12028")
 //commented 20-Sep-2018 @BadApple(bugUrl="https://issues.apache.org/jira/browse/SOLR-12028") // added 17-Aug-2018
-  @ShardsFixed(num = 3)
   public void test() throws Exception {
     testParallelCollectionAPICalls();
     testTaskExclusivity();
