99def054fef6f766980a2f1d0f34bd9322bdc5b0
markrmiller <markrmiller@apache.org>
2018-12-09 15:51:17 -0600

    SOLR-12801: use waitForActiveCollection call

--- solr/core/src/test/org/apache/solr/cloud/TestMiniSolrCloudClusterSSL.java
+++ solr/core/src/test/org/apache/solr/cloud/TestMiniSolrCloudClusterSSL.java
@@ -263,8 +263,7 @@ public class TestMiniSolrCloudClusterSSL extends SolrTestCaseJ4 {
     CollectionAdminRequest.createCollection(collection, CONF_NAME, NUM_SERVERS, 1)
         .withProperty("config", "solrconfig-tlog.xml")
         .process(cloudClient);
-    ZkStateReader zkStateReader = cloudClient.getZkStateReader();
-    AbstractDistribZkTestBase.waitForRecoveriesToFinish(collection, zkStateReader, true, true, 330);
+    cluster.waitForActiveCollection(collection, NUM_SERVERS, NUM_SERVERS);
     assertEquals("sanity query", 0, cloudClient.query(collection, params("q","*:*")).getStatus());
   }
   
