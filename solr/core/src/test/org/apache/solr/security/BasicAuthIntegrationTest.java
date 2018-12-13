f4a0f8e19e22c77778c665fefb135b1cbf75f702
Chris Hostetter <hossman@apache.org>
2018-12-13 14:25:11 -0700

    Fix bad merge that duplicated import statements causing precommit failures

--- solr/core/src/test/org/apache/solr/security/BasicAuthIntegrationTest.java
+++ solr/core/src/test/org/apache/solr/security/BasicAuthIntegrationTest.java
@@ -69,9 +69,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.util.Collections.singletonMap;
-
 public class BasicAuthIntegrationTest extends SolrCloudTestCase {
 
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
