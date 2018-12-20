272178eff5c414e9df76e5cd4bd5bfbcf0e74249
Jason Gerlowski <gerlowskija@apache.org>
2018-12-20 11:06:02 -0500

    SOLR-13045: Allow SimDistribStateManager to create top-level data nodes
    While working on a related issue in SimDistribStateManager, I noticed
    that `createData()` only worked successfully on nodes nested more than
    one level under root.  (i.e. `createData("/foo", someData, mode)` would
    fail, while the same with "/foo/bar" wouldn't).  This was due to an edge
    case in SimDistribStateManager's path building logic.  This commit fixes
    this issue.

--- solr/core/src/test/org/apache/solr/cloud/autoscaling/sim/TestSimDistribStateManager.java
+++ solr/core/src/test/org/apache/solr/cloud/autoscaling/sim/TestSimDistribStateManager.java
@@ -18,6 +18,7 @@
 package org.apache.solr.cloud.autoscaling.sim;
 
 import java.lang.invoke.MethodHandles;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -260,6 +261,12 @@ public class TestSimDistribStateManager extends SolrTestCaseJ4 {
     assertTrue(stateManager.hasData("/createMode/ephemeral_seq"));
     kids = stateManager.listData("/createMode/ephemeral_seq");
     assertEquals(0, kids.size());
+  }
+
+  @Test
+  public void testCanCreateNodesWithDataAtTopLevel() throws Exception {
+    final String path = stateManager.createData("/topLevelNodeWithData", new String("helloworld").getBytes(StandardCharsets.UTF_8), CreateMode.PERSISTENT);
+    assertEquals("/topLevelNodeWithData", path);
   }
 
   static class OnceWatcher implements Watcher {
