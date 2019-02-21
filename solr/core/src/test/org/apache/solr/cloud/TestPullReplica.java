6e81def01c328cfdbff3efed930678397fee2d13
Tomas Fernandez Lobbe <tflobbe@apache.org>
2019-02-20 21:15:29 -0800

    Fix TestPullReplica.testKillLeader
    Somewhere along the way, this was broken and since the test is being excluded, nobody noticed

--- solr/core/src/test/org/apache/solr/cloud/TestPullReplica.java
+++ solr/core/src/test/org/apache/solr/cloud/TestPullReplica.java
@@ -472,7 +472,7 @@ public class TestPullReplica extends SolrCloudTestCase {
     if (removeReplica) {
       CollectionAdminRequest.addReplicaToShard(collectionName, "shard1", Replica.Type.NRT).process(cluster.getSolrClient());
     } else {
-      leaderJetty.stop();
+      leaderJetty.start();
     }
     waitForState("Expected collection to be 1x2", collectionName, clusterShape(1, 2));
     unIgnoreException("No registered leader was found"); // Should have a leader from now on
