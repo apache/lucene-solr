973088f9bc163ccf155897f7dae149d185220de8
Shalin Shekhar Mangar <shalin@apache.org>
2019-03-05 10:06:11 +0530

    Adding version 7.7.2

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -229,11 +229,18 @@ public final class Version {
 
   /**
    * Match settings and bugs in Lucene's 7.7.1 release.
+   * @deprecated (7.7.2) Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_7_7_1 = new Version(7, 7, 1);
+
+  /**
+   * Match settings and bugs in Lucene's 7.7.2 release.
    * <p>
    * Use this to get the latest &amp; greatest settings, bug
    * fixes, etc, for Lucene.
    */
-  public static final Version LUCENE_7_7_1 = new Version(7, 7, 1);
+  public static final Version LUCENE_7_7_2 = new Version(7, 7, 2);
 
   // To add a new version:
   //  * Only add above this comment
@@ -254,7 +261,7 @@ public final class Version {
    * some defaults may have changed and may break functionality 
    * in your application.
    */
-  public static final Version LATEST = LUCENE_7_7_1;
+  public static final Version LATEST = LUCENE_7_7_2;
 
   /**
    * Constant for backwards compatibility.
