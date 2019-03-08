9c186d19edf54495385596d730300c872469f6c4
Sascha Szott <szott@gmx.de>
2019-03-08 23:10:02 +0100

    docu change: use class TopDocs instead of Hits
    `Hits` was removed in Lucene 3.0

--- lucene/queries/src/java/org/apache/lucene/queries/mlt/MoreLikeThis.java
+++ lucene/queries/src/java/org/apache/lucene/queries/mlt/MoreLikeThis.java
@@ -92,9 +92,9 @@ import org.apache.lucene.util.PriorityQueue;
  * Reader target = ... // orig source of doc you want to find similarities to
  * Query query = mlt.like( target);
  * 
- * Hits hits = is.search(query);
- * // now the usual iteration thru 'hits' - the only thing to watch for is to make sure
- * //you ignore the doc if it matches your 'target' document, as it should be similar to itself
+ * TopDocs topDocs = is.search(query);
+ * // now the usual iteration thru 'topDocs' - the only thing to watch for is to make sure
+ * // you ignore the doc if it matches your 'target' document, as it should be similar to itself
  *
  * </pre>
  * <p>
