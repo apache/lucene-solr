c5bd9d86616e06783bd06c525d94b9a47a773e87
Sascha Szott <szott@gmx.de>
2019-03-09 11:49:51 +0100

    Ranking size parameter n is mandatory

--- lucene/queries/src/java/org/apache/lucene/queries/mlt/MoreLikeThis.java
+++ lucene/queries/src/java/org/apache/lucene/queries/mlt/MoreLikeThis.java
@@ -92,7 +92,7 @@ import org.apache.lucene.util.PriorityQueue;
  * Reader target = ... // orig source of doc you want to find similarities to
  * Query query = mlt.like( target);
  * 
- * TopDocs topDocs = is.search(query);
+ * TopDocs topDocs = is.search(query, 10);
  * // now the usual iteration thru 'topDocs' - the only thing to watch for is to make sure
  * // you ignore the doc if it matches your 'target' document, as it should be similar to itself
  *
