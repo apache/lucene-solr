6e0afdb15dfd2cb4f6df3f81c3f908329dc9b37b
jimczi <jimczi@apache.org>
2019-03-14 13:23:55 +0100

    Add 8.0.0 back compat test indexes

--- lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
@@ -310,7 +310,9 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
     "7.7.0-cfs",
     "7.7.0-nocfs",
     "7.7.1-cfs",
-    "7.7.1-nocfs"
+    "7.7.1-nocfs",
+    "8.0.0-cfs",
+    "8.0.0-nocfs"
   };
 
   public static String[] getOldNames() {
@@ -329,7 +331,8 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
     "sorted.7.5.0",
     "sorted.7.6.0",
     "sorted.7.7.0",
-    "sorted.7.7.1"
+    "sorted.7.7.1",
+    "sorted.8.0.0"
   };
 
   public static String[] getOldSortedNames() {
