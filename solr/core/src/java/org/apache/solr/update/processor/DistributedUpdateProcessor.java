2d4c07010c39543e4046a9c20705ca74f19365f4
moshebla <internet@localhost.localdomain>
2019-03-20 16:31:54 +0200

    SOLR-12638: remove unused imports

--- solr/core/src/java/org/apache/solr/update/processor/DistributedUpdateProcessor.java
+++ solr/core/src/java/org/apache/solr/update/processor/DistributedUpdateProcessor.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
 
 import com.google.common.annotations.VisibleForTesting;
