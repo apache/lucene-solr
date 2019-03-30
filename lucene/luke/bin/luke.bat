2ef450d9fdaee0c034299fdb4bb2d9d5a63ccba1
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2019-03-30 15:00:57 +0900

    Fix start script for windows

--- lucene/luke/bin/luke.bat
+++ lucene/luke/bin/luke.bat
@@ -1,6 +1,8 @@
 @echo off
 @setlocal enabledelayedexpansion
 
+cd /d %~dp0
+
 set JAVA_OPTIONS=%JAVA_OPTIONS% -Xmx1024m -Xms512m -XX:MaxMetaspaceSize=256m
 
 set CLASSPATHS=.\*;.\lib\*;..\core\*;..\codecs\*;..\backward-codecs\*;..\queries\*;..\queryparser\*;..\suggest\*;..\misc\*
