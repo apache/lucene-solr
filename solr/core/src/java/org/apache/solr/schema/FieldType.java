cfddbe1126523f84b9d7f7f98c55af9e8fd0f405
Jason Gerlowski <gerlowskija@apache.org>
2019-04-03 07:06:21 -0400

    SOLR-13331: Fix AtomicUpdate 'remove' ops in SolrJ
    The racent change introducing ByteArrayUtf8CharSequence altered the
    NamedLists produced by atomic-update requests so that they include
    instances of this class for requests coming in as javabin.  This is a
    problem for 'remove' atomic-updates, which need to be able to compare
    these ByteArrayUtf8CharSequence instances with existing field values
    represented as Strings.  equals() would always return false, and
    'remove' operations would have no effect.
    
    This commit converts items as necessary to allow atomic-update
    operations to work as expected.

--- solr/core/src/java/org/apache/solr/schema/FieldType.java
+++ solr/core/src/java/org/apache/solr/schema/FieldType.java
@@ -1181,6 +1181,9 @@ public abstract class FieldType extends FieldProperties {
   /**Converts any Object to a java Object native to this field type
    */
   public Object toNativeType(Object val) {
+    if (val instanceof CharSequence) {
+      return ((CharSequence) val).toString();
+    }
     return val;
   }
   
