03d76c611b2ebe368edd4e3245d5be2a36cd73b0
Erick Erickson <Erick.Erickson@gmail.com>
2019-04-05 08:47:36 -0700

    SOLR-13075: Harden SaslZkACLProviderTest. Put AwaitsFix back

--- solr/core/src/test/org/apache/solr/cloud/SaslZkACLProviderTest.java
+++ solr/core/src/test/org/apache/solr/cloud/SaslZkACLProviderTest.java
@@ -115,6 +115,7 @@ public class SaslZkACLProviderTest extends SolrTestCaseJ4 {
   }
 
   @Test
+  @AwaitsFix(bugUrl = "https://issues.apache.org/jira/browse/SOLR-13075")
   public void testSaslZkACLProvider() throws Exception {
     // Test with Sasl enabled
     SolrZkClient zkClient = new SolrZkClientWithACLs(zkServer.getZkAddress(), AbstractZkTestCase.TIMEOUT);
