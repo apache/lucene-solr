e460356abeb1bd075a885d905a1d0873469bbd43
jimczi <jimczi@apache.org>
2019-04-08 13:29:49 +0200

    LUCENE-8477: Restore public ctr for FilteredIntervalsSource

--- lucene/sandbox/src/java/org/apache/lucene/search/intervals/FilteredIntervalsSource.java
+++ lucene/sandbox/src/java/org/apache/lucene/search/intervals/FilteredIntervalsSource.java
@@ -83,7 +83,7 @@ public abstract class FilteredIntervalsSource extends IntervalsSource {
    * @param name  the name of the filter
    * @param in    the source to filter
    */
-  private FilteredIntervalsSource(String name, IntervalsSource in) {
+  public FilteredIntervalsSource(String name, IntervalsSource in) {
     this.name = name;
     this.in = in;
   }
