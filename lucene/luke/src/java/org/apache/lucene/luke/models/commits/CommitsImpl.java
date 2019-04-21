e960a4ab813c78ee9ce9223ced53a990ac66f999
Tomoko Uchida <tomoko@apache.org>
2019-04-22 00:36:41 +0900

    LUCENE-2562: Preserve map key order for the user interface.

--- lucene/luke/src/java/org/apache/lucene/luke/models/commits/CommitsImpl.java
+++ lucene/luke/src/java/org/apache/lucene/luke/models/commits/CommitsImpl.java
@@ -200,7 +200,7 @@ public final class CommitsImpl extends LukeModel implements Commits {
     if (dir == null) {
       return Collections.emptyMap();
     }
-    return Collections.unmodifiableMap(commitMap);
+    return new TreeMap<>(commitMap);
   }
 
   private SegmentInfos findSegmentInfos(long commitGen) throws LukeException, IOException {
