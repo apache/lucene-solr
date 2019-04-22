cfd9de894d18888f0f1b9e368994b972a81f449c
Atri Sharma <atri@apache.org>
2019-04-22 19:44:52 +0530

    LUCENE-8757: Improving Default Segments To Thread Mapping Algorithm
    The current slicing algorithm assigns a thread per segment, which
    can be detrimental to performance in case the distribution has
    a large number of small segments. The patch introduces a slicing
    algorithm which coalesces smaller segments to a single thread,
    thus reducing the impact of context switching by limiting the
    number of threads
    
    Signed-off-by: Adrien Grand <jpountz@gmail.com>

--- lucene/test-framework/src/java/org/apache/lucene/util/LuceneTestCase.java
+++ lucene/test-framework/src/java/org/apache/lucene/util/LuceneTestCase.java
@@ -1933,6 +1933,15 @@ public abstract class LuceneTestCase extends Assert {
         ret = random.nextBoolean()
             ? new AssertingIndexSearcher(random, r, ex)
             : new AssertingIndexSearcher(random, r.getContext(), ex);
+      } else if (random.nextBoolean()) {
+        int maxDocPerSlice = 1 + random.nextInt(100000);
+        int maxSegmentsPerSlice = 1 + random.nextInt(20);
+        ret = new IndexSearcher(r, ex) {
+          @Override
+          protected LeafSlice[] slices(List<LeafReaderContext> leaves) {
+            return slices(leaves, maxDocPerSlice, maxSegmentsPerSlice);
+          }
+        };
       } else {
         ret = random.nextBoolean()
             ? new IndexSearcher(r, ex)
