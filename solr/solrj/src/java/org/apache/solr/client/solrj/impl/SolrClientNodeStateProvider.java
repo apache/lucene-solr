06b0c62453909878349b3aeaac6200b0869d6bd7
Cao Manh Dat <datcm@apache.org>
2019-05-07 09:40:45 +0100

    SOLR-13449: SolrClientNodeStateProvider always retries on requesting metrics from other nodes

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/SolrClientNodeStateProvider.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/SolrClientNodeStateProvider.java
@@ -199,6 +199,7 @@ public class SolrClientNodeStateProvider implements NodeStateProvider, MapWriter
       while (cnt++ < 3) {
         try {
           rsp = ctx.invoke(solrNode, CommonParams.METRICS_PATH, params);
+          break;
         } catch (SolrException | SolrServerException | IOException e) {
           boolean hasCauseIOException = false;
           Throwable cause = e;
@@ -305,6 +306,7 @@ public class SolrClientNodeStateProvider implements NodeStateProvider, MapWriter
         while (cnt++ < retries) {
           try {
             rsp = snitchContext.invoke(solrNode, CommonParams.METRICS_PATH, params);
+            break;
           } catch (SolrException | SolrServerException | IOException e) {
             if (e instanceof SolrServerException) {
               
