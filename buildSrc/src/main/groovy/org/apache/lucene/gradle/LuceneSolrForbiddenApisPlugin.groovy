0577aade62134d32f0c618dda03f7be18c27a115
Uwe Schindler <uschindler@apache.org>
2019-05-20 01:32:07 +0200

    SOLR-13452: Fobiddenapis: Detect if "servlet-api.jar" is on classpath before task execution and add signatures at this time (after dependencies are resolved)

--- buildSrc/src/main/groovy/org/apache/lucene/gradle/LuceneSolrForbiddenApisPlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/lucene/gradle/LuceneSolrForbiddenApisPlugin.groovy
@@ -21,20 +21,26 @@ class LuceneSolrForbiddenApisPlugin implements Plugin<Project> {
     project.tasks.withType(CheckForbiddenApis) { task ->
       task.bundledSignatures = ['jdk-unsafe', 'jdk-deprecated', 'jdk-non-portable', 'jdk-reflection']
       task.signaturesURLs = [ getClass().getResource('/forbidden/base.txt') ]
+      
       if (task.name.endsWith('Test') || project.name ==~ /.*?\btest-framework/) {
         task.signaturesURLs += getClass().getResource('/forbidden/tests.txt')
       } else {
         task.bundledSignatures += 'jdk-system-out'
       }
+      
       if (project.group ==~ /.*?\.lucene(?:\.\w+)?/) {
         task.signaturesURLs += getClass().getResource('/forbidden/lucene.txt')
       } else if (project.group ==~ /.*?\.solr(?:\.\w+)?/) {
-        task.failOnUnresolvableSignatures = false;
         task.signaturesURLs += getClass().getResource((project.name == 'solrj') ? '/forbidden/solrj.txt' : '/forbidden/solr.txt')
-        task.signaturesURLs += getClass().getResource('/forbidden/servlet-api.txt')
         task.bundledSignatures += [ 'commons-io-unsafe-' + COMMONS_IO_VERSION ]
+        
+        // we delay adding the servlet-api checks until we figured out that we have a servlet-api.jar on forbidden's classpath:
+        task.doFirst{
+          if (task.classpath.filter { it.name ==~ /.*?\bservlet-api\b.*?\.jar/ }.empty == false) {
+            task.signaturesURLs += getClass().getResource('/forbidden/servlet-api.txt')
+          }
+        }
       }
-      //println "forbidden config of " + task.name + " group " + project.group + " name:" + project.name + " sigs: " + task.signaturesURLs
     }
   }
   
