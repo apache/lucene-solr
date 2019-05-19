1afac60d5b832058a7dcc344e80d573ee7462d4e
Uwe Schindler <uschindler@apache.org>
2019-05-19 22:48:30 +0200

    SOLR-13452: Cleanup of forbiddenapis to not hack around Gradle's problems, add ideas for servlet-apis.

--- buildSrc/src/main/groovy/org/apache/lucene/gradle/LuceneSolrForbiddenApisPlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/lucene/gradle/LuceneSolrForbiddenApisPlugin.groovy
@@ -11,31 +11,31 @@ class LuceneSolrForbiddenApisPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.pluginManager.apply(ForbiddenApisPlugin.class)
         project.forbiddenApis {
-            failOnUnsupportedJava = false
-            bundledSignatures = ['jdk-unsafe', 'jdk-deprecated', 'jdk-non-portable', 'jdk-reflection']
-            signaturesURLs = [getClass().getResource('/forbidden/base.txt')]
-            suppressAnnotations = ['**.SuppressForbidden']
+          failOnUnsupportedJava = false
+          suppressAnnotations = ['**.SuppressForbidden']
         }
 
-        project.tasks.withType(CheckForbiddenApis) {
-            if (name.endsWith('Test') || name.startsWith('Test')) {
-                signaturesURLs = project.forbiddenApis.signaturesURLs +
-                        [getClass().getResource('/forbidden/tests.txt')]
+        project.tasks.withType(CheckForbiddenApis) { task ->
+            bundledSignatures = ['jdk-unsafe', 'jdk-deprecated', 'jdk-non-portable', 'jdk-reflection']
+            signaturesURLs = [ getClass().getResource('/forbidden/base.txt') ]
+            if (task.name.endsWith('Test')) {
+              task.signaturesURLs += getClass().getResource('/forbidden/tests.txt')
             }
             if (project.group.matches(".*?\\.lucene(?:\\.\\w+)?")) {
-                signaturesURLs = project.forbiddenApis.signaturesURLs +
-                        [getClass().getResource('/forbidden/lucene.txt')]
+              task.signaturesURLs += getClass().getResource('/forbidden/lucene.txt')
+            } else if (project.name.equals("solrj")) {
+              signaturesURLs += getClass().getResource('/forbidden/solrj.txt')
+            } else if (project.group.matches(".*?\\.solr(?:\\.\\w+)?")) {
+              task.signaturesURLs += getClass().getResource('/forbidden/solr.txt')
+              // TODO:
+              task.signaturesURLs += getClass().getResource('/forbidden/servlet-api.txt')
+              /* something like this would also work for commons-io - does not yet fully work
+              project.configurations.compileOnly.allDependencies.matching { it.name == 'javax.servlet-api' }.all {
+                  task.signaturesURLs += getClass().getResource('/forbidden/servlet-api.txt')
+              }
+              */
             }
-            if (project.group.matches(".*?\\.solr(?:\\.\\w+)?")) {
-                 signaturesURLs = project.forbiddenApis.signaturesURLs +
-                        [getClass().getResource('/forbidden/solr.txt'), getClass().getResource('/forbidden/servlet-api.txt')]
-            }
-            
-            if (project.name.equals("solrj")) {
-              signaturesURLs = project.forbiddenApis.signaturesURLs +
-                      [getClass().getResource('/forbidden/solrj.txt')]
-            }
-            //println "group " + project.group + " name:" + project.name + " sigs: " + signaturesURLs
+            println "group " + project.group + " name:" + project.name + " sigs: " + task.signaturesURLs
         }
     }
 }
