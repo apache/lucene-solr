3be713bcbb88a0c424b2b248323846e55e753d03
markrmiller <markrmiller@apache.org>
2019-06-02 10:38:41 -0500

    SOLR-13452: Don't include internal module outputs in jarChecksums.

--- buildSrc/src/main/groovy/org/apache/lucene/gradle/JarChecksum.groovy
+++ buildSrc/src/main/groovy/org/apache/lucene/gradle/JarChecksum.groovy
@@ -15,6 +15,10 @@ package org.apache.lucene.gradle
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import org.gradle.api.artifacts.ResolvedArtifact
+
+import javax.inject.Inject
 import org.gradle.api.DefaultTask
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.file.RelativePath
@@ -29,43 +33,65 @@ import java.nio.file.Files
 
 class JarChecksum extends DefaultTask {
   
-  @InputDirectory
-  File inputDir
+  private File inputDir
   
-  @OutputDirectory
-  File target
+  private File target
   
-  @TaskAction
-  void checksums() {
-    def skip = ['testRuntimeClasspath', 'testCompileClasspath']
+  @Inject
+  public JarChecksum(File inputDir,  File target) {
     
-    project.delete project.fileTree(dir: target.getAbsolutePath(), include: '**/*.jar.sha1')
     
-    File tmpDir = File.createTempDir()
-    tmpDir.deleteOnExit()
-    tmpDir.mkdirs()
-    
-    project.allprojects.each { p ->
-      p.configurations.each { Configuration config ->
-        if (skip.contains(config.name)) {  
-          config.files.each {
-            File destFile = new File(tmpDir, it.name)
-            if (it.name.endsWith(".jar") && it.exists() && !destFile.exists()) {
-              Files.copy(it.toPath(), destFile.toPath());
+    doLast({
+      File tmpDir = File.createTempDir()
+      tmpDir.deleteOnExit()
+      tmpDir.mkdirs()
+      
+      List<File> deps = new ArrayList<>()
+      project.allprojects.each { p ->
+        p.configurations.each { Configuration config ->
+          if (config.isCanBeResolved()) {
+            config.getResolvedConfiguration().getResolvedArtifacts().forEach( { ra -> deps.add(ra.getFile()) })
+            // exclude our jar and jarTest outputs
+            def ourJars = new ArrayList()
+            project.rootProject.subprojects.each{ subproject ->
+              if (subproject.hasProperty('jar')) {
+                ourJars.addAll(subproject.jar.outputs.files)
+                if (subproject.hasProperty('jarTest')) {
+                  ourJars.addAll(subproject.jarTest.outputs.files)
+                }
+              }
+            }
+            deps = deps - ourJars
+            
+            // copy files to tmp dir
+            deps.each {
+              def file = it
+              File destFile = new File(tmpDir, file.name)
+              if (file.name.endsWith(".jar") && file.exists() && !destFile.exists()) {
+                Files.copy(file.toPath(), destFile.toPath())
+              }
             }
           }
         }
-        
       }
-    }
+      
+      project.delete project.fileTree(dir: target.getAbsolutePath(), include: '**/*.jar.sha1')
+      
+      ant.checksum(algorithm: "SHA1", fileext: ".sha1", todir: target.getAbsolutePath()) {
+        ant.fileset(dir: tmpDir.getAbsolutePath())
+      }
+      
+      project.delete(tmpDir)
+      
+      ant.fixcrlf(srcdir: target.getAbsolutePath(), includes: "**/*.jar.sha1", eol: "lf", fixlast: "true", encoding: "US-ASCII")
+      
+    })
     
-    ant.checksum(algorithm: "SHA1", fileext: ".sha1", todir: target.getAbsolutePath()) {
-      ant.fileset(dir: tmpDir.getAbsolutePath())
-    }
+  }
+  
+  @TaskAction
+  void checksums() {
     
-    project.delete(tmpDir)
-    
-    ant.fixcrlf(srcdir: target.getAbsolutePath(), includes: "**/*.jar.sha1", eol: "lf", fixlast: "true", encoding: "US-ASCII")
   }
 }
 
