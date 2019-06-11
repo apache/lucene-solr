142a20bb0b24b657da8e267a5ebe205f35dc0637
Alan Woodward <romseygeek@apache.org>
2019-06-11 09:56:10 +0100

    LUCENE-8843: Fix precommit

--- lucene/core/src/java/org/apache/lucene/util/IOUtils.java
+++ lucene/core/src/java/org/apache/lucene/util/IOUtils.java
@@ -18,7 +18,6 @@ package org.apache.lucene.util;
 
 import java.io.BufferedReader;
 import java.io.Closeable;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
