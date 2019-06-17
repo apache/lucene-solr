163853083c7b4375c83153f3c03395692fdd20f7
Julie Tibshirani <julie.tibshirani@elastic.co>
2019-06-17 11:28:23 -0700

    Undo all changes to SortedNumericDocValuesRangeQuery.

--- lucene/core/src/java/org/apache/lucene/document/SortedNumericDocValuesRangeQuery.java
+++ lucene/core/src/java/org/apache/lucene/document/SortedNumericDocValuesRangeQuery.java
@@ -27,36 +27,15 @@ import org.apache.lucene.index.NumericDocValues;
 import org.apache.lucene.index.SortedNumericDocValues;
 import org.apache.lucene.search.ConstantScoreScorer;
 import org.apache.lucene.search.ConstantScoreWeight;
-import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.DocValuesFieldExistsQuery;
-import org.apache.lucene.search.Explanation;
-import org.apache.lucene.search.FieldComparator;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.LeafFieldComparator;
-import org.apache.lucene.search.Matches;
-import org.apache.lucene.search.MatchesUtils;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.QueryVisitor;
 import org.apache.lucene.search.ScoreMode;
 import org.apache.lucene.search.Scorer;
-import org.apache.lucene.search.Sort;
-import org.apache.lucene.search.SortField;
 import org.apache.lucene.search.TwoPhaseIterator;
 import org.apache.lucene.search.Weight;
 
-/**
- * A range query that executes using {@link SortedNumericDocValues}.
- *
- * By default, the query uses a {@link TwoPhaseIterator} where the leading iterator scans
- * over all doc values, and each document's values are checked against the range bounds.
- *
- * When the index is sorted on the same field as the query, we can instead perform binary search
- * on the field's doc values to find the documents at the lower and upper ends of the range. This
- * optimized execution strategy is only used if the following conditions hold:
- *   - The segment is sorted, and its primary sort is on the same field as the query.
- *   - The segments must have at most one field value per document (otherwise we cannot easily
- *     determine the matching document IDs through a binary search).
- */
 abstract class SortedNumericDocValuesRangeQuery extends Query {
 
   private final String field;
@@ -126,30 +105,8 @@ abstract class SortedNumericDocValuesRangeQuery extends Query {
     return new ConstantScoreWeight(this, boost) {
 
       @Override
-      public Explanation explain(LeafReaderContext context, int doc) throws IOException {
-        boolean exists = docMatches(context, doc);
-        if (exists) {
-          String explanation = getQuery().toString() + (score() == 1f ? "" : "^" + score());
-          return Explanation.match(score(), explanation);
-        } else {
-          return Explanation.noMatch(getQuery().toString() + " doesn't match id " + doc);
-        }
-      }
-
-      @Override
-      public Matches matches(LeafReaderContext context, int doc) throws IOException {
-        boolean matches = docMatches(context, doc);
-        return matches ? MatchesUtils.MATCH_WITH_NO_TERMS : null;
-      }
-
-      private boolean docMatches(LeafReaderContext context, int doc) throws IOException {
-        SortedNumericDocValues values = getValues(context.reader(), field);
-        if (values == null) {
-          return false;
-        } else {
-          TwoPhaseIterator iterator = getTwoPhaseIterator(values);
-          return iterator.approximation().advance(doc) == doc && iterator.matches();
-        }
+      public boolean isCacheable(LeafReaderContext ctx) {
+        return DocValues.isCacheable(ctx, field);
       }
 
       @Override
@@ -158,200 +115,46 @@ abstract class SortedNumericDocValuesRangeQuery extends Query {
         if (values == null) {
           return null;
         }
-
         final NumericDocValues singleton = DocValues.unwrapSingleton(values);
+        final TwoPhaseIterator iterator;
         if (singleton != null) {
-          Sort indexSort = context.reader().getMetaData().getSort();
-          if (indexSort != null
-              && indexSort.getSort().length > 0
-              && indexSort.getSort()[0].getField().equals(field)) {
-
-            SortField sortField = indexSort.getSort()[0];
-            DocIdSetIterator disi = getDocIdSetIterator(sortField, context, singleton);
-            return new ConstantScoreScorer(this, score(), scoreMode, disi);
-          }
-        }
-
-        TwoPhaseIterator twoPhaseIterator = getTwoPhaseIterator(values);
-        return new ConstantScoreScorer(this, score(), scoreMode, twoPhaseIterator);
-      }
-
-      @Override
-      public boolean isCacheable(LeafReaderContext ctx) {
-        return DocValues.isCacheable(ctx, field);
-      }
-    };
-  }
-
-  private TwoPhaseIterator getTwoPhaseIterator(SortedNumericDocValues values) {
-    final NumericDocValues singleton = DocValues.unwrapSingleton(values);
-    if (singleton != null) {
-      return new TwoPhaseIterator(singleton) {
-        @Override
-        public boolean matches() throws IOException {
-          final long value = singleton.longValue();
-          return value >= lowerValue && value <= upperValue;
-        }
-
-        @Override
-        public float matchCost() {
-          return 2; // 2 comparisons
-        }
-      };
-    } else {
-      return new TwoPhaseIterator(values) {
-        @Override
-        public boolean matches() throws IOException {
-          for (int i = 0, count = values.docValueCount(); i < count; ++i) {
-            final long value = values.nextValue();
-            if (value < lowerValue) {
-              continue;
+          iterator = new TwoPhaseIterator(singleton) {
+            @Override
+            public boolean matches() throws IOException {
+              final long value = singleton.longValue();
+              return value >= lowerValue && value <= upperValue;
             }
-            // Values are sorted, so the first value that is >= lowerValue is our best candidate
-            return value <= upperValue;
-          }
-          return false; // all values were < lowerValue
+
+            @Override
+            public float matchCost() {
+              return 2; // 2 comparisons
+            }
+          };
+        } else {
+          iterator = new TwoPhaseIterator(values) {
+            @Override
+            public boolean matches() throws IOException {
+              for (int i = 0, count = values.docValueCount(); i < count; ++i) {
+                final long value = values.nextValue();
+                if (value < lowerValue) {
+                  continue;
+                }
+                // Values are sorted, so the first value that is >= lowerValue is our best candidate
+                return value <= upperValue;
+              }
+              return false; // all values were < lowerValue
+            }
+
+            @Override
+            public float matchCost() {
+              return 2; // 2 comparisons
+            }
+          };
         }
-
-        @Override
-        public float matchCost() {
-          return 2; // 2 comparisons
-        }
-      };
-    }
-  }
-
-  /**
-   * Computes the document IDs that lie within the range [lowerValue, upperValue] by
-   * performing binary search on the field's doc values.
-   *
-   * Because doc values only allow forward iteration, we need to reload the field comparator
-   * every time the binary search accesses an earlier element.
-   *
-   * We must also account for missing values when performing the binary search. For this
-   * reason, we load the {@link FieldComparator} instead of checking the docvalues directly.
-   * The returned {@link DocIdSetIterator} makes sure to wrap the original docvalues to skip
-   * over documents with no value.
-   */
-  private DocIdSetIterator getDocIdSetIterator(SortField sortField,
-                                               LeafReaderContext context,
-                                               DocIdSetIterator delegate) throws IOException {
-    int maxDoc = context.reader().maxDoc();
-    if (maxDoc <= 0) {
-      return DocIdSetIterator.empty();
-    }
-
-    long lower = sortField.getReverse() ? upperValue : lowerValue;
-    long upper = sortField.getReverse() ? lowerValue : upperValue;
-
-    // Perform a binary search to find the first document with value >= lower.
-    ValueComparator comparator = loadComparator(sortField, lower, context);
-    int low = 0;
-    int high = maxDoc - 1;
-
-    while (low <= high) {
-      int mid = (low + high) >>> 1;
-      if (comparator.compare(mid) <= 0) {
-        high = mid - 1;
-        comparator = loadComparator(sortField, lower, context);
-      } else {
-        low = mid + 1;
+        return new ConstantScoreScorer(this, score(), scoreMode, iterator);
       }
-    }
-    int firstDocIdInclusive = high + 1;
 
-    // Perform a binary search to find the first document with value > upper.
-    // Since we know that upper >= lower, we can initialize the lower bound
-    // of the binary search to the result of the previous search.
-    comparator = loadComparator(sortField, upper, context);
-    low = firstDocIdInclusive;
-    high = maxDoc - 1;
-
-    while (low <= high) {
-      int mid = (low + high) >>> 1;
-      if (comparator.compare(mid) < 0) {
-        high = mid - 1;
-        comparator = loadComparator(sortField, upper, context);
-      } else {
-        low = mid + 1;
-      }
-    }
-
-    int lastDocIdExclusive = high + 1;
-    return new BoundedDocSetIdIterator(firstDocIdInclusive, lastDocIdExclusive, delegate);
-  }
-
-  /**
-   * Compares the given document's value with a stored reference value.
-   */
-  private interface ValueComparator {
-    int compare(int docID) throws IOException;
-  }
-
-  private static ValueComparator loadComparator(SortField sortField,
-                                                long topValue,
-                                                LeafReaderContext context) throws IOException {
-    @SuppressWarnings("unchecked")
-    FieldComparator<Long> fieldComparator = (FieldComparator<Long>) sortField.getComparator(1, 0);
-    fieldComparator.setTopValue(topValue);
-
-    LeafFieldComparator leafFieldComparator = fieldComparator.getLeafComparator(context);
-    int direction = sortField.getReverse() ? -1 : 1;
-
-    return doc -> {
-      int value = leafFieldComparator.compareTop(doc);
-      return direction * value;
     };
-  }
-
-  /**
-   * A doc ID set iterator that wraps a delegate iterator and only returns doc IDs in
-   * the range [firstDocInclusive, lastDoc).
-   */
-  private static class BoundedDocSetIdIterator extends DocIdSetIterator {
-    private final int firstDoc;
-    private final int lastDoc;
-    private final DocIdSetIterator delegate;
-
-    private int docID = -1;
-
-    BoundedDocSetIdIterator(int firstDoc,
-                            int lastDoc,
-                            DocIdSetIterator delegate) {
-      this.firstDoc = firstDoc;
-      this.lastDoc = lastDoc;
-      this.delegate = delegate;
-    }
-
-    @Override
-    public int docID() {
-      return docID;
-    }
-
-    @Override
-    public int nextDoc() throws IOException {
-      return advance(docID + 1);
-    }
-
-    @Override
-    public int advance(int target) throws IOException {
-      if (target < firstDoc) {
-        target = firstDoc;
-      }
-
-      int result = delegate.advance(target);
-      if (result < lastDoc) {
-        docID = result;
-        return docID;
-      } else {
-        return NO_MORE_DOCS;
-      }
-    }
-
-    @Override
-    public long cost() {
-      return lastDoc - firstDoc;
-    }
   }
 
 }
