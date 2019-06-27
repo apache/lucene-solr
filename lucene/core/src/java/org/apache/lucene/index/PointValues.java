58b81f7f1bd8072a8f04beefad2d2609677412a2
iverase <ivera@apache.org>
2019-06-27 14:32:18 +0200

    Add more javadocs

--- lucene/core/src/java/org/apache/lucene/index/PointValues.java
+++ lucene/core/src/java/org/apache/lucene/index/PointValues.java
@@ -209,7 +209,8 @@ public abstract class PointValues {
     void visit(int docID, byte[] packedValue) throws IOException;
 
     /** Similar to {@link IntersectVisitor#visit(int, byte[])} but in this case the packedValue
-     * can have more than one docID associated to it. */
+     * can have more than one docID associated to it. The provided iterator should not escape the
+     * scope of this method so that implementations of PointValues are free to reuse it,*/
     default void visit(DocIdSetIterator iterator, byte[] packedValue) throws IOException {
       int docID;
       while ((docID = iterator.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
