51f43407966955cd1dd443b73ff30c976b2d6542
iverase <ivera@apache.org>
2019-07-11 12:47:34 +0200

    Add next minor version 8.3.0

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -154,11 +154,18 @@ public final class Version {
 
   /**
    * Match settings and bugs in Lucene's 8.2.0 release.
+   * @deprecated (8.3.0) Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_8_2_0 = new Version(8, 2, 0);
+
+  /**
+   * Match settings and bugs in Lucene's 8.3.0 release.
    * <p>
    * Use this to get the latest &amp; greatest settings, bug
    * fixes, etc, for Lucene.
    */
-  public static final Version LUCENE_8_2_0 = new Version(8, 2, 0);
+  public static final Version LUCENE_8_3_0 = new Version(8, 3, 0);
 
   // To add a new version:
   //  * Only add above this comment
@@ -179,7 +186,7 @@ public final class Version {
    * some defaults may have changed and may break functionality 
    * in your application.
    */
-  public static final Version LATEST = LUCENE_8_2_0;
+  public static final Version LATEST = LUCENE_8_3_0;
 
   /**
    * Constant for backwards compatibility.
