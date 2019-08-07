c09ddcb7180ccc4abaef25f654ffc053421353a4
nppoly <nppoly@foxmail.com>
2019-08-07 10:35:55 +0200

    LUCENE-8755: Spatial-extras quad and packed-quad trees now index  points a little faster, and also fix an edge case bug.  Fixes #824
    (cherry picked from commit 26628b2717a73235db56fde94f7f5b64cbc5b8b2)

--- solr/core/src/test/org/apache/solr/search/TestSolr4Spatial2.java
+++ solr/core/src/test/org/apache/solr/search/TestSolr4Spatial2.java
@@ -61,6 +61,13 @@ public class TestSolr4Spatial2 extends SolrTestCaseJ4 {
   }
 
   @Test
+  public void testQuadTreeRobustness() {
+    assertU(adoc("id", "0", "oslocation", "244502.06 639062.07"));
+    // old (pre 8.3.0) still works
+    assertU(adoc("id", "0", "oslocationold", "244502.06 639062.07"));
+  }
+
+  @Test
   public void testBBox() throws Exception {
     String fieldName = random().nextBoolean() ? "bbox" : "bboxD_dynamic";
     assertU(adoc("id", "0"));//nothing
