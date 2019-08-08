299d92da5cc6315a98ef656a66ab7b285ecb4e3d
Jason Gerlowski <gerlowskija@apache.org>
2019-08-07 21:17:48 -0400

    SOLR-13622: Rename FilesStream -> CatStream
    Also fixes an 'cat' OS-dependent bug in StreamExpressionTest.

--- solr/core/src/java/org/apache/solr/handler/SolrDefaultStreamFactory.java
+++ solr/core/src/java/org/apache/solr/handler/SolrDefaultStreamFactory.java
@@ -34,7 +34,7 @@ public class SolrDefaultStreamFactory extends DefaultStreamFactory {
   public SolrDefaultStreamFactory() {
     super();
     this.withFunctionName("analyze",  AnalyzeEvaluator.class);
-    this.withFunctionName("files", FilesStream.class);
+    this.withFunctionName("cat", CatStream.class);
     this.withFunctionName("classify", ClassifyStream.class);
     this.withFunctionName("haversineMeters", HaversineMetersEvaluator.class);
   }
