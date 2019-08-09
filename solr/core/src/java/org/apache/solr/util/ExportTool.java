acc3e47218c11a26839ad912e600294db2d0fda8
noble <noble@apache.org>
2019-08-09 14:41:25 +1000

    SOLR-13682: refactored and cleaned up

--- solr/core/src/java/org/apache/solr/util/ExportTool.java
+++ solr/core/src/java/org/apache/solr/util/ExportTool.java
@@ -20,14 +20,14 @@ package org.apache.solr.util;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Consumer;
+import java.util.Set;
 
+import com.google.common.collect.ImmutableSet;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
@@ -74,11 +74,16 @@ public class ExportTool extends SolrCLI.ToolBase {
 
   }
 
+  static Set<String> formats = ImmutableSet.of(JAVABIN, "jsonl");
+
   @Override
   protected void runImpl(CommandLine cli) throws Exception {
     Info info = new Info();
     String url = cli.getOptionValue("url");
     info.format = cli.getOptionValue("format", "jsonl");
+    if (!formats.contains(info.format)) {
+      throw new IllegalArgumentException("format must be one of :" + formats);
+    }
     info.query = cli.getOptionValue("query", "*:*");
     info.fields = cli.getOptionValue("fields");
     int idx = url.lastIndexOf('/');
@@ -86,129 +91,74 @@ public class ExportTool extends SolrCLI.ToolBase {
     info.coll = url.substring(idx + 1);
     info.out = cli.getOptionValue("out",
         JAVABIN.equals(info.format) ? info.coll + ".javabin" : info.coll + ".json");
+
     String maxDocsStr = cli.getOptionValue("limit", "100");
     info.limit = Long.parseLong(maxDocsStr);
     if (info.limit == -1) info.limit = Long.MAX_VALUE;
-    if (JAVABIN.equals(info.format)) {
-      writeJavabinDocs(info);
-    } else {
-      writeJsonLDocs(info);
-    }
+    DocsSink sink = JAVABIN.equals(info.format) ? new JavabinSink(info) : new JsonSink(info);
+    streamDocsWithCursorMark(info, sink);
   }
 
-  public static void writeJsonLDocs(Info info) throws IOException, SolrServerException {
+  interface DocsSink {
+    void start() throws IOException;
+
+    void accept(SolrDocument document) throws IOException;
+
+    void end() throws IOException;
+  }
+
+  private static void streamDocsWithCursorMark(Info info,
+                                               DocsSink sink) throws SolrServerException, IOException {
     SolrClient solrClient = new CloudSolrClient.Builder(Collections.singletonList(info.baseurl)).build();
-    FileOutputStream fos = new FileOutputStream(info.out);
-    Writer writer = FastWriter.wrap(new OutputStreamWriter(fos));
-
-    SolrJSONWriter jsonw = new SolrJSONWriter(writer);
-    jsonw.setIndent(false);
-    Consumer<SolrDocument> consumer = doc -> {
-      try {
-        Map m = new LinkedHashMap(doc.size());
-        doc.forEach((s, field) -> {
-          if (s.equals("_version_")) return;
-          if (field instanceof List) {
-            if (((List) field).size() == 1) {
-              field = ((List) field).get(0);
-            }
-          }
-          m.put(s, field);
-        });
-        jsonw.writeObj(m);
-        writer.flush();
-        writer.append('\n');
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-    };
-
-    try {
-      streamDocsWithCursorMark(info, solrClient, consumer);
-    } finally {
-      jsonw.close();
-      solrClient.close();
-      fos.close();
-    }
-  }
-
-  public static void writeJavabinDocs(Info info) throws IOException, SolrServerException {
-    SolrClient solrClient = new CloudSolrClient.Builder(Collections.singletonList(info.baseurl)).build();
-    FileOutputStream fos = new FileOutputStream(info.out);
-    JavaBinCodec codec = new JavaBinCodec(fos, null);
-    codec.writeTag(JavaBinCodec.NAMED_LST, 2);
-    codec.writeStr("params");
-    codec.writeNamedList(new NamedList<>());
-    codec.writeStr("docs");
-    codec.writeTag(JavaBinCodec.ITERATOR);
-    long[] docsWritten = new long[1];
-    docsWritten[0] = 0;
-    Consumer<SolrDocument> consumer = doc -> {
-      try {
-        SolrInputDocument document = new SolrInputDocument();
-        doc.forEach((s, o) -> {
-          if (s.equals("_version_")) return;
-          if (o instanceof List) {
-            if (((List) o).size() == 1) o = ((List) o).get(0);
-          }
-          document.addField(s, o);
-        });
-
-        codec.writeSolrInputDocument(document);
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-    };
-    try {
-      streamDocsWithCursorMark(info, solrClient, consumer);
-    } finally {
-      codec.writeTag(JavaBinCodec.END);
-      solrClient.close();
-      codec.close();
-      fos.close();
-    }
-  }
-
-  private static void streamDocsWithCursorMark(Info info, SolrClient solrClient,
-                                               Consumer<SolrDocument> consumer) throws SolrServerException, IOException {
     long[] docsWritten = new long[]{0};
     NamedList<Object> rsp1 = solrClient.request(new GenericSolrRequest(SolrRequest.METHOD.GET, "/schema/uniquekey",
         new MapSolrParams(Collections.singletonMap("collection", info.coll))));
     String uniqueKey = (String) rsp1.get("uniqueKey");
-    NamedList<Object> rsp;
-    SolrQuery q = (new SolrQuery(info.query))
-        .setParam("collection", info.coll)
-        .setRows(100)
-        .setSort(SolrQuery.SortClause.asc(uniqueKey));
-    if (info.fields != null) {
-      q.setParam(FL, info.fields);
-    }
 
-    String cursorMark = CursorMarkParams.CURSOR_MARK_START;
-    boolean done = false;
-    while (!done) {
-      if (docsWritten[0] >= info.limit) break;
-      QueryRequest request = new QueryRequest(q);
-
-      request.setResponseParser(new StreamingBinaryResponseParser(new StreamingResponseCallback() {
-        @Override
-        public void streamSolrDocument(SolrDocument doc) {
-          consumer.accept(doc);
-          docsWritten[0]++;
-        }
-
-        @Override
-        public void streamDocListInfo(long numFound, long start, Float maxScore) {
-
-        }
-      }));
-      q.set(CursorMarkParams.CURSOR_MARK_PARAM, cursorMark);
-      rsp = solrClient.request(request);
-      String nextCursorMark = (String) rsp.get(CursorMarkParams.CURSOR_MARK_NEXT);
-      if (nextCursorMark == null || Objects.equals(cursorMark, nextCursorMark)) {
-        break;
+    sink.start();
+    try {
+      NamedList<Object> rsp;
+      SolrQuery q = (new SolrQuery(info.query))
+          .setParam("collection", info.coll)
+          .setRows(100)
+          .setSort(SolrQuery.SortClause.asc(uniqueKey));
+      if (info.fields != null) {
+        q.setParam(FL, info.fields);
       }
-      cursorMark = nextCursorMark;
+
+      String cursorMark = CursorMarkParams.CURSOR_MARK_START;
+      boolean done = false;
+      while (!done) {
+        if (docsWritten[0] >= info.limit) break;
+        QueryRequest request = new QueryRequest(q);
+
+        request.setResponseParser(new StreamingBinaryResponseParser(new StreamingResponseCallback() {
+          @Override
+          public void streamSolrDocument(SolrDocument doc) {
+            try {
+              sink.accept(doc);
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+            docsWritten[0]++;
+          }
+
+          @Override
+          public void streamDocListInfo(long numFound, long start, Float maxScore) {
+
+          }
+        }));
+        q.set(CursorMarkParams.CURSOR_MARK_PARAM, cursorMark);
+        rsp = solrClient.request(request);
+        String nextCursorMark = (String) rsp.get(CursorMarkParams.CURSOR_MARK_NEXT);
+        if (nextCursorMark == null || Objects.equals(cursorMark, nextCursorMark)) {
+          break;
+        }
+        cursorMark = nextCursorMark;
+      }
+    } finally {
+      sink.end();
+
     }
   }
 
@@ -244,4 +194,94 @@ public class ExportTool extends SolrCLI.ToolBase {
           .withDescription("Comma separated fields. By default all fields are fetched")
           .create("fields")
   };
+
+  private static class JsonSink implements DocsSink {
+    private final Info info;
+    private SolrJSONWriter jsonw;
+    private FastWriter writer;
+    private FileOutputStream fos;
+
+    public JsonSink(Info info) {
+      this.info = info;
+    }
+
+    @Override
+    public void start() throws IOException {
+      fos = new FileOutputStream(info.out);
+      writer = FastWriter.wrap(new OutputStreamWriter(fos));
+      jsonw = new SolrJSONWriter(writer);
+      jsonw.setIndent(false);
+
+    }
+
+    @Override
+    public void end() throws IOException {
+      jsonw.close();
+      fos.close();
+
+    }
+
+    @Override
+    public void accept(SolrDocument doc) throws IOException {
+      Map m = new LinkedHashMap(doc.size());
+      doc.forEach((s, field) -> {
+        if (s.equals("_version_")) return;
+        if (field instanceof List) {
+          if (((List) field).size() == 1) {
+            field = ((List) field).get(0);
+          }
+        }
+        m.put(s, field);
+      });
+      jsonw.writeObj(m);
+      writer.flush();
+      writer.append('\n');
+
+    }
+  }
+
+  private static class JavabinSink implements DocsSink {
+    private final Info info;
+    JavaBinCodec codec;
+    FileOutputStream fos;
+
+    public JavabinSink(Info info) {
+      this.info = info;
+    }
+
+    @Override
+    public void start() throws IOException {
+      fos = new FileOutputStream(info.out);
+      codec = new JavaBinCodec(fos, null);
+      codec.writeTag(JavaBinCodec.NAMED_LST, 2);
+      codec.writeStr("params");
+      codec.writeNamedList(new NamedList<>());
+      codec.writeStr("docs");
+      codec.writeTag(JavaBinCodec.ITERATOR);
+
+    }
+
+    @Override
+    public void end() throws IOException {
+      codec.writeTag(JavaBinCodec.END);
+      codec.close();
+      fos.close();
+
+    }
+
+    @Override
+    public void accept(SolrDocument doc) throws IOException {
+      SolrInputDocument document = new SolrInputDocument();
+      doc.forEach((s, o) -> {
+        if (s.equals("_version_")) return;
+        if (o instanceof List) {
+          if (((List) o).size() == 1) o = ((List) o).get(0);
+        }
+        document.addField(s, o);
+      });
+
+      codec.writeSolrInputDocument(document);
+
+    }
+  }
 }
