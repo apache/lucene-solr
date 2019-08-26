86ab1e0497c582c2b3d4d7539479b2bdb82c6781
Namgyu Kim <namgyu@apache.org>
2019-08-27 03:09:03 +0900

    LUCENE-8954: refactor Nori analyzer
    Signed-off-by: Namgyu Kim <namgyu@apache.org>

--- lucene/analysis/nori/src/java/org/apache/lucene/analysis/ko/KoreanPartOfSpeechStopFilter.java
+++ lucene/analysis/nori/src/java/org/apache/lucene/analysis/ko/KoreanPartOfSpeechStopFilter.java
@@ -18,6 +18,7 @@ package org.apache.lucene.analysis.ko;
 
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -36,7 +37,7 @@ public final class KoreanPartOfSpeechStopFilter extends FilteringTokenFilter {
   /**
    * Default list of tags to filter.
    */
-  public static final Set<POS.Tag> DEFAULT_STOP_TAGS = Arrays.asList(
+  public static final Set<POS.Tag> DEFAULT_STOP_TAGS = new HashSet<>(Arrays.asList(
       POS.Tag.E,
       POS.Tag.IC,
       POS.Tag.J,
@@ -55,7 +56,7 @@ public final class KoreanPartOfSpeechStopFilter extends FilteringTokenFilter {
       POS.Tag.UNA,
       POS.Tag.NA,
       POS.Tag.VSV
-  ).stream().collect(Collectors.toSet());
+  ));
 
   /**
    * Create a new {@link KoreanPartOfSpeechStopFilter} with the default
