4e71226185113ad8a848fe9f155eb3368e5b73f6
Atri Sharma <atris@amazon.com>
2019-09-13 18:39:25 +0530

    Fix erratic camelcase

--- lucene/core/src/test/org/apache/lucene/search/TestTopDocsCollector.java
+++ lucene/core/src/test/org/apache/lucene/search/TestTopDocsCollector.java
@@ -110,7 +110,7 @@ public class TestTopDocsCollector extends LuceneTestCase {
     return tdc;
   }
 
-  private TopDocs doConcurrentSearchWithThreshold(int numResults, int thresHold) throws IOException {
+  private TopDocs doConcurrentSearchWithThreshold(int numResults, int threshold) throws IOException {
     Query q = new MatchAllDocsQuery();
     ExecutorService service = new ThreadPoolExecutor(4, 4, 0L, TimeUnit.MILLISECONDS,
         new LinkedBlockingQueue<Runnable>(),
@@ -118,7 +118,7 @@ public class TestTopDocsCollector extends LuceneTestCase {
     IndexSearcher searcher = new IndexSearcher(reader, service);
 
     CollectorManager collectorManager = TopScoreDocCollector.createSharedManager(numResults,
-        null, thresHold);
+        null, threshold);
 
     TopDocs tdc = (TopDocs) searcher.search(q, collectorManager);
 
