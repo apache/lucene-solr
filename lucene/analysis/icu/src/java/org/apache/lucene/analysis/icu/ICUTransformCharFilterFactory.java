948b5b5b41ac41d2c34a20594452d7fe44d36a47
Michael Gibney <michael@michaelgibney.net>
2019-10-02 13:25:16 -0400

    add ability to bypass i/o unicode normalization in ICUTransformCharFilter
    Many RuleBasedTransliterators do unicode normalization on input
    (usually decomposition) and output (usually composition). Since in the
    context of an analysis chain we'll probably be handling this externally,
    provide the ability to disable the unicode normalization baked into
    standard transforms. Necessitated dupication of some simple package-private
    code from ICU project, but the performance gains are significant for
    common use cases

--- lucene/analysis/icu/src/java/org/apache/lucene/analysis/icu/ICUTransformCharFilterFactory.java
+++ lucene/analysis/icu/src/java/org/apache/lucene/analysis/icu/ICUTransformCharFilterFactory.java
@@ -18,11 +18,12 @@ package org.apache.lucene.analysis.icu;
 
 import java.util.Arrays;
 import java.util.Map;
-
 import java.io.Reader;
 import org.apache.lucene.analysis.util.CharFilterFactory;
 
+import com.ibm.icu.impl.Utility;
 import com.ibm.icu.text.Transliterator;
+import com.ibm.icu.text.UTF16;
 
 /**
  * Factory for {@link ICUTransformCharFilter}.
@@ -54,7 +55,14 @@ public class ICUTransformCharFilterFactory extends CharFilterFactory {
     int dir = "forward".equals(direction) ? Transliterator.FORWARD : Transliterator.REVERSE;
     int tmpCapacityHint = getInt(args, "maxRollbackBufferCapacity", ICUTransformCharFilter.DEFAULT_MAX_ROLLBACK_BUFFER_CAPACITY);
     this.maxRollbackBufferCapacity = tmpCapacityHint == -1 ? Integer.MAX_VALUE : tmpCapacityHint;
-    transliterator = Transliterator.getInstance(id, dir);
+    boolean assumeExternalUnicodeNormalization = getBoolean(args, "assumeExternalUnicodeNormalization", false);
+    Transliterator stockTransliterator = Transliterator.getInstance(id, dir);
+    final String modifiedRules;
+    if (assumeExternalUnicodeNormalization || ((modifiedRules = modifyRules(false, stockTransliterator)) == null)) {
+      this.transliterator = stockTransliterator;
+    } else {
+      this.transliterator = Transliterator.createFromRules(stockTransliterator.getID(), modifiedRules, Transliterator.FORWARD);
+    }
     if (!args.isEmpty()) {
       throw new IllegalArgumentException("Unknown parameters: " + args);
     }
@@ -63,5 +71,118 @@ public class ICUTransformCharFilterFactory extends CharFilterFactory {
   @Override
   public Reader create(Reader input) {
     return new ICUTransformCharFilter(input, transliterator, maxRollbackBufferCapacity);
+  }
+
+  private static final char ID_DELIM = ';';
+
+  private static final String[] NORM_ID_ARR = new String[] {"NFC", "NFD", "NFKC", "NFKD", "FCC", "FCD"};
+  private static boolean isUnicodeNormalizationId(String id) {
+    if (id.indexOf(';') >= 0) {
+      // it's compound
+      return false;
+    }
+    if (id.startsWith("Any-")) {
+      id = id.substring("Any-".length());
+    }
+    for (String prefix : NORM_ID_ARR) {
+      if (id.startsWith(prefix)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static String modifyRules(boolean escapeUnprintable, Transliterator t) {
+    final Transliterator[] trans = t.getElements();
+    final int start;
+    final int limit;
+    if (trans.length == 1) {
+      return null;
+    } else {
+      final int lastIndex;
+      if (isUnicodeNormalizationId(trans[0].getID())) {
+        start = 1;
+        limit = isUnicodeNormalizationId(trans[lastIndex = trans.length - 1].getID()) ? lastIndex : trans.length;
+      } else if (isUnicodeNormalizationId(trans[lastIndex = trans.length - 1].getID())) {
+        start = 0;
+        limit = lastIndex;
+      } else {
+        return null;
+      }
+    }
+    // We do NOT call toRules() on our component transliterators, in
+    // general. If we have several rule-based transliterators, this
+    // yields a concatenation of the rules -- not what we want. We do
+    // handle compound RBT transliterators specially -- those for which
+    // compoundRBTIndex >= 0. For the transliterator at compoundRBTIndex,
+    // we do call toRules() recursively.
+    StringBuilder rulesSource = new StringBuilder();
+    if (t.getFilter() != null) {
+      // We might be a compound RBT and if we have a global
+      // filter, then emit it at the top.
+      rulesSource.append("::").append(t.getFilter().toPattern(escapeUnprintable)).append(ID_DELIM);
+    }
+    final int globalFilterEnd = rulesSource.length();
+    boolean hasAnonymousRBTs = false;
+    for (int i = start; i < limit; ++i) {
+      String rule;
+
+      // Anonymous RuleBasedTransliterators (inline rules and
+      // ::BEGIN/::END blocks) are given IDs that begin with
+      // "%Pass": use toRules() to write all the rules to the output
+      // (and insert "::Null;" if we have two in a row)
+      if (trans[i].getID().startsWith("%Pass")) {
+        hasAnonymousRBTs = true;
+        rule = trans[i].toRules(escapeUnprintable);
+        if (i > start && trans[i - 1].getID().startsWith("%Pass"))
+          rule = "::Null;" + rule;
+
+        // we also use toRules() on CompoundTransliterators (which we
+        // check for by looking for a semicolon in the ID)-- this gets
+        // the list of their child transliterators output in the right
+        // format
+      } else if (trans[i].getID().indexOf(';') >= 0) {
+        rule = trans[i].toRules(escapeUnprintable);
+
+        // for everything else, use baseToRules()
+      } else {
+        rule = baseToRules(escapeUnprintable, trans[i]);
+      }
+      _smartAppend(rulesSource, '\n');
+      rulesSource.append(rule);
+      _smartAppend(rulesSource, ID_DELIM);
+    }
+    return hasAnonymousRBTs ? rulesSource.toString() : rulesSource.substring(globalFilterEnd);
+  }
+
+  /**
+   * Append c to buf, unless buf is empty or buf already ends in c.
+   */
+  private static void _smartAppend(StringBuilder buf, char c) {
+    if (buf.length() != 0 &&
+        buf.charAt(buf.length() - 1) != c) {
+      buf.append(c);
+    }
+  }
+
+  public static  String baseToRules(boolean escapeUnprintable, Transliterator t) {
+    // The base class implementation of toRules munges the ID into
+    // the correct format. That is: foo => ::foo
+    // KEEP in sync with rbt_pars
+    if (escapeUnprintable) {
+      StringBuffer rulesSource = new StringBuffer();
+      String id = t.getID();
+      for (int i = 0; i < id.length();) {
+        int c = UTF16.charAt(id, i);
+        if (!Utility.escapeUnprintable(rulesSource, c)) {
+          UTF16.append(rulesSource, c);
+        }
+        i += UTF16.getCharCount(c);
+      }
+      rulesSource.insert(0, "::");
+      rulesSource.append(ID_DELIM);
+      return rulesSource.toString();
+    }
+    return "::" + t.getID() + ID_DELIM;
   }
 }
