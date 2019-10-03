92491618e9321d9675b579be4dc75a684a9edd42
yonik <yonik@apache.org>
2019-10-03 10:28:29 -0400

    SOLR-13815: add simple live split test to help debugging possible issue

--- solr/core/src/test/org/apache/solr/cloud/SplitShardTest.java
+++ solr/core/src/test/org/apache/solr/cloud/SplitShardTest.java
@@ -18,19 +18,32 @@
 package org.apache.solr.cloud;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.request.CollectionAdminRequest;
+import org.apache.solr.client.solrj.request.UpdateRequest;
+import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.cloud.DocCollection;
+import org.apache.solr.common.cloud.Replica;
 import org.apache.solr.common.cloud.Slice;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SplitShardTest extends SolrCloudTestCase {
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private final String COLLECTION_NAME = "splitshardtest-collection";
 
@@ -132,5 +145,129 @@ public class SplitShardTest extends SolrCloudTestCase {
     assertEquals("wrong range in s1_0", expected0, delta0);
     assertEquals("wrong range in s1_1", expected1, delta1);
   }
+
+
+  CloudSolrClient createCollection(String collectionName, int repFactor) throws Exception {
+
+      CollectionAdminRequest
+          .createCollection(collectionName, "conf", 1, repFactor)
+          .setMaxShardsPerNode(100)
+          .process(cluster.getSolrClient());
+
+    cluster.waitForActiveCollection(collectionName, 1, repFactor);
+
+    CloudSolrClient client = cluster.getSolrClient();
+    client.setDefaultCollection(collectionName);
+    return client;
+  }
+
+
+  long getNumDocs(CloudSolrClient client) throws Exception {
+    String collectionName = client.getDefaultCollection();
+    DocCollection collection = client.getZkStateReader().getClusterState().getCollection(collectionName);
+    Collection<Slice> slices = collection.getSlices();
+
+    long totCount = 0;
+    for (Slice slice : slices) {
+      if (!slice.getState().equals(Slice.State.ACTIVE)) continue;
+      long lastReplicaCount = -1;
+      for (Replica replica : slice.getReplicas()) {
+        SolrClient replicaClient = getHttpSolrClient(replica.getBaseUrl() + "/" + replica.getCoreName());
+        long numFound = 0;
+        try {
+          numFound = replicaClient.query(params("q", "*:*", "distrib", "false")).getResults().getNumFound();
+          log.info("Replica count=" + numFound + " for " + replica);
+        } finally {
+          replicaClient.close();
+        }
+        if (lastReplicaCount >= 0) {
+          assertEquals("Replica doc count for " + replica, lastReplicaCount, numFound);
+        }
+        lastReplicaCount = numFound;
+      }
+      totCount += lastReplicaCount;
+    }
+
+
+    long cloudClientDocs = client.query(new SolrQuery("*:*")).getResults().getNumFound();
+    assertEquals("Sum of shard count should equal distrib query doc count", totCount, cloudClientDocs);
+    return totCount;
+  }
+
+
+  void doLiveSplitShard(String collectionName, int repFactor) throws Exception {
+    final CloudSolrClient client = createCollection(collectionName, repFactor);
+
+    final AtomicBoolean doIndex = new AtomicBoolean(true);
+    final AtomicInteger docsIndexed = new AtomicInteger();
+    Thread indexThread = null;
+    try {
+      // start indexing client before we initiate a shard split
+      indexThread = new Thread(() -> {
+        while (doIndex.get()) {
+          try {
+            Thread.sleep(10);  // cap indexing rate at 100 docs per second...
+            int currDoc = docsIndexed.get();
+
+            // Try all docs in the same update request
+            UpdateRequest updateReq = new UpdateRequest();
+            updateReq.add(sdoc("id", "doc_" + currDoc));
+            UpdateResponse ursp = updateReq.commit(client, collectionName);
+            assertEquals(0, ursp.getStatus());  // for now, don't accept any failures
+            if (ursp.getStatus() == 0) {
+              docsIndexed.incrementAndGet();
+            }
+          } catch (Exception e) {
+            fail(e.getMessage());
+            break;
+          }
+        }
+      });
+      indexThread.start();
+
+      Thread.sleep(100);  // wait for a few docs to be indexed before invoking split
+      int docCount = docsIndexed.get();
+
+      CollectionAdminRequest.SplitShard splitShard = CollectionAdminRequest.splitShard(collectionName)
+          .setShardName("shard1");
+      splitShard.process(client);
+      waitForState("Timed out waiting for sub shards to be active.",
+          collectionName, activeClusterShape(2, 3*repFactor));  // 2 repFactor for the new split shards, 1 repFactor for old replicas
+
+      // make sure that docs were able to be indexed during the split
+      assertTrue(docsIndexed.get() > docCount);
+
+      Thread.sleep(100);  // wait for a few more docs to be indexed after split
+
+    } finally {
+      // shut down the indexer
+      doIndex.set(false);
+      if (indexThread != null) {
+        indexThread.join();
+      }
+    }
+
+    assertTrue(docsIndexed.get() > 0);
+
+    long numDocs = getNumDocs(client);
+    if (numDocs != docsIndexed.get()) {
+      // Find out what docs are missing.
+      for (int i = 0; i < docsIndexed.get(); i++) {
+        String id = "doc_" + i;
+        long cloudClientDocs = client.query(new SolrQuery("id:" + id)).getResults().getNumFound();
+        if (cloudClientDocs != 1) {
+          log.error("MISSING DOCUMENT " + id);
+        }
+      }
+    }
+
+    assertEquals("Documents are missing!", docsIndexed.get(), numDocs);
+  }
+
+  @Test
+  public void testLiveSplit() throws Exception {
+    doLiveSplitShard("livesplit1", 1);
+  }
+
 
 }
