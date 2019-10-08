b55035eefb7049609f5299e5b584ca4b1f9975bb
noble <noble@apache.org>
2019-10-08 14:18:48 +1100

    error message fixed

--- solr/solrj/src/java/org/apache/solr/common/util/Utils.java
+++ solr/solrj/src/java/org/apache/solr/common/util/Utils.java
@@ -763,7 +763,7 @@ public class Utils {
     int statusCode = rsp.getStatusLine().getStatusCode();
     if (statusCode != 200) {
       try {
-        log.error("Failed a request to : {} ,  status :{}  body {}", url, rsp.getStatusLine(), EntityUtils.toString(rsp.getEntity(), StandardCharsets.UTF_8));
+        log.error("Failed a request to: {}, status: {}, body: {}", url, rsp.getStatusLine(), EntityUtils.toString(rsp.getEntity(), StandardCharsets.UTF_8));
       } catch (IOException e) {
         log.error("could not print error", e);
       }
