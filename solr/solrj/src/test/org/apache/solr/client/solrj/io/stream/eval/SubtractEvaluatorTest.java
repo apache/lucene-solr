821d30604b091d9ef6f915544f4d2edcaa4cb8ef
Joel Bernstein <jbernste@apache.org>
2019-10-10 21:18:45 -0400

    SOLR-13829: RecursiveEvaluator casts Continuous numbers to Discrete Numbers, causing mismatch

--- solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/eval/SubtractEvaluatorTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/eval/SubtractEvaluatorTest.java
@@ -51,8 +51,7 @@ public class SubtractEvaluatorTest extends SolrTestCase {
     values.put("a", 1);
     values.put("b", 2);
     result = evaluator.evaluate(new Tuple(values));
-    Assert.assertTrue(result instanceof Long);
-    Assert.assertEquals(-1L, result);
+    Assert.assertEquals(-1D, result);
     
     values.clear();
     values.put("a", 1.1);
@@ -65,8 +64,7 @@ public class SubtractEvaluatorTest extends SolrTestCase {
     values.put("a", 1.1);
     values.put("b", 2.1);
     result = evaluator.evaluate(new Tuple(values));
-    Assert.assertTrue(result instanceof Long);
-    Assert.assertEquals(-1L, result);
+    Assert.assertEquals(-1D, result);
   }
 
   @Test(expected = IOException.class)
@@ -140,8 +138,7 @@ public class SubtractEvaluatorTest extends SolrTestCase {
     values.put("c", 3);
     values.put("d", 4);
     result = evaluator.evaluate(new Tuple(values));
-    Assert.assertTrue(result instanceof Long);
-    Assert.assertEquals(-8L, result);
+    Assert.assertEquals(-8D, result);
     
     values.clear();
     values.put("a", 1.1);
@@ -173,8 +170,7 @@ public class SubtractEvaluatorTest extends SolrTestCase {
     values.put("c", 3);
     values.put("d", 4);
     result = evaluator.evaluate(new Tuple(values));
-    Assert.assertTrue(result instanceof Long);
-    Assert.assertEquals(0L, result);
+    Assert.assertEquals(0D, result);
         
     values.clear();
     values.put("a", 123456789123456789L);
@@ -182,7 +178,6 @@ public class SubtractEvaluatorTest extends SolrTestCase {
     values.put("c", 123456789123456789L);
     values.put("d", 123456789123456789L);
     result = evaluator.evaluate(new Tuple(values));
-    Assert.assertTrue(result instanceof Long);
-    Assert.assertEquals(0L, result);
+    Assert.assertEquals(0D, result);
   }
 }
