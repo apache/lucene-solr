df15579279d72fff5bef288e603a73014cccefc3
Michael Gibney <michael@michaelgibney.net>
2019-10-25 17:01:05 -0400

    initial tests (Hoss's patch)

--- solr/core/src/test/org/apache/solr/search/function/SortByFunctionTest.java
+++ solr/core/src/test/org/apache/solr/search/function/SortByFunctionTest.java
@@ -15,7 +15,32 @@
  * limitations under the License.
  */
 package org.apache.solr.search.function;
-import org.apache.solr.SolrTestCaseJ4;
+
+import java.lang.invoke.MethodHandles;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.apache.lucene.util.TestUtil;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.embedded.JettySolrRunner;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.CollectionAdminRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.cloud.AbstractDistribZkTestBase;
+import org.apache.solr.cloud.SolrCloudTestCase;
+
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 
 
@@ -23,111 +48,131 @@ import org.junit.BeforeClass;
  *
  * @see TestSortByMinMaxFunction
  **/
-public class SortByFunctionTest extends SolrTestCaseJ4 {
+public class SortByFunctionTest extends SolrCloudTestCase {
 
+  private static final String DEBUG_LABEL = MethodHandles.lookup().lookupClass().getName();
+  private static final String COLLECTION_NAME = DEBUG_LABEL + "_collection";
+
+  /** A basic client for operations at the cloud level, default collection will be set */
+  private static CloudSolrClient CLOUD_CLIENT;
+  /** One client per node */
+  private static ArrayList<HttpSolrClient> CLIENTS = new ArrayList<>(5);
   
   @BeforeClass
   public static void beforeClass() throws Exception {
-    initCore("solrconfig.xml", "schema.xml");
-  }
+    // multi replicas should not matter...
+    final int repFactor = usually() ? 1 : 2;
+    // ... but we definitely want to test multiple shards
+    final int numShards = TestUtil.nextInt(random(), 1, (usually() ? 2 :3));
+    final int numNodes = (numShards * repFactor);
+   
+    final String configName = DEBUG_LABEL + "_config-set";
+    final Path configDir = Paths.get(TEST_HOME(), "collection1", "conf");
+    
+    configureCluster(numNodes).addConfig(configName, configDir).configure();
+    
+    Map<String, String> collectionProperties = new LinkedHashMap<>();
+    collectionProperties.put("config", "solrconfig.xml");
+    collectionProperties.put("schema", "schema.xml");
+    collectionProperties.put("solr.test.sys.prop1", "unused");
+    collectionProperties.put("solr.test.sys.prop2", "unused");
+    CollectionAdminRequest.createCollection(COLLECTION_NAME, configName, numShards, repFactor)
+        .setProperties(collectionProperties)
+        .process(cluster.getSolrClient());
 
+    CLOUD_CLIENT = cluster.getSolrClient();
+    CLOUD_CLIENT.setDefaultCollection(COLLECTION_NAME);
+
+    waitForRecoveriesToFinish(CLOUD_CLIENT);
+
+    for (JettySolrRunner jetty : cluster.getJettySolrRunners()) {
+      CLIENTS.add(getHttpSolrClient(jetty.getBaseUrl() + "/" + COLLECTION_NAME + "/"));
+    }
+  }
   
-  @Override
-  public void setUp() throws Exception {
-    super.setUp();
-    assertU(delQ("*:*"));
-    assertU(commit());
+  @AfterClass
+  private static void afterClass() throws Exception {
+    CLOUD_CLIENT.close(); CLOUD_CLIENT = null;
+    for (HttpSolrClient client : CLIENTS) {
+      client.close();
+    }
+    CLIENTS = null;
   }
 
+  @Before
+  public void clearCollection() throws Exception {
+    assertNull(CLOUD_CLIENT.deleteByQuery("*:*").getException());
+    assertNull(CLOUD_CLIENT.commit().getException());
+  }
+
+  /** macro */
+  public void assertAdd(SolrInputDocument doc) throws Exception {
+    assertNull(getRandClient(random()).add(doc).getException());
+  }
+  /** macro */
+  public void assertCommit() throws Exception {
+    assertNull(getRandClient(random()).commit().getException());
+  }
+
+  /** 
+   * given query params and a list of uniqueKey values, asserts that documents 
+   * returned by the request match the list of ids and are in the specified order.
+   */
+  public void assertSortedResults(SolrParams p, String... ids) throws Exception {
+    QueryResponse rsp = getRandClient(random()).query(p);
+    assertNull(rsp.getException());
+    SolrDocumentList results = rsp.getResults();
+    assertEquals(ids.length, results.size());
+    int i = 0;
+    for (String id : ids) {
+      assertEquals("#" + i, id, results.get(i).getFieldValue("id"));
+      i++;
+    }
+  }
+  
   public void test() throws Exception {
-    assertU(adoc("id", "1", "x_td1", "0", "y_td1", "2", "w_td1", "25", "z_td1", "5", "f_t", "ipod"));
-    assertU(adoc("id", "2", "x_td1", "2", "y_td1", "2", "w_td1", "15", "z_td1", "5", "f_t", "ipod ipod ipod ipod ipod"));
-    assertU(adoc("id", "3", "x_td1", "3", "y_td1", "2", "w_td1", "55", "z_td1", "5", "f_t", "ipod ipod ipod ipod ipod ipod ipod ipod ipod"));
-    assertU(adoc("id", "4", "x_td1", "4", "y_td1", "2", "w_td1", "45", "z_td1", "5", "f_t", "ipod ipod ipod ipod ipod ipod ipod"));
-    assertU(commit());
+    assertAdd(sdoc("id", "1", "x_td1", "0", "y_td1", "2", "w_td1", "25", "z_td1", "5", "f_t", "ipod"));
+    assertAdd(sdoc("id", "2", "x_td1", "2", "y_td1", "2", "w_td1", "15", "z_td1", "5", "f_t", "ipod ipod ipod ipod ipod"));
+    assertAdd(sdoc("id", "3", "x_td1", "3", "y_td1", "2", "w_td1", "55", "z_td1", "5", "f_t", "ipod ipod ipod ipod ipod ipod ipod ipod ipod"));
+    assertAdd(sdoc("id", "4", "x_td1", "4", "y_td1", "2", "w_td1", "45", "z_td1", "5", "f_t", "ipod ipod ipod ipod ipod ipod ipod"));
+    assertCommit();
 
-    assertQ(req("fl", "*,score", "q", "*:*"),
-            "//*[@numFound='4']",
-            "//float[@name='score']='1.0'",
-            "//result/doc[1]/str[@name='id'][.='1']",
-            "//result/doc[2]/str[@name='id'][.='2']",
-            "//result/doc[3]/str[@name='id'][.='3']",
-            "//result/doc[4]/str[@name='id'][.='4']"
-    );
-    assertQ(req("fl", "*,score", "q", "*:*", "sort", "score desc"),
-            "//*[@numFound='4']",
-            "//float[@name='score']='1.0'",
-            "//result/doc[1]/str[@name='id'][.='1']",
-            "//result/doc[2]/str[@name='id'][.='2']",
-            "//result/doc[3]/str[@name='id'][.='3']",
-            "//result/doc[4]/str[@name='id'][.='4']"
-    );
-    assertQ(req("fl", "id,score", "q", "f_t:ipod", "sort", "score desc"),
-            "//*[@numFound='4']",
-            "//result/doc[1]/str[@name='id'][.='1']",
-            "//result/doc[2]/str[@name='id'][.='2']",
-            "//result/doc[3]/str[@name='id'][.='3']",
-            "//result/doc[4]/str[@name='id'][.='4']"
-    );
+    assertSortedResults(params("fl", "id,score", "q", "f_t:ipod", "sort", "score desc"),
+                        "1", "2", "3", "4");
 
+    assertSortedResults(params("fl", "*,score", "q", "*:*", "sort", "sum(x_td1, y_td1) desc"),
+                        "4", "3", "2", "1");
 
-    assertQ(req("fl", "*,score", "q", "*:*", "sort", "sum(x_td1, y_td1) desc"),
-            "//*[@numFound='4']",
-            "//float[@name='score']='1.0'",
-            "//result/doc[1]/str[@name='id'][.='4']",
-            "//result/doc[2]/str[@name='id'][.='3']",
-            "//result/doc[3]/str[@name='id'][.='2']",
-            "//result/doc[4]/str[@name='id'][.='1']"
-    );
-    assertQ(req("fl", "*,score", "q", "*:*", "sort", "sum(x_td1, y_td1) asc"),
-            "//*[@numFound='4']",
-            "//float[@name='score']='1.0'",
-            "//result/doc[1]/str[@name='id'][.='1']",
-            "//result/doc[2]/str[@name='id'][.='2']",
-            "//result/doc[3]/str[@name='id'][.='3']",
-            "//result/doc[4]/str[@name='id'][.='4']"
-    );
+    assertSortedResults(params("fl", "*,score", "q", "*:*", "sort", "sum(x_td1, y_td1) asc"),
+                        "1", "2", "3", "4");
+
     //the function is equal, w_td1 separates
-    assertQ(req("q", "*:*", "fl", "id", "sort", "sum(z_td1, y_td1) asc, w_td1 asc"),
-            "//*[@numFound='4']",
-            "//result/doc[1]/str[@name='id'][.='2']",
-            "//result/doc[2]/str[@name='id'][.='1']",
-            "//result/doc[3]/str[@name='id'][.='4']",
-            "//result/doc[4]/str[@name='id'][.='3']"
-    );
+    assertSortedResults(params("q", "*:*", "fl", "id", "sort", "sum(z_td1, y_td1) asc, w_td1 asc"),
+                        "2", "1", "4", "3");
+
   }
   
   public void testSortJoinDocFreq() throws Exception
   {
-    assertU(adoc("id", "4", "id_s1", "D", "links_mfacet", "A", "links_mfacet", "B", "links_mfacet", "C" ) );
-    assertU(adoc("id", "3", "id_s1", "C", "links_mfacet", "A", "links_mfacet", "B" ) );
-    assertU(commit()); // Make sure it uses two readers
-    assertU(adoc("id", "2", "id_s1", "B", "links_mfacet", "A" ) );
-    assertU(adoc("id", "1", "id_s1", "A"  ) );
-    assertU(commit());
+    assertAdd(sdoc("id", "4", "id_s1", "D", "links_mfacet", "A", "links_mfacet", "B", "links_mfacet", "C" ) );
+    assertAdd(sdoc("id", "3", "id_s1", "C", "links_mfacet", "A", "links_mfacet", "B" ) );
+    assertCommit(); // Make sure it uses two readers
+    assertAdd(sdoc("id", "2", "id_s1", "B", "links_mfacet", "A" ) );
+    assertAdd(sdoc("id", "1", "id_s1", "A"  ) );
+    assertCommit();
 
-    assertQ(req("q", "links_mfacet:B", "fl", "id", "sort", "id asc"),
-            "//*[@numFound='2']",
-            "//result/doc[1]/str[@name='id'][.='3']",
-            "//result/doc[2]/str[@name='id'][.='4']"
-    );
+    assertSortedResults(params("q", "links_mfacet:B", "fl", "id", "sort", "id asc"),
+                        "3", "4");
     
-    assertQ(req("q", "*:*", "fl", "id", "sort", "joindf(id_s1, links_mfacet) desc"),
-            "//*[@numFound='4']",
-            "//result/doc[1]/str[@name='id'][.='1']",
-            "//result/doc[2]/str[@name='id'][.='2']",
-            "//result/doc[3]/str[@name='id'][.='3']",
-            "//result/doc[4]/str[@name='id'][.='4']"
-    );
+    assertSortedResults(params("q", "*:*", "fl", "id", "sort", "joindf(id_s1, links_mfacet) desc"),
+                        "1", "2", "3", "4");
 
-    assertQ(req("q", "*:*", "fl", "id", "sort", "joindf(id_s1, links_mfacet) asc"),
-            "//*[@numFound='4']",
-            "//result/doc[1]/str[@name='id'][.='4']",
-            "//result/doc[2]/str[@name='id'][.='3']",
-            "//result/doc[3]/str[@name='id'][.='2']",
-            "//result/doc[4]/str[@name='id'][.='1']"
-    );
+    assertSortedResults(params("q", "*:*", "fl", "id", "sort", "joindf(id_s1, links_mfacet) asc"),
+                        "4", "3", "2", "1");
   }
+
+  // nocommit: test explicit "sort=field(multivalued_str_field,min|max) asc|desc"
+  // nocommit: test implicit "sort=multivalued_str_field asc|desc"
   
   /**
    * The sort clauses to test in <code>testFieldSortSpecifiedAsFunction</code>.
@@ -137,7 +182,6 @@ public class SortByFunctionTest extends SolrTestCaseJ4 {
   protected String[] getFieldFunctionClausesToTest() {
     return new String[] { "primary_tl1", "field(primary_tl1)" };
   }
-  
 
   /**
    * Sort by function normally compares the double value, but if a function is specified that identifies
@@ -169,47 +213,44 @@ public class SortByFunctionTest extends SolrTestCaseJ4 {
     assertEquals("WTF? big longs cast to double aren't equivalent?",
                  (double)Z, (double)X, 0.0D);
     
-    int docId = 0;
-    for (int i = 0; i < 3; i++) {
-      assertU(adoc(sdoc("id", ++docId, "primary_tl1", X, "secondary_tl1", i,
-                        "multi_l_dv", X, "multi_l_dv", A)));
-      assertU(adoc(sdoc("id", ++docId, "primary_tl1", Y, "secondary_tl1", i,
-                        "multi_l_dv", Y, "multi_l_dv", B)));
-      assertU(adoc(sdoc("id", ++docId, "primary_tl1", Z, "secondary_tl1", i,
-                        "multi_l_dv", Z, "multi_l_dv", C)));
+    for (int i = 1; i <= 3; i++) {
+      // inconsistent id structure to ensure lexigraphic/numeric id sort isn't a factor
+      assertAdd(sdoc("id", "99" + i, "primary_tl1", X, "secondary_tl1", i,
+                     "multi_l_dv", X, "multi_l_dv", A));
+      assertAdd(sdoc("id", i + "55", "primary_tl1", Y, "secondary_tl1", i,
+                     "multi_l_dv", Y, "multi_l_dv", B));
+      assertAdd(sdoc("id", "6" + i + "6", "primary_tl1", Z, "secondary_tl1", i,
+                     "multi_l_dv", Z, "multi_l_dv", C));
     }
-    assertU(commit());
+    assertCommit();
 
     // all of these sorts should result in the exact same order
     // min/max of a field is tested in TestSortByMinMaxFunction
     for (String primarySort : getFieldFunctionClausesToTest()) {
-
-      assertQ(req("q", "*:*",
-                  "sort", primarySort + " asc, secondary_tl1 asc")
-              , "//*[@numFound='9']"
-              //
-              , "//result/doc[1]/long[@name='primary_tl1'][.='"+X+"']"
-              , "//result/doc[1]/long[@name='secondary_tl1'][.='0']"
-              , "//result/doc[2]/long[@name='primary_tl1'][.='"+X+"']"
-              , "//result/doc[2]/long[@name='secondary_tl1'][.='1']"
-              , "//result/doc[3]/long[@name='primary_tl1'][.='"+X+"']"
-              , "//result/doc[3]/long[@name='secondary_tl1'][.='2']"
-              //
-              , "//result/doc[4]/long[@name='primary_tl1'][.='"+Y+"']"
-              , "//result/doc[4]/long[@name='secondary_tl1'][.='0']"
-              , "//result/doc[5]/long[@name='primary_tl1'][.='"+Y+"']"
-              , "//result/doc[5]/long[@name='secondary_tl1'][.='1']"
-              , "//result/doc[6]/long[@name='primary_tl1'][.='"+Y+"']"
-              , "//result/doc[6]/long[@name='secondary_tl1'][.='2']"
-              //
-              , "//result/doc[7]/long[@name='primary_tl1'][.='"+Z+"']"
-              , "//result/doc[7]/long[@name='secondary_tl1'][.='0']"
-              , "//result/doc[8]/long[@name='primary_tl1'][.='"+Z+"']"
-              , "//result/doc[8]/long[@name='secondary_tl1'][.='1']"
-              , "//result/doc[9]/long[@name='primary_tl1'][.='"+Z+"']"
-              , "//result/doc[9]/long[@name='secondary_tl1'][.='2']"
-              );
+      assertSortedResults(params("q", "*:*",
+                                 "sort", primarySort + " asc, secondary_tl1 asc"),
+                          "991", "992", "993",
+                          "155", "255", "355",
+                          "616", "626", "636");
     }
   }
   
+  /** 
+   * returns a random SolrClient -- either a CloudSolrClient, or an HttpSolrClient pointed 
+   * at a node in our cluster 
+   */
+  public static SolrClient getRandClient(Random rand) {
+    int numClients = CLIENTS.size();
+    int idx = TestUtil.nextInt(rand, 0, numClients);
+
+    return (idx == numClients) ? CLOUD_CLIENT : CLIENTS.get(idx);
+  }
+  
+  public static void waitForRecoveriesToFinish(CloudSolrClient client) throws Exception {
+    assert null != client.getDefaultCollection();
+    AbstractDistribZkTestBase.waitForRecoveriesToFinish(client.getDefaultCollection(),
+                                                        client.getZkStateReader(),
+                                                        true, true, 330);
+  }
+
 }
