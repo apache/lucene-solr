8f07cb2e08530a06304ac713e2c370c376417a4a
noble <noble@apache.org>
2019-11-02 15:05:59 +1100

    wrong import

--- solr/solrj/src/java/org/apache/solr/common/util/JsonSchemaCreator.java
+++ solr/solrj/src/java/org/apache/solr/common/util/JsonSchemaCreator.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.solr.common.annotation.Property;
 
 public class JsonSchemaCreator {
