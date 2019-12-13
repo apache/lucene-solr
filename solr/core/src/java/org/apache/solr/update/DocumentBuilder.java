422de99acf7cfab004e1e976c1ab47870dc6cfba
noble <noble@apache.org>
2019-12-13 17:48:14 +1100

    SOLR-14013: javabin performance regressions

--- solr/core/src/java/org/apache/solr/update/DocumentBuilder.java
+++ solr/core/src/java/org/apache/solr/update/DocumentBuilder.java
@@ -135,7 +135,7 @@ public class DocumentBuilder {
     // Load fields from SolrDocument to Document
     for( SolrInputField field : doc ) {
 
-      if (field.getFirstRawValue() instanceof SolrDocumentBase) {
+      if (field.getFirstValue() instanceof SolrDocumentBase) {
         if (ignoreNestedDocs) {
           continue;
         }
@@ -159,7 +159,7 @@ public class DocumentBuilder {
       // load each field value
       boolean hasField = false;
       try {
-        Iterator it = field.getRawIterator();
+        Iterator it = field.iterator();
         while (it.hasNext()) {
           Object v = it.next();
           if( v == null ) {
