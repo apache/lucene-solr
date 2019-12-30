bbb6e418e42ae518a74fc0f97360cd0666a78e80
Bruno Roustant <broustant@salesforce.com>
2019-12-30 12:23:50 +0100

    LUCENE-9105: UniformSplit postings format detects corrupted index and better handles IO exceptions.
    Closes #1105

--- lucene/codecs/src/java/org/apache/lucene/codecs/uniformsplit/UniformSplitTermsReader.java
+++ lucene/codecs/src/java/org/apache/lucene/codecs/uniformsplit/UniformSplitTermsReader.java
@@ -29,6 +29,7 @@ import java.util.Map;
 import org.apache.lucene.codecs.CodecUtil;
 import org.apache.lucene.codecs.FieldsProducer;
 import org.apache.lucene.codecs.PostingsReaderBase;
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.FieldInfos;
 import org.apache.lucene.index.IndexFileNames;
 import org.apache.lucene.index.SegmentReadState;
@@ -99,7 +100,7 @@ public class UniformSplitTermsReader extends FieldsProducer {
        CodecUtil.retrieveChecksum(blockInput);
 
        seekFieldsMetadata(blockInput);
-       Collection<FieldMetadata> fieldMetadataCollection = parseFieldsMetadata(blockInput, state.fieldInfos);
+       Collection<FieldMetadata> fieldMetadataCollection = parseFieldsMetadata(blockInput, state.fieldInfos, state.segmentInfo.maxDoc());
 
        fieldToTermsMap = new HashMap<>();
        this.blockInput = blockInput;
@@ -132,11 +133,14 @@ public class UniformSplitTermsReader extends FieldsProducer {
    * @param indexInput {@link IndexInput} must be positioned to the fields metadata
    *                   details by calling {@link #seekFieldsMetadata(IndexInput)} before this call.
    */
-  protected static Collection<FieldMetadata> parseFieldsMetadata(IndexInput indexInput, FieldInfos fieldInfos) throws IOException {
-    Collection<FieldMetadata> fieldMetadataCollection = new ArrayList<>();
-    int fieldsNumber = indexInput.readVInt();
-    for (int i = 0; i < fieldsNumber; i++) {
-      fieldMetadataCollection.add(FieldMetadata.read(indexInput, fieldInfos));
+  protected static Collection<FieldMetadata> parseFieldsMetadata(IndexInput indexInput, FieldInfos fieldInfos, int maxNumDocs) throws IOException {
+    int numFields = indexInput.readVInt();
+    if (numFields < 0) {
+      throw new CorruptIndexException("Illegal number of fields= " + numFields, indexInput);
+    }
+    Collection<FieldMetadata> fieldMetadataCollection = new ArrayList<>(numFields);
+    for (int i = 0; i < numFields; i++) {
+      fieldMetadataCollection.add(FieldMetadata.read(indexInput, fieldInfos, maxNumDocs));
     }
     return fieldMetadataCollection;
   }
