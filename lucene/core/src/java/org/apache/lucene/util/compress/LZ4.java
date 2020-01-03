00eed87ec4945fbbdd6906887169cda843580b2c
Adrien Grand <jpountz@gmail.com>
2020-01-03 10:52:42 +0100

    Add license of LZ4.

--- lucene/core/src/java/org/apache/lucene/util/compress/LZ4.java
+++ lucene/core/src/java/org/apache/lucene/util/compress/LZ4.java
@@ -1,18 +1,28 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * LZ4 Library
+ * Copyright (c) 2011-2016, Yann Collet
+ * All rights reserved.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.apache.lucene.util.compress;
 
@@ -27,7 +37,7 @@ import org.apache.lucene.util.packed.PackedInts;
 /**
  * LZ4 compression and decompression routines.
  *
- * http://code.google.com/p/lz4/
+ * https://github.com/lz4/lz4/tree/dev/lib
  * http://fastcompression.blogspot.fr/p/lz4.html
  *
  * The high-compression option is a simpler version of the one of the original
@@ -175,8 +185,6 @@ public final class LZ4 {
    * A record of previous occurrences of sequences of 4 bytes.
    */
   static abstract class HashTable {
-
-    
 
     /** Reset this hash table in order to compress the given content. */
     abstract void reset(byte[] b, int off, int len);
