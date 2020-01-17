be939ff9dccf6dcb3012745c87f31f7e04d7be8c
Erick Erickson <Erick.Erickson@gmail.com>
2020-01-17 18:54:23 -0500

    LUCENE-9134: Port ant-regenerate tasks to Gradle build

--- lucene/queryparser/src/java/org/apache/lucene/queryparser/classic/CharStream.java
+++ lucene/queryparser/src/java/org/apache/lucene/queryparser/classic/CharStream.java
@@ -71,7 +71,7 @@ interface CharStream {
    * Backs up the input stream by amount steps. Lexer calls this method if it
    * had already read some characters, but could not use them to match a
    * (longer) token. So, they will be used again as the prefix of the next
-   * token and it is the implementation's responsibility to do this right.
+   * token and it is the implemetation's responsibility to do this right.
    */
   void backup(int amount);
 
@@ -112,4 +112,4 @@ interface CharStream {
   void Done();
 
 }
-/* JavaCC - OriginalChecksum=6a5b704d1e6e4e036de8c95b24d47787 (do not edit this line) */
+/* JavaCC - OriginalChecksum=30b94cad7b10d0d81e3a59a1083939d0 (do not edit this line) */
