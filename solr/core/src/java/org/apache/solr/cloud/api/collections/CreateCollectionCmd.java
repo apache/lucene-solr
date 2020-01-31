14cbbb6e52f774fc4ed3cf9dde5e271accdb12a6
Andy Vuong <andyvvv.101@gmail.com>
2020-01-31 12:21:23 -0800

    Add missing condition

--- solr/core/src/java/org/apache/solr/cloud/api/collections/CreateCollectionCmd.java
+++ solr/core/src/java/org/apache/solr/cloud/api/collections/CreateCollectionCmd.java
@@ -109,13 +109,14 @@ public class CreateCollectionCmd implements OverseerCollectionMessageHandler.Cmd
     if (ocmh.zkStateReader.aliasesManager != null) { // not a mock ZkStateReader
       ocmh.zkStateReader.aliasesManager.update();
     }
+    final boolean sharedIndex = message.getBool(SHARED_INDEX, false);
     final Aliases aliases = ocmh.zkStateReader.getAliases();
     final String collectionName = message.getStr(NAME);
     final boolean waitForFinalState = message.getBool(WAIT_FOR_FINAL_STATE, false);
     final String alias = message.getStr(ALIAS, collectionName);
     log.info("Create collection {}", collectionName);
     
-    if (!ocmh.overseer.getCoreContainer().isSharedStoreEnabled()) {
+    if (sharedIndex && !ocmh.overseer.getCoreContainer().isSharedStoreEnabled()) {
       throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, 
           "Collection " + collectionName + " cannot be created because shared storage is not enabled on this cluster."
               + " Check solr.xml for the correct configurations");
