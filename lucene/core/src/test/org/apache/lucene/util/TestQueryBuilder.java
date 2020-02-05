468caa38c2849f8d8ee2488b09e46e9d6c8781b6
Alan Woodward <romseygeek@apache.org>
2020-02-05 10:45:35 +0000

    imports

--- lucene/core/src/test/org/apache/lucene/util/TestQueryBuilder.java
+++ lucene/core/src/test/org/apache/lucene/util/TestQueryBuilder.java
@@ -38,10 +38,6 @@ import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.SynonymQuery;
 import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.search.spans.SpanNearQuery;
-import org.apache.lucene.search.spans.SpanOrQuery;
-import org.apache.lucene.search.spans.SpanQuery;
-import org.apache.lucene.search.spans.SpanTermQuery;
 import org.apache.lucene.util.automaton.CharacterRunAutomaton;
 import org.apache.lucene.util.automaton.RegExp;
 
