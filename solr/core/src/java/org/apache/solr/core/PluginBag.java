224c038f9b9a40517ff5378971d3c8aac7c4aafa
Houston Putman <houston@apache.org>
2020-02-18 12:24:31 -0500

    SOLR-13669: DIH: Add System property toggle for use of dataConfig param (#1260)
    (cherry picked from commit 325824cd391c8e71f36f17d687f52344e50e9715)
    
    Addresses CVE-2019-0193, backported from Solr 8.1.2

--- solr/core/src/java/org/apache/solr/core/PluginBag.java
+++ solr/core/src/java/org/apache/solr/core/PluginBag.java
@@ -393,7 +393,7 @@ public class PluginBag<T> implements AutoCloseable {
       this.core = core;
       this.resourceLoader = loader;
       if (loader instanceof MemClassLoader) {
-        if (!"true".equals(System.getProperty("enable.runtime.lib"))) {
+        if (!RuntimeLib.isEnabled()) {
           String s = "runtime library loading is not enabled, start Solr with -Denable.runtime.lib=true";
           log.warn(s);
           solrException = new SolrException(SolrException.ErrorCode.SERVER_ERROR, s);
@@ -491,6 +491,10 @@ public class PluginBag<T> implements AutoCloseable {
         if (jarContent != null) return;
         jarContent = coreContainer.getBlobRepository().getBlobIncRef(name + "/" + version);
       }
+    }
+
+    public static boolean isEnabled() {
+      return Boolean.getBoolean("enable.runtime.lib");
     }
 
     public String getName() {
