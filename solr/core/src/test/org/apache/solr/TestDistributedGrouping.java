071524c6a370f839e026f24dc2ea4f93c48a7322
Ishan Chattopadhyaya <ishan@apache.org>
2020-04-06 22:33:31 +0530

    SOLR-13004: Fixing unit tests to handle the integer/long conversion

--- solr/core/src/test/org/apache/solr/TestDistributedGrouping.java
+++ solr/core/src/test/org/apache/solr/TestDistributedGrouping.java
@@ -311,8 +311,8 @@ public class TestDistributedGrouping extends BaseDistributedSearchTestCase {
     QueryResponse rsp = client.query(params);
     NamedList nl = (NamedList<?>) rsp.getResponse().get("grouped");
     nl = (NamedList<?>) nl.getVal(0);
-    long matches = (Long) nl.getVal(0);
-    long groupCount = (Long) nl.get("ngroups");
+    long matches = ((Number) nl.getVal(0)).longValue();
+    long groupCount = ((Number) nl.get("ngroups")).longValue();
     assertEquals(100 * shardsArr.length, matches);
     assertEquals(shardsArr.length, groupCount);
 
