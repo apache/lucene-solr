4a34014b512cf7e87b09dc5e314130286cd6ec95
Alan Woodward <romseygeek@apache.org>
2020-04-24 10:48:12 +0100

    javadoc

--- lucene/expressions/src/java/org/apache/lucene/expressions/SimpleBindings.java
+++ lucene/expressions/src/java/org/apache/lucene/expressions/SimpleBindings.java
@@ -27,16 +27,16 @@ import org.apache.lucene.search.DoubleValuesSource;
 import org.apache.lucene.search.SortField;
 
 /**
- * Simple class that binds expression variable names to {@link SortField}s
+ * Simple class that binds expression variable names to {@link DoubleValuesSource}s
  * or other {@link Expression}s.
  * <p>
  * Example usage:
  * <pre class="prettyprint">
  *   SimpleBindings bindings = new SimpleBindings();
  *   // document's text relevance score
- *   bindings.add(new SortField("_score", SortField.Type.SCORE));
+ *   bindings.add("_score", DoubleValuesSource.SCORES);
  *   // integer NumericDocValues field
- *   bindings.add(new SortField("popularity", SortField.Type.INT));
+ *   bindings.add("popularity", DoubleValuesSource.fromIntField("popularity"));
  *   // another expression
  *   bindings.add("recency", myRecencyExpression);
  *   
@@ -56,7 +56,6 @@ public final class SimpleBindings extends Bindings {
   /** 
    * Adds a SortField to the bindings.
    *
-   * @deprecated use {@link #add(String, DoubleValuesSource)}
    * @see DoubleValuesSource#fromIntField(String)
    * @see DoubleValuesSource#fromLongField(String)
    * @see DoubleValuesSource#fromFloatField(String)
