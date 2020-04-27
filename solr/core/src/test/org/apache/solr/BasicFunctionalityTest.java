a225853cddc224ef900df85762eb2b2f9fff9b36
Tomas Fernandez Lobbe <tflobbe@apache.org>
2020-04-27 16:32:39 -0700

    Fix BasicFunctionalityTest

--- solr/core/src/test/org/apache/solr/BasicFunctionalityTest.java
+++ solr/core/src/test/org/apache/solr/BasicFunctionalityTest.java
@@ -34,6 +34,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LazyDocument;
 import org.apache.lucene.index.IndexableField;
+import org.apache.solr.common.HitCountRelation;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.MapSolrParams;
@@ -1031,7 +1032,7 @@ public class BasicFunctionalityTest extends SolrTestCaseJ4 {
     
     assertQ("Match all docs, but count only until certain value. Return an approximate count",
             req("q", "val_s:foo", "minExactHits", "2", "rows", "2")
-            ,"//*[@isExactHitCount='false']"
+            ,"//*[@hitCountRelation='" + HitCountRelation.GREATER_THAN_OR_EQUAL_TO + "']"
             );
   }
 
