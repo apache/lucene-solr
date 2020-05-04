d30a14f5622377f028d2320d9c647943d9dd4787
marcussorealheis <marcuseagan@gmail.com>
2020-05-04 10:41:46 -0700

    add set prefix for Windows.

--- solr/bin/solr.in.cmd
+++ solr/bin/solr.in.cmd
@@ -206,5 +206,5 @@ REM set SOLR_SECURITY_MANAGER_ENABLED=true
 REM This variable provides you with the option to disable the Admin UI. if you uncomment the variable below and
 REM change the value to true. The option is configured as a system property as defined in SOLR_START_OPTS in the start
 REM scripts.
-REM SOLR_ADMIN_UI_DISABLED=false
+REM set SOLR_ADMIN_UI_DISABLED=false
 
