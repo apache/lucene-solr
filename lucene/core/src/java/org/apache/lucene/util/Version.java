ed0196a6b47b5b3dccfefc6b6eee01393294d1ee
noble <noble@apache.org>
2020-05-14 11:17:29 +1000

    post release steps for 7.7.3 release

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -250,11 +250,18 @@ public final class Version {
 
   /**
    * Match settings and bugs in Lucene's 7.7.3 release.
+   * @deprecated (7.7.4) Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_7_7_3 = new Version(7, 7, 3);
+
+  /**
+   * Match settings and bugs in Lucene's 7.7.4 release.
    * <p>
    * Use this to get the latest &amp; greatest settings, bug
    * fixes, etc, for Lucene.
    */
-  public static final Version LUCENE_7_7_3 = new Version(7, 7, 3);
+  public static final Version LUCENE_7_7_4 = new Version(7, 7, 4);
 
   // To add a new version:
   //  * Only add above this comment
@@ -275,7 +282,7 @@ public final class Version {
    * some defaults may have changed and may break functionality 
    * in your application.
    */
-  public static final Version LATEST = LUCENE_7_7_3;
+  public static final Version LATEST = LUCENE_7_7_4;
 
   /**
    * Constant for backwards compatibility.
