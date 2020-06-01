7f010d4c4f0196b4e0aff3562c1bb9ff3f2aebff
noble <noble@apache.org>
2020-06-01 10:37:20 +1000

    removed unused imports

--- solr/core/src/java/org/apache/solr/api/AnnotatedApi.java
+++ solr/core/src/java/org/apache/solr/api/AnnotatedApi.java
@@ -33,7 +33,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.solr.client.solrj.SolrRequest;
