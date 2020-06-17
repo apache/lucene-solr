34ab53072911ac346bded7bbf8ec7140248f331f
Simon Willnauer <simonw@apache.org>
2020-06-17 23:43:17 +0200

    LUCENE-9408: Ensure OneMerge#mergeFinished is only called once
    in the case of an exception it's possible that some OneMerge instances
    will be closed multiple times. This commit ensures that mergeFinished is
    really just called once instead of multiple times.

--- lucene/core/src/java/org/apache/lucene/index/MergePolicy.java
+++ lucene/core/src/java/org/apache/lucene/index/MergePolicy.java
@@ -256,11 +256,9 @@ public abstract class MergePolicy {
     /** Called by {@link IndexWriter} after the merge is done and all readers have been closed.
      * @param success true iff the merge finished successfully ie. was committed */
     public void mergeFinished(boolean success) throws IOException {
-      mergeCompleted.complete(success);
-      // https://issues.apache.org/jira/browse/LUCENE-9408
-      // if (mergeCompleted.complete(success) == false) {
-      //   throw new IllegalStateException("merge has already finished");
-      // }
+      if (mergeCompleted.complete(success) == false) {
+        throw new IllegalStateException("merge has already finished");
+      }
     }
 
     /** Wrap the reader in order to add/remove information to the merged segment. */
