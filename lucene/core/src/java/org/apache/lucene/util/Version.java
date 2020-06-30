924c8415af7548d937da92fb62be466c14e0d1e6
Bruno Roustant <broustant@salesforce.com>
2020-06-30 15:16:18 +0200

    Add next minor version 8.7.0

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -217,11 +217,18 @@ public final class Version {
 
   /**
    * Match settings and bugs in Lucene's 8.6.0 release.
+   * @deprecated (8.7.0) Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_8_6_0 = new Version(8, 6, 0);
+
+  /**
+   * Match settings and bugs in Lucene's 8.7.0 release.
    * <p>
    * Use this to get the latest &amp; greatest settings, bug
    * fixes, etc, for Lucene.
    */
-  public static final Version LUCENE_8_6_0 = new Version(8, 6, 0);
+  public static final Version LUCENE_8_7_0 = new Version(8, 7, 0);
 
   // To add a new version:
   //  * Only add above this comment
@@ -242,7 +249,7 @@ public final class Version {
    * some defaults may have changed and may break functionality
    * in your application.
    */
-  public static final Version LATEST = LUCENE_8_6_0;
+  public static final Version LATEST = LUCENE_8_7_0;
 
   /**
    * Constant for backwards compatibility.
