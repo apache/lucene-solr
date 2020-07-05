87436b0b9af2aedcf86904dbe78c0c4631d6c551
Chris Hostetter <hossman@apache.org>
2020-07-05 13:28:08 -0700

    SOLR-14595: add AwaitsFix test to TestJsonFacetRefinement demonstrating problem, and work around to randomized testing in TestCloudJSONFacetSKGEquiv
    (cherry picked from commit fea6c1b9daa243e48297b4ba5a1ece68b8790a5b)

--- solr/core/src/test/org/apache/solr/search/facet/TestCloudJSONFacetSKGEquiv.java
+++ solr/core/src/test/org/apache/solr/search/facet/TestCloudJSONFacetSKGEquiv.java
@@ -307,7 +307,7 @@ public class TestCloudJSONFacetSKGEquiv extends SolrCloudTestCase {
     } 
 
   }
-  
+
   /** 
    * Test some small, hand crafted, but non-trivial queries that are
    * easier to trace/debug then a pure random monstrosity.
@@ -717,7 +717,7 @@ public class TestCloudJSONFacetSKGEquiv extends SolrCloudTestCase {
       final String facetField = randomFacetField(random());
       return new TermFacet(facetField,
                            map("limit", randomLimitParam(random()),
-                               "overrequest", randomOverrequestParam(random()),
+                               "overrequest", randomOverrequestParam(random(), sort),
                                "prefix", randomPrefixParam(random(), facetField),
                                "perSeg", randomPerSegParam(random()),
                                "sort", sort,
@@ -896,11 +896,25 @@ public class TestCloudJSONFacetSKGEquiv extends SolrCloudTestCase {
     
     /**
      * picks a random value for the "overrequest" param, biased in favor of interesting test cases.
+     * <p>
+     * <b>NOTE:</b> due to variations in overrequest behavior betewen <code>metod:enum<code> and other 
+     * processors (see <a href="https://issues.apache.org/jira/browse/SOLR-14595">SOLR-14595</a>) this 
+     * method takes in the "sort" param and returns a constant value of <code>0</code> if the sort is 
+     * <code>index asc</code> to ensure that the set of candidate buckets considered during merging 
+     * (and refinement) is consistent regardless of what processor is used (and/or what sort is used 
+     * on the parent facet)
+     * </p>
      *
      * @return a number to specify in the request, or null to specify nothing (trigger default behavior)
      * @see #UNIQUE_FIELD_VALS
+     * @see <a href="https://issues.apache.org/jira/browse/SOLR-14595">SOLR-14595</a>
      */
-    public static Integer randomOverrequestParam(final Random r) {
+    public static Integer randomOverrequestParam(final Random r, final String sort) {
+
+      if ("index asc".equals(sort)) {
+        return 0; // test work around for SOLR-14595
+      }
+      
       switch(r.nextInt(10)) {
         case 0:
         case 1:
