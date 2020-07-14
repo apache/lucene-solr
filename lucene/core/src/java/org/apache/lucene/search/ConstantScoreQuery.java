f26d746f83225acd6b27970e6e05320f63c31ff0
jeng832 <jeng832@gmail.com>
2020-07-15 00:25:09 +0900

    Ex

--- lucene/core/src/java/org/apache/lucene/search/ConstantScoreQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/ConstantScoreQuery.java
@@ -31,11 +31,17 @@ import org.apache.lucene.util.Bits;
  */
 public final class ConstantScoreQuery extends Query {
   private final Query query;
+  private final boolean explainable;
 
   /** Strips off scores from the passed in Query. The hits will get a constant score
    * of 1. */
   public ConstantScoreQuery(Query query) {
+    this(query, false);
+  }
+
+  public ConstantScoreQuery(Query query, boolean explainable) {
     this.query = Objects.requireNonNull(query, "Query must not be null");
+    this.explainable = explainable;
   }
 
   /** Returns the encapsulated query. */
@@ -43,12 +49,16 @@ public final class ConstantScoreQuery extends Query {
     return query;
   }
 
+  public boolean isExplainable() {
+    return explainable;
+  }
+
   @Override
   public Query rewrite(IndexReader reader) throws IOException {
     Query rewritten = query.rewrite(reader);
 
     if (rewritten != query) {
-      return new ConstantScoreQuery(rewritten);
+      return new ConstantScoreQuery(rewritten, explainable);
     }
 
     if (rewritten.getClass() == ConstantScoreQuery.class) {
@@ -56,7 +66,7 @@ public final class ConstantScoreQuery extends Query {
     }
 
     if (rewritten.getClass() == BoostQuery.class) {
-      return new ConstantScoreQuery(((BoostQuery) rewritten).getQuery());
+      return new ConstantScoreQuery(((BoostQuery) rewritten).getQuery(), explainable);
     }
 
     return super.rewrite(reader);
@@ -169,6 +179,31 @@ public final class ConstantScoreQuery extends Query {
           return innerWeight.isCacheable(ctx);
         }
 
+        @Override
+        public Explanation explain(LeafReaderContext context, int doc) throws IOException {
+          final Scorer s = scorer(context);
+          final boolean exists;
+          if (s == null) {
+            exists = false;
+          } else {
+            final TwoPhaseIterator twoPhase = s.twoPhaseIterator();
+            if (twoPhase == null) {
+              exists = s.iterator().advance(doc) == doc;
+            } else {
+              exists = twoPhase.approximation().advance(doc) == doc && twoPhase.matches();
+            }
+          }
+          if (explainable) {
+            return Explanation.match(score(), getQuery().toString() + (score() == 1f ? "" : "^" + score()), getQuery().createWeight(searcher, scoreMode, boost).explain(context, doc));
+          }
+
+          if (exists) {
+            return Explanation.match(
+                    score(), getQuery().toString() + (score() == 1f ? "" : "^" + score()));
+          } else {
+            return Explanation.noMatch(getQuery().toString() + " doesn't match id " + doc);
+          }
+        }
       };
     } else {
       return innerWeight;
