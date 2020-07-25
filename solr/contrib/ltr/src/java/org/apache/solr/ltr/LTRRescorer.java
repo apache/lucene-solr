3ddb17a029134100a2a21abeb0840f32b52f7705
shiming.li <limingnihao@live.com>
2020-07-25 21:32:50 +0800

    SOLR-14607: optimize imports

--- solr/contrib/ltr/src/java/org/apache/solr/ltr/LTRRescorer.java
+++ solr/contrib/ltr/src/java/org/apache/solr/ltr/LTRRescorer.java
@@ -21,7 +21,6 @@ import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.apache.lucene.index.ExitableDirectoryReader;
 import org.apache.lucene.index.LeafReaderContext;
