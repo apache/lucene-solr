d7cc54c23bc290fcf66792a9b95daceee416cf34
markrmiller@gmail.com <markrmiller@gmail.com>
2020-07-27 10:26:18 -0500

    @384 Tone down and harden.

--- solr/core/src/test/org/apache/solr/cloud/TestRandomFlRTGCloud.java
+++ solr/core/src/test/org/apache/solr/cloud/TestRandomFlRTGCloud.java
@@ -207,7 +207,7 @@ public class TestRandomFlRTGCloud extends SolrCloudTestCase {
   
   public void testRandomizedUpdatesAndRTGs() throws Exception {
 
-    final int maxNumDocs = atLeast( 100);
+    final int maxNumDocs = atLeast( TEST_NIGHTLY ? 100 : 35);
     final int numSeedDocs = random().nextInt(maxNumDocs / 10); // at most ~10% of the max possible docs
     final int numIters = atLeast(maxNumDocs * (TEST_NIGHTLY ? 10 : 2));
     final SolrInputDocument[] knownDocs = new SolrInputDocument[maxNumDocs];
