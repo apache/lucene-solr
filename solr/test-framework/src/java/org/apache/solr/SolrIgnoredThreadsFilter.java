faeceaab0bfad4da4d98a4d2de6f3b37d68889e5
markrmiller@gmail.com <markrmiller@gmail.com>
2020-07-28 12:51:06 -0500

    @430 Add this one as well.

--- solr/test-framework/src/java/org/apache/solr/SolrIgnoredThreadsFilter.java
+++ solr/test-framework/src/java/org/apache/solr/SolrIgnoredThreadsFilter.java
@@ -67,7 +67,7 @@ public class SolrIgnoredThreadsFilter implements ThreadFilter {
 
     // randomizedtesting claims this leaks, but the thread is already TERMINATED state
     // I think it can be resolved, but for now ...
-    if (threadName.startsWith("executeInOrderTest") || threadName.startsWith("testStress")) {
+    if (threadName.startsWith("executeInOrderTest") || threadName.startsWith("testStress") || threadName.startsWith("testLockWhenQueueIsFull_test")) {
       return true;
     }
 
