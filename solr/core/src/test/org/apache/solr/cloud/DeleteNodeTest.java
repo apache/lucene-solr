a666736641289ad0a5cfe7168ba5afc77cc08273
markrmiller@gmail.com <markrmiller@gmail.com>
2020-07-28 10:40:43 -0500

    @425 Enable DeleteNodeTest.

--- solr/core/src/test/org/apache/solr/cloud/DeleteNodeTest.java
+++ solr/core/src/test/org/apache/solr/cloud/DeleteNodeTest.java
@@ -40,7 +40,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Ignore // nocommit flakey debug
 public class DeleteNodeTest extends SolrCloudTestCase {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
