993b09c3be11942cbe3bd468936711c8c23d7daa
Jan Høydahl <janhoy@users.noreply.github.com>
2020-07-29 10:33:02 +0200

    SOLR-14671: Parsing dynamic ZK config sometimes cause NuberFormatException (#1701)
    (cherry picked from commit ebb5219b1b7d7baba9af6653f946cc1c66b40a93)

--- solr/solrj/src/java/org/apache/solr/common/cloud/ZkDynamicConfig.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/ZkDynamicConfig.java
@@ -132,6 +132,7 @@ public class ZkDynamicConfig {
       if (!m.matches()) {
         throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, "Could not parse dynamic zk config line: " + line);
       }
+      String clientPortStr = m.group("clientPort");
       return new Server(
           Integer.parseInt(m.group("serverId")),
           m.group("address"),
@@ -139,7 +140,7 @@ public class ZkDynamicConfig {
           Integer.parseInt(m.group("leaderElectionPort")),
           m.group("role"),
           m.group("clientPortAddress"),
-          Integer.parseInt(m.group("clientPort"))
+          clientPortStr != null ? Integer.parseInt(clientPortStr) : null
       );
     }
   }
