6d49c41ae819107e6fbfcada2528dad31cc6243b
MarcusSorealheis <marcuseagan@gmail.com>
2020-08-02 01:54:26 -0700

    fix imports again. oof.

--- solr/core/src/java/org/apache/solr/packagemanager/PackageManager.java
+++ solr/core/src/java/org/apache/solr/packagemanager/PackageManager.java
@@ -22,7 +22,15 @@ import static org.apache.solr.packagemanager.PackageUtils.getMapper;
 import java.io.Closeable;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.MultiValuedMap;
