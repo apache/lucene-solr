408c140097757b4f92334c728fd0a3c0aad08b24
noblepaul <noble.paul@gmail.com>
2020-08-03 11:57:55 +1000

    Another proposal

---
+++ solr/core/src/java/org/apache/solr/cloud/assign/AssignContext.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.solr.cloud.assign;
+
+import org.apache.solr.cluster.api.SolrCluster;
+import org.apache.solr.common.util.SimpleMap;
+
+import java.util.Set;
+
+/**An interface through which the plugin can interact with the Assign framework system
+ */
+public interface AssignContext {
+
+    /**get an instance of {@link SolrCluster} with enough data prefetched
+     */
+    SolrCluster getCluster(PrefetchHint... hints);
+
+    /**Fetch metrics from a given node
+     *
+     * @param nodeName Nam eof the node
+     * @param metricsKeys full name of metrics keys
+     */
+    SimpleMap<Object> fetchMetrics(String nodeName, Set<String> metricsKeys);
+
+}
