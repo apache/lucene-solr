83569665e7a235a208752e3733586d2cb5c1a4e0
Julie Tibshirani <julie.tibshirani@elastic.co>
2020-08-04 08:08:10 -0700

    LUCENE-9446: In boolean rewrite, remove MatchAllDocsQuery filter clauses (#1709)
    Previously, we only removed 'match all' FILTER clauses if there was at least one
    MUST clause. Now they're also removed if there is another distinct FILTER clause.
    
    This lets boolean queries like `#field:value #*:*` be written to `#field:value`.

--- lucene/core/src/test/org/apache/lucene/search/TestBooleanRewrites.java
+++ lucene/core/src/test/org/apache/lucene/search/TestBooleanRewrites.java
@@ -312,11 +312,27 @@ public class TestBooleanRewrites extends LuceneTestCase {
         .add(new TermQuery(new Term("foo", "baz")), Occur.MUST)
         .add(new MatchAllDocsQuery(), Occur.FILTER)
         .build();
-    BooleanQuery expected = new BooleanQuery.Builder()
+    Query expected = new BooleanQuery.Builder()
         .setMinimumNumberShouldMatch(bq.getMinimumNumberShouldMatch())
         .add(new TermQuery(new Term("foo", "bar")), Occur.MUST)
         .add(new TermQuery(new Term("foo", "baz")), Occur.MUST)
         .build();
+    assertEquals(expected, searcher.rewrite(bq));
+
+    bq = new BooleanQuery.Builder()
+            .add(new TermQuery(new Term("foo", "bar")), Occur.FILTER)
+            .add(new MatchAllDocsQuery(), Occur.FILTER)
+            .build();
+    expected = new BoostQuery(new ConstantScoreQuery(
+            new TermQuery(new Term("foo", "bar"))), 0.0f);
+    assertEquals(expected, searcher.rewrite(bq));
+
+    bq = new BooleanQuery.Builder()
+            .add(new MatchAllDocsQuery(), Occur.FILTER)
+            .add(new MatchAllDocsQuery(), Occur.FILTER)
+            .build();
+    expected = new BoostQuery(new ConstantScoreQuery(
+            new MatchAllDocsQuery()), 0.0f);
     assertEquals(expected, searcher.rewrite(bq));
   }
 
