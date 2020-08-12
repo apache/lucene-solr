9b43d2e3ff651bb1361ba979617b037276aa6d93
markrmiller@gmail.com <markrmiller@gmail.com>
2020-08-11 23:23:12 -0500

    @521 Remove silly wait.

--- solr/core/src/java/org/apache/solr/update/processor/DistributedZkUpdateProcessor.java
+++ solr/core/src/java/org/apache/solr/update/processor/DistributedZkUpdateProcessor.java
@@ -1095,18 +1095,6 @@ public class DistributedZkUpdateProcessor extends DistributedUpdateProcessor {
       }
     }
 
-    int count = 0;
-    while (((isLeader && !localIsLeader) || (isSubShardLeader && !localIsLeader)) && count < 5) {
-      count++;
-      // re-getting localIsLeader since we published to ZK first before setting localIsLeader value
-      localIsLeader = cloudDesc.isLeader();
-      try {
-        Thread.sleep(500);
-      } catch (InterruptedException e) {
-        ParWork.propegateInterrupt(e);
-      }
-    }
-
     if ((isLeader && !localIsLeader) || (isSubShardLeader && !localIsLeader)) {
       log.error("ClusterState says we are the leader, but locally we don't think so");
       throw new SolrException(SolrException.ErrorCode.SERVICE_UNAVAILABLE,
