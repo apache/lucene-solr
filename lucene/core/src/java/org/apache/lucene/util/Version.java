58a6f956d0cfe974d608b8929f899273c4c3c68b
Houston Putman <hputman@apple.com>
2020-08-14 16:35:46 -0400

    Add bugfix version 8.6.1

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -117,6 +117,13 @@ public final class Version {
   public static final Version LUCENE_8_6_0 = new Version(8, 6, 0);
 
   /**
+   * Match settings and bugs in Lucene's 8.6.1 release.
+   * @deprecated Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_8_6_1 = new Version(8, 6, 1);
+
+  /**
    * Match settings and bugs in Lucene's 9.0.0 release.
    * <p>
    * Use this to get the latest &amp; greatest settings, bug
