1bac7831bd9c8743216712126384d686c541718b
Andrzej Bialecki <ab@apache.org>
2020-08-17 19:32:18 +0200

    SOLR-14749: Initial draft.

---
+++ solr/core/src/java/org/apache/solr/cloud/events/ClusterSingleton.java
@@ -0,0 +1,14 @@
+package org.apache.solr.cloud.events;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Intended for {@link org.apache.solr.core.CoreContainer} plugins that should be
+ * enabled only one instance per cluster.
+ * <p>Implementation detail: currently these plugins are instantiated on the
+ * Overseer leader, and closed when the current node loses its leadership.</p>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClusterSingleton {
+}
