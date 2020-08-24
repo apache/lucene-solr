51f6440aff00ccc6a009b982af8981b4abd38c25
Simon Willnauer <simonw@apache.org>
2020-08-24 18:08:05 +0200

    LUCENE-9478: Prevent DWPTDeleteQueue from referencing itself and leaking memory
    In LUCENE-9304 we introduced some fixes that unfortunately hold on to the previous
    DWPTDeleteQueue which is essentially leaking IW memory and cause applications to fail.
    This fixes the memory leak and adds a test to ensure it's not leaking memory.

--- lucene/core/src/java/org/apache/lucene/index/DocumentsWriterDeleteQueue.java
+++ lucene/core/src/java/org/apache/lucene/index/DocumentsWriterDeleteQueue.java
@@ -573,6 +573,10 @@ final class DocumentsWriterDeleteQueue implements Accountable, Closeable {
     }
   }
 
+  private static LongSupplier getPrevMaxSeqIdSupplier(AtomicLong nextSeqNo) {
+    return () -> nextSeqNo.get() - 1;
+  }
+
   /**
    * Advances the queue to the next queue on flush. This carries over the the generation to the next queue and
    * set the {@link #getMaxSeqNo()} based on the given maxNumPendingOps. This method can only be called once, subsequently
@@ -593,7 +597,7 @@ final class DocumentsWriterDeleteQueue implements Accountable, Closeable {
     return new DocumentsWriterDeleteQueue(infoStream, generation + 1, seqNo + 1,
         // don't pass ::getMaxCompletedSeqNo here b/c otherwise we keep an reference to this queue
         // and this will be a memory leak since the queues can't be GCed
-        () -> nextSeqNo.get() - 1);
+        getPrevMaxSeqIdSupplier(nextSeqNo));
 
   }
 
