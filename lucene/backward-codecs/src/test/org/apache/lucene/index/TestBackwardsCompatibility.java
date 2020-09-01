03814fc070a2e6e9f7d58aae4d39caf579f95d67
iverase <ivera@apache.org>
2020-09-01 11:22:00 +0200

    Add back-compat indices for 8.6.2

--- lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/index/TestBackwardsCompatibility.java
@@ -311,7 +311,9 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
     "8.6.0-cfs",
     "8.6.0-nocfs",
     "8.6.1-cfs",
-    "8.6.1-nocfs"
+    "8.6.1-nocfs",
+    "8.6.2-cfs",
+    "8.6.2-nocfs"
   };
 
   public static String[] getOldNames() {
@@ -331,7 +333,8 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
     "sorted.8.5.1",
     "sorted.8.5.2",
     "sorted.8.6.0",
-    "sorted.8.6.1"
+    "sorted.8.6.1",
+    "sorted.8.6.2"
   };
 
   public static String[] getOldSortedNames() {
