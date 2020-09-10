9e11edca66cd8cd3b9cfc35a19aa5215421808f1
Dawid Weiss <dawid.weiss@carrotsearch.com>
2020-09-10 16:18:27 +0200

    Make the list modifiable.

--- solr/core/src/test/org/apache/solr/cloud/hdfs/HdfsTestUtil.java
+++ solr/core/src/test/org/apache/solr/cloud/hdfs/HdfsTestUtil.java
@@ -19,6 +19,7 @@ package org.apache.solr.cloud.hdfs;
 import java.io.File;
 import java.lang.invoke.MethodHandles;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -121,13 +122,13 @@ public class HdfsTestUtil {
    * Ensure that the tests are picking up the modified Hadoop classes
    */
   private static void checkOverriddenHadoopClasses() {
-    List<Class<?>> modifiedHadoopClasses = Arrays.asList(
+    List<Class<?>> modifiedHadoopClasses = new ArrayList<>(Arrays.asList(
         DiskChecker.class,
         FileUtil.class,
         HardLink.class,
         HttpServer2.class,
         NameNodeResourceChecker.class,
-        RawLocalFileSystem.class);
+        RawLocalFileSystem.class));
     // Dodge weird scope errors from the compiler (SOLR-14417)
     try {
       modifiedHadoopClasses.add(
