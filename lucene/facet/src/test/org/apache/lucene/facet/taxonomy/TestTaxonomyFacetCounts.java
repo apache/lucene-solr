71222c288a83e4a05d0be6020f6903ad340d959d
Ankur Goel <goankur@amazon.com>
2020-09-18 18:17:44 -0700

    LUCENE-9444 Utility class to get facet labels from taxonomy for a facet field. This is useful if a facet field is requested in the list of return fields for each hit.

--- lucene/facet/src/test/org/apache/lucene/facet/taxonomy/TestTaxonomyFacetCounts.java
+++ lucene/facet/src/test/org/apache/lucene/facet/taxonomy/TestTaxonomyFacetCounts.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
