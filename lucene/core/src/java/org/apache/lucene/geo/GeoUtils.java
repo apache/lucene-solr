5628c969c4172d62f98924d8ea975a01fc5a0dae
iverase <ivera@apache.org>
2020-09-24 09:47:25 +0200

    doh, check overlap properly

--- lucene/core/src/java/org/apache/lucene/geo/GeoUtils.java
+++ lucene/core/src/java/org/apache/lucene/geo/GeoUtils.java
@@ -219,8 +219,8 @@ public final class GeoUtils {
 
   /** uses orient method to compute whether two line overlap each other */
   public static boolean lineOverlapLine(double a1x, double a1y, double b1x, double b1y, double a2x, double a2y, double b2x, double b2y) {
-    if (orient(a2x, a2y, b2x, b2y, a1x, a1y) * orient(a2x, a2y, b2x, b2y, b1x, b1y) == 0 &&
-        orient(a1x, a1y, b1x, b1y, a2x, a2y) * orient(a1x, a1y, b1x, b1y, b2x, b2y) == 0) {
+    if (orient(a2x, a2y, b2x, b2y, a1x, a1y) == 0 && orient(a2x, a2y, b2x, b2y, b1x, b1y) == 0 &&
+        orient(a1x, a1y, b1x, b1y, a2x, a2y) == 0 && orient(a1x, a1y, b1x, b1y, b2x, b2y) == 0) {
       return true;
     }
     return false;
