3ee471ca31b3aae31d3c97b8faef86f2b907a6f6
Bruno Roustant <broustant@salesforce.com>
2020-10-01 10:07:42 +0200

    SOLR-14905: Upgrade commons-io version to 2.8.0. Closes #1934

--- solr/core/src/test/org/apache/hadoop/fs/FileUtil.java
+++ solr/core/src/test/org/apache/hadoop/fs/FileUtil.java
@@ -597,12 +597,7 @@ public class FileUtil {
       File[] allFiles = dir.listFiles();
       if(allFiles != null) {
         for (int i = 0; i < allFiles.length; i++) {
-          boolean isSymLink;
-          try {
-            isSymLink = org.apache.commons.io.FileUtils.isSymlink(allFiles[i]);
-          } catch(IOException ioe) {
-            isSymLink = true;
-          }
+          boolean isSymLink = org.apache.commons.io.FileUtils.isSymlink(allFiles[i]);
           if(!isSymLink) {
             size += getDU(allFiles[i]);
           }
