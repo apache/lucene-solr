b70eaeee5acd9b166e51d4eb5a27eecac984f762
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2020-10-05 17:52:23 +0900

    LUCENE-9558: Clean up package name conflicts for analyzers-icu. (#1946)

--- lucene/analysis/icu/src/java/org/apache/lucene/analysis/icu/tokenattributes/ICUCollatedTermAttributeImpl.java
+++ lucene/analysis/icu/src/java/org/apache/lucene/analysis/icu/tokenattributes/ICUCollatedTermAttributeImpl.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.lucene.collation.tokenattributes;
+package org.apache.lucene.analysis.icu.tokenattributes;
 
 
 import org.apache.lucene.analysis.tokenattributes.CharTermAttributeImpl;
