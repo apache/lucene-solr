485f82102ca92637e885a94b1b6233d8da3f5d07
David Smiley <dsmiley@salesforce.com>
2020-10-06 15:15:02 -0400

    SOLR-14915: Move DOMUtil and PropertiesUtil to SolrJ

--- solr/solrj/src/java/org/apache/solr/common/util/PropertiesUtil.java
+++ solr/solrj/src/java/org/apache/solr/common/util/PropertiesUtil.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.solr.util;
+package org.apache.solr.common.util;
 
 import org.apache.solr.common.SolrException;
 
