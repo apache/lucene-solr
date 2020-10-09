b43fe6dfa7fd8e993bd4dab7ba59331f0ab0bfd0
Jason Gerlowski <jason.gerlowski@lucidworks.com>
2020-10-09 11:03:34 -0400

    Add next bugfix version 8.6.4

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -238,11 +238,18 @@ public final class Version {
 
   /**
    * Match settings and bugs in Lucene's 8.6.3 release.
+   * @deprecated (8.6.4) Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_8_6_3 = new Version(8, 6, 3);
+
+  /**
+   * Match settings and bugs in Lucene's 8.6.4 release.
    * <p>
    * Use this to get the latest &amp; greatest settings, bug
    * fixes, etc, for Lucene.
    */
-  public static final Version LUCENE_8_6_3 = new Version(8, 6, 3);
+  public static final Version LUCENE_8_6_4 = new Version(8, 6, 4);
 
   // To add a new version:
   //  * Only add above this comment
@@ -263,7 +270,7 @@ public final class Version {
    * some defaults may have changed and may break functionality
    * in your application.
    */
-  public static final Version LATEST = LUCENE_8_6_3;
+  public static final Version LATEST = LUCENE_8_6_4;
 
   /**
    * Constant for backwards compatibility.
