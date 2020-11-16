12b7911fc0dc7a37b367a9b4e97b680abefd2f19
markrmiller@gmail.com <markrmiller@gmail.com>
2020-11-16 07:56:26 -0600

    @1225 Bump max connections per host.

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/Http2SolrClient.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/Http2SolrClient.java
@@ -1046,7 +1046,7 @@ public class Http2SolrClient extends SolrClient {
     private SSLConfig sslConfig = defaultSSLConfig;
     private Integer idleTimeout = Integer.getInteger("solr.http2solrclient.default.idletimeout", 120000);
     private Integer connectionTimeout;
-    private Integer maxConnectionsPerHost = 4;
+    private Integer maxConnectionsPerHost = 16;
     private boolean useHttp1_1 = Boolean.getBoolean("solr.http1");
     protected String baseSolrUrl;
     protected Map<String,String> headers = new ConcurrentHashMap<>();
