704f242c37513ae5d91266ffab653b1784a09a16
Michael Froh <msfroh@gmail.com>
2020-11-18 20:09:41 +0000

    LUCENE-9617: Reset lowestUnassignedFieldNumber in FieldNumbers.clear()
    FieldNumbers.clear() is called from IndexWriter.deleteAll(), which is
    supposed to completely reset the state of the index. This includes
    clearing all known fields.
    
    Prior to this change, it would allocate progressively higher field
    numbers, which results in larger and  larger arrays for
    FieldInfos.byNumber, effectively "leaking" field numbers every time
    deleteAll() is called.

--- lucene/core/src/test/org/apache/lucene/index/TestFieldInfos.java
+++ lucene/core/src/test/org/apache/lucene/index/TestFieldInfos.java
@@ -187,4 +187,23 @@ public class TestFieldInfos extends LuceneTestCase {
     writer.close();
     dir.close();
   }
+
+  public void testFieldNumbersAutoIncrement() {
+    FieldInfos.FieldNumbers fieldNumbers = new FieldInfos.FieldNumbers("softDeletes");
+    for (int i = 0; i < 10; i++) {
+      fieldNumbers.addOrGet("field" + i, -1, IndexOptions.NONE, DocValuesType.NONE,
+          0, 0, 0, 0,
+          VectorValues.SearchStrategy.NONE, false);
+    }
+    int idx = fieldNumbers.addOrGet("EleventhField", -1, IndexOptions.NONE, DocValuesType.NONE,
+        0, 0, 0, 0,
+        VectorValues.SearchStrategy.NONE, false);
+    assertEquals("Field numbers 0 through 9 were allocated", 10, idx);
+
+    fieldNumbers.clear();
+    idx = fieldNumbers.addOrGet("PostClearField", -1, IndexOptions.NONE, DocValuesType.NONE,
+        0, 0, 0, 0,
+        VectorValues.SearchStrategy.NONE, false);
+    assertEquals("Field numbers should reset after clear()", 0, idx);
+  }
 }
