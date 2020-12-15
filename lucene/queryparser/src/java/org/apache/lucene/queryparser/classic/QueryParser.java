11d5461f99df08fd6f5042dee5c269832febda48
Przemek Bruski <przemyslaw.bruski@gmail.com>
2020-12-15 06:56:21 +0100

    LUCENE-9021: re-use the LookaheadSuccess exception in Lucene QueryParser
    Co-authored-by: Przemek Bruski <pbruski@atlassian.com>

--- lucene/queryparser/src/java/org/apache/lucene/queryparser/classic/QueryParser.java
+++ lucene/queryparser/src/java/org/apache/lucene/queryparser/classic/QueryParser.java
@@ -814,7 +814,7 @@ public class QueryParser extends QueryParserBase implements QueryParserConstants
   }
 
   static private final class LookaheadSuccess extends java.lang.Error { }
-  final private LookaheadSuccess jj_ls = new LookaheadSuccess();
+  static final private LookaheadSuccess jj_ls = new LookaheadSuccess();
   private boolean jj_scan_token(int kind) {
     if (jj_scanpos == jj_lastpos) {
       jj_la--;
