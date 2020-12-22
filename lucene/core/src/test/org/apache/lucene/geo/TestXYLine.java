72822305dbebd5f5b057ee083ddf41db394863b0
Dawid Weiss <dawid.weiss@carrotsearch.com>
2020-12-22 09:34:13 +0100

    :lucene:core - src/**/org/apache/lucene/geo/**

--- lucene/core/src/test/org/apache/lucene/geo/TestXYLine.java
+++ lucene/core/src/test/org/apache/lucene/geo/TestXYLine.java
@@ -16,66 +16,90 @@
  */
 package org.apache.lucene.geo;
 
-
 import java.util.Arrays;
-
 import org.apache.lucene.util.LuceneTestCase;
 
 public class TestXYLine extends LuceneTestCase {
 
   /** null x not allowed */
   public void testLineNullXs() {
-    IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
-      new XYLine(null, new float[] { -66, -65, -65, -66, -66 });
-    });
+    IllegalArgumentException expected =
+        expectThrows(
+            IllegalArgumentException.class,
+            () -> {
+              new XYLine(null, new float[] {-66, -65, -65, -66, -66});
+            });
     assertTrue(expected.getMessage().contains("x must not be null"));
   }
 
   /** null y not allowed */
   public void testPolygonNullYs() {
-    IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
-      new XYLine(new float[] {18, 18, 19, 19, 18 }, null);
-    });
+    IllegalArgumentException expected =
+        expectThrows(
+            IllegalArgumentException.class,
+            () -> {
+              new XYLine(new float[] {18, 18, 19, 19, 18}, null);
+            });
     assertTrue(expected.getMessage().contains("y must not be null"));
   }
 
   /** needs at least 3 vertices */
   public void testLineEnoughPoints() {
-    IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
-      new XYLine(new float[] {18}, new float[] { -66});
-    });
+    IllegalArgumentException expected =
+        expectThrows(
+            IllegalArgumentException.class,
+            () -> {
+              new XYLine(new float[] {18}, new float[] {-66});
+            });
     assertTrue(expected.getMessage().contains("at least 2 line points required"));
   }
 
   /** lines needs same number of x as y */
   public void testLinesBogus() {
-    IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
-      new XYLine(new float[] { 18, 18, 19, 19 }, new float[] { -66, -65, -65, -66, -66 });
-    });
+    IllegalArgumentException expected =
+        expectThrows(
+            IllegalArgumentException.class,
+            () -> {
+              new XYLine(new float[] {18, 18, 19, 19}, new float[] {-66, -65, -65, -66, -66});
+            });
     assertTrue(expected.getMessage().contains("must be equal length"));
   }
 
   /** line values cannot be NaN */
   public void testLineNaN() {
-    IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
-      new XYLine(new float[] { 18, 18, 19, Float.NaN, 18 }, new float[] { -66, -65, -65, -66, -66 });
-    });
+    IllegalArgumentException expected =
+        expectThrows(
+            IllegalArgumentException.class,
+            () -> {
+              new XYLine(
+                  new float[] {18, 18, 19, Float.NaN, 18}, new float[] {-66, -65, -65, -66, -66});
+            });
     assertTrue(expected.getMessage(), expected.getMessage().contains("invalid value NaN"));
   }
 
   /** line values cannot be finite */
   public void testLinePositiveInfinite() {
-    IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
-      new XYLine(new float[] { 18, 18, 19, 19, 18 }, new float[] { -66, Float.POSITIVE_INFINITY, -65, -66, -66 });
-    });
+    IllegalArgumentException expected =
+        expectThrows(
+            IllegalArgumentException.class,
+            () -> {
+              new XYLine(
+                  new float[] {18, 18, 19, 19, 18},
+                  new float[] {-66, Float.POSITIVE_INFINITY, -65, -66, -66});
+            });
     assertTrue(expected.getMessage(), expected.getMessage().contains("invalid value Inf"));
   }
 
   /** line values cannot be finite */
   public void testLineNegativeInfinite() {
-    IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
-      new XYLine(new float[] { 18, 18, 19, 19, 18 }, new float[] { -66, -65, -65, Float.NEGATIVE_INFINITY, -66 });
-    });
+    IllegalArgumentException expected =
+        expectThrows(
+            IllegalArgumentException.class,
+            () -> {
+              new XYLine(
+                  new float[] {18, 18, 19, 19, 18},
+                  new float[] {-66, -65, -65, Float.NEGATIVE_INFINITY, -66});
+            });
     assertTrue(expected.getMessage(), expected.getMessage().contains("invalid value -Inf"));
   }
 
@@ -86,14 +110,13 @@ public class TestXYLine extends LuceneTestCase {
     assertEquals(line, copy);
     assertEquals(line.hashCode(), copy.hashCode());
     XYLine otherLine = ShapeTestUtil.nextLine();
-    if (Arrays.equals(line.getX(), otherLine.getX()) == false  ||
-        Arrays.equals(line.getY(), otherLine.getY()) == false) {
+    if (Arrays.equals(line.getX(), otherLine.getX()) == false
+        || Arrays.equals(line.getY(), otherLine.getY()) == false) {
       assertNotEquals(line, otherLine);
       assertNotEquals(line.hashCode(), otherLine.hashCode());
     } else {
       assertEquals(line, otherLine);
       assertEquals(line.hashCode(), otherLine.hashCode());
     }
-
   }
 }
