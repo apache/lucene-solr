ff2158f32427f6d983c8e95b77baa0bde8bbf7ed
epugh@opensourceconnections.com <>
2021-01-15 16:43:55 -0500

    Rename contrib module to generic "scripting" to be a home for all scripting based logic.

--- solr/contrib/scripting/src/test/org/apache/solr/update/processor/scripting/ScriptUpdateProcessorFactoryTest.java
+++ solr/contrib/scripting/src/test/org/apache/solr/update/processor/scripting/ScriptUpdateProcessorFactoryTest.java
@@ -35,7 +35,7 @@ import org.junit.BeforeClass;
 /**
  * Tests {@link ScriptUpdateProcessorFactory}.
  *
- * TODO: This test, to run from an IDE, requires a working directory of &lt;path-to&gt;/solr/contrib/scripting-update-processor/src/test-files.  Fix!
+ * TODO: This test, to run from an IDE, requires a working directory of &lt;path-to&gt;/solr/contrib/scripting/src/test-files.  Fix!
  */
 public class ScriptUpdateProcessorFactoryTest extends UpdateProcessorTestBase {
 
