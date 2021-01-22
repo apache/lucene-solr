f78e23b4b82d597f228be0c02ffae59ca48079ac
Christine Poerschke <cpoerschke@apache.org>
2021-01-22 17:59:53 +0000

    Revert "SOLR-15073: Fix ClassCastException in SystemInfoHandler.getSecurityInfo (#2210)"
    This reverts commit ec4917c45eaf7dccdcc98fba08484eb24e1ff5f2.
    
    Resolved Conflicts:
    	solr/CHANGES.txt

--- solr/core/src/java/org/apache/solr/handler/admin/SystemInfoHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/SystemInfoHandler.java
@@ -321,14 +321,6 @@ public class SystemInfoHandler extends RequestHandlerBase
    */
   public SimpleOrderedMap<Object> getSecurityInfo(SolrQueryRequest req)
   {
-    return getSecurityInfo(cc, req);
-  }
-
-  /**
-   * Get Security Info
-   */
-  public static SimpleOrderedMap<Object> getSecurityInfo(CoreContainer cc, SolrQueryRequest req)
-  {
     SimpleOrderedMap<Object> info = new SimpleOrderedMap<>();
 
     if (cc != null) {
@@ -349,7 +341,7 @@ public class SystemInfoHandler extends RequestHandlerBase
       // Mapped roles for this principal
       @SuppressWarnings("resource")
       AuthorizationPlugin auth = cc==null? null: cc.getAuthorizationPlugin();
-      if (auth instanceof RuleBasedAuthorizationPluginBase) {
+      if (auth != null) {
         RuleBasedAuthorizationPluginBase rbap = (RuleBasedAuthorizationPluginBase) auth;
         Set<String> roles = rbap.getUserRoles(req.getUserPrincipal());
         info.add("roles", roles);
