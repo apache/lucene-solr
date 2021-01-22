43b785d5e7ecfe1c45be6b15653a8017ed4dd497
Mike Drob <mdrob@apache.org>
2021-01-22 11:52:21 -0600

    SOLR-15120 Reduce duplicated core creation work
    Use j.u.c collections instead of sync block
    Rework how we load implicit handlers
    Additional debug and trace logging for zookeeper comms

--- solr/solrj/src/java/org/apache/solr/common/cloud/ZkCmdExecutor.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/ZkCmdExecutor.java
@@ -21,9 +21,15 @@ import org.apache.solr.common.cloud.ConnectionManager.IsClosed;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.KeeperException.NodeExistsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 
 public class ZkCmdExecutor {
+  public static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
   private long retryDelay = 1500L; // 1 second would match timeout, so 500 ms over for padding
   private int retryCount;
   private double timeouts;
@@ -59,16 +65,18 @@ public class ZkCmdExecutor {
   /**
    * Perform the given operation, retrying if the connection fails
    */
-  @SuppressWarnings("unchecked")
-  public <T> T retryOperation(ZkOperation operation)
+  public <T> T retryOperation(ZkOperation<T> operation)
       throws KeeperException, InterruptedException {
     KeeperException exception = null;
     for (int i = 0; i < retryCount; i++) {
       try {
+        if (log.isTraceEnabled()) {
+          log.trace("Begin zookeeper operation {}, attempt={}", operation, i);
+        }
         if (i > 0 && isClosed()) {
           throw new AlreadyClosedException();
         }
-        return (T) operation.execute();
+        return operation.execute();
       } catch (KeeperException.ConnectionLossException e) {
         if (exception == null) {
           exception = e;
@@ -79,6 +87,10 @@ public class ZkCmdExecutor {
         }
         if (i != retryCount -1) {
           retryDelay(i);
+        }
+      } finally {
+        if (log.isTraceEnabled()) {
+          log.trace("End zookeeper operation {}", operation);
         }
       }
     }
