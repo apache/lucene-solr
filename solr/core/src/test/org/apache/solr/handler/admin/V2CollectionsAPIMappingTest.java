472b7a49f29c1469a4ad6dee50e1fa067ded9d8b
Jason Gerlowski <jason.gerlowski@lucidworks.com>
2021-02-01 14:43:03 -0500

    Fix precommit, rd 1

--- solr/core/src/test/org/apache/solr/handler/admin/V2CollectionsAPIMappingTest.java
+++ solr/core/src/test/org/apache/solr/handler/admin/V2CollectionsAPIMappingTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.solr.handler.admin;
 
 import com.google.common.collect.Maps;
@@ -24,6 +40,7 @@ import org.apache.solr.request.LocalSolrQueryRequest;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.response.SolrQueryResponse;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
@@ -56,6 +73,11 @@ public class V2CollectionsAPIMappingTest extends SolrTestCaseJ4 {
 
     private ArgumentCaptor<SolrQueryRequest> queryRequestCaptor;
     private CollectionsHandler mockCollectionsHandler;
+
+    @BeforeClass
+    public static void ensureWorkingMockito() {
+        assumeWorkingMockito();
+    }
 
     @Before
     public void setupApiBag() throws Exception {
