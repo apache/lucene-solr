b9aec5b9e2488fa764b92aa5cf3232881690d6c3
Nazerke Seidan <nseidan@salesforce.com>
2021-02-01 19:49:54 +0100

    Use params for nodes=all

--- solr/webapp/web/js/angular/services.js
+++ solr/webapp/web/js/angular/services.js
@@ -61,7 +61,7 @@ solrAdminServices.factory('System',
     return $resource('admin/info/logging', {'wt':'json', '_':Date.now()}, {
       "events": {params: {since:'0'}},
       "levels": {},
-      "setLevel": {'nodes': 'all'}
+      "setLevel": {params: {nodes:'all'}}
       });
   }])
 .factory('Zookeeper',
