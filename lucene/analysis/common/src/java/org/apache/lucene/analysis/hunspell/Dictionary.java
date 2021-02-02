8bf2dec5b417e398d8e9d196a9856c0a6e1350f3
Dawid Weiss <dawid.weiss@carrotsearch.com>
2021-02-02 15:44:56 +0100

    Remove unused import.

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/hunspell/Dictionary.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/hunspell/Dictionary.java
@@ -30,7 +30,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
