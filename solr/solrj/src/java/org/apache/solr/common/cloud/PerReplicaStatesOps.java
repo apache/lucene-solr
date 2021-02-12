f5e62f6ad804884b26479e8497589024445c8231
Ishan Chattopadhyaya <ishan@apache.org>
2021-02-12 17:15:07 +0530

    Fixing precommit

--- solr/solrj/src/java/org/apache/solr/common/cloud/PerReplicaStatesOps.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/PerReplicaStatesOps.java
@@ -69,7 +69,7 @@ public class PerReplicaStatesOps {
         try {
             zkClient.multi(ops, true);
         } catch (KeeperException e) {
-          log.error("multi op exception : " + e.getMessage() + zkClient.getChildren(znode, null, true));
+          log.error("Multi-op exception: {}", zkClient.getChildren(znode, null, true));
           throw e;
         }
 
