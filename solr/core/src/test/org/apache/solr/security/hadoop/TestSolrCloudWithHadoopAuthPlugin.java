0d899131683e400ce11f03852b59279ca3803d83
Mike Drob <mdrob@apache.org>
2021-04-06 12:37:33 -0500

    SOLR-15233 Missed test backport

--- solr/core/src/test/org/apache/solr/security/hadoop/TestSolrCloudWithHadoopAuthPlugin.java
+++ solr/core/src/test/org/apache/solr/security/hadoop/TestSolrCloudWithHadoopAuthPlugin.java
@@ -119,13 +119,13 @@ public class TestSolrCloudWithHadoopAuthPlugin extends SolrCloudAuthTestCase {
         NUM_SHARDS, REPLICATION_FACTOR);
     create.process(solrClient);
     // The metrics counter for wrong credentials here really just means  
-    assertAuthMetricsMinimums(6, 3, 0, 3, 0, 0);
+    assertAuthMetricsMinimums(2, 1, 0, 1, 0, 0);
 
     SolrInputDocument doc = new SolrInputDocument();
     doc.setField("id", "1");
     solrClient.add(collectionName, doc);
     solrClient.commit(collectionName);
-    assertAuthMetricsMinimums(10, 5, 0, 5, 0, 0);
+    assertAuthMetricsMinimums(4, 2, 0, 2, 0, 0);
 
     SolrQuery query = new SolrQuery();
     query.setQuery("*:*");
@@ -136,6 +136,6 @@ public class TestSolrCloudWithHadoopAuthPlugin extends SolrCloudAuthTestCase {
     deleteReq.process(solrClient);
     AbstractDistribZkTestBase.waitForCollectionToDisappear(collectionName,
         solrClient.getZkStateReader(), true, true, 330);
-    assertAuthMetricsMinimums(14, 8, 0, 6, 0, 0);
+    assertAuthMetricsMinimums(6, 4, 0, 2, 0, 0);
   }
 }
