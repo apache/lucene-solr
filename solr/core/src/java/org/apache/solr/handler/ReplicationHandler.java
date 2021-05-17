9e9043052cca111b0cc7d6610455fc190b039734
Timothy Potter <thelabdude@gmail.com>
2021-05-17 11:16:10 -0600

    SOLR-11904: Mark ReplicationHandler's polling thread as a Solr server thread so the PKI Interceptor is activated (backport fixes from main to branch_8x)

--- solr/core/src/java/org/apache/solr/handler/ReplicationHandler.java
+++ solr/core/src/java/org/apache/solr/handler/ReplicationHandler.java
@@ -1248,6 +1248,7 @@ public class ReplicationHandler extends RequestHandlerBase implements SolrCoreAw
         log.info("Poll disabled");
         return;
       }
+      ExecutorUtil.setServerThreadFlag(true); // so PKI auth works
       try {
         log.debug("Polling for index modifications");
         markScheduledExecutionStart();
@@ -1255,6 +1256,8 @@ public class ReplicationHandler extends RequestHandlerBase implements SolrCoreAw
         if (pollListener != null) pollListener.onComplete(core, fetchResult);
       } catch (Exception e) {
         log.error("Exception in fetching index", e);
+      } finally {
+        ExecutorUtil.setServerThreadFlag(null);
       }
     };
     executorService = Executors.newSingleThreadScheduledExecutor(
