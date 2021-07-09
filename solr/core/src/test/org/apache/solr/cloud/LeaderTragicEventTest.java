836613249429bdb7e40476b422b52e8a6aecdbd9
Mike Drob <mdrob@apache.org>
2021-07-09 12:33:59 -0700

    SOLR-15526 Use new cluster for each LeaderTragicEvent test

--- solr/core/src/test/org/apache/solr/cloud/LeaderTragicEventTest.java
+++ solr/core/src/test/org/apache/solr/cloud/LeaderTragicEventTest.java
@@ -53,24 +53,21 @@ public class LeaderTragicEventTest extends SolrCloudTestCase {
 
   private String collection;
 
-  @BeforeClass
-  public static void setupCluster() throws Exception {
-    configureCluster(2)
-        .addConfig("config", TEST_PATH().resolve("configsets").resolve("cloud-minimal").resolve("conf"))
-        .configure();
-  }
-
   @Before
   public void setUp() throws Exception {
     super.setUp();
     collection = getSaferTestName();
+    configureCluster(2)
+        .addConfig("config", TEST_PATH().resolve("configsets").resolve("cloud-minimal").resolve("conf"))
+        .configure();
     cluster.getSolrClient().setDefaultCollection(collection);
   }
 
   @After
   public void tearDown() throws Exception {
-    super.tearDown();
     CollectionAdminRequest.deleteCollection(collection).process(cluster.getSolrClient());
+    cluster.shutdown();
+    super.tearDown();
   }
 
   @Test
