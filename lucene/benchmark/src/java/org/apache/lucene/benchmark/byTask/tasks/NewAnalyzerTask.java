63c34c8d84a9cfb008cece754ff4db00add4ca9f
ZhangLei <xiaoshi2014@gmail.com>
2021-09-08 11:45:08 +0800

    LUCENE-10091 Fix some old errors in the benchmark module

--- lucene/benchmark/src/java/org/apache/lucene/benchmark/byTask/tasks/NewAnalyzerTask.java
+++ lucene/benchmark/src/java/org/apache/lucene/benchmark/byTask/tasks/NewAnalyzerTask.java
@@ -16,16 +16,17 @@
  */
 package org.apache.lucene.benchmark.byTask.tasks;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.benchmark.byTask.PerfRunData;
-import org.apache.lucene.benchmark.byTask.utils.AnalyzerFactory;
-import org.apache.lucene.util.Version;
-
 import java.io.IOException;
 import java.io.StreamTokenizer;
 import java.io.StringReader;
-import java.util.*;
 import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.benchmark.byTask.PerfRunData;
+import org.apache.lucene.benchmark.byTask.utils.AnalyzerFactory;
 
 /**
  * Create a new {@link org.apache.lucene.analysis.Analyzer} and set it in the getRunData() for use by all future tasks.
@@ -42,14 +43,13 @@ public class NewAnalyzerTask extends PerfTask {
   
   public static final Analyzer createAnalyzer(String className) throws Exception{
     final Class<? extends Analyzer> clazz = Class.forName(className).asSubclass(Analyzer.class);
-    try {
-      // first try to use a ctor with version parameter (needed for many new Analyzers that have no default one anymore
-      Constructor<? extends Analyzer> cnstr = clazz.getConstructor(Version.class);
-      return cnstr.newInstance(Version.LATEST);
-    } catch (NoSuchMethodException nsme) {
-      // otherwise use default ctor
-      return clazz.newInstance();
+    Constructor<? extends Analyzer> cnstr;
+    if (className.equals("org.apache.lucene.analysis.core.StopAnalyzer")) {
+      cnstr = clazz.getConstructor(CharArraySet.class);
+      return cnstr.newInstance(CharArraySet.EMPTY_SET);
     }
+    cnstr = clazz.getConstructor();
+    return cnstr.newInstance();
   }
 
   @Override
