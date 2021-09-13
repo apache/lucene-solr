dc44016b045254b94b54bc24c484da63d7825fae
Vigya Sharma <vigyas@amazon.com>
2021-09-13 10:01:16 -0700

    fix imports in TestCommonGramsFilterFactory

--- lucene/analysis/common/src/test/org/apache/lucene/analysis/commongrams/TestCommonGramsFilterFactory.java
+++ lucene/analysis/common/src/test/org/apache/lucene/analysis/commongrams/TestCommonGramsFilterFactory.java
@@ -23,9 +23,12 @@ import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.MockTokenizer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
-import org.apache.lucene.util.ClasspathResourceLoader;
-import org.apache.lucene.util.ResourceLoader;
+import org.apache.lucene.analysis.util.BaseTokenStreamFactoryTestCase;
+import org.apache.lucene.analysis.util.ClasspathResourceLoader;
+import org.apache.lucene.analysis.util.ResourceLoader;
 import org.apache.lucene.util.Version;
+
+import static org.junit.Assert.assertTrue;
 
 public class TestCommonGramsFilterFactory extends BaseTokenStreamFactoryTestCase {
 
