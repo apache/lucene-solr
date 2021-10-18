380e30650dd1db3e4e41948aaee787406abdac98
Mayya Sharipova <mayya.sharipova@elastic.co>
2021-10-18 17:43:54 -0400

    Add bugfix version 8.10.1

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -286,6 +286,13 @@ public final class Version {
   public static final Version LUCENE_8_10_0 = new Version(8, 10, 0);
 
   /**
+   * Match settings and bugs in Lucene's 8.10.1 release.
+   * @deprecated Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_8_10_1 = new Version(8, 10, 1);
+
+  /**
    * Match settings and bugs in Lucene's 8.11.0 release.
    * <p>
    * Use this to get the latest &amp; greatest settings, bug
