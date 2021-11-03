6fe38214de439fe957d41475a998422cfb57ad6c
Joel Bernstein <jbernste@apache.org>
2021-11-03 09:07:38 -0400

    SOLR-15761: Remove JdbcStream from Lang

--- solr/solrj/src/java/org/apache/solr/client/solrj/io/Lang.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/io/Lang.java
@@ -47,7 +47,6 @@ public class Lang {
         .withFunctionName("facet2D", Facet2DStream.class)
         .withFunctionName("update", UpdateStream.class)
         .withFunctionName("delete", DeleteStream.class)
-        .withFunctionName("jdbc", JDBCStream.class)
         .withFunctionName("topic", TopicStream.class)
         .withFunctionName("commit", CommitStream.class)
         .withFunctionName("random", RandomFacadeStream.class)
